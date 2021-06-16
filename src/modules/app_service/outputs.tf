ea844689050ad48409cd35be711a217e356d77af
pasqualedevita <>
2021-06-16 13:48:26 +0200

    reduce pr scope

--- src/modules/app_service/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "plan_name" {
-  value = azurerm_app_service_plan.app_service_plan.name
-}
-
-output "fqdn" {
-  value = azurerm_app_service.app_service.default_site_hostname
-}
--- src/variables.tf
+++ src/variables.tf
@@ -18,67 +18,22 @@ variable "tags" {
     CreatedBy = "Terraform"
   }
 }
-variable "plan_name" {
-  type        = string
-  description = "Name of the service plan"
-}
-# variable "name" {
-#   description = "Function App Name"
-#   type        = string
-#   deformat("%s-auth-ms", var.prefix)
-# }
+
+## Monitor
 variable "law_sku" {
   type        = string
-  description = "Sku of the Log Analytics Workspac"
+  description = "Sku of the Log Analytics Workspace"
   default     = "PerGB2018"
 }
-variable "always_on" {
-  type        = bool
-  default     = false
-  description = "Should the app be loaded at all times?"
 
-}
-variable "linux_fx_version" {
-  type        = string
-  description = " Linux Framework and version for the Function App."
-  default     = null
-}
-variable "app_command_line" {
-  type        = string
-  description = "App command line to launch"
-  default     = null
-}
-variable "health_check_path" {
-  type        = string
-  description = "The health check path to be pinged by App Service."
-  default     = null
-}
-variable "law_daily_quota_gb" {
-  type        = number
-  description = "The workspace daily quota for ingestion in GB."
-  default     = -1
-}
 variable "law_retention_in_days" {
   type        = number
   description = "The workspace data retention in days"
   default     = 30
 }
-variable "key_vault_owner_id" {
-  type        = string
-  description = "Key Vaut Owner id"
-  default     = "standard"
-}
-variable "key_vault_sku" {
-  type        = string
-  description = "Key Vaut SKU"
-  default     = "standard"
-}
 
-variable "cert_name" {
-  type = string
-  default = "ecpa-d-cert-client-auth"
+variable "law_daily_quota_gb" {
+  type        = number
+  description = "The workspace daily quota for ingestion in GB."
+  default     = -1
 }
-# variable "app_service_certificate_name" {
-#   type        = string
-#   description = "Name of the SSL/TLS certificate for the App Service"
-# }
