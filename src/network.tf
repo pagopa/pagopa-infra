9ae5007b5d25a86cee28c37c931b0ff37da44d50
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2021-09-21 14:21:01 +0200

    Update src/network.tf
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

--- src/network.tf
+++ src/network.tf
@@ -10,7 +10,7 @@ module "vnet_internal" {
   name                = format("%s-vnet", local.project)
   location            = azurerm_resource_group.rg_vnet.location
   resource_group_name = azurerm_resource_group.rg_vnet.name
-  address_space       = var.cidr_vnet
+  address_space       = var.cidr_vnet_internal
 
   tags = var.tags
 
