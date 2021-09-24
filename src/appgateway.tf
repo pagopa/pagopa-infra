cdc5ee13bb1e80a4bcf0b98fe80cb62e32f5bbdc
pasqualespica <pasqualespica@gmail.com>
2021-09-24 14:27:51 +0200

    [PAG-711] fix resource name

--- src/appgateway.tf
+++ src/appgateway.tf
@@ -10,7 +10,7 @@ resource "azurerm_public_ip" "appgateway_public_ip" {
 }
 
 # Subnet to host the application gateway
-module "appgateway-snet" {
+module "appgateway_snet" {
   source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v1.0.7"
   name                 = format("%s-appgateway-snet", local.project)
   address_prefixes     = var.cidr_subnet_appgateway
@@ -47,7 +47,7 @@ module "app_gw" {
   sku_tier = "WAF_v2"
 
   # Networking
-  subnet_id    = module.appgateway-snet.id
+  subnet_id    = module.appgateway_snet.id
   public_ip_id = azurerm_public_ip.appgateway_public_ip.id
 
   # Configure backends
