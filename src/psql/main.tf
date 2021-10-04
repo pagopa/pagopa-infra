1f4d155bd7814f310584989ae0a6b88fb6dbbb09
Simone Infante <simone.infante@pagopa.it>
2021-10-04 09:26:56 +0200

    [PM-163] add db, schema and user for mock-psp

--- src/psql/main.tf
+++ src/psql/main.tf
@@ -1,30 +1,30 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "= 2.76.0"
-#     }
-#     azuread = {
-#       source  = "hashicorp/azuread"
-#       version = "= 2.3.0"
-#     }
-#     postgresql = {
-#       source  = "cyrilgdn/postgresql"
-#       version = "= 1.14.0"
-#     }
-#   }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 2.76.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "= 2.3.0"
+    }
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "= 1.14.0"
+    }
+  }
 
-#   backend "azurerm" {}
-# }
+  backend "azurerm" {}
+}
 
-# provider "azurerm" {
-#   features {}
-# }
+provider "azurerm" {
+  features {}
+}
 
-# data "azurerm_subscription" "current" {}
+data "azurerm_subscription" "current" {}
 
-# data "azurerm_client_config" "current" {}
+data "azurerm_client_config" "current" {}
 
-# locals {
-#   project = format("%s-%s", var.prefix, var.env_short)
-# }
+locals {
+  project = format("%s-%s", var.prefix, var.env_short)
+}
