0071a841038691631455ec611cef27059e72d185
pasqualespica <pasqualespica@gmail.com>
2021-10-06 09:27:27 +0200

    [PAG-851] add draft devportal nm3 fix 3

--- src/apim_nodo_services.tf
+++ src/apim_nodo_services.tf
@@ -20,17 +20,38 @@ module "apim_nodo_pagamenti_product" {
   policy_xml = file("./api_product/nodo_pagamenti_api/_base_policy.xml")
 }
 
-resource "azurerm_api_management_product_api" "apim_nodo_pagamenti_products" {
+resource "azurerm_api_management_product_api" "apim_nodo_pagamenti_products_psp_c" {
   product_id          = module.apim_nodo_pagamenti_product[0].product_id
-  api_name            = resource.azurerm_api_management_api.apim_nodo_pagamenti_api.name
+  api_name            = resource.azurerm_api_management_api.apim_nodo_pagamenti_api_psp_cli.name # api ref
   api_management_name = module.apim.name
   resource_group_name = azurerm_resource_group.rg_api.name
 }
 
-resource "azurerm_api_management_api" "apim_nodo_pagamenti_api" {
+resource "azurerm_api_management_product_api" "apim_nodo_pagamenti_products_psp_s" {
+  product_id          = module.apim_nodo_pagamenti_product[0].product_id
+  api_name            = resource.azurerm_api_management_api.apim_nodo_pagamenti_api_psp_svr.name # api ref
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+}
+
+# resource "azurerm_api_management_product_api" "apim_nodo_pagamenti_products_ec_c" {
+#   product_id          = module.apim_nodo_pagamenti_product[0].product_id
+#   api_name            = resource.azurerm_api_management_api.apim_nodo_pagamenti_api_ec_cli.name # api ref
+#   api_management_name = module.apim.name
+#   resource_group_name = azurerm_resource_group.rg_api.name
+# }
+
+resource "azurerm_api_management_product_api" "apim_nodo_pagamenti_products_ec_s" {
+  product_id          = module.apim_nodo_pagamenti_product[0].product_id
+  api_name            = resource.azurerm_api_management_api.apim_nodo_pagamenti_api_ec_svr.name # api ref
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+}
+
+resource "azurerm_api_management_api" "apim_nodo_pagamenti_api_psp_svr" {
   # source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
 
-  name                = format("%s-nodo-pagamenti-api", var.env_short)
+  name                = format("%s-nodo-pagamenti-psp-svr-api", var.env_short)
   api_management_name = module.apim.name
   resource_group_name = azurerm_resource_group.rg_api.name
   # product_ids           = [module.apim_nodo_pagamenti_product[0].product_id]
@@ -38,9 +59,9 @@ resource "azurerm_api_management_api" "apim_nodo_pagamenti_api" {
   revision              = "1"
 
 
-  description  = "nodo pagamenti api"
-  display_name = "nodo pagamenti api"
-  path         = "nodopagamenti/api"
+  description  = "nodo pagamenti api psp server"
+  display_name = "nodo pagamenti api psp server"
+  path         = "nodopagamenti/pspsvr/api"
   protocols    = ["https"]
 
   service_url = null
@@ -55,3 +76,90 @@ resource "azurerm_api_management_api" "apim_nodo_pagamenti_api" {
   }
 
 }
+
+resource "azurerm_api_management_api" "apim_nodo_pagamenti_api_psp_cli" {
+  # source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
+
+  name                = format("%s-nodo-pagamenti-psp-cli-api", var.env_short)
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+  # product_ids           = [module.apim_nodo_pagamenti_product[0].product_id]
+  subscription_required = false
+  revision              = "1"
+
+
+  description  = "nodo pagamenti api psp client"
+  display_name = "nodo pagamenti api psp client"
+  path         = "nodopagamenti/pspcli/api"
+  protocols    = ["https"]
+
+  service_url = null
+
+  soap_pass_through = true
+
+  # wsdl example
+  # https://raw.githubusercontent.com/monodot/camel-demos/master/simple-tests/src/main/resources/wsdl/BookService.wsdl
+  import {
+    content_format = "wsdl-link"
+    content_value  = format("https://raw.githubusercontent.com/pagopa/pagopa-infra/PAG-851-devporta-nodo-nm3/src/api/nodopagamenti_api/v1/nodeForPsp.wsdl")
+  }
+
+}
+
+resource "azurerm_api_management_api" "apim_nodo_pagamenti_api_ec_svr" {
+  # source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
+
+  name                = format("%s-nodo-pagamenti-ec-svr-api", var.env_short)
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+  # product_ids           = [module.apim_nodo_pagamenti_product[0].product_id]
+  subscription_required = false
+  revision              = "1"
+
+
+  description  = "nodo pagamenti api ec server"
+  display_name = "nodo pagamenti api ec server"
+  path         = "nodopagamenti/ecsvr/api"
+  protocols    = ["https"]
+
+  service_url = null
+
+  soap_pass_through = true
+
+  # wsdl example
+  # https://raw.githubusercontent.com/monodot/camel-demos/master/simple-tests/src/main/resources/wsdl/BookService.wsdl
+  import {
+    content_format = "wsdl-link"
+    content_value  = format("https://raw.githubusercontent.com/pagopa/pagopa-infra/PAG-851-devporta-nodo-nm3/src/api/nodopagamenti_api/v1/paForNode.wsdl")
+  }
+
+}
+
+# resource "azurerm_api_management_api" "apim_nodo_pagamenti_api_ec_cli" {
+#   # source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
+
+#   name                = format("%s-nodo-pagamenti-ec-cli-api", var.env_short)
+#   api_management_name = module.apim.name
+#   resource_group_name = azurerm_resource_group.rg_api.name
+#   # product_ids           = [module.apim_nodo_pagamenti_product[0].product_id]
+#   subscription_required = false
+#   revision              = "1"
+
+
+#   description  = "nodo pagamenti api ec client"
+#   display_name = "nodo pagamenti api ec client"
+#   path         = "nodopagamenti/eccli/api"
+#   protocols    = ["https"]
+
+#   service_url = null
+
+#   soap_pass_through = true
+
+#   # wsdl example
+#   # https://raw.githubusercontent.com/monodot/camel-demos/master/simple-tests/src/main/resources/wsdl/BookService.wsdl
+#   import {
+#     content_format = "wsdl-link"
+#     content_value  = format("https://raw.githubusercontent.com/pagopa/pagopa-infra/PAG-851-devporta-nodo-nm3/src/api/nodopagamenti_api/v1/nodeForPsp.wsdl")
+#   }
+
+# }
