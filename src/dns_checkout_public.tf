e3173f8e6ed71fcb13c58561dbb0922bfd90ba97
Simone Infante <simone.infante@pagopa.it>
2021-10-08 14:59:40 +0200

    [IOPAY-295] add dns record for uat and dev

--- src/dns_checkout_public.tf
+++ src/dns_checkout_public.tf
@@ -1,4 +1,4 @@
-resource "azurerm_dns_zone" "checkout" {
+resource "azurerm_dns_zone" "checkout_public" {
   count               = (var.dns_zone_checkout == null || var.external_domain == null) ? 0 : 1
   name                = join(".", [var.dns_zone_checkout, var.external_domain])
   resource_group_name = azurerm_resource_group.rg_vnet.name
@@ -6,19 +6,34 @@ resource "azurerm_dns_zone" "checkout" {
   tags = var.tags
 }
 
-# Prod ONLY record to UAT public DNS delegation
-# resource "azurerm_dns_ns_record" "uat_checkout" {
-#   count               = var.env_short == "p" ? 1 : 0
-#   name                = "uat"
-#   zone_name           = azurerm_dns_zone.checkout[0].name
-#   resource_group_name = azurerm_resource_group.rg_vnet.name
-#   records = [
-#     # todo delegation records after create DNS zone in UAT env
-#     "ns1-07.azure-dns.com.",
-#     "ns2-07.azure-dns.net.",
-#     "ns3-07.azure-dns.org.",
-#     "ns4-07.azure-dns.info.",
-#   ]
-#   ttl  = var.dns_default_ttl_sec
-#   tags = var.tags
-# }
+//Prod ONLY record to DEV public DNS delegation
+resource "azurerm_dns_ns_record" "dev_checkout" {
+  count               = var.env_short == "p" ? 1 : 0
+  name                = "dev"
+  zone_name           = azurerm_dns_zone.checkout_public[0].name
+  resource_group_name = azurerm_resource_group.rg_vnet.name
+  records = [
+    "ns1-03.azure-dns.com.",
+    "ns2-03.azure-dns.net.",
+    "ns3-03.azure-dns.org.",
+    "ns4-03.azure-dns.info."
+  ]
+  ttl  = var.dns_default_ttl_sec
+  tags = var.tags
+}
+
+//Prod ONLY record to UAT public DNS delegation
+resource "azurerm_dns_ns_record" "uat_checkout" {
+  count               = var.env_short == "p" ? 1 : 0
+  name                = "uat"
+  zone_name           = azurerm_dns_zone.checkout_public[0].name
+  resource_group_name = azurerm_resource_group.rg_vnet.name
+  records = [
+    "ns1-02.azure-dns.com.",
+    "ns2-02.azure-dns.net.",
+    "ns3-02.azure-dns.org.",
+    "ns4-02.azure-dns.info."
+  ]
+  ttl  = var.dns_default_ttl_sec
+  tags = var.tags
+}
