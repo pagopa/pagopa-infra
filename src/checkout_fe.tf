668a7d3e7d3405c53ed66cda724ab7d2759f3eef
Simone Infante <simone.infante@pagopa.it>
2021-10-08 11:57:13 +0200

    refactoring

--- src/checkout_fe.tf
+++ src/checkout_fe.tf
@@ -10,6 +10,32 @@ resource "azurerm_resource_group" "checkout_fe_rg" {
 }
 
 /**
+ * Checkout storage account
+ **/
+module "checkout_sa" {
+  count = var.checkout_enabled ? 1 : 0
+
+  source = "git::https://github.com/pagopa/azurerm.git//storage_account?ref=v1.0.7"
+
+  name                     = replace(format("%s-checkout-sa", local.project), "-", "")
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  access_tier              = "Hot"
+  enable_versioning        = true
+  resource_group_name      = azurerm_resource_group.checkout_fe_rg[0].name
+  location                 = var.location
+  allow_blob_public_access = true
+
+  lock_enabled = true
+  lock_name    = format("%s-checkout-sa-lock", local.project)
+  lock_level   = "CanNotDelete"
+  lock_notes   = null
+
+  tags = var.tags
+}
+
+/**
  * Checkout cdn profile
  **/
 resource "azurerm_cdn_profile" "checkout_cdn_p" {
@@ -23,131 +49,34 @@ resource "azurerm_cdn_profile" "checkout_cdn_p" {
 }
 
 /**
- * Checkout storage account
- **/
-resource "azurerm_storage_account" "checkout_sa" {
-  name                      = replace(format("%s-checkout-cdn-sa", local.project), "-", "")
-  resource_group_name       = azurerm_resource_group.checkout_fe_rg[0].name
-  location                  = var.location
-  account_kind              = "StorageV2"
-  account_tier              = "Standard"
-  account_replication_type  = "GRS"
-  access_tier               = "Hot"
-  enable_https_traffic_only = true
-  min_tls_version           = "TLS1_2"
-  allow_blob_public_access  = true
-
-  dynamic "static_website" {
-    for_each = [{}]
-    content {
-      index_document     = var.index_path
-      error_404_document = var.custom_404_path
-    }
-  }
-
-  dynamic "blob_properties" {
-    for_each = var.soft_delete_retention != null || length(var.cors_rule) != 0 ? [{}] : []
-    content {
-      dynamic "delete_retention_policy" {
-        for_each = var.soft_delete_retention != null ? [{}] : []
-        content {
-          days = var.soft_delete_retention
-        }
-      }
-      dynamic "cors_rule" {
-        for_each = var.cors_rule
-        content {
-          allowed_origins    = cors_rule.value.allowed_origins
-          allowed_methods    = cors_rule.value.allowed_methods
-          allowed_headers    = cors_rule.value.allowed_headers
-          exposed_headers    = cors_rule.value.exposed_headers
-          max_age_in_seconds = cors_rule.value.max_age_in_seconds
-        }
-      }
-    }
-  }
-
-
-  dynamic "network_rules" {
-    for_each = var.network_rules == null ? [] : [var.network_rules]
-
-    content {
-      default_action             = length(network_rules.value["ip_rules"]) == 0 && length(network_rules.value["virtual_network_subnet_ids"]) == 0 ? network_rules.value["default_action"] : "Deny"
-      bypass                     = network_rules.value["bypass"]
-      ip_rules                   = network_rules.value["ip_rules"]
-      virtual_network_subnet_ids = network_rules.value["virtual_network_subnet_ids"]
-    }
-  }
-
-  tags = var.tags
-}
-
-# Enable advanced threat protection
-resource "azurerm_advanced_threat_protection" "advanced_threat_protection" {
-  target_resource_id = azurerm_storage_account.checkout_sa.id
-  enabled            = false
-}
-
-resource "azurerm_template_deployment" "versioning" {
-  depends_on          = [azurerm_storage_account.checkout_sa]
-  name                = format("%s-sa-versioning", local.project)
-  resource_group_name = azurerm_resource_group.checkout_fe_rg[0].name
-  deployment_mode     = "Incremental"
-  parameters = {
-    "storageAccount" = azurerm_storage_account.checkout_sa.name
-  }
-
-  template_body = <<DEPLOY
-        {
-            "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
-            "contentVersion": "1.0.0.0",
-            "parameters": {
-                "storageAccount": {
-                    "type": "string",
-                    "metadata": {
-                        "description": "Storage Account Name"}
-                }
-            },
-            "variables": {},
-            "resources": [
-                {
-                    "type": "Microsoft.Storage/storageAccounts/blobServices",
-                    "apiVersion": "2019-06-01",
-                    "name": "[concat(parameters('storageAccount'), '/default')]",
-                    "properties": {
-                        "IsVersioningEnabled": true
-                    }
-                }
-            ]
-        }
-    DEPLOY
-}
-
-# Lock scoping to a Subscription, Resource Group or Resource
-resource "azurerm_management_lock" "management_lock" {
-  count      = var.lock ? 1 : 0
-  name       = azurerm_resource_group.checkout_fe_rg[0].name
-  scope      = var.lock_scope
-  lock_level = var.lock_level
-  notes      = var.lock_notes
-}
-
-/**
  * CDN endpoint
  */
-
 resource "azurerm_cdn_endpoint" "checkout_cdn_e" {
-  name                          = format("%s-checkout-cdn-p", local.project)
+  count = var.checkout_enabled ? 1 : 0
+
+  name                          = format("%s-checkout-cdn-e", local.project)
   resource_group_name           = azurerm_resource_group.checkout_fe_rg[0].name
   location                      = var.location
   profile_name                  = azurerm_cdn_profile.checkout_cdn_p[0].name
-  is_https_allowed              = true
-  is_http_allowed               = true
   querystring_caching_behaviour = "BypassCaching"
-  origin_host_header            = azurerm_storage_account.checkout_sa.primary_web_host
+  origin_host_header            = module.checkout_sa[0].primary_web_host
+
+  # allow HTTP, HSTS will make future connections over HTTPS
+  is_http_allowed = true
 
   origin {
     name      = "primary"
-    host_name = azurerm_storage_account.checkout_sa.primary_web_host
+    host_name = module.checkout_sa[0].primary_web_host
   }
+
+  global_delivery_rule {
+
+    modify_response_header_action {
+      action = "Overwrite"
+      name   = "Strict-Transport-Security"
+      value  = "max-age=31536000"
+    }
+
+  }
+
 }
--- src/variables.tf
+++ src/variables.tf
@@ -378,86 +378,8 @@ variable "checkout_enabled" {
   default = false
 }
 
-variable "index_path" {
-  description = "path from your repo root to index.html"
-  default     = "index.html"
-}
-
-variable "custom_404_path" {
-  description = "path from your repo root to your custom 404 page"
-  default     = "index.html"
-}
-
-variable "soft_delete_retention" {
-  description = "Number of retention days for soft delete. If set to null it will disable soft delete all together."
-  type        = number
-  default     = null
-}
-
-variable "cors_rule" {
-  description = "CORS rules for storage account."
-  type = list(object({
-    allowed_origins    = list(string)
-    allowed_methods    = list(string)
-    allowed_headers    = list(string)
-    exposed_headers    = list(string)
-    max_age_in_seconds = number
-  }))
-  default = []
-}
-
 variable "allow_blob_public_access" {
   description = "Allow or disallow public access to all blobs or containers in the storage account."
   type        = bool
   default     = false
-}
-
-variable "network_rules" {
-  type = object({
-    default_action             = string # Valid option Deny Allow
-    bypass                     = set(string)
-    ip_rules                   = list(string)
-    virtual_network_subnet_ids = list(string)
-  })
-  default = null
-}
-
-variable "lock" {
-  type        = bool
-  description = "Lock the storage account"
-  default     = false
-}
-
-variable "lock_scope" {
-  type        = string
-  description = "Specifies the scope at which the Management Lock should be created. Usually this is the resource id."
-  default     = null
-}
-
-variable "lock_level" {
-  type        = string
-  description = "Specifies the scope at which the Management Lock should be created."
-  default     = "CanNotDelete"
-
-  validation {
-    condition = (
-      var.lock_level == "CanNotDelete" ||
-      var.lock_level == "ReadOnly"
-    )
-    error_message = "Lock level can be CanNotDelete or ReadOnly."
-  }
-}
-
-variable "lock_notes" {
-  type        = string
-  description = "Specifies some notes about the lock. Maximum of 512 characters"
-  default     = null
-
-  validation {
-    condition = (
-      var.lock_notes != null ? length(var.lock_notes) <= 512 : true
-    )
-    error_message = "Notes Maximum of 512 characters."
-  }
-
 }
