19ef625e976fb34c61815fa9d3027cce95f65249
Simone infante <52280205+infantesimone@users.noreply.github.com>
2021-10-20 10:32:13 +0200

    Update src/checkout_function.tf
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

--- src/checkout_function.tf
+++ src/checkout_function.tf
@@ -56,7 +56,6 @@ module "checkout_function" {
     WEBSITE_NODE_DEFAULT_VERSION   = "14.16.0"
     FUNCTIONS_WORKER_PROCESS_COUNT = 4
     NODE_ENV                       = "production"
-    AzureWebJobsStorage            = module.checkout_function[0].storage_account.primary_access_key
 
     // Keepalive fields are all optionals
     FETCH_KEEPALIVE_ENABLED             = "true"
