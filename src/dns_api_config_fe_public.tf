35fb5cbc2c907cbb97b1b33cd5d917592b747ef9
Francesco Cesareo <cesareo.francesco@gmail.com>
2021-11-09 17:34:20 +0100

    update dns records

--- src/dns_api_config_fe_public.tf
+++ src/dns_api_config_fe_public.tf
@@ -13,22 +13,6 @@ resource "azurerm_dns_ns_record" "dev_api_config_fe" {
   zone_name           = azurerm_dns_zone.api_config_fe_public[0].name
   resource_group_name = azurerm_resource_group.rg_vnet.name
   records = [
-    "ns1-08.azure-dns.com.",
-    "ns2-08.azure-dns.net.",
-    "ns3-08.azure-dns.org.",
-    "ns4-08.azure-dns.info."
-  ]
-  ttl  = var.dns_default_ttl_sec
-  tags = var.tags
-}
-
-//Prod ONLY record to UAT public DNS delegation
-resource "azurerm_dns_ns_record" "uat_api_config_fe" {
-  count               = var.env_short == "p" ? 1 : 0
-  name                = "uat"
-  zone_name           = azurerm_dns_zone.api_config_fe_public[0].name
-  resource_group_name = azurerm_resource_group.rg_vnet.name
-  records = [
     "ns1-07.azure-dns.com.",
     "ns2-07.azure-dns.net.",
     "ns3-07.azure-dns.org.",
@@ -37,3 +21,19 @@ resource "azurerm_dns_ns_record" "uat_api_config_fe" {
   ttl  = var.dns_default_ttl_sec
   tags = var.tags
 }
+
+//Prod ONLY record to UAT public DNS delegation
+//resource "azurerm_dns_ns_record" "uat_api_config_fe" {
+//  count               = var.env_short == "p" ? 1 : 0
+//  name                = "uat"
+//  zone_name           = azurerm_dns_zone.api_config_fe_public[0].name
+//  resource_group_name = azurerm_resource_group.rg_vnet.name
+//  records = [
+//    "ns1-07.azure-dns.com.",
+//    "ns2-07.azure-dns.net.",
+//    "ns3-07.azure-dns.org.",
+//    "ns4-07.azure-dns.info."
+//  ]
+//  ttl  = var.dns_default_ttl_sec
+//  tags = var.tags
+//}
