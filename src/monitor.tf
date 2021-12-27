a7aee014631bf0ba74012311cdd43ed46405d7b4
Walter Traspadini <walter.traspadini@pagopa.it>
2021-12-27 10:44:31 +0100

    Update cdn and web test module to fix some tests (#88)
    * update cdn and web test module to fix some tests
    
    * fix removed parameter
    
    * update terreform verion and azurerm version
    
    * update readme file and  terraofrm version

--- src/monitor.tf
+++ src/monitor.tf
@@ -62,25 +62,22 @@ locals {
     {
       host = trimsuffix(azurerm_dns_a_record.dns_a_api.fqdn, "."),
       path = "",
-    http_status = 200 },
+    },
     {
       host = trimsuffix(azurerm_dns_a_record.dns_a_portal.fqdn, "."),
       path = "",
-    http_status = 200 },
+    },
     {
-      host        = trimsuffix(azurerm_dns_a_record.dns_a_management.fqdn, "."),
-      path        = "/ServiceStatus",
-      http_status = 200
+      host = trimsuffix(azurerm_dns_a_record.dns_a_management.fqdn, "."),
+      path = "/ServiceStatus",
     },
     length(module.api_config_fe_cdn.*.fqdn) == 0 ? null : {
-      host        = module.api_config_fe_cdn[0].fqdn,
-      path        = "",
-      http_status = 200
+      host = module.api_config_fe_cdn[0].fqdn,
+      path = "",
     },
     length(module.checkout_cdn.*.fqdn) == 0 ? null : {
-      host        = module.checkout_cdn[0].fqdn,
-      path        = "",
-      http_status = 200
+      host = module.checkout_cdn[0].fqdn,
+      path = "",
     },
   ]
 
@@ -88,7 +85,7 @@ locals {
 
 module "web_test_api" {
   for_each = { for v in local.test_urls : v.host => v if v != null }
-  source   = "git::https://github.com/pagopa/azurerm.git//application_insights_web_test_preview?ref=v2.0.15"
+  source   = "git::https://github.com/pagopa/azurerm.git//application_insights_web_test_preview?ref=v2.0.18"
 
   subscription_id                   = data.azurerm_subscription.current.subscription_id
   name                              = format("%s-test", each.value.host)
@@ -96,7 +93,6 @@ module "web_test_api" {
   resource_group                    = azurerm_resource_group.monitor_rg.name
   application_insight_name          = azurerm_application_insights.application_insights.name
   request_url                       = format("https://%s%s", each.value.host, each.value.path)
-  expected_http_status              = each.value.http_status
   ssl_cert_remaining_lifetime_check = 7
 
   actions = [
