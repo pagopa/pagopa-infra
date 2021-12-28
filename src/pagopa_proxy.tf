a7ae9aff4d04235637d36521c443bd6a666f6382
Giovanni Berti <giovanni.berti@pagopa.it>
2021-12-28 11:37:28 +0100

    Align app configuration with currently deployed config

--- src/pagopa_proxy.tf
+++ src/pagopa_proxy.tf
@@ -32,7 +32,7 @@ module "pagopa_proxy_redis" {
   resource_group_name   = azurerm_resource_group.pagopa_proxy_rg[0].name
   location              = azurerm_resource_group.pagopa_proxy_rg[0].location
   capacity              = var.pagopa_proxy_redis_capacity
-  enable_non_ssl_port   = true
+  enable_non_ssl_port   = false
   family                = var.pagopa_proxy_redis_family
   sku_name              = var.pagopa_proxy_redis_sku_name
   enable_authentication = true
@@ -50,54 +50,37 @@ module "pagopa_proxy_app_service" {
 
   # App service plan vars
   plan_name     = format("%s-plan-pagopa-proxy", local.project)
-  plan_kind     = "Linux"
+  plan_kind     = "Windows"
   plan_sku_tier = var.pagopa_proxy_tier
   plan_sku_size = var.pagopa_proxy_size
-  plan_reserved = true # Mandatory for Linux plan
 
   # App service plan
   name                = format("%s-app-pagopa-proxy", local.project)
   client_cert_enabled = false
   always_on           = var.pagopa_proxy_always_on
-  linux_fx_version    = "NODE|14-lts"
   health_check_path   = "/ping"
 
   app_settings = {
+    WEBSITE_NODE_DEFAULT_VERSION = "14.16.0"
+    WEBSITE_RUN_FROM_PACKAGE     = "1"
+
     # Monitoring
-    APPINSIGHTS_INSTRUMENTATIONKEY                  = azurerm_application_insights.application_insights.instrumentation_key
-    APPLICATIONINSIGHTS_CONNECTION_STRING           = format("InstrumentationKey=%s", azurerm_application_insights.application_insights.instrumentation_key)
-    APPINSIGHTS_PROFILERFEATURE_VERSION             = "1.0.0"
-    APPINSIGHTS_SNAPSHOTFEATURE_VERSION             = "1.0.0"
-    APPLICATIONINSIGHTS_CONFIGURATION_CONTENT       = ""
-    ApplicationInsightsAgent_EXTENSION_VERSION      = "~3"
-    DiagnosticServices_EXTENSION_VERSION            = "~3"
-    InstrumentationEngine_EXTENSION_VERSION         = "disabled"
-    SnapshotDebugger_EXTENSION_VERSION              = "disabled"
-    XDT_MicrosoftApplicationInsights_BaseExtensions = "disabled"
-    XDT_MicrosoftApplicationInsights_Mode           = "recommended"
-    XDT_MicrosoftApplicationInsights_PreemptSdk     = "disabled"
-    WEBSITE_HEALTHCHECK_MAXPINGFAILURES             = 10
-    TIMEOUT_DELAY                                   = 300
+    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.application_insights.instrumentation_key
 
     # proxy-specific env vars
-    # Taken from https://github.com/pagopa/io-infrastructure/blob/6f43d141e7695b96cdb192535a30b78efdd9df81/infrastructure/kubernetes/pagopa-pagopa_proxy.yml
-    PAGOPA_HOST    = "localhost"
-    PAGOPA_PORT    = 80
-    PAGOPA_HOST                = "http://10.250.1.182"
-    PAGOPA_HOST_HEADER         = "nodopa.sia.eu"
+    PAGOPA_HOST                = data.azurerm_key_vault_secret.pagopa_proxy_host[0].value
+    PAGOPA_PORT                = data.azurerm_key_vault_secret.pagopa_proxy_port[0].value
+    PAGOPA_PASSWORD            = data.azurerm_key_vault_secret.pagopa_proxy_password[0].value
+    PAGOPA_ID_PSP              = data.azurerm_key_vault_secret.pagopa_proxy_id_psp[0].value
+    PAGOPA_ID_INT_PSP          = data.azurerm_key_vault_secret.pagopa_proxy_id_intermediario_psp[0].value
     PAGOPA_ID_CANALE           = data.azurerm_key_vault_secret.pagopa_proxy_id_canale[0].value
     PAGOPA_ID_CANALE_PAGAMENTO = data.azurerm_key_vault_secret.pagopa_proxy_id_canale_pagamento[0].value
-    PAGOPA_ID_INT_PSP          = data.azurerm_key_vault_secret.pagopa_proxy_id_intermediario_psp[0].value
-    PAGOPA_ID_PSP              = data.azurerm_key_vault_secret.pagopa_proxy_id_psp[0].value
-    PAGOPA_PASSWORD            = data.azurerm_key_vault_secret.pagopa_proxy_password[0].value
-    PAGOPA_PORT                = 80
-    PAGOPA_TIMEOUT_MSEC        = 500000
-    REDIS_DB_PASSWORD          = data.azurerm_key_vault_secret.pagopa_proxy_redis_db_passsword[0].value
-    REDIS_DB_PORT              = 6380
-    REDIS_DB_URL               = module.pagopa_proxy_redis[0].hostname
-    REDIS_USE_CLUSTER          = true
-    WINSTON_LOG_LEVEL          = "debug"
+    PAGOPA_WS_URI              = data.azurerm_key_vault_secret.pagopa_proxy_ws_uri[0].value
 
+    REDIS_DB_URL      = module.pagopa_proxy_redis[0].hostname
+    REDIS_DB_PORT     = module.pagopa_proxy_redis[0].ssl_port
+    REDIS_DB_PASSWORD = module.pagopa_proxy_redis[0].primary_access_key
+    REDIS_USE_CLUSTER = true
   }
 
   allowed_subnets = [module.apim_snet.id]
@@ -107,6 +90,24 @@ module "pagopa_proxy_app_service" {
   subnet_id   = module.pagopa_proxy_snet[0].id
 
   tags = var.tags
+}
+
+data "azurerm_key_vault_secret" "pagopa_proxy_host" {
+  count        = var.pagopa_proxy_enabled ? 1 : 0
+  name         = "pagopa-proxy-host"
+  key_vault_id = module.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "pagopa_proxy_port" {
+  count        = var.pagopa_proxy_enabled ? 1 : 0
+  name         = "pagopa-proxy-port"
+  key_vault_id = module.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "pagopa_proxy_password" {
+  count        = var.pagopa_proxy_enabled ? 1 : 0
+  name         = "pagopa-proxy-password"
+  key_vault_id = module.key_vault.id
 }
 
 data "azurerm_key_vault_secret" "pagopa_proxy_id_canale" {
@@ -133,14 +134,8 @@ data "azurerm_key_vault_secret" "pagopa_proxy_id_psp" {
   key_vault_id = module.key_vault.id
 }
 
-data "azurerm_key_vault_secret" "pagopa_proxy_password" {
+data "azurerm_key_vault_secret" "pagopa_proxy_ws_uri" {
   count        = var.pagopa_proxy_enabled ? 1 : 0
-  name         = "pagopa-proxy-password"
-  key_vault_id = module.key_vault.id
-}
-
-data "azurerm_key_vault_secret" "pagopa_proxy_redis_db_passsword" {
-  count        = var.pagopa_proxy_enabled ? 1 : 0
-  name         = "pagopa-proxy-redis-db-password"
+  name         = "pagopa-proxy-ws-uri"
   key_vault_id = module.key_vault.id
 }
