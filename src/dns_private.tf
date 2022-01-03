1f43f52eb6ecdded32f45ac1fd3e59c44d4c5622
Giovanni Berti <giovanni.berti@pagopa.it>
2022-01-03 12:15:26 +0100

    Add missing virtual network link for pagopa-proxy redis dns zone

--- src/dns_private.tf
+++ src/dns_private.tf
@@ -23,10 +23,21 @@ resource "azurerm_private_dns_a_record" "private_dns_a_record_db_nodo" {
   records             = var.dns_a_reconds_dbnodo_ips
 }
 
-resource "azurerm_private_dns_zone" "privatelink_redis_pagopa_proxy" {
+resource "azurerm_private_dns_zone" "redis_pagopa_proxy_dns_zone" {
   count               = var.pagopa_proxy_redis_private_endpoint_enabled ? 1 : 0
   name                = "privatelink.redis.cache.windows.net"
   resource_group_name = azurerm_resource_group.data.name
+
+  tags = var.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "redis_pagopa_proxy_vnet_link" {
+  count                 = var.pagopa_proxy_redis_private_endpoint_enabled ? 1 : 0
+  name                  = module.vnet.name
+  resource_group_name   = azurerm_resource_group.rg_vnet.name
+  private_dns_zone_name = azurerm_private_dns_zone.redis_pagopa_proxy_dns_zone
+  virtual_network_id    = module.vnet.id
+  registration_enabled  = false
 
   tags = var.tags
 }
