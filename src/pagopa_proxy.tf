516756ac1ccfd3028b37d3e5c602d1191f234549
Giovanni Berti <giovanni.berti@pagopa.it>
2022-01-03 12:15:26 +0100

    Add missing virtual network link for pagopa-proxy redis dns zone

--- src/pagopa_proxy.tf
+++ src/pagopa_proxy.tf
@@ -44,7 +44,7 @@ module "pagopa_proxy_redis" {
     enabled              = var.pagopa_proxy_redis_private_endpoint_enabled
     virtual_network_id   = azurerm_resource_group.rg_vnet.id
     subnet_id            = module.pagopa_proxy_snet[0].id
-    private_dns_zone_ids = var.pagopa_proxy_redis_private_endpoint_enabled ? [azurerm_private_dns_zone.privatelink_redis_pagopa_proxy[0].id] : []
+    private_dns_zone_ids = var.pagopa_proxy_redis_private_endpoint_enabled ? [azurerm_private_dns_zone.redis_pagopa_proxy_dns_zone[0].id] : []
   }
 }
 
