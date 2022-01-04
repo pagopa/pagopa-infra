fe1b306c93da2ee4d0f90fbc9ee5c06d7d2be97b
Giovanni Berti <giovanni.berti@pagopa.it>
2022-01-04 15:28:25 +0100

    Add apim config for pagopa proxy

--- src/apim_checkout.tf
+++ src/apim_checkout.tf
@@ -3,7 +3,7 @@
 ##############
 
 module "apim_checkout_product" {
-  count  = var.checkout_enabled ? 1 : 0
+  count  = var.checkout_enabled || var.pagopa_proxy_enabled ? 1 : 0
   source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v1.0.16"
 
   product_id   = "checkout"
@@ -29,6 +29,14 @@ locals {
     display_name          = "Checkout payment activation API"
     description           = "API to support payment activation"
     path                  = "api/checkout/payments"
+    subscription_required = false
+    service_url           = null
+  }
+
+  apim_checkout_payment_activations_api = {
+    display_name          = "Checkout payment activations API [new]"
+    description           = "API to support payment activation [new]"
+    path                  = "api/checkout/activations"
     subscription_required = false
     service_url           = null
   }
@@ -69,6 +77,46 @@ module "apim_checkout_payments_api_v1" {
   })
 
   xml_content = templatefile("./api/checkout/checkout_payments/v1/_base_policy.xml.tpl", {
+    origin = format("https://%s.%s/", var.dns_zone_checkout, var.external_domain)
+  })
+}
+
+# Payment activation APIs (new)
+resource "azurerm_api_management_api_version_set" "checkout_payment_activations_api" {
+  count = var.pagopa_proxy_enabled ? 1 : 0
+
+  name                = format("%s-checkout-payment-activations-api", var.env_short)
+  resource_group_name = azurerm_resource_group.rg_api.name
+  api_management_name = module.apim.name
+  display_name        = local.apim_checkout_payment_activations_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+module "apim_checkout_payment_activations_api_v1" {
+  count = var.pagopa_proxy_enabled ? 1 : 0
+
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v2.0.23"
+
+  name                  = format("%s-checkout-payment-activations-api", var.env_short)
+  api_management_name   = module.apim.name
+  resource_group_name   = azurerm_resource_group.rg_api.name
+  product_ids           = [module.apim_checkout_product[0].product_id]
+  subscription_required = local.apim_checkout_payment_activations_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.checkout_payment_activations_api[0].id
+  api_version           = "v1"
+  service_url           = local.apim_checkout_payment_activations_api.service_url
+
+  description  = local.apim_checkout_payment_activations_api.description
+  display_name = local.apim_checkout_payment_activations_api.display_name
+  path         = local.apim_checkout_payment_activations_api.path
+  protocols    = ["https"]
+
+  content_format = "swagger-json"
+  content_value = templatefile("./api/checkout/checkout_payment_activations/v1/_swagger.json.tpl", {
+    host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
+  })
+
+  xml_content = templatefile("./api/checkout/checkout_payment_activations/v1/_base_policy.xml.tpl", {
     origin = format("https://%s.%s/", var.dns_zone_checkout, var.external_domain)
   })
 }
