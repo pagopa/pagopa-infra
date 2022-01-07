baae8e702aeb55b5d3851358c38d2324d07d0957
Simone Infante <simone.infante@pagopa.it>
2022-01-07 09:21:09 +0100

    [IOPAY-405] add nodeForPSP and nodoPerPSP ws in apim

--- src/apim_nodo_services.tf
+++ src/apim_nodo_services.tf
@@ -174,3 +174,117 @@ resource "azurerm_api_management_api" "apim_nodo_pagamenti_api_ec_svr" {
 #   }
 
 # }
+
+############################
+## WS node for psp (NM3) ##
+############################
+locals {
+  apim_node_for_psp_api = {
+    display_name          = "Node for PSP WS (NM3)"
+    description           = "Web services to support PSP in payment activations, defined in nodeForPsp.wsdl"
+    path                  = "api/node-for-psp"
+    subscription_required = false
+    service_url           = null
+  }
+}
+
+resource "azurerm_api_management_api_version_set" "node_for_psp_api" {
+  name                = format("%s-node-for-psp-api", var.env_short)
+  resource_group_name = azurerm_resource_group.rg_api.name
+  api_management_name = module.apim.name
+  display_name        = local.apim_node_for_psp_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+resource "azurerm_api_management_api" "apim_node_for_psp_api_v1" {
+  name                  = format("%s-node-for-psp-api", var.env_short)
+  api_management_name   = module.apim.name
+  resource_group_name   = azurerm_resource_group.rg_api.name
+  subscription_required = local.apim_node_for_psp_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.node_for_psp_api.id
+  version               = "v1"
+  service_url           = local.apim_node_for_psp_api.service_url
+  revision              = "1"
+
+  description  = local.apim_node_for_psp_api.description
+  display_name = local.apim_node_for_psp_api.display_name
+  path         = local.apim_node_for_psp_api.path
+  protocols    = ["https"]
+
+  soap_pass_through = true
+
+  import {
+    content_format = "wsdl"
+    content_value  = file("./api/nodopagamenti_api/nodeForPsp/v1/nodeForPsp.wsdl")
+    wsdl_selector {
+      service_name  = "nodeForPsp_Service"
+      endpoint_name = "nodeForPsp_Port"
+    }
+  }
+
+}
+
+resource "azurerm_api_management_api_policy" "apim_node_for_psp_policy" {
+  api_name            = resource.azurerm_api_management_api.apim_node_for_psp_api_v1.name
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  xml_content = file("./api/nodopagamenti_api/nodeForPsp/v1/_base_policy.xml")
+}
+
+######################
+## WS nodo per psp ##
+######################
+locals {
+  apim_nodo_per_psp_api = {
+    display_name          = "Nodo per PSP WS"
+    description           = "Web services to support PSP in payment activations, defined in nodoPerPsp.wsdl"
+    path                  = "api/nodo-per-psp"
+    subscription_required = false
+    service_url           = null
+  }
+}
+
+resource "azurerm_api_management_api_version_set" "nodo_per_psp_api" {
+  name                = format("%s-nodo-per-psp-api", var.env_short)
+  resource_group_name = azurerm_resource_group.rg_api.name
+  api_management_name = module.apim.name
+  display_name        = local.apim_nodo_per_psp_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+resource "azurerm_api_management_api" "apim_nodo_per_psp_api_v1" {
+  name                  = format("%s-nodo-per-psp-api", var.env_short)
+  api_management_name   = module.apim.name
+  resource_group_name   = azurerm_resource_group.rg_api.name
+  subscription_required = local.apim_nodo_per_psp_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.nodo_per_psp_api.id
+  version               = "v1"
+  service_url           = local.apim_nodo_per_psp_api.service_url
+  revision              = "1"
+
+  description  = local.apim_nodo_per_psp_api.description
+  display_name = local.apim_nodo_per_psp_api.display_name
+  path         = local.apim_nodo_per_psp_api.path
+  protocols    = ["https"]
+
+  soap_pass_through = true
+
+  import {
+    content_format = "wsdl"
+    content_value  = file("./api/nodopagamenti_api/nodoPerPsp/v1/nodoPerPsp.wsdl")
+    wsdl_selector {
+      service_name  = "PagamentiTelematiciPspNodoservice"
+      endpoint_name = "PPTPort"
+    }
+  }
+
+}
+
+resource "azurerm_api_management_api_policy" "apim_nodo_per_psp_policy" {
+  api_name            = resource.azurerm_api_management_api.apim_nodo_per_psp_api_v1.name
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  xml_content = file("./api/nodopagamenti_api/nodoPerPsp/v1/_base_policy.xml")
+}
