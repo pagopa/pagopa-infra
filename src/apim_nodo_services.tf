898e4ed92037fdf36e2b1b875c796acb846da5f4
Giovanni Berti <giovanni.berti@pagopa.it>
2022-01-10 16:18:50 +0100

    Add `nodeForIO` SOAP APIs to APIM

--- src/apim_nodo_services.tf
+++ src/apim_nodo_services.tf
@@ -288,3 +288,60 @@ resource "azurerm_api_management_api_policy" "apim_nodo_per_psp_policy" {
 
   xml_content = file("./api/nodopagamenti_api/nodoPerPsp/v1/_base_policy.xml")
 }
+
+######################
+## WS nodo for IO   ##
+######################
+locals {
+  apim_node_for_io_api = {
+    display_name          = "Node for IO WS"
+    description           = "Web services to support activeIO, defined in nodeForIO.wsdl"
+    path                  = "api/node-for-io"
+    subscription_required = false
+    service_url           = null
+  }
+}
+
+resource "azurerm_api_management_api_version_set" "node_for_io_api" {
+  name                = format("%s-nodo-for-io-api", var.env_short)
+  resource_group_name = azurerm_resource_group.rg_api.name
+  api_management_name = module.apim.name
+  display_name        = local.apim_node_for_io_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+resource "azurerm_api_management_api" "apim_node_for_io_api_v1" {
+  name                  = format("%s-node-for-io-api", var.env_short)
+  api_management_name   = module.apim.name
+  resource_group_name   = azurerm_resource_group.rg_api.name
+  subscription_required = local.apim_node_for_io_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.node_for_io_api.id
+  version               = "v1"
+  service_url           = local.apim_node_for_io_api.service_url
+  revision              = "1"
+
+  description  = local.apim_node_for_io_api.description
+  display_name = local.apim_node_for_io_api.display_name
+  path         = local.apim_node_for_io_api.path
+  protocols    = ["https"]
+
+  soap_pass_through = true
+
+  import {
+    content_format = "wsdl"
+    content_value  = file("./api/nodopagamenti_api/nodeForIO/v1/nodeForIO.wsdl")
+    wsdl_selector {
+      service_name  = "nodeForIO_Service"
+      endpoint_name = "nodeForIO_Port"
+    }
+  }
+
+}
+
+resource "azurerm_api_management_api_policy" "apim_node_for_io_policy" {
+  api_name            = resource.azurerm_api_management_api.apim_node_for_io_api_v1.name
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  xml_content = file("./api/nodopagamenti_api/nodeForIO/v1/_base_policy.xml")
+}
