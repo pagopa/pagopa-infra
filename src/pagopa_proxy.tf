0ffb45a172f4b7b13d4b263506c8f37a96aba7d4
Simone Infante <simone.infante@pagopa.it>
2022-01-20 20:39:12 +0100

    add linux_fx_version

--- src/pagopa_proxy.tf
+++ src/pagopa_proxy.tf
@@ -95,6 +95,8 @@ module "pagopa_proxy_app_service" {
   plan_sku_tier = var.pagopa_proxy_tier
   plan_sku_size = var.pagopa_proxy_size
 
+  linux_fx_version  = "NODE|14-lts"
+
   # App service plan
   name                = format("%s-app-pagopa-proxy", local.project)
   client_cert_enabled = false
