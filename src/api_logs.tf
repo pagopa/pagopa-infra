a32884c732bdce480929b7505528a32bf1b71e00
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-01-20 14:56:47 +0100

    fix cdinfo apim path

--- src/api_logs.tf
+++ src/api_logs.tf
@@ -7,7 +7,9 @@ locals {
     azurerm_api_management_api.apim_node_for_psp_api_v1.name,
     azurerm_api_management_api.apim_nodo_per_psp_api_v1.name,
     azurerm_api_management_api.apim_node_for_io_api_v1.name,
-    azurerm_api_management_api.apim_psp_for_node_api_v1.name
+    azurerm_api_management_api.apim_psp_for_node_api_v1.name,
+    azurerm_api_management_api.apim_nodo_per_pa_api_v1.name,
+    azurerm_api_management_api.apim_cd_info_wisp_v1.name
   ]
 
 }
