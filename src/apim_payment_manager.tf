fbbbed316b60a3491e95bed58dad3cd45fb8ede7
Simone Infante <simone.infante@pagopa.it>
2022-02-09 09:20:09 +0100

    refactorinf to save certs in kv

--- src/apim_payment_manager.tf
+++ src/apim_payment_manager.tf
@@ -478,12 +478,11 @@ locals {
     path                  = "wallet"
     subscription_required = false
     service_url           = null
-    metadata              = file("./api/payment_manager_api/wisp/sp_metadata_${var.env}.xml")
   }
 }
 
-data "azurerm_key_vault_secret" "wisp_ip" {
-  name         = "wisp-ip"
+data "azurerm_key_vault_secret" "pm_wisp_metadata" {
+  name         = "pm-wisp-metadata"
   key_vault_id = module.key_vault.id
 }
 
@@ -510,7 +509,6 @@ module "apim_pm_wisp_api_v1" {
 
   xml_content = templatefile("./api/payment_manager_api/wisp/_base_policy.xml.tpl", {
     origin = format("https://api.%s.%s", var.dns_zone_prefix, var.external_domain)
-
   })
 }
 
@@ -520,7 +518,7 @@ resource "azurerm_api_management_api_operation_policy" "get_spid_metadata_api" {
   resource_group_name = azurerm_resource_group.rg_api.name
   operation_id        = "GETSpidMetadata"
 
-  xml_content = templatefile("./api/payment_manager_api/wisp/_spid_metadata_policy.xml.tpl", { metadata = local.apim_pm_wisp_api.metadata })
+  xml_content = templatefile("./api/payment_manager_api/wisp/_spid_metadata_policy.xml.tpl", { metadata = data.azurerm_key_vault_secret.pm_wisp_metadata })
 }
 
 ########################
