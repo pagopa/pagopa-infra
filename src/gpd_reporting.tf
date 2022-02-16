21ead59708bfa29513f6b2db17dd9389077f6569
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-02-16 08:51:17 +0100

    add gpd service

--- src/gpd_reporting.tf
+++ src/gpd_reporting.tf
@@ -381,13 +381,13 @@ module "flows" {
   account_replication_type   = "LRS"
   access_tier                = "Hot"
   versioning_name            = "versioning"
-  enable_versioning          = var.gdp_enable_versioning
+  enable_versioning          = var.gpd_enable_versioning
   resource_group_name        = azurerm_resource_group.gpd_rg.name
   location                   = var.location
-  advanced_threat_protection = var.gdp_reporting_advanced_threat_protection
+  advanced_threat_protection = var.gpd_reporting_advanced_threat_protection
   allow_blob_public_access   = false
 
-  blob_properties_delete_retention_policy_days = var.gdp_reporting_delete_retention_days
+  blob_properties_delete_retention_policy_days = var.gpd_reporting_delete_retention_days
 
   tags = var.tags
 }
