ea402f6bff4d07e531c3f4b63dff8e10a94f02d8
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-02-18 08:39:09 +0100

    os_type linux to all fn 2

--- src/reporting_function.tf
+++ src/reporting_function.tf
@@ -28,14 +28,13 @@ module "reporting_fdr_function_snet" {
 module "reporting_fdr_function" {
   source = "git::https://github.com/pagopa/azurerm.git//function_app?ref=v2.2.0"
 
-  resource_group_name = azurerm_resource_group.reporting_fdr_rg.name
-  name                = format("%s-fn-reportingfdr", local.project)
-  location            = var.location
-  health_check_path   = "info"
-  subnet_id           = module.reporting_fdr_function_snet[0].id
-  runtime_version     = "~3"
-  os_type             = "linux"
-
+  resource_group_name                      = azurerm_resource_group.reporting_fdr_rg.name
+  name                                     = format("%s-fn-reportingfdr", local.project)
+  location                                 = var.location
+  health_check_path                        = "info"
+  subnet_id                                = module.reporting_fdr_function_snet[0].id
+  runtime_version                          = "~3"
+  os_type                                  = "linux"
   always_on                                = var.reporting_fdr_function_always_on
   application_insights_instrumentation_key = azurerm_application_insights.application_insights.instrumentation_key
 
