3951bb332104fec3c7d21aea63c10349a2c158bf
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-03-08 16:36:07 +0100

    add subnet to gpd

--- src/gpd.tf
+++ src/gpd.tf
@@ -80,7 +80,8 @@ module "gpd_app_service" {
 
   }
 
-  allowed_subnets = [module.apim_snet.id, module.reporting_function_snet.id, module.payments_snet.id]
+  allowed_subnets = [module.apim_snet.id, module.reporting_function_snet.id, module.payments_snet.id, module.canoneunico_function_snet.id]
+
   allowed_ips     = []
 
   subnet_id = module.gpd_snet[0].id
