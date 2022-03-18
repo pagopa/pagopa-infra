18c2de4aa28f3e67a3cf4b238802a46e517138ac
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-03-18 16:45:13 +0100

    fix app service

--- src/api_config.tf
+++ src/api_config.tf
@@ -72,6 +72,9 @@ module "api_config_app_service" {
     WEBSITE_HEALTHCHECK_MAXPINGFAILURES             = 10
     TIMEOUT_DELAY                                   = 300
     # Integration with private DNS (see more: https://docs.microsoft.com/en-us/answers/questions/85359/azure-app-service-unable-to-resolve-hostname-of-vi.html)
+    WEBSITE_ADD_SITENAME_BINDINGS_IN_APPHOST_CONFIG = "1"
+    WEBSITE_RUN_FROM_PACKAGE = "1"
+    WEBSITE_VNET_ROUTE_ALL = "1"
     WEBSITE_DNS_SERVER = "168.63.129.16"
     # Spring Environment
     SPRING_DATASOURCE_USERNAME = data.azurerm_key_vault_secret.db_nodo_usr.value
