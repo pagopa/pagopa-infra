fb93f3c8999357aec40add58e4506f960a9f9eb9
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-04-08 08:38:26 +0200

    add canoneunico_parsing_csv_error alert

--- src/canoneunico.tf
+++ src/canoneunico.tf
@@ -375,7 +375,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "canoneunico_gpd_error" {
     custom_webhook_payload = "{}"
   }
   data_source_id = azurerm_application_insights.application_insights.id
-  description    = "Problem with GPD"
+  description    = "CU Problem with GPD"
   enabled        = true
   query = format(<<-QUERY
   traces
@@ -383,6 +383,39 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "canoneunico_gpd_error" {
     | order by timestamp desc
     | where message contains "CuCreateDebtPositionFunction"
     | where message contains "Update entity with ERROR status"
+  QUERY
+    , module.canoneunico_function.name
+  )
+  severity    = 2
+  frequency   = 5
+  time_window = 5
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 1
+  }
+}
+
+
+resource "azurerm_monitor_scheduled_query_rules_alert" "canoneunico_parsing_csv_error" {
+  count = var.env_short != "d" ? 1 : 0
+
+  name                = format("%s-cu-csv-parsing-alert", module.canoneunico_function.name)
+  resource_group_name = azurerm_resource_group.canoneunico_rg.name
+  location            = var.location
+
+  action {
+    action_group           = [azurerm_monitor_action_group.email.id, azurerm_monitor_action_group.slack.id]
+    email_subject          = "Email Header"
+    custom_webhook_payload = "{}"
+  }
+  data_source_id = azurerm_application_insights.application_insights.id
+  description    = "CU CSV parsing problem"
+  enabled        = true
+  query = format(<<-QUERY
+  traces
+    | where cloud_RoleName == "%s"
+    | order by timestamp desc
+    | where message contains "[CuCsvParsingFunction Error] Validation Error"
   QUERY
     , module.canoneunico_function.name
   )
