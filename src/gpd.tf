6fd57ce7b3cb7b21c13cc52f72fb2f364f64bd8b
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-04-15 11:49:07 +0200

    gpd autoscaling

--- src/gpd.tf
+++ src/gpd.tf
@@ -132,63 +132,63 @@ module "gpd_app_service_slot_staging" {
   tags = var.tags
 }
 
-# resource "azurerm_monitor_autoscale_setting" "gpd_app_service_autoscale" {
-#   name                = format("%s-autoscale-gpd", local.project)
-#   resource_group_name = azurerm_resource_group.gpd_rg.name
-#   location            = azurerm_resource_group.gpd_rg.location
-#   target_resource_id  = module.gpd_app_service.id
+resource "azurerm_monitor_autoscale_setting" "gpd_app_service_autoscale" {
+  name                = format("%s-autoscale-gpd", local.project)
+  resource_group_name = azurerm_resource_group.gpd_rg.name
+  location            = azurerm_resource_group.gpd_rg.location
+  target_resource_id  = module.gpd_app_service.id
 
-#   profile {
-#     name = "default"
+  profile {
+    name = "default"
 
-#     capacity {
-#       default = var.gpd_autoscale_default
-#       minimum = var.gpd_autoscale_minimum
-#       maximum = var.gpd_autoscale_maximum
-#     }
+    capacity {
+      default = var.gpd_autoscale_default
+      minimum = var.gpd_autoscale_minimum
+      maximum = var.gpd_autoscale_maximum
+    }
 
-#     rule {
-#       metric_trigger {
-#         metric_name              = "Requests"
-#         metric_resource_id       = module.gpd_app_service.id
-#         metric_namespace         = "microsoft.web/sites"
-#         time_grain               = "PT1M"
-#         statistic                = "Average"
-#         time_window              = "PT5M"
-#         time_aggregation         = "Average"
-#         operator                 = "GreaterThan"
-#         threshold                = 250
-#         divide_by_instance_count = false
-#       }
+    rule {
+      metric_trigger {
+        metric_name              = "Requests"
+        metric_resource_id       = module.gpd_app_service.plan_id
+        metric_namespace         = "microsoft.web/sites"
+        time_grain               = "PT1M"
+        statistic                = "Average"
+        time_window              = "PT5M"
+        time_aggregation         = "Average"
+        operator                 = "GreaterThan"
+        threshold                = 130
+        divide_by_instance_count = false
+      }
 
-#       scale_action {
-#         direction = "Increase"
-#         type      = "ChangeCount"
-#         value     = "2"
-#         cooldown  = "PT5M"
-#       }
-#     }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT5M"
+      }
+    }
 
-#     rule {
-#       metric_trigger {
-#         metric_name              = "Requests"
-#         metric_resource_id       = module.gpd_app_service.id
-#         metric_namespace         = "microsoft.web/sites"
-#         time_grain               = "PT1M"
-#         statistic                = "Average"
-#         time_window              = "PT5M"
-#         time_aggregation         = "Average"
-#         operator                 = "LessThan"
-#         threshold                = 250
-#         divide_by_instance_count = false
-#       }
+    rule {
+      metric_trigger {
+        metric_name              = "Requests"
+        metric_resource_id       = module.gpd_app_service.plan_id
+        metric_namespace         = "microsoft.web/sites"
+        time_grain               = "PT1M"
+        statistic                = "Average"
+        time_window              = "PT5M"
+        time_aggregation         = "Average"
+        operator                 = "LessThan"
+        threshold                = 100
+        divide_by_instance_count = false
+      }
 
-#       scale_action {
-#         direction = "Decrease"
-#         type      = "ChangeCount"
-#         value     = "1"
-#         cooldown  = "PT5M"
-#       }
-#     }
-#   }
-# }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+  }
+}
