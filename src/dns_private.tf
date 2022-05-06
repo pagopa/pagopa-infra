dacea3f70a746e5dc0a3e8e90c10c06f7fcb6c5c
Simone Infante <simone.infante@pagopa.it>
2022-05-06 17:44:16 +0200

    feat: add privatelink for acr pagopa

--- src/dns_private.tf
+++ src/dns_private.tf
@@ -95,3 +95,10 @@ resource "azurerm_private_dns_zone_virtual_network_link" "postgres_vnet" {
   private_dns_zone_name = azurerm_private_dns_zone.postgres[0].name
   virtual_network_id    = module.vnet.id
 }
+
+resource "azurerm_private_dns_zone" "privatelink_azurecr_pagopa" {
+  name                = "privatelink.azurecr.io"
+  resource_group_name = azurerm_resource_group.rg_vnet.name
+
+  tags = var.tags
+}
