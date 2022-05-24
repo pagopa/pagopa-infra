e25db5cd8626912fc0e42490a15bc6d7df87db25
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-05-24 15:57:51 +0200

    api-config for checkout

--- src/apim_api_config.tf
+++ src/apim_api_config.tf
@@ -111,3 +111,72 @@ resource "azurerm_api_management_authorization_server" "apiconfig-oauth2" {
   client_authentication_method = ["Body"]
 
 }
+
+###########################
+## Products for Checkout ##
+###########################
+
+module "apim_api_config_checkout_product" {
+  count  = var.api_config_enabled ? 1 : 0
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v1.0.84"
+
+  product_id   = "product-api-config-checkout"
+  display_name = "ApiConfig for Checkout"
+  description  = "Product for API Configuration of the Node for Checkout"
+
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  published             = true
+  subscription_required = true
+  approval_required     = false
+
+  policy_xml = file("./api_product/apiconfig_api/_base_policy_checkout.xml")
+}
+
+########################
+##  API for Checkout  ##
+########################
+
+resource "azurerm_api_management_api_version_set" "api_config_checkout_api" {
+  count = var.api_config_enabled ? 1 : 0
+
+  name                = format("%s-api-config-checkout-api", var.env_short)
+  resource_group_name = azurerm_resource_group.rg_api.name
+  api_management_name = module.apim.name
+  display_name        = "ApiConfig for Checkout"
+  versioning_scheme   = "Segment"
+}
+
+module "apim_api_config_checkout_api" {
+  count  = var.api_config_enabled ? 1 : 0
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v2.0.12"
+
+  name                  = format("%s-api-config-checkout-api", var.env_short)
+  api_management_name   = module.apim.name
+  resource_group_name   = azurerm_resource_group.rg_api.name
+  product_ids           = [module.apim_api_config_checkout_product[0].product_id]
+
+  subscription_required = true
+
+  version_set_id = azurerm_api_management_api_version_set.api_config_checkout_api[0].id
+  api_version    = "v1"
+
+  description  = "Api configuration for Checkout"
+  display_name = "ApiConfig for Checkout"
+  path         = "apiconfig/checkout/api"
+  protocols    = ["https"]
+
+  service_url = format("https://%s/apiconfig/api/v1", module.api_config_app_service[0].default_site_hostname)
+
+  content_format = "openapi"
+  content_value = templatefile("./api/apiconfig_api/checkout/v1/_openapi.json.tpl", {
+    host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
+  })
+
+  xml_content = templatefile("./api/apiconfig_api/checkout/v1/_base_policy.xml.tpl", {
+    origin                 = format("https://%s.%s.%s", var.cname_record_name, var.dns_zone_prefix, var.external_domain)
+    pagopa_tenant_id       = local.pagopa_tenant_id
+    apiconfig_be_client_id = local.apiconfig_be_client_id
+  })
+}
