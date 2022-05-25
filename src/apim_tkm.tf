3906111fe0696cf6a60fadc2b5b04e465bf961a5
Simone Infante <simone.infante@pagopa.it>
2022-05-25 15:59:36 +0200

    fix: add subscription limit

--- src/apim_tkm.tf
+++ src/apim_tkm.tf
@@ -15,6 +15,7 @@ module "apim_tkm_product" {
   published             = true
   subscription_required = true
   approval_required     = true
+  subscriptions_limit   = 1000
 
   policy_xml = file("./api_product/tkm_api/_base_policy.xml")
 }
