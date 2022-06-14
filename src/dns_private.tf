35ec7952b7e54d0717fc0840ba8052b14fdb3a9d
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-06-14 22:28:47 +0200

    fix

--- src/dns_private.tf
+++ src/dns_private.tf
@@ -133,6 +133,13 @@ resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_documents_
   tags = var.tags
 }
 
+resource "azurerm_private_dns_zone" "internal_platform_pagopa_it" {
+  name                = "internal.${var.dns_zone_prefix}.${var.external_domain}"
+  resource_group_name = azurerm_resource_group.rg_vnet.name
+
+  tags = var.tags
+}
+
 resource "azurerm_private_dns_zone_virtual_network_link" "internal_platform_pagopa_it_private_vnet" {
   name                  = module.vnet_integration.name
   resource_group_name   = azurerm_resource_group.rg_vnet.name
