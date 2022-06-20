94d12d088e4d3664c473bba36c416cf20cbc73d8
Simone Infante <simone.infante@pagopa.it>
2022-06-20 10:34:41 +0200

    feat: add sessions api

--- src/ecommerce/04_apim_ecommerce.tf
+++ src/ecommerce/04_apim_ecommerce.tf
@@ -113,3 +113,50 @@ module "apim_ecommerce_payment_instruments_service_api_v1" {
   
   xml_content = file("./api/ecommerce-payment-instruments-service/v1/_base_policy.xml.tpl")
 }
+
+##########################
+## API sessions service ##
+##########################
+locals {
+  apim_ecommerce_sessions_service_api = {
+    display_name          = "ecommerce pagoPA - sessions service API"
+    description           = "API to support sessions in ecommerce"
+    path                  = "ecommerce/payment-sessions-service"
+    subscription_required = false
+    service_url           = null
+  }
+}
+
+# Sessions service APIs
+resource "azurerm_api_management_api_version_set" "ecommerce_sessions_service_api" {
+  name                = format("%s-sessions-service-api", local.project)
+  resource_group_name = local.pagopa_apim_rg
+  api_management_name = local.pagopa_apim_name
+  display_name        = local.apim_ecommerce_sessions_service_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+module "apim_ecommerce_sessions_service_api_v1" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v2.18.3"
+
+  name                  = format("%s-sessions-service-api", local.project)
+  api_management_name   = local.pagopa_apim_name
+  resource_group_name   = local.pagopa_apim_rg
+  product_ids           = [module.apim_ecommerce_product.product_id]
+  subscription_required = local.apim_ecommerce_sessions_service_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.ecommerce_sessions_service_api.id
+  api_version           = "v1"
+
+  description  = local.apim_ecommerce_sessions_service_api.description
+  display_name = local.apim_ecommerce_sessions_service_api.display_name
+  path         = local.apim_ecommerce_sessions_service_api.path
+  protocols    = ["https"]
+  service_url  = local.apim_ecommerce_sessions_service_api.service_url
+
+  content_format = "openapi"
+  content_value = templatefile("./api/ecommerce-sessions-service/v1/_openapi.json.tpl", {
+    host = local.apim_hostname
+  })
+  
+  xml_content = file("./api/ecommerce-sessions-service/v1/_base_policy.xml.tpl")
+}
