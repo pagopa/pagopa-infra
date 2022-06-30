98b8162a2bee7c93820ca117d2b8f84659fd649c
Simone Infante <simone.infante@pagopa.it>
2022-06-30 17:29:05 +0200

    fix: pgs api name

--- src/apim_payment_transactions_gateway.tf
+++ src/apim_payment_transactions_gateway.tf
@@ -39,7 +39,7 @@ locals {
   }
 }
 
-resource "azurerm_api_management_api_version_set" "payment_transactions_gateway_api" {
+resource "azurerm_api_management_api_version_set" "payment_transactions_gateway_internal_api" {
 
   name                = format("%s-payment-transactions-gateway-internal-api", local.project)
   resource_group_name = azurerm_resource_group.rg_api.name
@@ -57,7 +57,7 @@ module "apim_payment_transactions_gateway_internal_api_v1" {
   resource_group_name   = azurerm_resource_group.rg_api.name
   product_ids           = [module.apim_payment_transactions_gateway_product.product_id]
   subscription_required = local.apim_payment_transactions_gateway_internal_api.subscription_required
-  version_set_id        = azurerm_api_management_api_version_set.payment_transactions_gateway_api.id
+  version_set_id        = azurerm_api_management_api_version_set.payment_transactions_gateway_internal_api.id
   api_version           = "v1"
   service_url           = local.apim_payment_transactions_gateway_internal_api.service_url
 
