d0b9ea1f4686e93ec348c9cdab1aa50597e60548
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-07-15 12:36:55 +0200

    initial configuration

---
+++ src/shared-common/03_cosmosdb_iuvgenerator.tf
@@ -0,0 +1,68 @@
+resource "azurerm_resource_group" "gps_rg" {
+  name     = format("%s-rg", local.project)
+  location = var.location
+
+  tags = var.tags
+}
+
+module "iuvgenerator_cosmosdb_snet" {
+  source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v1.0.51"
+  name                 = format("%s-cosmosdb-snet", local.project)
+  address_prefixes     = var.cidr_subnet_iuvgenerator_cosmosdb
+  resource_group_name  = local.vnet_resource_group_name
+  virtual_network_name = local.vnet_name
+
+  enforce_private_link_endpoint_network_policies = true
+
+  service_endpoints = [
+    "Microsoft.Web",
+    "Microsoft.AzureCosmosDB",
+    "Microsoft.Storage",
+  ]
+}
+
+module "iuvgenerator_cosmosdb_account" {
+  source   = "git::https://github.com/pagopa/azurerm.git//cosmosdb_account?ref=v2.1.18"
+  name     = format("%s-iuv-generator-cosmos-account", local.project)
+  location = var.location
+
+  resource_group_name = azurerm_resource_group.gps_rg.name
+  offer_type          = var.cosmos_iuvgenerator_db_params.offer_type
+  kind                = var.cosmos_iuvgenerator_db_params.kind
+
+  public_network_access_enabled    = var.cosmos_iuvgenerator_db_params.public_network_access_enabled
+  main_geo_location_zone_redundant = var.cosmos_iuvgenerator_db_params.main_geo_location_zone_redundant
+
+  enable_free_tier          = var.cosmos_iuvgenerator_db_params.enable_free_tier
+  enable_automatic_failover = true
+
+  capabilities       = var.cosmos_iuvgenerator_db_params.capabilities
+  consistency_policy = var.cosmos_iuvgenerator_db_params.consistency_policy
+
+  main_geo_location_location = var.location
+  additional_geo_locations   = var.cosmos_iuvgenerator_db_params.additional_geo_locations
+  backup_continuous_enabled  = var.cosmos_iuvgenerator_db_params.backup_continuous_enabled
+
+  is_virtual_network_filter_enabled = var.cosmos_iuvgenerator_db_params.is_virtual_network_filter_enabled
+
+  ip_range = ""
+
+  # add data.azurerm_subnet.<my_service>.id
+  allowed_virtual_network_subnet_ids = var.cosmos_iuvgenerator_db_params.public_network_access_enabled ? [] : [data.azurerm_subnet.aks_subnet.id]
+
+  # private endpoint
+  private_endpoint_name    = format("%s-iuv-generator-cosmos-sql-endpoint", local.project)
+  private_endpoint_enabled = var.cosmos_iuvgenerator_db_params.private_endpoint_enabled
+  subnet_id                = module.iuvgenerator_cosmosdb_snet.id
+  private_dns_zone_ids     = [data.azurerm_private_dns_zone.cosmos.id]
+
+  tags = var.tags
+}
+
+# cosmosdb table storage
+resource "azurerm_cosmosdb_table" "example" {
+  name                = "iuvstable"
+  resource_group_name = azurerm_resource_group.gps_rg.name
+  account_name        = module.iuvgenerator_cosmosdb_account.name
+  throughput          = 400
+}
