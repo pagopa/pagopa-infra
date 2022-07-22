6eb8935b95eb8bf70765e6b43fb20900ce2932fc
Simone Infante <simone.infante@pagopa.it>
2022-07-22 19:11:08 +0200

    feat: add tkm internal api

--- src/apim_tkm.tf
+++ src/apim_tkm.tf
@@ -72,6 +72,54 @@ module "apim_tkm_consent_manager_api_v1" {
   })
 }
 
+##########################################
+## API tkm-ms-consent-manager internal  ##
+##########################################
+locals {
+  apim_tkm_consent_manager_internal_api = {
+    # params for all api versions
+    display_name          = "Token Manager- tkm-ms-consent-manager-internal"
+    description           = "RESTful APIs provided for consent management exposed to the issuers"
+    path                  = "tkm/internal/tkmconsentmanager"
+    subscription_required = true
+    service_url           = null
+  }
+}
+
+resource "azurerm_api_management_api_version_set" "tkm_consent_manager_internal_api" {
+
+  name                = format("%s-tkm-consent-manager-internal-api", local.project)
+  resource_group_name = azurerm_resource_group.rg_api.name
+  api_management_name = module.apim.name
+  display_name        = local.apim_tkm_consent_manager_internal_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+module "apim_tkm_consent_manager_internal_api_v1" {
+
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
+
+  name                  = format("%s-tkm-consent-manager-internal-api", local.project)
+  api_management_name   = module.apim.name
+  resource_group_name   = azurerm_resource_group.rg_api.name
+  product_ids           = [module.apim_tkm_product.product_id]
+  subscription_required = local.apim_tkm_consent_manager_internal_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.tkm_consent_manager_internal_api.id
+  api_version           = "v1"
+  service_url           = local.apim_tkm_consent_manager_internal_api.service_url
+
+  description  = local.apim_tkm_consent_manager_internal_api.description
+  display_name = local.apim_tkm_consent_manager_internal_api.display_name
+  path         = local.apim_tkm_consent_manager_internal_api.path
+  protocols    = ["https"]
+
+  content_format = "openapi"
+  content_value = templatefile("./api/tkm_api/tkm-ms-consent-manager-internal/v1/_openapi.json.tpl", {
+    host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
+  })
+
+  xml_content = file("./api/tkm_api/tkm-ms-consent-manager-internal/v1/_base_policy.xml.tpl")
+}
 
 #################################
 ## API tkm-ms-card-manager  ##
