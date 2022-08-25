1e89d6d95a34c1f4f7ef795b8a532cf54ba9ca71
Simone Infante <simone.infante@pagopa.it>
2022-08-25 10:22:27 +0200

    fix: update policy for pgs

--- src/apim_payment_transactions_gateway.tf
+++ src/apim_payment_transactions_gateway.tf
@@ -71,7 +71,9 @@ module "apim_payment_transactions_gateway_update_api_v1" {
     host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
   })
 
-  xml_content = file("./api/payment_transactions_gateway_api/update/v1/_base_policy.xml.tpl")
+  xml_content = templatefile("./api/payment_transactions_gateway_api/update/v1/_base_policy.xml.tpl", {
+    hostname = var.env_short == "d" ? "{{pm-gtw-hostname}}" : "{{pm-onprem-hostname}}"
+  })
 }
 
 ################################################
@@ -120,7 +122,9 @@ module "apim_payment_transactions_gateway_internal_api_v1" {
     host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
   })
 
-  xml_content = file("./api/payment_transactions_gateway_api/internal/v1/_base_policy.xml.tpl")
+  xml_content = templatefile("./api/payment_transactions_gateway_api/internal/v1/_base_policy.xml.tpl", {
+    hostname = var.env_short == "d" ? "{{pm-gtw-hostname}}" : "{{pm-onprem-hostname}}"
+  })
 }
 
 ###############################################
@@ -170,6 +174,7 @@ module "apim_payment_transactions_gateway_external_api_v1" {
   })
 
   xml_content = templatefile("./api/payment_transactions_gateway_api/external/v1/_base_policy.xml.tpl", {
-    origin = var.env_short == "d" ? "*" : "https://${var.dns_zone_checkout}.${var.external_domain}/"
+    origin = var.env_short == "d" ? "*" : "https://${var.dns_zone_checkout}.${var.external_domain}/",
+    hostname = var.env_short == "d" ? "{{pm-gtw-hostname}}" : "{{pm-onprem-hostname}}"
   })
 }
