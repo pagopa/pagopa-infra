55161914e5e70565376a446079695e6551c7a148
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-09-11 23:33:05 +0200

    added multiple records in platform dns private

--- src/core/dns_private.tf
+++ src/core/dns_private.tf
@@ -1,3 +1,4 @@
+# Private DNS zone: nodo dei pagamenti
 
 resource "azurerm_private_dns_zone" "db_nodo_dns_zone" {
   name                = var.private_dns_zone_db_nodo_pagamenti
@@ -7,7 +8,7 @@ resource "azurerm_private_dns_zone" "db_nodo_dns_zone" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "db_nodo_dns_zone_virtual_link" {
-  name                  = format("%s-db_nodo-private-dns-zone-link", local.project)
+  name                  = "${local.project}-db_nodo-private-dns-zone-link"
   resource_group_name   = azurerm_resource_group.data.name
   private_dns_zone_name = azurerm_private_dns_zone.db_nodo_dns_zone.name
   virtual_network_id    = module.vnet_integration.id
@@ -22,6 +23,8 @@ resource "azurerm_private_dns_a_record" "private_dns_a_record_db_nodo" {
   ttl                 = 60
   records             = var.dns_a_reconds_dbnodo_ips
 }
+
+# Private dns zone: redis
 
 resource "azurerm_private_dns_zone" "privatelink_redis_cache_windows_net" {
   count               = var.redis_private_endpoint_enabled ? 1 : 0
@@ -42,23 +45,30 @@ resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_redis_cach
   tags = var.tags
 }
 
+# Private dns zone: storage queue
+
 resource "azurerm_private_dns_zone" "privatelink_queue_core_windows_net" {
-  count               = var.storage_queue_private_endpoint_enabled ? 1 : 0
+  count = var.storage_queue_private_endpoint_enabled ? 1 : 0
+
   name                = "privatelink.queue.core.windows.net"
   resource_group_name = azurerm_resource_group.rg_vnet.name
 
   tags = var.tags
 }
 
+# Private dns zone: platform.pagopa.it
+
 resource "azurerm_private_dns_zone" "platform_private_dns_zone" {
-  name                = format("%s.%s", var.dns_zone_prefix, var.external_domain)
+  name                = "${var.dns_zone_prefix}.${var.external_domain}"
   resource_group_name = azurerm_resource_group.rg_vnet.name
 
   tags = var.tags
 }
 
 resource "azurerm_private_dns_a_record" "platform_dns_a_private" {
-  name                = "api"
+
+  for_each = toset(var.platform_private_dns_zone_records)
+  name                = each.key
   zone_name           = azurerm_private_dns_zone.platform_private_dns_zone.name
   resource_group_name = azurerm_resource_group.rg_vnet.name
   ttl                 = var.dns_default_ttl_sec
