8d414496f6f927e549125d6e2c0a01e66a6fac56
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-09-14 12:21:06 +0200

    add payment-manager/mock-services and pmmockserviceapi for FE in UAT env too

--- src/core/apim_payment_manager.tf
+++ src/core/apim_payment_manager.tf
@@ -1075,7 +1075,7 @@ locals {
 }
 
 module "apim_pm_mock_services_fe" {
-  count  = var.env_short == "d" ? 1 : 0
+  count  = var.env_short != "p" ? 1 : 0
   source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.90"
 
   name                  = format("%s-pm-mock-services-fe-api", local.project)
@@ -1112,7 +1112,7 @@ locals {
 }
 
 resource "azurerm_api_management_api_version_set" "apim_pm_mock_services_api" {
-  count = var.env_short == "d" ? 1 : 0
+  count = var.env_short != "p" ? 1 : 0
 
   name                = format("%s-pm-mock-services-api", local.project)
   resource_group_name = azurerm_resource_group.rg_api.name
@@ -1123,7 +1123,7 @@ resource "azurerm_api_management_api_version_set" "apim_pm_mock_services_api" {
 
 module "apim_pm_mock_services_api_v1" {
 
-  count = var.env_short == "d" ? 1 : 0
+  count = var.env_short != "p" ? 1 : 0
 
   source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.90"
 
@@ -1163,7 +1163,7 @@ locals {
 }
 
 resource "azurerm_api_management_api_version_set" "apim_pm_test_utility_api" {
-  count = var.env_short == "d" ? 1 : 0
+  count = var.env_short != "p" ? 1 : 0
 
   name                = format("%s-pm-test-utility-api", local.project)
   resource_group_name = azurerm_resource_group.rg_api.name
@@ -1174,7 +1174,7 @@ resource "azurerm_api_management_api_version_set" "apim_pm_test_utility_api" {
 
 module "apim_pm_test_utility_api_v1" {
 
-  count = var.env_short == "d" ? 1 : 0
+  count = var.env_short != "p" ? 1 : 0
 
   source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.90"
 
