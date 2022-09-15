94a14fe10a127d74414f8c95ac34c3637c2560ef
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-09-15 19:57:10 +0200

    Update dns_public.tf

--- src/core/dns_public.tf
+++ src/core/dns_public.tf
@@ -88,7 +88,7 @@ resource "azurerm_dns_a_record" "dns_a_api" {
   name                = "api"
   zone_name           = azurerm_dns_zone.public[0].name
   resource_group_name = azurerm_resource_group.rg_vnet.name
-  ttl                 = var.dns_default_ttl_sec
+  ttl                 = "60" # var.dns_default_ttl_sec
   records             = [azurerm_public_ip.appgateway_public_ip.ip_address]
   tags                = var.tags
 }
