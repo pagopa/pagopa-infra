ddf32de3fa9eaa56dae3cef034423e9be00401ef
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-09-26 18:51:31 +0200

    add X-Orginal-Host-For app-gw rewrite rules

--- src/core/appgateway.tf
+++ src/core/appgateway.tf
@@ -319,6 +319,36 @@ module "app_gw" {
           response_header_configurations = []
           url                            = null
         },
+        {
+          name          = "http-deny-path2"
+          rule_sequence = 2
+          condition = {
+            variable    = "var_uri_path"
+            pattern     = join("|", var.app_gateway_deny_paths_2)
+            ignore_case = true
+            negate      = false
+          }
+          request_header_configurations  = []
+          response_header_configurations = []
+          url = {
+            path         = "notfound"
+            query_string = null
+          }
+        },
+        {
+          name          = "add-original-host-header"
+          rule_sequence = 101
+          condition     = null
+
+          request_header_configurations = [
+            {
+              header_name  = "X-Orginal-Host-For"
+              header_value = "{var_host}"
+            },
+          ]
+          url                            = null
+          response_header_configurations = []
+        },
       ]
     }
   ]
