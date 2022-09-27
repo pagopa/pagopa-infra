44b04779bc7f9fddd2d4f8b29c1beaa87aceecd2
andrea-putzu <andrea.putzu@pagopa.it>
2022-09-27 15:54:18 +0200

    feat: Fix params

--- src/core/dns_selc_public.tf
+++ src/core/dns_selc_public.tf
@@ -13,10 +13,10 @@ resource "azurerm_dns_ns_record" "dev_selc" {
   zone_name           = azurerm_dns_zone.selc_public[0].name
   resource_group_name = azurerm_resource_group.rg_vnet.name
   records = [
-    "ns1-09.azure-dns.com.",
-    "ns2-09.azure-dns.net.",
-    "ns3-09.azure-dns.org.",
-    "ns4-09.azure-dns.info."
+    "ns1-07.azure-dns.com.",
+    "ns2-07.azure-dns.net.",
+    "ns3-07.azure-dns.org.",
+    "ns4-07.azure-dns.info."
   ]
   ttl  = var.dns_default_ttl_sec
   tags = var.tags
