c6907892ec7edecacb67539372d42926cf1b68fe
Jacopo <jacopo.carlini@gmail.com>
2022-09-29 11:15:33 +0200

    fix

--- src/domains/shared-app/99_variables.tf
+++ src/domains/shared-app/99_variables.tf
@@ -118,14 +118,20 @@ variable "tls_cert_check_helm" {
   description = "tls cert helm chart configuration"
 }
 
-variable "influxdb2_helm_version" {
-  type        = string
-  default     = "2.1.0"
-  description = "version of influxdb's helm chart"
-}
-
-variable "influxdb2_helm_image_tag" {
-  type        = string
-  default     = "influxdb:2.2.0-alpine@sha256:f3b54d91cae591fc3fde20299bd0b262f6f6d9a1f73b98d623b501e82c49d5fb"
-  description = "image an tag of influxdb2"
+variable "influxdb2_helm" {
+  type = object({
+    chart_version = string,
+    image = object({
+      name = string,
+      tag  = string
+    })
+  })
+  default = {
+    chart_version = "2.1.0",
+    image = {
+      name = "influxdb",
+      tag  = "2.2.0-alpine"
+    }
+  }
+  description = "influxdb2 helm chart configuration"
 }
