77e27c4b078868d6f8be9ca92415dee53fa75837
andrea-putzu <andrea.putzu@pagopa.it>
2022-09-29 17:03:16 +0200

    feat: adding apim for pagopa-selfcare api

--- src/domains/selc-app/99_locals.tf
+++ src/domains/selc-app/99_locals.tf
@@ -23,10 +23,10 @@ locals {
 
   apim_hostname = "api.${var.apim_dns_zone_prefix}.${var.external_domain}"
   selc_hostname = var.env == "prod" ? "weuprod.selc.internal.platform.pagopa.it" : "weu${var.env}.selc.internal.${var.env}.platform.pagopa.it"
-
+  
   # selc
   dns_zone_selc     = "selc"
   dns_zone_platform = var.env == "prod" ? "platform" : "${var.env}.platform"
   external_domain   = "pagopa.it"
-
 }
+ 
