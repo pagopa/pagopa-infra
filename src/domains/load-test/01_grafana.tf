b86ee719472ae5d2fcaf6bd16d297bb5be3b7240
Riccardo Trivellato <riccardo.trivellato@gmail.com>
2022-10-03 13:06:28 +0200

    feat: change resource group

--- src/domains/load-test/01_grafana.tf
+++ src/domains/load-test/01_grafana.tf
@@ -1,9 +1,16 @@
+resource "azurerm_resource_group" "load_test" {
+  
+  name     = "${var.prefix}-${var.env_short}-load-test-rg"
+  location = var.location
+
+}
+
 module "grafana_managed" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//grafana?ref=feature/grafana"
 
   name = "${var.env_short}-grafana"
 
-  resource_group_name = var.terraform_remote_state_core.resource_group_name
+  resource_group_name = azurerm_resource_group.load_test.name
 
   tags = var.tags
 
@@ -13,7 +20,7 @@ resource "azurerm_dns_cname_record" "grafana" {
 
   name                = "grafana.${var.dns_zone_prefix}.${var.external_domain}"
   zone_name           = var.dns_zone_prefix
-  resource_group_name = var.terraform_remote_state_core.resource_group_name
+  resource_group_name = azurerm_resource_group.load_test.name
   ttl                 = 3600
   record              = module.grafana_managed.endpoint
 
