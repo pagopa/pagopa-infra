57c94dac800ab30828ead0de59a55e4a64f6ad2d
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-10-19 18:26:26 +0200

    biz events datastore configuration

---
+++ src/domains/bizevents-common/03_cosmosdb_bizevents_datastore.tf
@@ -0,0 +1,122 @@
+resource "azurerm_resource_group" "bizevents_rg" {
+  name     = format("%s-rg", local.project)
+  location = var.location
+
+  tags = var.tags
+}
+
+module "bizevents_datastore_cosmosdb_snet" {
+  source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v1.0.90"
+  name                 = format("%s-datastore-cosmosdb-snet", local.project)
+  address_prefixes     = var.cidr_subnet_bizevents_datastore_cosmosdb
+  resource_group_name  = local.vnet_resource_group_name
+  virtual_network_name = local.vnet_name
+
+  enforce_private_link_endpoint_network_policies = true
+
+  service_endpoints = [
+    "Microsoft.Web",
+    "Microsoft.AzureCosmosDB",
+    "Microsoft.Storage",
+  ]
+}
+
+module "bizevents_datastore_cosmosdb_account" {
+  source   = "git::https://github.com/pagopa/azurerm.git//cosmosdb_account?ref=v2.1.18"
+  name     = format("%s-datastore-cosmos-account", local.project)
+  location = var.location
+
+  resource_group_name = azurerm_resource_group.bizevents_rg.name
+  offer_type          = var.bizevents_datastore_cosmos_db_params.offer_type
+  kind                = var.bizevents_datastore_cosmos_db_params.kind
+
+  public_network_access_enabled    = var.bizevents_datastore_cosmos_db_params.public_network_access_enabled
+  main_geo_location_zone_redundant = var.bizevents_datastore_cosmos_db_params.main_geo_location_zone_redundant
+
+  enable_free_tier          = var.bizevents_datastore_cosmos_db_params.enable_free_tier
+  enable_automatic_failover = true
+
+  capabilities       = var.bizevents_datastore_cosmos_db_params.capabilities
+  consistency_policy = var.bizevents_datastore_cosmos_db_params.consistency_policy
+
+  main_geo_location_location = var.location
+  additional_geo_locations   = var.bizevents_datastore_cosmos_db_params.additional_geo_locations
+  backup_continuous_enabled  = var.bizevents_datastore_cosmos_db_params.backup_continuous_enabled
+
+  is_virtual_network_filter_enabled = var.bizevents_datastore_cosmos_db_params.is_virtual_network_filter_enabled
+
+  ip_range = ""
+
+  # add data.azurerm_subnet.<my_service>.id
+  allowed_virtual_network_subnet_ids = var.bizevents_datastore_cosmos_db_params.public_network_access_enabled ? [] : [data.azurerm_subnet.aks_subnet.id]
+
+  # private endpoint
+  private_endpoint_name    = format("%s-datastore-cosmos-sql-endpoint", local.project)
+  private_endpoint_enabled = var.bizevents_datastore_cosmos_db_params.private_endpoint_enabled
+  subnet_id                = module.bizevents_datastore_cosmosdb_snet.id
+  private_dns_zone_ids     = [data.azurerm_private_dns_zone.cosmos.id]
+
+  tags = var.tags
+}
+
+# cosmosdb database for marketplace
+module "bizevents_datastore_cosmosdb_database" {
+  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_database?ref=v2.1.15"
+  name                = "db"
+  resource_group_name = azurerm_resource_group.bizevents_rg.name
+  account_name        = module.bizevents_datastore_cosmosdb_account.name
+}
+
+### Containers
+locals {
+  # TODO
+  bizevents_datastore_cosmosdb_containers = [
+    {
+      name               = "bundles",
+      partition_key_path = "/idPsp",
+    },
+    {
+      name               = "archivedbundles",
+      partition_key_path = "/idPsp",
+    },
+    {
+      name               = "cibundles",
+      partition_key_path = "/ciFiscalCode",
+    },
+    {
+      name               = "archivedcibundles",
+      partition_key_path = "/ciFiscalCode",
+    },
+    {
+      name               = "bundlerequests",
+      partition_key_path = "/idPsp",
+    },
+    {
+      name               = "archivedbundlerequests",
+      partition_key_path = "/idPsp",
+    },
+    {
+      name               = "bundleoffers",
+      partition_key_path = "/ciFiscalCode",
+    },
+    {
+      name               = "archivedbundleoffers",
+      partition_key_path = "/ciFiscalCode",
+    },
+  ]
+}
+
+# cosmosdb container for biz events datastore
+module "bizevents_datastore_cosmosdb_containers" {
+  source   = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_container?ref=v2.1.8"
+  for_each = { for c in local.bizevents_datastore_cosmosdb_containers : c.name => c }
+
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.afm_rg.name
+  account_name        = module.bizevents_datastore_cosmosdb_account.name
+  database_name       = module.bizevents_datastore_cosmosdb_database.name
+  partition_key_path  = each.value.partition_key_path
+  throughput          = lookup(each.value, "throughput", null)
+
+  autoscale_settings = contains(var.bizevents_datastore_cosmos_db_params.capabilities, "EnableServerless") ? null : lookup(each.value, "autoscale_settings", null)
+}
---
+++ src/domains/bizevents-common/env/weu-uat/terraform.tfvars
@@ -0,0 +1,61 @@
+prefix         = "pagopa"
+env_short      = "u"
+env            = "uat"
+domain         = "bizevents"
+location       = "westeurope"
+location_short = "weu"
+instance       = "uat"
+
+tags = {
+  CreatedBy   = "Terraform"
+  Environment = "Prod"
+  Owner       = "IO"
+  Source      = "https://github.com/pagopa/pagopa-infra/tree/main/src/bizevents"
+  CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
+}
+
+lock_enable = true
+
+terraform_remote_state_core = {
+  resource_group_name  = "io-infra-rg"
+  storage_account_name = "pagopainfraterraformuat"
+  container_name       = "azureadstate"
+  key                  = "uat.terraform.tfstate"
+}
+
+### External resources
+
+monitor_resource_group_name                 = "pagopa-u-monitor-rg"
+log_analytics_workspace_name                = "pagopa-u-law"
+log_analytics_workspace_resource_group_name = "pagopa-u-monitor-rg"
+ingress_load_balancer_ip                    = "10.1.100.250"
+
+external_domain          = "pagopa.it"
+dns_zone_internal_prefix = "internal.uat.platform"
+
+# CosmosDB Biz Events Datastore
+bizevents_datastore_cosmos_db_params = {
+  kind         = "GlobalDocumentDB"
+  capabilities = []
+  offer_type   = "Standard"
+  consistency_policy = {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+  server_version                   = "4.0"
+  main_geo_location_zone_redundant = false
+  enable_free_tier                 = false
+
+  private_endpoint_enabled      = true
+  public_network_access_enabled = false
+
+  additional_geo_locations = []
+
+  is_virtual_network_filter_enabled = true
+
+  backup_continuous_enabled = false
+
+}
+
+cidr_subnet_bizevents_datastore_cosmosdb = ["10.1.156.0/24"]
