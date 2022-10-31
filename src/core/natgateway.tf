b1f6ff0834f4d2c32acca99f1d75154f22b5c964
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-10-31 13:32:37 +0100

    add apim forwarder + apim forward + natgw connection

--- src/core/natgateway.tf
+++ src/core/natgateway.tf
@@ -1,3 +1,9 @@
+locals {
+  subnet_in_nat_gw_ids = [
+    module.node_forwarder_snet.id # pagopa-node-forwarder ( aka GAD replacemnet )
+  ]
+}
+
 module "nat_gw" {
   count  = var.nat_gateway_enabled ? 1 : 0
   source = "git::https://github.com/pagopa/azurerm.git//nat_gateway?ref=v1.0.90"
@@ -7,7 +13,7 @@ module "nat_gw" {
   location            = azurerm_resource_group.rg_vnet.location
   public_ips_count    = var.nat_gateway_public_ips
   zone                = "1"
-  subnet_ids          = []
+  subnet_ids          = local.subnet_in_nat_gw_ids
 
   tags = var.tags
 }
