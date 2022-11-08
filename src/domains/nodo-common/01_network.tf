b12981b76a9e8968e5523d9184f9d74c12b766d9
ma_cloud_arch79 <94049389+matteoalongi@users.noreply.github.com>
2022-11-08 12:35:49 +0100

    Update src/domains/nodo-common/01_network.tf
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

--- src/domains/nodo-common/01_network.tf
+++ src/domains/nodo-common/01_network.tf
@@ -25,18 +25,3 @@ data "azurerm_subnet" "aks_subnet" {
   virtual_network_name = local.vnet_name
   resource_group_name  = local.vnet_resource_group_name
 }
-
-#
-# Private DNS Zone for Postgres Databases
-#
-resource "azurerm_private_dns_zone" "postgres" {
-  name                = "private.postgres.database.azure.com"
-  resource_group_name = data.azurerm_resource_group.rg_vnet.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "postgres_vnet" {
-  name                  = "${local.project}-postgres-vnet-private-dns-zone-link"
-  resource_group_name   = data.azurerm_resource_group.rg_vnet.name
-  private_dns_zone_name = azurerm_private_dns_zone.postgres.name
-  virtual_network_id    = data.azurerm_virtual_network.vnet.id
-}
