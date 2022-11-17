1ee229cb4d5a965c5bb1fe0f26e6463151c18aef
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-11-17 17:46:31 +0100

    wip

--- src/domains/nodo-common/02_security.tf
+++ src/domains/nodo-common/02_security.tf
@@ -33,32 +33,3 @@ resource "azurerm_key_vault_secret" "db_nodo_cfg_login_password" {
   }
 }
 
-#tfsec:ignore:azure-keyvault-ensure-secret-expiry tfsec:ignore:azure-keyvault-content-type-for-secret
-resource "azurerm_key_vault_secret" "db_nodo_cfg_login" {
-  name         = "db-nodo-cfg-login"
-  value        = "<TO_UPDATE_MANUALLY_BY_PORTAL>"
-  content_type = "text/plain"
-
-  key_vault_id = module.key_vault.id
-
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
-}
-
-#tfsec:ignore:azure-keyvault-ensure-secret-expiry tfsec:ignore:azure-keyvault-content-type-for-secret
-resource "azurerm_key_vault_secret" "db_nodo_cfg_login_password" {
-  name         = "db-nodo-cfg-login-password"
-  value        = "<TO_UPDATE_MANUALLY_BY_PORTAL>"
-  content_type = "text/plain"
-
-  key_vault_id = module.key_vault.id
-
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
-}
