324266fa24e5931ea5c3b3c638fe67fdc94d3e58
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-11-18 10:47:39 +0100

    Update src/domains/nodo-secret/99_main.tf

--- src/domains/nodo-secret/99_main.tf
+++ src/domains/nodo-secret/99_main.tf
@@ -12,14 +12,6 @@ terraform {
       source  = "hashicorp/null"
       version = "= 3.1.1"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "= 2.11.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "= 2.5.1"
-    }
   }
 
   backend "azurerm" {}
