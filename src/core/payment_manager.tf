5dd15e5df9c302ab16e6fd3fdbf41787079828cb
Simone Infante <simone.infante@pagopa.it>
2022-12-05 16:15:08 +0100

    feat: add alert for wallet and restapi-cd

--- src/core/payment_manager.tf
+++ src/core/payment_manager.tf
@@ -1,6 +1,6 @@
-resource "azurerm_resource_group" "payment_manager_rg" {
+resource "azurerm_resource_group" "payment_manager_monitor_rg" {
   count    = var.env_short == "p" ? 1 : 0
-  name     = format("%s-payment-manager-rg", local.project)
+  name     = format("%s-payment-manager-monitor-rg", local.project)
   location = var.location
   tags     = var.tags
 }
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "payment_manager_rg" {
 resource "azurerm_monitor_scheduled_query_rules_alert" "pm_restapi_availability" {
   count               = var.env_short == "p" ? 1 : 0
   name                = "${local.project}-pm-restapi-availability-alert"
-  resource_group_name = azurerm_resource_group.payment_manager_rg[0].name
+  resource_group_name = azurerm_resource_group.payment_manager_monitor_rg[0].name
   location            = var.location
 
   action {
@@ -23,6 +23,79 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "pm_restapi_availability"
   query = (<<-QUERY
 requests
 | where url startswith 'https://api.platform.pagopa.it/payment-manager/pp-restapi'
+| summarize
+    Total=count(),
+    Success=count(toint(resultCode) >= 200 and toint(resultCode) < 500 and toint(duration) < 2000)
+    by Time=bin(timestamp, 15m)
+| extend Availability=((Success * 1.0) / Total) * 100
+| extend Watermark=99
+| project Watermark, Availability, Time
+  QUERY
+  )
+  severity    = 1
+  frequency   = 30
+  time_window = 30
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 2
+  }
+}
+
+# Availability: Payment Manager - pp-restapi-CD
+resource "azurerm_monitor_scheduled_query_rules_alert" "pm_restapi_cd_availability" {
+  count               = var.env_short == "p" ? 1 : 0
+  name                = "${local.project}-pm-restapi-cd-availability-alert"
+  resource_group_name = azurerm_resource_group.payment_manager_monitor_rg[0].name
+  location            = var.location
+
+  action {
+    action_group           = [azurerm_monitor_action_group.email.id, azurerm_monitor_action_group.slack.id]
+    email_subject          = "Email Header"
+    custom_webhook_payload = "{}"
+  }
+  data_source_id = azurerm_application_insights.application_insights.id
+  description    = "Availability pm-restapi-cd greater than or equal 99%"
+  enabled        = true
+  query = (<<-QUERY
+requests
+| where url startswith 'https://api.platform.pagopa.it/pp-restapi-CD'
+| summarize
+    Total=count(),
+    Success=count(toint(resultCode) >= 200 and toint(resultCode) < 500 and toint(duration) < 2000)
+    by Time=bin(timestamp, 15m)
+| extend Availability=((Success * 1.0) / Total) * 100
+| extend Watermark=99
+| project Watermark, Availability, Time
+  QUERY
+  )
+  severity    = 1
+  frequency   = 30
+  time_window = 30
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 2
+  }
+}
+
+
+# Availability: Payment Manager - pp-wallet-CD
+resource "azurerm_monitor_scheduled_query_rules_alert" "pm_wallet_availability" {
+  count               = var.env_short == "p" ? 1 : 0
+  name                = "${local.project}-pm-restapi-wallet-availability-alert"
+  resource_group_name = azurerm_resource_group.payment_manager_monitor_rg[0].name
+  location            = var.location
+
+  action {
+    action_group           = [azurerm_monitor_action_group.email.id, azurerm_monitor_action_group.slack.id]
+    email_subject          = "Email Header"
+    custom_webhook_payload = "{}"
+  }
+  data_source_id = azurerm_application_insights.application_insights.id
+  description    = "Availability pm-wallet greater than or equal 99%"
+  enabled        = true
+  query = (<<-QUERY
+requests
+| where url startswith 'https://api.platform.pagopa.it/wallet'
 | summarize
     Total=count(),
     Success=count(toint(resultCode) >= 200 and toint(resultCode) < 500 and toint(duration) < 2000)
