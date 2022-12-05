351c969153f38c9d0219be4253786a5de36b5c64
Simone Infante <simone.infante@pagopa.it>
2022-12-05 16:19:08 +0100

    feat: alert for pgs

--- src/core/payment_manager.tf
+++ src/core/payment_manager.tf
@@ -113,3 +113,39 @@ requests
     threshold = 2
   }
 }
+
+# Availability: Payment Manager - payment-gateway
+resource "azurerm_monitor_scheduled_query_rules_alert" "pm_payment_gateway_availability" {
+  count               = var.env_short == "p" ? 1 : 0
+  name                = "${local.project}-pm-payment-gateway-availability-alert"
+  resource_group_name = azurerm_resource_group.payment_manager_monitor_rg[0].name
+  location            = var.location
+
+  action {
+    action_group           = [azurerm_monitor_action_group.email.id, azurerm_monitor_action_group.slack.id]
+    email_subject          = "Email Header"
+    custom_webhook_payload = "{}"
+  }
+  data_source_id = azurerm_application_insights.application_insights.id
+  description    = "Availability pm-payment-gateway greater than or equal 99%"
+  enabled        = true
+  query = (<<-QUERY
+requests
+| where url startswith 'https://api.platform.pagopa.it/payment-manager/payment-gateway/'
+| summarize
+    Total=count(),
+    Success=count(toint(resultCode) >= 200 and toint(resultCode) < 500 and toint(duration) < 2000)
+    by Time=bin(timestamp, 15m)
+| extend Availability=((Success * 1.0) / Total) * 100
+| extend Watermark=99
+| project Watermark, Availability, Time
+  QUERY
+  )
+  severity    = 1
+  frequency   = 30
+  time_window = 30
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 2
+  }
+}
