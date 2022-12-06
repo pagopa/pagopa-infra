4dd4b026da1c0777afab263bf10ffab9c1b7247d
AngeloCaporaso <angelo.caporaso@pagopa.it>
2022-12-06 17:49:09 +0100

    PAGOPA-350 reactoring (gps):move query rules alert
    Co-authored-by: Federico Ruzzier <FedericoRuzzier@users.noreply.github.com>
    Co-authored-by: Andrea De Rinaldis <andrea-deri@users.noreply.github.com>

--- src/domains/gps-common/00_monitor.tf
+++ src/domains/gps-common/00_monitor.tf
@@ -21,3 +21,36 @@ data "azurerm_monitor_action_group" "email" {
   resource_group_name = var.monitor_resource_group_name
   name                = local.monitor_action_group_email_name
 }
+
+##Alert
+resource "azurerm_monitor_scheduled_query_rules_alert" "payments_gpd_inconsistency_error" {
+  count = var.env_short == "p" ? 1 : 0
+
+  name                = format("%s-gpd-payments-api-alert", var.env_short)
+  resource_group_name = azurerm_resource_group.gps_rg.name
+  location            = var.location
+
+  action {
+    action_group           = [data.azurerm_monitor_action_group.email.name, data.azurerm_monitor_action_group.slack.id]
+    email_subject          = "[Payments] call GPD payment position error"
+    custom_webhook_payload = "{}"
+  }
+  data_source_id = data.azurerm_application_insights.application_insights.id
+  description    = "Payments API Call Warning"
+  enabled        = true
+  query = format(<<-QUERY
+  traces
+    | where cloud_RoleName == "%s"
+    | order by timestamp desc
+    | where message contains "[getGPDCheckedReceiptsList] Non-blocking error"
+  QUERY
+    , format("%s-gpd-payments-api-alert", var.env_short)
+  )
+  severity    = 1
+  frequency   = 15
+  time_window = 15
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 1
+  }
+}
--- src/domains/gps-common/03_storage_account.tf
+++ src/domains/gps-common/03_storage_account.tf
@@ -25,36 +25,3 @@ resource "azurerm_storage_table" "payments_receipts_table" {
   name                 = format("%sreceiptstable", module.payments_receipt.name)
   storage_account_name = module.payments_receipt.name
 }
-
-##Alert
-resource "azurerm_monitor_scheduled_query_rules_alert" "payments_gpd_inconsistency_error" {
-  count = var.env_short == "p" ? 1 : 0
-
-  name                = format("%s-gpd-payments-api-alert", var.env_short)
-  resource_group_name = azurerm_resource_group.gps_rg.name
-  location            = var.location
-
-  action {
-    action_group           = [data.azurerm_monitor_action_group.email.name, data.azurerm_monitor_action_group.slack.id]
-    email_subject          = "[Payments] call GPD payment position error"
-    custom_webhook_payload = "{}"
-  }
-  data_source_id = data.azurerm_application_insights.application_insights.id
-  description    = "Payments API Call Warning"
-  enabled        = true
-  query = format(<<-QUERY
-  traces
-    | where cloud_RoleName == "%s"
-    | order by timestamp desc
-    | where message contains "[getGPDCheckedReceiptsList] Non-blocking error"
-  QUERY
-    , format("%s-gpd-payments-api-alert", var.env_short)
-  )
-  severity    = 1
-  frequency   = 15
-  time_window = 15
-  trigger {
-    operator  = "GreaterThanOrEqual"
-    threshold = 1
-  }
-}
