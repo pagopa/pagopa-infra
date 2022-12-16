d108a682054cbd1d70ef269e731260e5b25a93c5
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-12-16 16:41:51 +0100

    PAGOPA-65 added afm-marketplace host and subscription key

--- src/core/security.tf
+++ src/core/security.tf
@@ -306,3 +306,18 @@ resource "azurerm_key_vault_secret" "apiconfig_cosmos_key" {
     ]
   }
 }
+
+#tfsec:ignore:azure-keyvault-ensure-secret-expiry tfsec:ignore:azure-keyvault-content-type-for-secret
+resource "azurerm_key_vault_secret" "apiconfig_afm_marketplace_subscription_key" {
+  name         = "apiconfig-afm-marketplace-subscription-key"
+  value        = "<TO_UPDATE_MANUALLY_BY_PORTAL>"
+  content_type = "text/plain"
+
+  key_vault_id = module.key_vault.id
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
