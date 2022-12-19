ce7b7d5bdbb607b0202227123e2bdaccaa5ec009
ma_cloud_arch79 <matteoalongi79@gmail.com>
2022-12-19 19:36:24 +0100

    first elk domain release

---
+++ src/domains/elk-monitoring/05_elastic_stack.tf
@@ -0,0 +1,131 @@
+#data "azurerm_private_dns_zone" "internal" {
+#  name                = local.internal_dns_zone_name
+#  resource_group_name = local.internal_dns_zone_resource_group_name
+#}
+
+#resource "azurerm_private_dns_a_record" "kibana_ingress" {
+#  name                = local.kibana_hostname
+#  zone_name           = data.azurerm_private_dns_zone.internal.name
+#  resource_group_name = local.internal_dns_zone_resource_group_name
+#  ttl                 = 3600
+#  records             = [var.ingress_load_balancer_ip]
+#}
+
+resource "kubernetes_storage_class" "kubernetes_storage_class_hot" {
+  metadata {
+    name = "${local.project}-elastic-aks-storage-hot"
+  }
+  storage_provisioner = "disk.csi.azure.com"
+  reclaim_policy      = "Retain"
+  parameters = {
+    skuName = var.elastic_hot_storage.storage_type
+  }
+  allow_volume_expansion = var.elastic_hot_storage.allow_volume_expansion
+
+}
+
+resource "kubernetes_storage_class" "kubernetes_storage_class_warm" {
+  metadata {
+    name = "${local.project}-elastic-aks-storage-warm"
+  }
+  storage_provisioner = "disk.csi.azure.com"
+  reclaim_policy      = "Retain"
+  parameters = {
+    skuName = var.elastic_warm_storage.storage_type
+  }
+  allow_volume_expansion = var.elastic_warm_storage.allow_volume_expansion
+
+}
+
+resource "kubernetes_storage_class" "kubernetes_storage_class_cold" {
+  metadata {
+    name = "${local.project}-elastic-aks-storage-cold"
+  }
+  storage_provisioner = "disk.csi.azure.com"
+  reclaim_policy      = "Retain"
+  parameters = {
+    skuName = var.elastic_cold_storage.storage_type
+  }
+  allow_volume_expansion = var.elastic_cold_storage.allow_volume_expansion
+
+}
+
+### ECK CRDS YAML
+#data "template_file" "crds_tpl" {
+#  template = "${file("${path.module}/elk/crds.yaml.tpl")}"
+#}
+#resource "local_file" "crds_file" {
+#  filename = "${path.module}/elk/crds.yaml"
+#  content  = templatefile("${path.module}/elk/crds.yaml.tpl",{})
+
+#    provisioner "local-exec" {
+#    command = "kubectl apply -f ${self.filename}"
+#  }
+#}
+#resource "local_file" "operator_file" {
+#  filename = "${path.module}/elk/operator.yaml"
+#  content  = templatefile("${path.module}/elk/operator.yaml.tpl",{})
+#  
+#    provisioner "local-exec" {
+#    command = "kubectl apply -f ${self.filename}"
+#  }
+#}
+#resource "null_resource" "crds_null" {
+
+#  provisioner "local-exec" {
+#    command = "kubectl -n elastic-system apply -f ${path.module}/elk/crds.yaml"
+#  }
+#}
+
+#resource "null_resource" "operator_null" {
+#
+#  provisioner "local-exec" {
+#    command = "kubectl -n elastic-system apply -f ${path.module}/elk/operator.yaml"
+#  }
+#}
+
+resource "local_file" "elastic_file" {
+  filename = "${path.module}/elk/elastic.yaml"
+  content = templatefile("${path.module}/elk/elastic.yaml.tpl", {
+    num_node_balancer      = var.elastic_cluster_config.num_node_balancer
+    num_node_master        = var.elastic_cluster_config.num_node_master
+    num_node_hot           = var.elastic_cluster_config.num_node_hot
+    num_node_warm          = var.elastic_cluster_config.num_node_warm
+    num_node_cold          = var.elastic_cluster_config.num_node_cold
+    storage_size_balancer  = var.elastic_cluster_config.storage_size_balancer
+    storage_size_master    = var.elastic_cluster_config.storage_size_master
+    storage_size_hot       = var.elastic_cluster_config.storage_size_hot
+    storage_size_warm      = var.elastic_cluster_config.storage_size_warm
+    storage_size_cold      = var.elastic_cluster_config.storage_size_cold
+    storage_class_balancer = "${local.project}-elastic-aks-storage-hot"
+    storage_class_master   = "${local.project}-elastic-aks-storage-hot"
+    storage_class_hot      = "${local.project}-elastic-aks-storage-hot"
+    storage_class_warm     = "${local.project}-elastic-aks-storage-warm"
+    storage_class_cold     = "${local.project}-elastic-aks-storage-cold"
+  })
+
+  provisioner "local-exec" {
+    command = "kubectl -n elastic-system apply -f ${self.filename}"
+  }
+
+}
+
+resource "kubernetes_manifest" "kibana_manifest" {
+
+  manifest = yamldecode(file("${path.module}/elk/kibana.yaml"))
+
+
+}
+
+resource "kubernetes_manifest" "ingress_manifest" {
+
+  manifest = yamldecode(file("${path.module}/elk/ingress.yaml"))
+}
+
+resource "kubernetes_manifest" "apm_manifest" {
+
+  manifest = yamldecode(file("${path.module}/elk/apm.yaml"))
+
+
+}
+
---
+++ src/domains/elk-monitoring/env/weu-dev/terraform.tfvars
@@ -0,0 +1,81 @@
+prefix          = "pagopa"
+env_short       = "d"
+env             = "dev"
+domain          = "elk"
+location        = "westeurope"
+location_short  = "weu"
+location_string = "West Europe"
+instance        = "dev"
+
+tags = {
+  CreatedBy   = "Terraform"
+  Environment = "Dev"
+  Owner       = "IO"
+  Source      = "https://github.com/pagopa/pagopa-infra/tree/main/src/bizevents"
+  CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
+}
+
+### External resources
+
+monitor_resource_group_name                 = "pagopa-d-monitor-rg"
+log_analytics_workspace_name                = "pagopa-d-law"
+log_analytics_workspace_resource_group_name = "pagopa-d-monitor-rg"
+
+external_domain          = "pagopa.it"
+dns_zone_internal_prefix = "internal.dev.platform"
+apim_dns_zone_prefix     = "dev.platform"
+
+# chart releases: https://github.com/pagopa/aks-microservice-chart-blueprint/releases
+# image tags: https://github.com/pagopa/infra-ssl-check/releases
+tls_cert_check_helm = {
+  chart_version = "1.21.0"
+  image_name    = "ghcr.io/pagopa/infra-ssl-check"
+  image_tag     = "v1.2.2@sha256:22f4b53177cc8891bf10cbd0deb39f60e1cd12877021c3048a01e7738f63e0f9"
+}
+
+elastic_node_pool = {
+  enabled         = true
+  name            = "elastic01"
+  vm_size         = "Standard_B8ms"
+  os_disk_type    = "Managed"
+  os_disk_size_gb = "300"
+  node_count_min  = "1" #TODO change to 2 or 3 in prod
+  node_count_max  = "2"
+  node_labels = {
+    elastic : "eck",
+  },
+  node_taints           = [],
+  node_tags             = { elastic : "yes" },
+  elastic_pool_max_pods = "250",
+}
+
+elastic_hot_storage = {
+  storage_type           = "StandardSSD_LRS"
+  allow_volume_expansion = true
+  initialStorageSize     = "100Gi"
+}
+elastic_warm_storage = {
+  storage_type           = "StandardSSD_LRS"
+  allow_volume_expansion = true
+  initialStorageSize     = "100Gi"
+}
+elastic_cold_storage = {
+  storage_type           = "Standard_LRS"
+  allow_volume_expansion = true
+  initialStorageSize     = "100Gi"
+}
+
+elastic_cluster_config = {
+  num_node_balancer     = "1"
+  num_node_master       = "1"
+  num_node_hot          = "1"
+  num_node_warm         = "1"
+  num_node_cold         = "1"
+  storage_size_balancer = "20Gi"
+  storage_size_master   = "20Gi"
+  storage_size_hot      = "50Gi"
+  storage_size_warm     = "100Gi"
+  storage_size_cold     = "100Gi"
+}
+
+enable_iac_pipeline = true
