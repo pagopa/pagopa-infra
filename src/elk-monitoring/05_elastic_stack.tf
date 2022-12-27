220bfc0df3d497581a6c30f939094df9fb15d231
ma_cloud_arch79 <matteoalongi79@gmail.com>
2022-12-27 16:33:44 +0100

    add elastic_stack module and move to root

---
+++ src/elk-monitoring/05_elastic_stack.tf
@@ -0,0 +1,26 @@
+module "elastic_stack" {
+  source = "git::https://github.com/pagopa/azurerm.git//elastic_stack?ref=v3.10.0"
+
+  balancer_node_number    = var.elastic_cluster_config.num_node_balancer
+  master_node_number      = var.elastic_cluster_config.num_node_master
+  hot_node_number         = var.elastic_cluster_config.num_node_hot
+  warm_node_number        = var.elastic_cluster_config.num_node_warm
+  cold_node_number        = var.elastic_cluster_config.num_node_cold
+  balancer_storage_size   = var.elastic_cluster_config.storage_size_balancer
+  master_storage_size     = var.elastic_cluster_config.storage_size_master
+  hot_storage_size        = var.elastic_cluster_config.storage_size_hot
+  warm_storage_size       = var.elastic_cluster_config.storage_size_warm
+  cold_storage_size       = var.elastic_cluster_config.storage_size_cold
+  balancer_storage_class  = "${local.project}-elastic-aks-storage-hot"
+  master_storage_class    = "${local.project}-elastic-aks-storage-hot"
+  hot_storage_class       = "${local.project}-elastic-aks-storage-hot"
+  warm_storage_class      = "${local.project}-elastic-aks-storage-warm"
+  cold_storage_class      = "${local.project}-elastic-aks-storage-cold"
+
+  kibana_external_domain  = "https://dev-pagopa.westeurope.cloudapp.azure.com/kibana" ####TEMP "${local.apim_hostname}/kibana"
+}
+
+
+
+
+
