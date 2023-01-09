952bbd301fe71d0da0dfde18378d6254624b77ef
Cesare Caccuri <cesarecaccuri@gmail.com>
2023-01-09 15:25:50 +0100

    fix 100-nodo-create.sh script for cloud and local tested on nodo-flexible-postgresql-2

--- src/psql/liquidbase_nodo.sh
+++ src/psql/liquidbase_nodo.sh
@@ -45,8 +45,8 @@ printf "Resource Group Name: %s\n" "${resource_group_name}"
 
 
 # flexible-server
-psql_server_name=$(az postgres flexible-server list -o tsv --query "[?contains(name,'nodo-flexible')].{Name:name}" | head -1)
-psql_server_private_fqdn=$(az postgres flexible-server list -o tsv --query "[?contains(name,'nodo-flexible')].{Name:fullyQualifiedDomainName}" | head -1)
+psql_server_name=$(az postgres flexible-server list -o tsv --query "[?contains(name,'nodo-flexible-postgresql')].{Name:name}" | head -1)
+psql_server_private_fqdn=$(az postgres flexible-server list -o tsv --query "[?contains(name,'nodo-flexible-postgresql')].{Name:fullyQualifiedDomainName}" | head -1)
 
 # kv
 keyvault_name=$(az keyvault list -o tsv --query "[?contains(name,'nodo-kv')].{Name:name}")
@@ -55,50 +55,45 @@ keyvault_name=$(az keyvault list -o tsv --query "[?contains(name,'nodo-kv')].{Na
 administrator_login=$(az keyvault secret show --name db-administrator-login --vault-name "${keyvault_name}" -o tsv --query value)
 administrator_login_password=$(az keyvault secret show --name db-administrator-login-password --vault-name "${keyvault_name}" -o tsv --query value)
 
-nodo_cfg_user=$(az keyvault secret show --name db-nodo-cfg-login --vault-name "${keyvault_name}" -o tsv --query value)
-nodo_cfg_user_password=$(az keyvault secret show --name db-nodo-cfg-login-password --vault-name "${keyvault_name}" -o tsv --query value)
+nodo_cfg_user_password=$(az keyvault secret show --name db-cfg-password --vault-name "${keyvault_name}" -o tsv --query value)
+nodo_online_user_password=$(az keyvault secret show --name db-online-password --vault-name "${keyvault_name}" -o tsv --query value)
+nodo_offline_user_password=$(az keyvault secret show --name db-offline-password --vault-name "${keyvault_name}" -o tsv --query value)
+nodo_re_user_password=$(az keyvault secret show --name db-re-password --vault-name "${keyvault_name}" -o tsv --query value)
+nodo_wfespe_user_password=$(az keyvault secret show --name db-wfesp-password --vault-name "${keyvault_name}" -o tsv --query value)
 
-nodo_online_user=$(az keyvault secret show --name db-nodo-cfg-login --vault-name "${keyvault_name}" -o tsv --query value)
-nodo_online_user_password=$(az keyvault secret show --name db-nodo-cfg-login-password --vault-name "${keyvault_name}" -o tsv --query value)
-
-nodo_offline_user=$(az keyvault secret show --name db-nodo-cfg-login --vault-name "${keyvault_name}" -o tsv --query value)
-nodo_offline_user_password=$(az keyvault secret show --name db-nodo-cfg-login-password --vault-name "${keyvault_name}" -o tsv --query value)
-
-nodo_cfg_user=$(az keyvault secret show --name db-nodo-cfg-login --vault-name "${keyvault_name}" -o tsv --query value)
-nodo_cfg_user_password=$(az keyvault secret show --name db-nodo-cfg-login-password --vault-name "${keyvault_name}" -o tsv --query value)
-
-printf "Creating env file"
+echo "Creating env file"
 cd nodo
 
-echo 'POSTGRES_DB_HOST="'${psql_server_private_fqdn}'"
+echo '
+POSTGRES_DB_HOST="'${psql_server_private_fqdn}'"
 POSTGRES_DB_PORT=5432
 POSTGRES_DB="nodo"
 POSTGRES_USER="'${administrator_login}'"
 POSTGRES_PASSWORD="'${administrator_login_password}'"
-NODO_CFG_USERNAME="'${nodo_cfg_user}'"
+NODO_CFG_USERNAME="cfg"
 NODO_CFG_PASSWORD="'${nodo_cfg_user_password}'"
-NODO_CFG_SCHEMA="nodo4_cfg_dev"
-NODO_ONLINE_USERNAME="nodo_online_dev"
-NODO_ONLINE_PASSWORD="nodo_online_dev"
-NODO_ONLINE_SCHEMA="nodo_online_dev"
-NODO_OFFLINE_USERNAME="nodo_offline_dev"
-NODO_OFFLINE_PASSWORD="nodo_offline_dev"
-NODO_OFFLINE_SCHEMA="nodo_offline_dev"
-NODO_WFESP_USERNAME="wfesp_dev"
-NODO_WFESP_PASSWORD="wfesp_dev"
-NODO_WFESP_SCHEMA="wfesp_dev"
-NODO_RE_USERNAME="re_dev"
-NODO_RE_PASSWORD="re_dev"
-NODO_RE_SCHEMA="re_dev"
-LQB_CONTEXTS="dev"
-' > dev.env
+NODO_CFG_SCHEMA="cfg"
+NODO_ONLINE_USERNAME="online"
+NODO_ONLINE_PASSWORD="'${nodo_online_user_password}'"
+NODO_ONLINE_SCHEMA="online"
+NODO_OFFLINE_USERNAME="offline"
+NODO_OFFLINE_PASSWORD="'${nodo_offline_user_password}'"
+NODO_OFFLINE_SCHEMA="offline"
+NODO_WFESP_USERNAME="wfesp"
+NODO_WFESP_PASSWORD="'${nodo_wfespe_user_password}'"
+NODO_WFESP_SCHEMA="wfesp"
+NODO_RE_USERNAME="re"
+NODO_RE_PASSWORD="'${nodo_re_user_password}'"
+NODO_RE_SCHEMA="re"
+LQB_CONTEXTS="dev"' > dev.env
 
 cat dev.env
 
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>> STEP#1 Configuring nodo db ( CREATE SCHEMAs ... ) "
-sh postgres-scripts/100-nodo-create.sh
-echo "<<<<<<<<<<<<<<<<<<<<<<<<<<< done!"
 
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>> STEP#1 Configuring nodo db ( CREATE SCHEMAs if not exists... ) "
+docker compose -f docker-compose-pg-init.yml up
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<< done!"
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>> STEP#2 Executing docker compose ( liquibase Migration to version $NODO_VERSION) "
 export NODO_VERSION=$NODO_VERSION
