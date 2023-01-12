49e34b6fdb5fd3efef5822aec9e62b8910b443f5
Marco Meretta <marco.meretta@gmail.com>
2023-01-12 17:25:04 +0100

    add cacerts

--- src/domains/nodo-secret/99_main.tf
+++ src/domains/nodo-secret/99_main.tf
@@ -16,6 +16,10 @@ terraform {
       source  = "hashicorp/external"
       version = "= 2.2.3"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.16.1"
+    }
   }
 
   backend "azurerm" {}
@@ -27,6 +31,11 @@ provider "azurerm" {
       purge_soft_delete_on_destroy = false
     }
   }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config-${var.prefix}-${var.env_short}-${var.location_short}-${var.env}-aks"
+  config_context = "${var.prefix}-${var.env_short}-${var.location_short}-${var.env}-aks"
 }
 
 data "azurerm_subscription" "current" {}
