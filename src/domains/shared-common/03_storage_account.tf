1d1dddc49f32c0edaec5c82ab913f739306a146c
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2023-01-13 11:40:00 +0100

    fix

--- src/domains/shared-common/03_storage_account.tf
+++ src/domains/shared-common/03_storage_account.tf
@@ -2,14 +2,14 @@
 module "poc_reporting_enrollment_sa" {
   source = "git::https://github.com/pagopa/azurerm.git//storage_account?ref=v4.0.0"
 
-  name                       = replace(format("%s-poc-reporting-enrollment-sa", local.project), "-", "")
+  name                       = replace(format("%s-penrol-sa", local.project), "-", "")
   account_kind               = "StorageV2"
   account_tier               = "Standard"
   account_replication_type   = "LRS"
   access_tier                = "Hot"
   versioning_name            = "versioning"
   enable_versioning          = var.poc_versioning
-  resource_group_name        = azurerm_resource_group.gps_rg.name
+  resource_group_name        = azurerm_resource_group.shared_rg.name
   location                   = var.location
   advanced_threat_protection = var.poc_advanced_threat_protection
   allow_blob_public_access   = false
