93745c0ce22f306c1be8dd0a47d7dfc0b5235041
ma_cloud_arch79 <matteoalongi79@gmail.com>
2023-01-16 14:54:02 +0100

    variable fix

--- src/grafana-monitoring/99_locals.tf
+++ src/grafana-monitoring/99_locals.tf
@@ -18,24 +18,7 @@ locals {
   vnet_name                = "${local.product}-vnet"
   vnet_resource_group_name = "${local.product}-vnet-rg"
 
-  acr_name                = replace("${local.product}commonacr", "-", "")
-  acr_resource_group_name = "${local.product}-container-registry-rg"
 
-  aks_name                = "${local.product}-${var.location_short}-${var.instance}-aks"
-  aks_resource_group_name = "${local.product}-${var.location_short}-${var.instance}-aks-rg"
-  aks_subnet_name         = "${var.prefix}-${var.env_short}-${var.location_short}-${var.env}-aks-snet"
 
-  ingress_hostname                      = "${var.location_short}${var.instance}.${var.domain}"
-  internal_dns_zone_name                = "${var.dns_zone_internal_prefix}.${var.external_domain}"
-  internal_dns_zone_resource_group_name = "${local.product}-vnet-rg"
 
-  pagopa_apim_name = "${local.product}-apim"
-  pagopa_apim_rg   = "${local.product}-api-rg"
-
-  apim_hostname = "api.${var.apim_dns_zone_prefix}.${var.external_domain}"
-
-  kibana_hostname       = "weu${var.env}.kibana.internal.${var.env}.platform.pagopa.it"
-  kibana_hostname_short = "weu${var.env}.kibana"
-
-  elk_namespace = "elastic-system"
 }
