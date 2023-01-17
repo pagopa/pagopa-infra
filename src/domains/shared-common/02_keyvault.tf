5279f94fdc6ed86fcb391a6ce38b4910652e3ce3
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-01-17 17:58:01 +0100

    feat(key-vault): add sa connection string PAGOPA-578

--- src/domains/shared-common/02_keyvault.tf
+++ src/domains/shared-common/02_keyvault.tf
@@ -58,6 +58,14 @@ resource "azurerm_key_vault_secret" "iuv_generator_cosmos_connection_string" {
   key_vault_id = module.key_vault.id
 }
 
+resource "azurerm_key_vault_secret" "storage_connection_string" {
+  name         = format("poc-reporting-enrollment-%s-sa-connection-string", var.env_short)
+  value        = module.poc_reporting_enrollment_sa.primary_connection_string
+  content_type = "text/plain"
+
+  key_vault_id = module.key_vault.id
+}
+
 #
 # IaC
 #
