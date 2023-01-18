331b0d07fcae79637061fa8ecea8ce80a05c49a6
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-01-18 14:20:06 +0100

    feat(key-vault): set up poc product subkey PAGOPA-578
    Co-authored-by: Federico Ruzzier <FedericoRuzzier@users.noreply.github.com>

--- src/domains/shared-common/02_keyvault.tf
+++ src/domains/shared-common/02_keyvault.tf
@@ -66,6 +66,21 @@ resource "azurerm_key_vault_secret" "storage_connection_string" {
   key_vault_id = module.key_vault.id
 }
 
+#tfsec:ignore:azure-keyvault-ensure-secret-expiry tfsec:ignore:azure-keyvault-content-type-for-secret
+resource "azurerm_key_vault_secret" "poc_reporting_enrollment_subscription_key" {
+  name         = format("poc-%s-reporting-enrollment-subscription-key", var.env_short)
+  value        = "<TO_UPDATE_MANUALLY_BY_PORTAL>"
+  content_type = "text/plain"
+
+  key_vault_id = module.key_vault.id
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
 #
 # IaC
 #
