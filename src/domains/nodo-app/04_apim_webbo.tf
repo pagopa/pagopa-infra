b9be4670d8667a09b892db19bcd522429adf07af
Marco Meretta <marco.meretta@gmail.com>
2023-01-19 13:06:18 +0100

    test webbo

--- src/domains/nodo-app/04_apim_webbo.tf
+++ src/domains/nodo-app/04_apim_webbo.tf
@@ -2,12 +2,12 @@
 ## Products ##
 ##############
 
-module "apim_webbo_product" {
+module "apim_web_bo_product" {
   source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v2.18.3"
 
-  product_id   = "webbo"
-  display_name = "WebBO for NDP"
-  description  = "WebBO for NDP"
+  product_id   = "web_bo"
+  display_name = "Web BO (BackOffice) for NDP"
+  description  = "Web BO (BackOffice) for NDP"
 
   api_management_name = local.pagopa_apim_name
   resource_group_name = local.pagopa_apim_rg
@@ -17,57 +17,58 @@ module "apim_webbo_product" {
   approval_required     = false
   subscriptions_limit   = 1000
 
-  policy_xml = file("./api_product/webbo-service/_base_policy.xml")
+  policy_xml = file("./api_product/web-bo-service/_base_policy.xml")
 }
 
-########################
-##    webbo  NDP    ##
-########################
+####################################
+##    Web BO (BackOffice)  NDP    ##
+####################################
 locals {
-  apim_webbo_service_api = {
-    display_name          = "WebBO for NDP"
-    description           = "API webbo for NDP"
-    path                  = "bo-nodo"
+  apim_web_bo_service_api = {
+    display_name          = "Web BO (BackOffice) for NDP"
+    description           = "API Web BO (BackOffice) for NDP"
+    path                  = "web-bo-ndp/service"
     subscription_required = false
     service_url           = null
   }
 }
 
-# resource "azurerm_api_management_api_version_set" "api_webbo_api" {
-#   name                = format("%s-webbo-service-api", var.env_short)
-#   resource_group_name = local.pagopa_apim_rg
-#   api_management_name = local.pagopa_apim_name
-#   display_name        = local.apim_webbo_service_api.display_name
-#   versioning_scheme   = "Segment"
-# }
+resource "azurerm_api_management_api_version_set" "api_web_bo_api" {
+
+  name                = format("%s-web-bo-service-api", var.env_short)
+  resource_group_name = local.pagopa_apim_rg
+  api_management_name = local.pagopa_apim_name
+  display_name        = local.apim_web_bo_service_api.display_name
+  versioning_scheme   = "Segment"
+}
 
 
-module "apim_api_webbo_api_v1" {
+module "apim_api_web_bo_api_v1" {
   source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v2.18.3"
 
-  name                  = format("%s-webbo-service-api", local.project)
+  name                  = format("%s-web-bo-service-api", local.project)
   api_management_name   = local.pagopa_apim_name
   resource_group_name   = local.pagopa_apim_rg
-  product_ids           = [module.apim_webbo_product.product_id]
-  subscription_required = local.apim_webbo_service_api.subscription_required
-  #version_set_id        = azurerm_api_management_api_version_set.api_webbo_api.id
-  #api_version           = "v1"
+  product_ids           = [module.apim_web_bo_product.product_id]
+  subscription_required = local.apim_web_bo_service_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.api_web_bo_api.id
+  api_version           = "v1"
 
-  description  = local.apim_webbo_service_api.description
-  display_name = local.apim_webbo_service_api.display_name
-  path         = local.apim_webbo_service_api.path
+  description  = local.apim_web_bo_service_api.description
+  display_name = local.apim_web_bo_service_api.display_name
+  path         = local.apim_web_bo_service_api.path
   protocols    = ["https"]
-  service_url  = local.apim_webbo_service_api.service_url
+  service_url  = local.apim_web_bo_service_api.service_url
 
   content_format = "openapi"
-  content_value = templatefile("./api/webbo-service/v1/_webbo.openapi.json.tpl", {
+  content_value = templatefile("./api/web-bo-service/v1/_web-bo.openapi.json.tpl", {
     host = local.apim_hostname
   })
 
-  xml_content = templatefile("./api/webbo-service/v1/_base_policy.xml", {
+  xml_content = templatefile("./api/web-bo-service/v1/_base_policy.xml", {
     hostname = local.nodo_hostname
     dns_pagopa_platform = format("api.%s.%s", var.apim_dns_zone_prefix, var.external_domain)
-    apim_base_path      = "/bo-nodo"
+    apim_base_path      = "/web-bo-ndp/service/v1"
   })
   
 }
