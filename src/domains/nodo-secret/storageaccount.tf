2c1a6d387949c8de95d4ea616873d3dc3d6f3296
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2023-01-24 15:05:43 +0100

    fix remove useless sftp module

--- src/domains/nodo-secret/storageaccount.tf
+++ src/domains/nodo-secret/storageaccount.tf
@@ -29,24 +29,4 @@ resource "azurerm_storage_share_file" "upload_firmatore" {
   storage_share_id = azurerm_storage_share.firmatore.id
 }
 
-module "firmatore_emulate_sogei_sftp" {
-  source = "git::https://github.com/pagopa/azurerm.git//storage_account?ref=v2.0.28"
-
-  name                       = replace("${local.product}-${var.domain}-firmatore-emulate-sogei-test", "-", "") # firmatoreemulatesogeitest<dev|uat|prod>
-  account_kind               = "StorageV2"
-  account_tier               = "Standard"
-  account_replication_type   = "LRS"
-  access_tier                = "Hot"
-  versioning_name            = "versioning"
-  enable_versioning          = false
-  resource_group_name        = azurerm_resource_group.sec_rg.name
-  location                   = var.location
-  advanced_threat_protection = false
-  allow_blob_public_access   = false
-
-  blob_properties_delete_retention_policy_days = 30
-
-  tags = var.tags
-}
-
 
