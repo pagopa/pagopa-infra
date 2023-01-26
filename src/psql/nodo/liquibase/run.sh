a79abcb6b8454257e047dbe1d9d58fb2c83295fa
Cesare Caccuri <cesarecaccuri@gmail.com>
2023-01-26 15:50:01 +0100

    liquibase integration data

--- src/psql/nodo/liquibase/run.sh
+++ src/psql/nodo/liquibase/run.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-export POSTGRES_DB_HOST="pagopa-d-weu-nodo-flexible-postgresql.postgres.database.azure.com"
+#export POSTGRES_DB_HOST="pagopa-d-weu-nodo-flexible-postgresql.postgres.database.azure.com"
+#export POSTGRES_DB_PORT="5432"
+#export POSTGRES_DB="nodo"
+#export NODO_CFG_SCHEMA="cfg"
+#export LQB_CONTEXTS="dev,integ"
+#export NODO_CFG_USERNAME="cfg"
+#export NODO_CFG_PASSWORD="password"
+
+CONTEXTS=$1
+DOMAIN=$2
+
+export POSTGRES_DB_HOST="localhost"
 export POSTGRES_DB_PORT="5432"
 export POSTGRES_DB="nodo"
 export NODO_CFG_SCHEMA="cfg"
-export LQB_CONTEXTS="dev,integ"
+export LQB_CONTEXTS=$CONTEXTS
 export NODO_CFG_USERNAME="cfg"
-export NODO_CFG_PASSWORD="password"
+export NODO_CFG_PASSWORD="cfg"
 
 echo "
 classpath: ./changelog/cfg/
@@ -18,11 +29,12 @@ password: ${NODO_CFG_PASSWORD}
 defaultSchemaName: ${NODO_CFG_SCHEMA}
 liquibaseSchemaName: ${NODO_CFG_SCHEMA}
 parameter.schema: ${NODO_CFG_SCHEMA}
+parameter.domain: ${DOMAIN}
 liquibase.hub.mode: OFF
 log-level: INFO
 " > cfg.properties
 
-#liquibase --defaultsFile=cfg.properties drop-all
+liquibase --defaultsFile=cfg.properties drop-all
 
 liquibase --defaultsFile=cfg.properties update --changelogFile="db.changelog-master-3.10.0.xml"
 
