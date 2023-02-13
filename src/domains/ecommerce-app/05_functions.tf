092017563826e53d77b423ff3ed277dae71d1d19
Simone Infante <simone.infante@pagopa.it>
2023-02-13 13:43:20 +0100

    feat: basic fn for dev and uat

--- src/domains/ecommerce-app/05_functions.tf
+++ src/domains/ecommerce-app/05_functions.tf
@@ -141,6 +141,7 @@ data "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_role_assignment" "transactions_functions_to_acr" {
+  depends_on           = [module.ecommerce_transactions_function_app]
   scope                = data.azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
   principal_id         = module.ecommerce_transactions_function_app.system_identity_principal
