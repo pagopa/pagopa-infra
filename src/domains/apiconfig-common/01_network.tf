b7ee6784217041d960216769b7a005ce00478067
Jacopo <jacopo.carlini@gmail.com>
2023-02-15 17:49:15 +0100

    apiconfig-common

---
+++ src/domains/apiconfig-common/01_network.tf
@@ -0,0 +1,35 @@
+data "azurerm_virtual_network" "vnet" {
+  name                = local.vnet_name
+  resource_group_name = local.vnet_resource_group_name
+}
+
+data "azurerm_private_dns_zone" "internal" {
+  name                = local.internal_dns_zone_name
+  resource_group_name = local.internal_dns_zone_resource_group_name
+}
+
+resource "azurerm_private_dns_a_record" "ingress" {
+  name                = local.ingress_hostname
+  zone_name           = data.azurerm_private_dns_zone.internal.name
+  resource_group_name = local.internal_dns_zone_resource_group_name
+  ttl                 = 3600
+  records             = [var.ingress_load_balancer_ip]
+}
+
+data "azurerm_subnet" "aks_subnet" {
+  name                 = local.aks_subnet_name
+  virtual_network_name = local.vnet_name
+  resource_group_name  = local.vnet_resource_group_name
+}
+
+data "azurerm_private_dns_zone" "cosmos" {
+  name                = local.cosmos_dns_zone_name
+  resource_group_name = local.cosmos_dns_zone_resource_group_name
+}
+
+data "azurerm_subnet" "apiconfig_subnet" {
+  # pagopa-u-<...>
+  name                 = format("%s-%s-api-config-snet", var.prefix, var.env_short)
+  virtual_network_name = format("%s-%s-vnet-integration", var.prefix, var.env_short)
+  resource_group_name  = format("%s-%s-vnet-rg", var.prefix, var.env_short)
+}
