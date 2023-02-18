ac92c195268f0f59bbe6802e5d92e90b85cf1e81
Marco Meretta <marco.meretta@gmail.com>
2023-02-18 01:04:44 +0100

    fix

--- src/elk-monitoring/05_elastic_stack.tf
+++ src/elk-monitoring/05_elastic_stack.tf
@@ -37,23 +37,7 @@ module "elastic_stack" {
     }
   }
 
-  # balancer_node_number   = var.elastic_cluster_config.num_node_balancer
-  # master_node_number     = var.elastic_cluster_config.num_node_master
-  # hot_node_number        = var.elastic_cluster_config.num_node_hot
-  # warm_node_number       = var.elastic_cluster_config.num_node_warm
-  # cold_node_number       = var.elastic_cluster_config.num_node_cold
-  # balancer_storage_size  = var.elastic_cluster_config.storage_size_balancer
-  # master_storage_size    = var.elastic_cluster_config.storage_size_master
-  # hot_storage_size       = var.elastic_cluster_config.storage_size_hot
-  # warm_storage_size      = var.elastic_cluster_config.storage_size_warm
-  # cold_storage_size      = var.elastic_cluster_config.storage_size_cold
-  # balancer_storage_class = "${local.project}-elastic-aks-storage-hot"
-  # master_storage_class   = "${local.project}-elastic-aks-storage-hot"
-  # hot_storage_class      = "${local.project}-elastic-aks-storage-hot"
-  # warm_storage_class     = "${local.project}-elastic-aks-storage-warm"
-  # cold_storage_class     = "${local.project}-elastic-aks-storage-cold"
-
-  kibana_external_domain = "https://kibana.dev.platform.pagopa.it/kibana" ####TEMP "${local.apim_hostname}/kibana"
+  kibana_external_domain = var.env_short == "p" ? "https://kibana.platform.pagopa.it/kibana" : "https://kibana.${var.env}.platform.pagopa.it/kibana"
 
   secret_name   = var.env_short == "p" ? "${var.location_short}${var.env}-kibana-internal-platform-pagopa-it" : "${var.location_short}${var.env}-kibana-internal-${var.env}-platform-pagopa-it"
   keyvault_name = module.key_vault.name
