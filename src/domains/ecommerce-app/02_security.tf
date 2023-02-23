65944d777812013868d26411d7318524919a77b6
Pietro Tota <pietro.tota@pagopa.it>
2023-02-23 11:56:27 +0100

    fix: refactoring secret name

--- src/domains/ecommerce-app/02_security.tf
+++ src/domains/ecommerce-app/02_security.tf
@@ -3,8 +3,8 @@ data "azurerm_key_vault" "kv" {
   resource_group_name = "${local.product}-${var.domain}-sec-rg"
 }
 
-resource "azurerm_key_vault_secret" "confidential-data-encryption-key" {
-  name         = "confidential-data-encryption-key"
+resource "azurerm_key_vault_secret" "email-confidential-data-encryption-key" {
+  name         = "email-confidential-data-encryption-key"
   value        = "<TO UPDATE MANUALLY ON PORTAL>" #base64 AES encryption key
   key_vault_id = data.azurerm_key_vault.kv.id
 }
