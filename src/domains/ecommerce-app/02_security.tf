888a88c15cb745ee07abb51f9140eb7c44dc32ee
Pietro Tota <pietro.tota@pagopa.it>
2023-02-24 11:54:16 +0100

    fix: move secret to common

--- src/domains/ecommerce-app/02_security.tf
+++ src/domains/ecommerce-app/02_security.tf
@@ -2,15 +2,3 @@ data "azurerm_key_vault" "kv" {
   name                = "${local.product}-${var.domain}-kv"
   resource_group_name = "${local.product}-${var.domain}-sec-rg"
 }
-
-resource "azurerm_key_vault_secret" "email-confidential-data-encryption-key" {
-  name         = "email-confidential-data-encryption-key"
-  value        = "<TO UPDATE MANUALLY ON PORTAL>" #base64 AES encryption key
-  key_vault_id = data.azurerm_key_vault.kv.id
-
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
-}
