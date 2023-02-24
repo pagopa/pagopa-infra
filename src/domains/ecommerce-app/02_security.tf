f15a480eef7ff9c1edba6961fa93a783516ee23f
Pietro Tota <pietro.tota@pagopa.it>
2023-02-24 11:00:22 +0100

    fix: lifecycle values

--- src/domains/ecommerce-app/02_security.tf
+++ src/domains/ecommerce-app/02_security.tf
@@ -7,4 +7,10 @@ resource "azurerm_key_vault_secret" "email-confidential-data-encryption-key" {
   name         = "email-confidential-data-encryption-key"
   value        = "<TO UPDATE MANUALLY ON PORTAL>" #base64 AES encryption key
   key_vault_id = data.azurerm_key_vault.kv.id
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 }
