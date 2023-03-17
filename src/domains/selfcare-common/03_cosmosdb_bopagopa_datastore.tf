bd0f4f32494830cb76de8299bda2455200c3ccba
Biagio Murano <biagio.murano@emea.nttdata.com>
2023-03-17 12:45:11 +0100

    created resource cosmo db

---
+++ src/domains/selfcare-common/03_cosmosdb_bopagopa_datastore.tf
@@ -0,0 +1,97 @@
+resource "azurerm_resource_group" "bopagopa_rg" {
+  name     = "${local.project}-rg"
+  location = var.location
+
+  tags = var.tags
+}
+
+module "bopagopa_datastore_cosmosdb_snet" {
+  source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v1.0.90"
+  name                 = "${local.project}-datastore-cosmosdb-snet"
+  address_prefixes     = var.cidr_subnet_bopagopa_datastore_cosmosdb
+  resource_group_name  = local.vnet_resource_group_name
+  virtual_network_name = local.vnet_name
+
+  enforce_private_link_endpoint_network_policies = true
+
+  service_endpoints = [
+    "Microsoft.Web",
+    "Microsoft.AzureCosmosDB",
+    "Microsoft.Storage",
+  ]
+}
+
+module "bopagopa_datastore_cosmosdb_account" {
+  source   = "git::https://github.com/pagopa/azurerm.git//cosmosdb_account?ref=v2.1.18"
+  name     = "${local.project}-ds-cosmos-account"
+  location = var.location
+
+  resource_group_name = azurerm_resource_group.bopagopa_rg.name
+  offer_type          = var.bopagopa_datastore_cosmos_db_params.offer_type
+  kind                = var.bopagopa_datastore_cosmos_db_params.kind
+
+  public_network_access_enabled    = var.bopagopa_datastore_cosmos_db_params.public_network_access_enabled
+  main_geo_location_zone_redundant = var.bopagopa_datastore_cosmos_db_params.main_geo_location_zone_redundant
+
+  enable_free_tier          = var.bopagopa_datastore_cosmos_db_params.enable_free_tier
+  enable_automatic_failover = true
+
+  capabilities       = var.bopagopa_datastore_cosmos_db_params.capabilities
+  consistency_policy = var.bopagopa_datastore_cosmos_db_params.consistency_policy
+
+  main_geo_location_location = var.location
+  additional_geo_locations   = var.bopagopa_datastore_cosmos_db_params.additional_geo_locations
+  backup_continuous_enabled  = var.bopagopa_datastore_cosmos_db_params.backup_continuous_enabled
+
+  is_virtual_network_filter_enabled = var.bopagopa_datastore_cosmos_db_params.is_virtual_network_filter_enabled
+
+  ip_range = ""
+
+  # add data.azurerm_subnet.<my_service>.id
+  # allowed_virtual_network_subnet_ids = var.bopagopa_datastore_cosmos_db_params.public_network_access_enabled ? var.env_short == "d" ? [] : [data.azurerm_subnet.aks_subnet.id] : [data.azurerm_subnet.aks_subnet.id]
+  allowed_virtual_network_subnet_ids = []
+
+  # private endpoint
+  private_endpoint_name    = "${local.project}-ds-cosmos-sql-endpoint"
+  private_endpoint_enabled = var.bopagopa_datastore_cosmos_db_params.private_endpoint_enabled
+  subnet_id                = module.bopagopa_datastore_cosmosdb_snet.id
+  private_dns_zone_ids     = [data.azurerm_private_dns_zone.cosmos.id]
+
+  tags = var.tags
+}
+
+# cosmosdb database
+module "bopagopa_datastore_cosmosdb_database" {
+  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_database?ref=v2.1.15"
+  name                = "db"
+  resource_group_name = azurerm_resource_group.bopagopa_rg.name
+  account_name        = module.bopagopa_datastore_cosmosdb_account.name
+}
+
+### Containers
+locals {
+  bopagopa_datastore_cosmosdb_containers = [
+    {
+      name               = "bo-pagopa",
+      partition_key_path = "/id",
+      default_ttl        = var.bopagopa_datastore_cosmos_db_params.container_default_ttl,
+      autoscale_settings = { max_throughput = 6000 }
+    },
+  ]
+}
+
+# cosmosdb container for bopagopa datastore
+module "bopagopa_datastore_cosmosdb_containers" {
+  source   = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_container?ref=v3.2.5"
+  for_each = { for c in local.bopagopa_datastore_cosmosdb_containers : c.name => c }
+
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.bopagopa_rg.name
+  account_name        = module.bopagopa_datastore_cosmosdb_account.name
+  database_name       = module.bopagopa_datastore_cosmosdb_database.name
+  partition_key_path  = each.value.partition_key_path
+  throughput          = lookup(each.value, "throughput", null)
+  default_ttl         = lookup(each.value, "default_ttl", null)
+
+  autoscale_settings = contains(var.bopagopa_datastore_cosmos_db_params.capabilities, "EnableServerless") ? null : lookup(each.value, "autoscale_settings", null)
+}
--- src/domains/selfcare-common/99_variables.tf
+++ src/domains/selfcare-common/99_variables.tf
@@ -105,3 +105,37 @@ variable "enable_iac_pipeline" {
   description = "If true create the key vault policy to allow used by azure devops iac pipelines."
   default     = false
 }
+
+
+variable "bopagopa_datastore_cosmos_db_params" {
+  type = object({
+    kind           = string
+    capabilities   = list(string)
+    offer_type     = string
+    server_version = string
+    consistency_policy = object({
+      consistency_level       = string
+      max_interval_in_seconds = number
+      max_staleness_prefix    = number
+    })
+    main_geo_location_zone_redundant = bool
+    enable_free_tier                 = bool
+    main_geo_location_zone_redundant = bool
+    additional_geo_locations = list(object({
+      location          = string
+      failover_priority = number
+      zone_redundant    = bool
+    }))
+    private_endpoint_enabled          = bool
+    public_network_access_enabled     = bool
+    is_virtual_network_filter_enabled = bool
+    backup_continuous_enabled         = bool
+    container_default_ttl             = number
+  })
+}
+
+variable "cidr_subnet_bopagopa_datastore_cosmosdb" {
+  type        = list(string)
+  description = "Cosmos DB address space"
+  default     = null
+}
