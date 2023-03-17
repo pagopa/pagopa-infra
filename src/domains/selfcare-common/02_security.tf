ed5a2ca3b006063275a5298b72435cac41f85b3b
Biagio Murano <biagio.murano@emea.nttdata.com>
2023-03-17 13:13:35 +0100

    fix

--- src/domains/selfcare-common/02_security.tf
+++ src/domains/selfcare-common/02_security.tf
@@ -95,19 +95,18 @@ resource "azurerm_key_vault_secret" "ai_connection_string" {
   key_vault_id = module.key_vault.id
 }
 
-######
 
-resource "azurerm_key_vault_secret" "cosmos_biz_connection_string" {
-  name         = format("cosmos-%s-biz-connection-string", var.env_short)
-  value        = module.bizevents_datastore_cosmosdb_account.connection_strings[0]
+resource "azurerm_key_vault_secret" "cosmos_bopagopa_connection_string" {
+  name         = format("cosmos-%s-bopagopa-connection-string", var.env_short)
+  value        = module.bopagopa_datastore_cosmosdb_account.connection_strings[0]
   content_type = "text/plain"
 
   key_vault_id = module.key_vault.id
 }
 
-resource "azurerm_key_vault_secret" "cosmos_biz_key" {
-  name         = format("cosmos-%s-biz-key", var.env_short)
-  value        = module.bizevents_datastore_cosmosdb_account.primary_key
+resource "azurerm_key_vault_secret" "cosmos_bopagopa_key" {
+  name         = format("cosmos-%s-bopagopa-key", var.env_short)
+  value        = module.bopagopa_datastore_cosmosdb_account.primary_key
   content_type = "text/plain"
 
   key_vault_id = module.key_vault.id
