479dcac3a3d5a2457f16f00e7533de533cb05c42
marcomeretta <121177306+marcomeretta@users.noreply.github.com>
2023-04-03 08:51:23 +0200

    feat: Poc fdr (#850)

--- src/elk-monitoring/05_elastic_stack.tf
+++ src/elk-monitoring/05_elastic_stack.tf
@@ -3,10 +3,13 @@ module "elastic_stack" {
     azurerm_kubernetes_cluster_node_pool.elastic
   ]
 
-  source = "git::https://github.com/pagopa/azurerm.git//elastic_stack?ref=v4.13.0"
+  source = "git::https://github.com/pagopa/azurerm.git//elastic_stack?ref=v4.14.0"
 
   namespace      = local.elk_namespace
   nodeset_config = var.nodeset_config
+
+  dedicated_log_instance_name = ["nodo","nodoreplica","nodocron","nodocronreplica","pagopawebbo","pagopawfespwfesp","pagopafdr"] 
+  
 
   eck_license = file("${path.module}/env/eck_license/pagopa-spa-4a1285e5-9c2c-4f9f-948a-9600095edc2f-orchestration.json")
 
@@ -20,9 +23,6 @@ module "elastic_stack" {
 
   kibana_internal_hostname = var.env_short == "p" ? "${var.location_short}${var.env}.kibana.internal.platform.pagopa.it" : "${var.location_short}${var.env}.kibana.internal.${var.env}.platform.pagopa.it"
 }
-# output "test1" {
-#   value = module.elastic_stack.test
-# }
 
 data "kubernetes_secret" "get_elastic_credential" {
   depends_on = [
@@ -38,282 +38,24 @@ data "kubernetes_secret" "get_elastic_credential" {
 locals {
   kibana_url             = var.env_short == "p" ? "https://elastic:${data.kubernetes_secret.get_elastic_credential.data.elastic}@kibana.platform.pagopa.it/kibana" : "https://elastic:${data.kubernetes_secret.get_elastic_credential.data.elastic}@kibana.${var.env}.platform.pagopa.it/kibana"
   elastic_url            = var.env_short == "p" ? "https://elastic:${data.kubernetes_secret.get_elastic_credential.data.elastic}@kibana.platform.pagopa.it/elastic" : "https://elastic:${data.kubernetes_secret.get_elastic_credential.data.elastic}@kibana.${var.env}.platform.pagopa.it/elastic"
-  replica_dashboard_path = var.env_short == "d" || var.env_short == "u" ? "nodo/dashboard-replica/*.ndjson" : "nodo/dashboard-replica/*.ndjson_trick_for_not_upload"
-  replica_query_path     = var.env_short == "d" || var.env_short == "u" ? "nodo/query-replica/*.ndjson" : "nodo/query-replica/*.ndjson_trick_for_not_upload"
-  ingest_pipeline        = { for filename in fileset(path.module, "nodo/pipeline/ingest_*.json") : replace(replace(basename(filename), "ingest_", ""), ".json", "") => replace(trimsuffix(trimprefix(file("${path.module}/${filename}"), "\""), "\""), "'", "'\\''") }
-  ilm_policy             = { for filename in fileset(path.module, "nodo/pipeline/ilm_*.json") : replace(replace(basename(filename), "ilm_", ""), ".json", "") => replace(trimsuffix(trimprefix(file("${path.module}/${filename}"), "\""), "\""), "'", "'\\''") }
-  component_template     = { for filename in fileset(path.module, "nodo/pipeline/component_*.json") : replace(replace(basename(filename), "component_", ""), ".json", "") => replace(trimsuffix(trimprefix(file("${path.module}/${filename}"), "\""), "\""), "'", "'\\''") }
-
-  fdr_ilm_policy         = { for filename in fileset(path.module, "fdr/ilm_policy_*.json") : replace(replace(basename(filename), "ilm_policy_", ""), ".json", "") => replace(trimsuffix(trimprefix(file("${path.module}/${filename}"), "\""), "\""), "'", "'\\''") }
-  fdr_component_template = { for filename in fileset(path.module, "fdr/component_*.json") : replace(replace(basename(filename), "component_", ""), ".json", "") => replace(trimsuffix(trimprefix(file("${path.module}/${filename}"), "\""), "\""), "'", "'\\''") }
-  fdr_index_template     = { for filename in fileset(path.module, "fdr/index_template_*.json") : replace(replace(basename(filename), "index_template_", ""), ".json", "") => replace(trimsuffix(trimprefix(file("${path.module}/${filename}"), "\""), "\""), "'", "'\\''") }
-  fdr_kibana_space       = file("${path.module}/fdr/space.json")
-  fdr_kibana_data_view   = file("${path.module}/fdr/data_view.json")
 }
 
-resource "null_resource" "ingest_pipeline" {
-  depends_on = [data.kubernetes_secret.get_elastic_credential]
-
-  for_each = local.ingest_pipeline
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X PUT "${local.elastic_url}/_ingest/pipeline/${each.key}" \
-      -H 'kbn-xsrf: true' \
-      -H 'Content-Type: application/json' \
-      -d '${each.value}'
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "ilm_policy" {
-  depends_on = [null_resource.ingest_pipeline]
-
-  for_each = local.ilm_policy
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X PUT "${local.elastic_url}/_ilm/policy/${each.key}" \
-      -H 'kbn-xsrf: true' \
-      -H 'Content-Type: application/json' \
-      -d '${each.value}'
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "component_template" {
-  depends_on = [null_resource.ilm_policy]
-
-  for_each = local.component_template
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X PUT "${local.elastic_url}/_component_template/${each.key}" \
-      -H 'kbn-xsrf: true' \
-      -H 'Content-Type: application/json' \
-      -d '${each.value}'
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "rollover" {
-  depends_on = [null_resource.component_template]
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X POST "${local.elastic_url}/logs-kubernetes.container_logs-default/_rollover/" -H 'kbn-xsrf: true'
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "upload_query" {
-  depends_on = [null_resource.rollover]
-
-  for_each = fileset(path.module, "nodo/query/*.ndjson")
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X POST "${local.kibana_url}/api/saved_objects/_import?overwrite=true" -H 'kbn-xsrf: true' --form "file=@./${each.value}"
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "upload_query_replica" {
-  depends_on = [null_resource.upload_query]
-
-  for_each = fileset(path.module, local.replica_query_path)
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X POST "${local.kibana_url}/api/saved_objects/_import?overwrite=true" -H 'kbn-xsrf: true' --form "file=@./${each.value}"
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "upload_dashboard" {
-  depends_on = [null_resource.upload_query_replica]
-
-  for_each = fileset(path.module, "nodo/dashboard/*.ndjson")
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X POST "${local.kibana_url}/api/saved_objects/_import?overwrite=true" -H 'kbn-xsrf: true' --form "file=@./${each.value}"
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "upload_dashboard_replica" {
-  depends_on = [null_resource.upload_dashboard]
-
-  for_each = fileset(path.module, local.replica_dashboard_path)
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X POST "${local.kibana_url}/api/saved_objects/_import?overwrite=true" -H 'kbn-xsrf: true' --form "file=@./${each.value}"
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-#################################### [FDR] ####################################
-resource "null_resource" "fdr_ilm_policy" {
-  depends_on = [null_resource.upload_dashboard_replica]
-
-  for_each = local.fdr_ilm_policy
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X PUT "${local.elastic_url}/_ilm/policy/${each.key}" \
-      -H 'kbn-xsrf: true' \
-      -H 'Content-Type: application/json' \
-      -d '${each.value}'
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "fdr_component_template" {
-  depends_on = [null_resource.fdr_ilm_policy]
-
-  for_each = local.fdr_component_template
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X PUT "${local.elastic_url}/_component_template/${each.key}" \
-      -H 'kbn-xsrf: true' \
-      -H 'Content-Type: application/json' \
-      -d '${each.value}'
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "fdr_index_template" {
-  depends_on = [null_resource.fdr_component_template]
-
-  for_each = local.fdr_index_template
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X PUT "${local.elastic_url}/_index_template/${each.key}" \
-      -H 'kbn-xsrf: true' \
-      -H 'Content-Type: application/json' \
-      -d '${each.value}'
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "fdr_kibana_space" {
-  depends_on = [null_resource.fdr_index_template]
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X POST "${local.kibana_url}/api/spaces/space" \
-      -H 'kbn-xsrf: true' \
-      -H 'Content-Type: application/json' \
-      -d '${local.fdr_kibana_space}'
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "fdr_kibana_data_view" {
-  depends_on = [null_resource.fdr_kibana_space]
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      data_view=$(curl -k -X POST "${local.kibana_url}/s/fdr/api/data_views/data_view" \
-        -H 'kbn-xsrf: true' \
-        -H 'Content-Type: application/json' \
-        -d '${local.fdr_kibana_data_view}')
-      
-      data_view_id=$(echo $data_view | jq -r ".data_view.id")
-
-      curl -k -X POST "${local.kibana_url}/s/fdr/api/data_views/default" \
-        -H 'kbn-xsrf: true' \
-        -H 'Content-Type: application/json' \
-        -d '{
-              "data_view_id": "'$data_view_id'",
-              "force": true
-            }'
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-
+## opentelemetry
 resource "helm_release" "opentelemetry_operator_helm" {
-  depends_on = [null_resource.fdr_kibana_data_view]
+  depends_on = [
+    module.elastic_stack
+  ]
 
-  provisioner "local-exec" {
-    when        = destroy
-    command     = "kubectl delete crd opentelemetrycollectors.opentelemetry.io"
-    interpreter = ["/bin/bash", "-c"]
-  }
-  provisioner "local-exec" {
-    when        = destroy
-    command     = "kubectl delete crd instrumentations.opentelemetry.io"
-    interpreter = ["/bin/bash", "-c"]
-  }
+  # provisioner "local-exec" {
+  #   when        = destroy
+  #   command     = "kubectl delete crd opentelemetrycollectors.opentelemetry.io"
+  #   interpreter = ["/bin/bash", "-c"]
+  # }
+  # provisioner "local-exec" {
+  #   when        = destroy
+  #   command     = "kubectl delete crd instrumentations.opentelemetry.io"
+  #   interpreter = ["/bin/bash", "-c"]
+  # }
 
   name       = "opentelemetry-operator"
   repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
@@ -327,12 +69,23 @@ resource "helm_release" "opentelemetry_operator_helm" {
 
 }
 
-resource "kubectl_manifest" "otel_collector" {
+data "kubernetes_secret" "get_apm_token" {
   depends_on = [
     helm_release.opentelemetry_operator_helm
   ]
+
+  metadata {
+    name      = "quickstart-apm-token"
+    namespace = local.elk_namespace
+  }
+}
+resource "kubectl_manifest" "otel_collector" {
+  depends_on = [
+    data.kubernetes_secret.get_apm_token
+  ]
   yaml_body = templatefile("${path.module}/env/opentelemetry_operator_helm/otel.yaml", {
     namespace = local.elk_namespace
+    apm_token = data.kubernetes_secret.get_apm_token.data.secret-token
   })
 
   force_conflicts = true
