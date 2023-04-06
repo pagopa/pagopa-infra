4d81bf586b1685f7429436038edbc5b9112bd172
Ignazio Bricco <ignazio.bricco@gmail.com>
2023-04-06 16:44:27 +0200

    NOD-185: fix dashboard Kibana

--- src/elk-monitoring/05_kibana_dashboard_ndp.tf
+++ src/elk-monitoring/05_kibana_dashboard_ndp.tf
@@ -9,24 +9,15 @@ locals {
 resource "null_resource" "ndp_nodo_upload_query" {
   depends_on = [null_resource.ndp_nodo_data_stream_rollover]
 
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
   for_each = fileset(path.module, local.query_path)
 
   provisioner "local-exec" {
     command     = <<EOT
-      curl -k -X POST "${local.kibana_url}/api/saved_objects/_import?overwrite=true" -H 'kbn-xsrf: true' --form "file=@./${each.value}"
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
-resource "null_resource" "ndp_nodoreplica_upload_query" {
-  depends_on = [null_resource.ndp_nodoreplica_data_stream_rollover]
-
-  for_each = fileset(path.module, local.replica_query_path)
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      curl -k -X POST "${local.kibana_url}/api/saved_objects/_import?overwrite=true" -H 'kbn-xsrf: true' --form "file=@./${each.value}"
+      curl -k -X POST "${local.kibana_url}/s/${local.ndp_space_name}/api/saved_objects/_import?overwrite=true" -H 'kbn-xsrf: true' --form "file=@./${each.value}"
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
@@ -35,24 +26,36 @@ resource "null_resource" "ndp_nodoreplica_upload_query" {
 resource "null_resource" "ndp_nodo_upload_dashboard" {
   depends_on = [null_resource.ndp_nodo_upload_query]
 
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
   for_each = fileset(path.module, local.dashboard_path)
 
   provisioner "local-exec" {
     command     = <<EOT
-      curl -k -X POST "${local.kibana_url}/api/saved_objects/_import?overwrite=true" -H 'kbn-xsrf: true' --form "file=@./${each.value}"
+      curl -k -X POST "${local.kibana_url}/s/${local.ndp_space_name}/api/saved_objects/_import?overwrite=true" \
+      -H 'kbn-xsrf: true' \
+      --form "file=@./${each.value}"
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
 }
 
 resource "null_resource" "ndp_nodoreplica_upload_dashboard" {
-  depends_on = [null_resource.ndp_nodoreplica_upload_query]
+  depends_on = [null_resource.ndp_nodo_upload_query]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 
   for_each = fileset(path.module, local.replica_dashboard_path)
 
   provisioner "local-exec" {
     command     = <<EOT
-      curl -k -X POST "${local.kibana_url}/api/saved_objects/_import?overwrite=true" -H 'kbn-xsrf: true' --form "file=@./${each.value}"
+      curl -k -X POST "${local.kibana_url}/s/${local.ndp_space_name}/api/saved_objects/_import?overwrite=true" \
+      -H 'kbn-xsrf: true' \
+      --form "file=@./${each.value}"
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
