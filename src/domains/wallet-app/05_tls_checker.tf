a434b4276d7dc1abf44497dc56239c32ab29549a
Simone Infante <simone.infante@pagopa.it>
2023-04-16 18:31:22 +0200

    fix: tls checker

--- src/domains/wallet-app/05_tls_checker.tf
+++ src/domains/wallet-app/05_tls_checker.tf
@@ -10,7 +10,7 @@ module "tls_checker" {
   helm_chart_image_name                      = var.tls_cert_check_helm.image_name
   helm_chart_image_tag                       = var.tls_cert_check_helm.image_tag
   location_string                            = var.location_string
-  application_insights_connection_string     = data.azurerm_application_insights.application_insights.connection_string
+  application_insights_connection_string     = "ai-${var.env_short}-connection-string"
   application_insights_resource_group        = data.azurerm_resource_group.monitor_rg.name
   application_insights_id                    = data.azurerm_application_insights.application_insights.id
   application_insights_action_group_ids      = [data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.email.id]
