f7bd9b8e567bca2e8730392ba4e401add9421131
Fernando-Granato <117268775+Fernando-Granato@users.noreply.github.com>
2023-04-16 22:53:16 +0200

    CHK-1376-new-wallet-domain-app (#869)
    Co-authored-by: Simone infante <52280205+infantesimone@users.noreply.github.com>
    Co-authored-by: Simone Infante <simone.infante@pagopa.it>

---
+++ src/domains/wallet-app/04_apim_wallet.tf
@@ -0,0 +1,21 @@
+##############
+## Products ##
+##############
+
+module "apim_wallet_product" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v6.3.0"
+
+  product_id   = "wallet"
+  display_name = "wallet pagoPA"
+  description  = "Product for wallet pagoPA"
+
+  api_management_name = local.pagopa_apim_name
+  resource_group_name = local.pagopa_apim_rg
+
+  published             = true
+  subscription_required = true
+  approval_required     = true
+  subscriptions_limit   = 1000
+
+  policy_xml = file("./api_product/_base_policy.xml")
+}
