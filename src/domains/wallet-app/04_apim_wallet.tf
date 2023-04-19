017a9e32525884e4944d158ac3a9342116d5b209
Fernando-Granato <fernando.granato@pagopa.it>
2023-04-19 09:40:34 +0200

    fix: name of apim_wallet_service_api_v1 and azurerm_api_management_api_version_set

--- src/domains/wallet-app/04_apim_wallet.tf
+++ src/domains/wallet-app/04_apim_wallet.tf
@@ -35,7 +35,7 @@ locals {
 
 # Wallet service APIs
 resource "azurerm_api_management_api_version_set" "wallet_service_api" {
-  name                = format("%s-wallet-service-api", local.project)
+  name                = format("%s-service-api", local.project)
   resource_group_name = local.pagopa_apim_rg
   api_management_name = local.pagopa_apim_name
   display_name        = local.apim_wallet_service_api.display_name
@@ -45,7 +45,7 @@ resource "azurerm_api_management_api_version_set" "wallet_service_api" {
 module "apim_wallet_service_api_v1" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.3.0"
 
-  name                  = format("%s-wallet-service-api", local.project)
+  name                  = format("%s-service-api", local.project)
   api_management_name   = local.pagopa_apim_name
   resource_group_name   = local.pagopa_apim_rg
   product_ids           = [module.apim_wallet_product.product_id]
