1ce5b9e4ddf3b24f293dd7212fa21180eb9ae39b
Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>
2023-04-21 14:27:08 +0200

    fix: Increase timeout APP Gateway for APIM (#896)

--- src/core/appgateway.tf
+++ src/core/appgateway.tf
@@ -251,7 +251,7 @@ module "app_gw" {
       fqdns                       = [azurerm_dns_a_record.dns_a_api.fqdn]
       probe                       = "/status-0123456789abcdef"
       probe_name                  = "probe-apim"
-      request_timeout             = 10
+      request_timeout             = 30 # workaround for FDR - set to 10s after new Fdr service deployed
       pick_host_name_from_backend = false
     }
 
