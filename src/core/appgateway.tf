573a795d06c6cf2de23b0772b6292171ce7d83cb
Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>
2023-05-03 23:34:39 +0200

    fix: Kibana fqnd listner (#921)

--- src/core/appgateway.tf
+++ src/core/appgateway.tf
@@ -282,10 +282,10 @@ module "app_gw" {
 
     kibana = {
       protocol                    = "Https"
-      host                        = "weu${var.env}.kibana.internal.${var.env}.platform.pagopa.it"
+      host                        = var.env == "prod" ? "weu${var.env}.kibana.internal.platform.pagopa.it" : "weu${var.env}.kibana.internal.${var.env}.platform.pagopa.it"
       port                        = 443
       ip_addresses                = [var.ingress_elk_load_balancer_ip]
-      fqdns                       = ["weu${var.env}.kibana.internal.${var.env}.platform.pagopa.it"]
+      fqdns                       = [var.env == "prod" ? "weu${var.env}.kibana.internal.platform.pagopa.it" : "weu${var.env}.kibana.internal.${var.env}.platform.pagopa.it"]
       probe                       = "/kibana"
       probe_name                  = "probe-kibana"
       request_timeout             = 10
