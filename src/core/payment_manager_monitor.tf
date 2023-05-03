742a0078f53125652256ceb7c47e27683559d36a
Simone Infante <simone.infante@pagopa.it>
2023-05-03 22:26:21 +0200

    feat: add restapi opsgenie

--- src/core/payment_manager_monitor.tf
+++ src/core/payment_manager_monitor.tf
@@ -13,7 +13,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "pm_restapi_availability"
   location            = var.location
 
   action {
-    action_group           = [azurerm_monitor_action_group.email.id, azurerm_monitor_action_group.slack.id, azurerm_monitor_action_group.mo_email.id]
+    action_group           = [azurerm_monitor_action_group.email.id, azurerm_monitor_action_group.slack.id, azurerm_monitor_action_group.mo_email.id, azurerm_monitor_action_group.pm_opsgenie[0].id]
     email_subject          = "Email Header"
     custom_webhook_payload = "{}"
   }
