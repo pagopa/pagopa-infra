6ce028178d6d7242a06b2530bcab94f3e7196a28
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-05-04 13:45:54 +0200

    add new group of api

--- src/domains/wallet-app/04_apim_wallet.tf
+++ src/domains/wallet-app/04_apim_wallet.tf
@@ -68,3 +68,30 @@ module "apim_wallet_service_api_v1" {
     hostname = local.wallet_hostname
   })
 }
+
+module "apim_wallet_service_notification_api_v1" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.3.0"
+
+  name                  = format("%s-service-api", local.project)
+  api_management_name   = local.pagopa_apim_name
+  resource_group_name   = local.pagopa_apim_rg
+  product_ids           = [module.apim_wallet_product.product_id]
+  subscription_required = local.apim_wallet_service_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.wallet_service_api.id
+  api_version           = "v1"
+
+  description  = local.apim_wallet_service_api.description
+  display_name = local.apim_wallet_service_api.display_name
+  path         = local.apim_wallet_service_api.path
+  protocols    = ["https"]
+  service_url  = local.apim_wallet_service_api.service_url
+
+  content_format = "openapi"
+  content_value = templatefile("./api/npg/v1/_openapi.json.tpl", {
+    hostname = local.apim_hostname
+  })
+
+  xml_content = templatefile("./api/npg/v1/_base_policy.xml.tpl", {
+    hostname = local.wallet_hostname
+  })
+}
