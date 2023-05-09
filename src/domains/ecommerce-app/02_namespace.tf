d20c7c0b55e43892d01474fe89624eed9f2ceb1e
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-05-09 12:22:28 +0200

    fix pod-identity module

--- src/domains/ecommerce-app/02_namespace.tf
+++ src/domains/ecommerce-app/02_namespace.tf
@@ -3,29 +3,6 @@ resource "kubernetes_namespace" "namespace" {
     name = var.domain
   }
 }
-
-# module "ingress" {
-#   source = "git::https://github.com/pagopa/azurerm.git//kubernetes_ingress?ref=v2.13.1"
-
-#   resource_group_name = local.aks_resource_group_name
-#   location            = var.location
-#   tenant_id           = data.azurerm_subscription.current.tenant_id
-#   cluster_name        = local.aks_name
-
-#   name      = kubernetes_namespace.namespace.metadata[0].name
-#   namespace = kubernetes_namespace.namespace.metadata[0].name
-#   key_vault = data.azurerm_key_vault.kv
-
-#   host = "${local.ingress_hostname}.${local.internal_dns_zone_name}"
-#   rules = [
-#     {
-#       path         = "/pagopa-ecommerce-transactions-service/(.*)"
-#       service_name = "pagopaecommercetransactionsservice-microservice-chart"
-#       service_port = 8080
-#     }
-#   ]
-# }
-
 module "pod_identity" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_pod_identity?ref=v6.6.0"
 
@@ -36,7 +13,7 @@ module "pod_identity" {
 
   identity_name = "${kubernetes_namespace.namespace.metadata[0].name}-pod-identity" // TODO add env in name
   namespace     = kubernetes_namespace.namespace.metadata[0].name
-  key_vault     = data.azurerm_key_vault.kv
+  key_vault_id     = data.azurerm_key_vault.kv.id
 
   secret_permissions = ["Get"]
 }
