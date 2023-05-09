f3a325f5f907ba9eee3479f38dc31130e9b86a99
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-05-09 16:36:06 +0200

    use last version of tls_cheker

---
+++ src/domains/ecommerce-app/.terraform.lock.hcl
@@ -0,0 +1,42 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azuread" {
+  version     = "2.38.0"
+  constraints = "2.38.0"
+  hashes = [
+    "h1:DkI/1QpUTAGSkoOc9HtADoBlkqDDRzApF5QRKzW17nk=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.45.0"
+  constraints = ">= 3.30.0, <= 3.45.0, <= 3.53.0"
+  hashes = [
+    "h1:gQLNY1I5e9kcle1p/VYEWb0eteQ/t5kUfnqVu2/GBNY=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/helm" {
+  version     = "2.7.1"
+  constraints = "2.7.1, <= 2.7.1"
+  hashes = [
+    "h1:OGZRkgiLBWmoA8/a9xZnEs5gsC5JhW+75++MkCPQbqw=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version     = "2.20.0"
+  constraints = "2.20.0"
+  hashes = [
+    "h1:A+uNz4B7oHqQA2lRX6u62MomG7s+uXlMpzNyBL0xIXg=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version     = "3.2.1"
+  constraints = "3.2.1, <= 3.2.1"
+  hashes = [
+    "h1:tSj1mL6OQ8ILGqR2mDu7OYYYWf+hoir0pf9KAQ8IzO8=",
+  ]
+}
--- src/domains/ecommerce-app/05_aks_middleware_tools.tf
+++ src/domains/ecommerce-app/05_aks_middleware_tools.tf
@@ -1,5 +1,5 @@
 module "tls_checker" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//tls_checker?ref=v6.6.0"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//tls_checker?ref=tls_cheker_improve_docs"
 
   https_endpoint                             = local.ecommerce_hostname
   alert_name                                 = local.ecommerce_hostname
@@ -10,15 +10,15 @@ module "tls_checker" {
   helm_chart_image_name                      = var.tls_cert_check_helm.image_name
   helm_chart_image_tag                       = var.tls_cert_check_helm.image_tag
   location_string                            = var.location_string
-  application_insights_connection_string     = data.azurerm_application_insights.application_insights.connection_string
   application_insights_resource_group        = data.azurerm_resource_group.monitor_rg.name
   application_insights_id                    = data.azurerm_application_insights.application_insights.id
   application_insights_action_group_ids  = [
     data.azurerm_monitor_action_group.slack.id,
     data.azurerm_monitor_action_group.email.id
   ]
+  kv_secret_name_for_application_insights_connection_string     = "applicationinsights-connection-string"
   keyvault_name                          = data.azurerm_key_vault.kv.name
-  keyvault_tenantid                      = data.azurerm_client_config.current.tenant_id
+  keyvault_tenant_id                      = data.azurerm_client_config.current.tenant_id
 }
 
 resource "helm_release" "cert_mounter" {
