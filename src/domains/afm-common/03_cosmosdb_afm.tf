54adcc0f09d75df971f21c66744873f0625e297f
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-05-12 16:01:56 +0200

    minor fix

--- src/domains/afm-common/03_cosmosdb_afm.tf
+++ src/domains/afm-common/03_cosmosdb_afm.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "afm_rg" {
 }
 
 module "afm_marketplace_cosmosdb_snet" {
-  source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v1.0.90"
+  source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v4.14.0"
   name                 = "${local.project}-marketplace-cosmosdb-snet"
   address_prefixes     = var.cidr_subnet_afm_marketplace_cosmosdb
   resource_group_name  = local.vnet_resource_group_name
