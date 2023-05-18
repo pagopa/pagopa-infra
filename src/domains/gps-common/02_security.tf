30a6d1743bd19efd07926992cefad0abe5b51b7e
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-05-18 12:55:07 +0200

    PAGOPA-848 removing azure storage conn key

--- src/domains/gps-common/02_security.tf
+++ src/domains/gps-common/02_security.tf
@@ -123,14 +123,6 @@ resource "azurerm_key_vault_secret" "gpd_reporting_enrollment_subscription_key" 
   }
 }
 
-resource "azurerm_key_vault_secret" "storage_connection_string" {
-  name         = format("gpd-payments-%s-sa-connection-string", var.env_short)
-  value        = module.payments_receipt_sa.primary_connection_string
-  content_type = "text/plain"
-
-  key_vault_id = module.key_vault.id
-}
-
 #tfsec:ignore:azure-keyvault-ensure-secret-expiry tfsec:ignore:azure-keyvault-content-type-for-secret
 resource "azurerm_key_vault_secret" "payments_cosmos_connection_string" {
   name         = format("gpd-payments-%s-cosmos-connection-string", var.env_short)
