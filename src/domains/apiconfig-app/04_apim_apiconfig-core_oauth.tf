7aaa144c0adc9fc074a92333dd5278bf5f03aa0b
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-05-18 09:53:35 +0200

    improvements

--- src/domains/apiconfig-app/04_apim_apiconfig-core_oauth.tf
+++ src/domains/apiconfig-app/04_apim_apiconfig-core_oauth.tf
@@ -13,7 +13,7 @@ resource "azurerm_api_management_api_version_set" "api_apiconfig_core_oauth_api_
 module "apim_api_apiconfig_core_oauth_api_v1_o" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v5.1.0"
 
-  name                  = format("%s-apiconfig-core-%s-oauth-api", local.project, local.postgres)
+  name                  = format("%s-apiconfig-core-%s-oauth-api", local.project, local.oracle)
   api_management_name   = local.pagopa_apim_name
   resource_group_name   = local.pagopa_apim_rg
   product_ids           = [module.apim_apiconfig_core_oauth_product.product_id]
@@ -23,22 +23,22 @@ module "apim_api_apiconfig_core_oauth_api_v1_o" {
   api_version    = "v1"
 
   description  = local.apiconfig_core_locals.description
-  display_name = "${local.apiconfig_core_locals.display_name} - Oauth ${local.postgres}"
+  display_name = "${local.apiconfig_core_locals.display_name} - Oauth ${local.oracle}"
 
-  path        = "${local.apiconfig_core_locals.path}/oauth/${local.postgres}"
+  path        = "${local.apiconfig_core_locals.path}/oauth/${local.oracle}"
   protocols   = ["https"]
   service_url = local.apiconfig_core_locals.service_url
 
   content_format = "openapi"
   content_value = templatefile("./api/apiconfig-core/oauth/v1/_openapi.json.tpl", {
     host    = local.apim_hostname
-    service = "oauth-${local.postgres}"
+    service = "oauth-${local.oracle}"
   })
 
   xml_content = templatefile("./api/apiconfig-core/oauth/v1/_base_policy.xml.tpl", {
     hostname = local.apiconfig_core_locals.hostname
     origin   = format("https://%s.%s.%s", var.cname_record_name, var.apim_dns_zone_prefix, var.external_domain)
-    database = local.postgres
+    database = local.oracle
 
     pagopa_tenant_id       = local.apiconfig_core_locals.pagopa_tenant_id
     apiconfig_be_client_id = local.apiconfig_core_locals.apiconfig_be_client_id
