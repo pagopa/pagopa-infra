ad6ff97c80ee8a938fefd780e772d0e400c98b79
Giovanni Berti <giovanni.berti@pagopa.it>
2023-05-31 12:48:56 +0200

    feat(checkout-domain): remove Checkout pagopa-proxy Redis from Core

--- src/domains/checkout-app/remove_checkout_resources.sh
+++ src/domains/checkout-app/remove_checkout_resources.sh
@@ -5,11 +5,9 @@ ARGS_AFTER_FIRST=("${ARGS[@]:1}")
 env=$1
 
 resources=(
-  'module.pagopa_proxy_snet'
-  'module.pagopa_proxy_app_service'
-  'module.pagopa_proxy_app_service_slot_staging'
-  'azurerm_monitor_autoscale_setting.pagopa_proxy_app_service_autoscale'
-  'azurerm_api_management_named_value.pagopa_appservice_proxy_url_value'
+  'module.pagopa_proxy_redis_snet'
+  'azurerm_resource_group.pagopa_proxy_rg'
+  'module.pagopa_proxy_redis'
 )
 
 cd ../../core
