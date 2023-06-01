3060c173d32336d270e414b314697c68f0c8d529
Jacopo Carlini <jacopo.carlini@gmail.com>
2023-06-01 12:36:03 +0200

    Pagopa 656 core migrare a nuovo dominio (#994)

--- src/core/variables.tf
+++ src/core/variables.tf
@@ -145,80 +145,6 @@ variable "cidr_subnet_mock_payment_gateway" {
   default     = null
 }
 
-# api_config
-
-variable "cidr_subnet_api_config" {
-  type        = list(string)
-  description = "Address prefixes subnet api config"
-  default     = null
-}
-
-variable "api_config_tier" {
-  type        = string
-  description = "Api config Plan tier"
-  default     = "Standard"
-}
-
-variable "api_config_size" {
-  type        = string
-  description = "Api Config Plan size"
-  default     = "S1"
-}
-
-variable "api_config_always_on" {
-  type        = bool
-  description = "Api Config always on property"
-  default     = true
-}
-
-variable "db_port" {
-  type        = number
-  description = "Port number of the DB"
-  default     = 1521
-}
-
-variable "db_service_name" {
-  type        = string
-  description = "Service Name of DB"
-  default     = null
-}
-
-variable "apiconfig_logging_level" {
-  type        = string
-  description = "Logging level of Api Config"
-  default     = "INFO"
-}
-
-variable "xsd_ica" {
-  type        = string
-  description = "XML Schema of Informatica Conto Accredito"
-  default     = "https://raw.githubusercontent.com/pagopa/pagopa-api/SANP3.2.0/xsd/InformativaContoAccredito_1_2_1.xsd"
-}
-
-variable "xsd_counterpart" {
-  type        = string
-  description = "XML Schema of Tabelle delle Controparti"
-  default     = "https://raw.githubusercontent.com/pagopa/pagopa-api/SANP3.2.0/xsd/TabellaDelleControparti_1_0_8.xsd"
-}
-
-variable "xsd_cdi" {
-  type        = string
-  description = "XML Schema of Catalogo Dati Informativi"
-  default     = "https://raw.githubusercontent.com/pagopa/pagopa-api/SANP3.2.0/xsd/CatalogoDatiInformativiPSP.xsd"
-}
-
-
-# api_config_fe
-variable "api_config_fe_enabled" {
-  type        = bool
-  description = "Api Config FE enabled"
-  default     = false
-}
-
-variable "cname_record_name" {
-  type = string
-}
-
 # nodo dei pagamenti
 
 variable "nodo_pagamenti_enabled" {
@@ -1662,7 +1588,6 @@ variable "platform_private_dns_zone_records" {
 }
 
 # node forwarder
-# api_config
 
 variable "cidr_subnet_node_forwarder" {
   type        = list(string)
