9cc77d1efe63061de2df0d35568764a556cea6c1
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2023-06-07 13:21:34 +0200

    feat: add aca app domain (#1011)

--- .utils/terraform_run_all.sh
+++ .utils/terraform_run_all.sh
@@ -17,6 +17,7 @@ ACTION="$1"
 array=(
     'src/aks-platform::weu-dev'
     'src/core::dev'
+    'src/domains/aca-app::weu-dev'
     'src/domains/aca-common::weu-dev'
     'src/domains/afm-app::weu-dev'
     'src/domains/afm-common::weu-dev'
