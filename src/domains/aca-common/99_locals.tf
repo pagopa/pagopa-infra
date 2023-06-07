83c80c2e207273b26f69e362b363a3710c5f5a36
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2023-06-07 18:46:05 +0200

    fix: add aks dns record (#1021)

--- src/domains/aca-common/99_locals.tf
+++ src/domains/aca-common/99_locals.tf
@@ -1,5 +1,8 @@
 locals {
-  project                  = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
-  product                  = "${var.prefix}-${var.env_short}"
-  monitor_appinsights_name = "${local.product}-appinsights"
+  project                               = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
+  product                               = "${var.prefix}-${var.env_short}"
+  monitor_appinsights_name              = "${local.product}-appinsights"
+  ingress_hostname                      = "${var.location_short}${var.instance}.${var.domain}"
+  internal_dns_zone_name                = "${var.dns_zone_internal_prefix}.${var.external_domain}"
+  internal_dns_zone_resource_group_name = "${local.product}-vnet-rg"
 }
