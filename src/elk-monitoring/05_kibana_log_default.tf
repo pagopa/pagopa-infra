4c45bae721d72b23c96645c245de7ccc7ffa9b0c
Marco Meretta <marco.meretta@gmail.com>
2023-06-08 18:53:10 +0200

    add apm policy

--- src/elk-monitoring/05_kibana_log_default.tf
+++ src/elk-monitoring/05_kibana_log_default.tf
@@ -12,7 +12,18 @@ locals {
     name = "default policy for the metrics index template installed by x-pack reviewed by PagoPA",
     managed = true
   }), "\""), "\""), "'", "'\\''")
+
+  logs_apm_key = "logs-apm.app_logs-default_policy"
+  logs_apm_ilm_policy = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/ilm-policy.json", {
+    name = "apm reviewed by PagoPA",
+    managed = true
+  }), "\""), "\""), "'", "'\\''")
   
+  logs_error_apm_key = "logs-apm.error_logs-default_policy"
+  logs_error_apm_ilm_policy = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/ilm-policy.json", {
+    name = "apm reviewed by PagoPA",
+    managed = true
+  }), "\""), "\""), "'", "'\\''")
 
 }
 
@@ -52,3 +63,38 @@ resource "null_resource" "metrics_ilm_policy" {
   }
 }
 
+resource "null_resource" "apm_log_ilm_policy" {
+  depends_on = [module.elastic_stack]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_ilm/policy/${local.logs_apm_key}" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.logs_apm_ilm_policy}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "apm_log_error_ilm_policy" {
+  depends_on = [module.elastic_stack]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_ilm/policy/${local.logs_error_apm_key}" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.logs_error_apm_ilm_policy}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
