cf75cad96a61d75d3fd90b94e4511b721fa93f69
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2023-06-08 15:33:03 +0200

    fix

--- src/domains/receipts-secrets/02_security.tf
+++ src/domains/receipts-secrets/02_security.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "sec_rg" {
 }
 
 module "key_vault" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault?ref=v6.4.1"
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault?ref=v2.13.1"
 
   name                       = "${local.product}-${var.domain}-kv"
   location                   = azurerm_resource_group.sec_rg.location
@@ -24,7 +24,7 @@ resource "azurerm_key_vault_access_policy" "ad_group_policy" {
   tenant_id = data.azurerm_client_config.current.tenant_id
   object_id = data.azuread_group.adgroup_admin.object_id
 
-  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", ]
+  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt"]
   secret_permissions      = ["Get", "List", "Set", "Delete", ]
   storage_permissions     = []
   certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Purge", "Recover", ]
@@ -39,7 +39,25 @@ resource "azurerm_key_vault_access_policy" "adgroup_developers_policy" {
   tenant_id = data.azurerm_client_config.current.tenant_id
   object_id = data.azuread_group.adgroup_developers.object_id
 
-  key_permissions     = ["Get", "List", "Update", "Create", "Import", "Delete", ]
+  key_permissions     = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt"]
+  secret_permissions  = ["Get", "List", "Set", "Delete", ]
+  storage_permissions = []
+  certificate_permissions = [
+    "Get", "List", "Update", "Create", "Import",
+    "Delete", "Restore", "Purge", "Recover"
+  ]
+}
+
+## ad group policy ##
+resource "azurerm_key_vault_access_policy" "adgroup_externals_policy" {
+  count = var.env_short != "p" ? 1 : 0
+
+  key_vault_id = module.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.adgroup_externals.object_id
+
+  key_permissions     = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt"]
   secret_permissions  = ["Get", "List", "Set", "Delete", ]
   storage_permissions = []
   certificate_permissions = [
@@ -67,58 +85,36 @@ resource "azurerm_key_vault_access_policy" "azdevops_iac_policy" {
   storage_permissions = []
 }
 
-resource "azurerm_key_vault_secret" "receipts_datastore_cosmos_pkey" {
-  name         = format("receipts-datastore-%s-cosmos-pkey", var.env_short)
-  value        = module.receipts_datastore_cosmosdb_account.primary_key
-  content_type = "text/plain"
-
-  key_vault_id = module.key_vault.id
+data "azuread_service_principal" "pipe_principal" {
+  count        = var.enable_iac_pipeline ? 1 : 0
+  display_name = format("pagopaspa-pagoPA-projects-%s", data.azurerm_subscription.current.subscription_id)
 }
 
-resource "azurerm_key_vault_secret" "ai_connection_string" {
-  name         = format("ai-%s-connection-string", var.env_short)
-  value        = data.azurerm_application_insights.application_insights.connection_string
-  content_type = "text/plain"
-
+resource "azurerm_key_vault_access_policy" "azdevops_pipe_policy" {
+  count        = var.enable_iac_pipeline ? 1 : 0
   key_vault_id = module.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azuread_service_principal.pipe_principal[0].object_id
+
+  secret_permissions      = ["Get", "List", "Set", ]
+  certificate_permissions = ["SetIssuers", "DeleteIssuers", "Purge", "List", "Get"]
+  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt"]
+
+  storage_permissions = []
 }
 
-resource "azurerm_key_vault_secret" "cosmos_receipts_connection_string" {
-  name         = format("cosmos-%s-receipts-connection-string", var.env_short)
-  value        = module.receipts_datastore_cosmosdb_account.connection_strings[0]
-  content_type = "text/plain"
+################
+##   Secrets  ##
+################
 
-  key_vault_id = module.key_vault.id
+# create json letsencrypt inside kv
+# requierd: Docker
+module "letsencrypt_receipt" {
+  source = "git::https://github.com/pagopa/azurerm.git//letsencrypt_credential?ref=v3.8.1"
+
+  prefix            = var.prefix
+  env               = var.env_short
+  key_vault_name    = "${local.product}-${var.domain}-kv"
+  subscription_name = local.subscription_name
 }
 
-resource "azurerm_key_vault_secret" "receipts_azurewebjobsstorage" {
-  name         = format("receipts-%s-azurewebjobsstorage", var.env_short)
-  value        = module.receipts_datastore_fn_sa.primary_connection_string
-  content_type = "text/plain"
-
-  key_vault_id = module.key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "cosmos_receipts_key" {
-  name         = format("cosmos-%s-receipts-key", var.env_short)
-  value        = module.receipts_datastore_cosmosdb_account.primary_key
-  content_type = "text/plain"
-
-  key_vault_id = module.key_vault.id
-}
-
-
-#tfsec:ignore:azure-keyvault-ensure-secret-expiry tfsec:ignore:azure-keyvault-content-type-for-secret
-resource "azurerm_key_vault_secret" "payment_manager_subscription_key" {
-  name         = "payment-manager-subscription-key"
-  value        = "<TO_UPDATE_MANUALLY_BY_PORTAL>"
-  content_type = "text/plain"
-
-  key_vault_id = module.key_vault.id
-
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
-}
---
+++ src/domains/receipts-secrets/99_locals.tf
@@ -0,0 +1,10 @@
+locals {
+  project = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
+  product = "${var.prefix}-${var.env_short}"
+
+  monitor_action_group_slack_name = "SlackPagoPA"
+  monitor_action_group_email_name = "PagoPA"
+
+  subscription_name = "${var.env}-${var.prefix}"
+
+}
