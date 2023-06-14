11315e5ecad8ec89ac651b2a5ef97e27ad8e594b
Simone Infante <simone.infante@pagopa.it>
2023-06-14 19:03:29 +0200

    fix: secret module name

--- src/domains/ecommerce-common/02_security.tf
+++ src/domains/ecommerce-common/02_security.tf
@@ -125,7 +125,7 @@ resource "azurerm_key_vault_secret" "ai_connection_string" {
   key_vault_id = module.key_vault.id
 }
 
-resource "azurerm_key_vault_secret" "sessions_jwt_token" {
+resource "azurerm_key_vault_secret" "sessions_jwt_secret" {
   name         = "sessions-jwt-secret"
   value        = "<TO UPDATE MANUALLY ON PORTAL>"
   key_vault_id = module.key_vault.id
