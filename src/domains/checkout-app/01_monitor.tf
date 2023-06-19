5c8b93980c76dbb259da4c28bc1d33eee9647831
Simone Infante <simone.infante@pagopa.it>
2023-06-19 11:25:24 +0200

    feat: add opsgenie wh for checkout

--- src/domains/checkout-app/01_monitor.tf
+++ src/domains/checkout-app/01_monitor.tf
@@ -16,3 +16,18 @@ data "azurerm_monitor_action_group" "email" {
   resource_group_name = var.monitor_resource_group_name
   name                = local.monitor_action_group_email_name
 }
+
+resource "azurerm_monitor_action_group" "checkout_opsgenie" { 
+
+  name                = "CheckoutOpsgenie"
+  resource_group_name = azurerm_resource_group.monitor_rg.name
+  short_name          = "CheckoutOpsgenie"
+
+  webhook_receiver {
+    name                    = "CheckoutOpsgenieWebhook"
+    service_uri             = "https://api.opsgenie.com/v1/json/azure?apiKey=${data.azurerm_key_vault_secret.monitor_pm_opsgenie_webhook_key[0].value}"
+    use_common_alert_schema = true
+  }
+
+  tags = var.tags
+}
--- src/domains/checkout-app/02_security.tf
+++ src/domains/checkout-app/02_security.tf
@@ -11,3 +11,8 @@ data "azurerm_key_vault_secret" "google_recaptcha_secret" {
   name         = "google-recaptcha-secret"
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
+
+data "azurerm_key_vault_secret" "monitor_checkout_opsgenie_webhook_key" {
+  resource_group_name = var.monitor_resource_group_name
+  name                = local.monitor_action_group_email_name
+}
