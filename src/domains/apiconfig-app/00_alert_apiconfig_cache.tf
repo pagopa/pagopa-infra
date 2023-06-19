36bb97a060609165b024c7538a801853ee28554c
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-06-19 17:01:46 +0200

    improvements

--- src/domains/apiconfig-app/00_alert_apiconfig_cache.tf
+++ src/domains/apiconfig-app/00_alert_apiconfig_cache.tf
@@ -2,52 +2,22 @@
 ## API-CONFIG CACHE ##
 ######################
 
-locals {
-
-  oracle_version     = "oracle"
-  postgresql_version = "postgresql"
-}
-
-# Pod OutOfMemory - ORACLE
-resource "azurerm_monitor_scheduled_query_rules_alert" "apiconfig_out_of_memory_oracle" {
-  name                = format("%s-%s-%s", local.apiconfig_cache_alert.pagopa_api_config_cache_name, "out-of-memory", local.oracle_version)
+# Pod OutOfMemory
+resource "azurerm_monitor_scheduled_query_rules_alert" "apiconfig_cache_out_of_memory" {
+  for_each = toset(["oracle", "postgresql"])
+  name                = format("%s-%s-%s", local.apiconfig_cache_alert.pagopa_api_config_cache_name, "out-of-memory", each.key)
   resource_group_name = azurerm_resource_group.api_config_rg.name
   location            = var.location
 
   action {
     action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
-    email_subject          = format("Out-of-memory %s", local.oracle_version)
+    email_subject          = format("Out-of-memory %s", each.key)
     custom_webhook_payload = "{}"
   }
   data_source_id = data.azurerm_application_insights.application_insights.id
-  description    = format("Problem to generate cache - %s", local.oracle_version)
+  description    = format("Problem to generate cache - %s", each.key)
   enabled        = true
-  query          = format(local.apiconfig_cache_alert.outOfMemory.query, format("%s-%s-%s", var.prefix, local.apiconfig_cache_locals.path, local.oracle_version))
-
-  severity    = local.apiconfig_cache_alert.outOfMemory.severity
-  frequency   = local.apiconfig_cache_alert.outOfMemory.frequency
-  time_window = local.apiconfig_cache_alert.outOfMemory.time_window
-  trigger {
-    operator  = "GreaterThanOrEqual"
-    threshold = 1
-  }
-}
-
-# Pod OutOfMemory - POSTGRESQL
-resource "azurerm_monitor_scheduled_query_rules_alert" "apiconfig_out_of_memory_postgresql" {
-  name                = format("%s-%s-%s", local.apiconfig_cache_alert.pagopa_api_config_cache_name, "out-of-memory", local.postgresql_version)
-  resource_group_name = azurerm_resource_group.api_config_rg.name
-  location            = var.location
-
-  action {
-    action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
-    email_subject          = format("Out-of-memory %s", local.postgresql_version)
-    custom_webhook_payload = "{}"
-  }
-  data_source_id = data.azurerm_application_insights.application_insights.id
-  description    = format("Problem to generate cache - %s", local.postgresql_version)
-  enabled        = true
-  query          = format(local.apiconfig_cache_alert.outOfMemory.query, format("%s-%s-%s", var.prefix, local.apiconfig_cache_locals.path, local.postgresql_version))
+  query          = format(local.apiconfig_cache_alert.outOfMemory.query, format("%s-%s-%s", var.prefix, local.apiconfig_cache_locals.path, each.key))
 
   severity    = local.apiconfig_cache_alert.outOfMemory.severity
   frequency   = local.apiconfig_cache_alert.outOfMemory.frequency
--- src/domains/apiconfig-app/00_alert_apiconfig_core.tf
+++ src/domains/apiconfig-app/00_alert_apiconfig_core.tf
@@ -51,13 +51,13 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "apiconfig_jdbc_connectio
   enabled        = true
 
   query = format(<<-QUERY
-    let threshold = 0.05;
+    let threshold = 5;
     traces
     | where cloud_RoleName == "%s"
     | where message contains "Invoking API operation" or message contains "Unable to acquire JDBC Connection"
     | order by timestamp desc
     | summarize Total=countif(message contains "Invoking API operation"), Error=countif(message contains "Unable to acquire JDBC Connection") by length=bin(timestamp, 5m)
-    | extend Problem=toreal(Error) / Total
+    | extend Problem=(toreal(Error) / Total) * 100
     | where Problem > threshold
     | order by length desc
     QUERY
