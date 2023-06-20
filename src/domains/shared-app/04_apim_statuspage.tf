755ef19e0268e402b4594329b626587d7fcc0efb
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-06-20 15:00:23 +0200

    [PAGOPA-1022] feat: updated status page URL references

--- src/domains/shared-app/04_apim_statuspage.tf
+++ src/domains/shared-app/04_apim_statuspage.tf
@@ -53,6 +53,11 @@ data "azurerm_linux_function_app" "gpd" {
   resource_group_name = format("%s-%s-gpd-rg", var.prefix, var.env_short)
 }
 
+data "azurerm_linux_function_app" "mockec" {
+  name                = format("%s-%s-app-mock-ec", var.prefix, var.env_short)
+  resource_group_name = format("%s-%s-mock-ec-rg", var.prefix, var.env_short)
+}
+
 module "apim_api_statuspage_api_v1" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.4.1"
 
@@ -84,16 +89,20 @@ module "apim_api_statuspage_api_v1" {
       "apiconfig"               = format("%s/apiconfig/api/v1", data.azurerm_linux_function_app.api_config.default_hostname)
       "apiconfigcacheo"         = format("%s/api-config-cache/o", format(local.aks_path, "apiconfig"))
       "apiconfigcachep"         = format("%s/api-config-cache/p", format(local.aks_path, "apiconfig"))
-      "apiconfigselfcare"       = format("%s/pagopa-api-config-selfcare", format(local.aks_path, "apiconfig"))
-      "authorizer"              = format("%s/", data.azurerm_linux_function_app.authorizer.default_hostname)
+      "apiconfigselfcare"       = format("%s/pagopa-api-config-selfcare-integration", format(local.aks_path, "apiconfig"))
       "bizevents"               = format("%s/pagopa-biz-events-service", format(local.aks_path, "bizevents"))
       "bizeventsdatastoreneg"   = format("%s/pagopa-negative-biz-events-datastore-service", format(local.aks_path, "bizevents"))
       "bizeventsdatastorepos"   = format("%s/pagopa-biz-events-datastore-service", format(local.aks_path, "bizevents"))
+      "fdrndpnew"               = format("%s/pagopa-fdr-service", format(local.aks_path, "fdr"))
       "gpd"                     = format("%s/", data.azurerm_linux_function_app.gpd.default_hostname)
       "gpdpayments"             = format("%s/pagopa-gpd-payments", format(local.aks_path, "gps"))
       "gpdenrollment"           = format("%s/pagopa-gpd-reporting-orgs-enrollment", format(local.aks_path, "gps"))
       "gps"                     = format("%s/pagopa-spontaneous-payments-service", format(local.aks_path, "gps"))
       "gpsdonation"             = format("%s/pagopa-gps-donation-service", format(local.aks_path, "gps"))
+      "mockec"                  = format("%s/", data.azurerm_linux_function_app.mockec.default_hostname)
+      "mocker"                  = format("%s/mocker", format(local.aks_path, "mocker"))
+      "pdfengine"               = format("%s/pagopa-pdf-engine", format(local.aks_path, "shared"))
+      "receiptpdfdatastore"     = format("%s/pagopa-receipt-pdf-datastore", format(local.aks_path, "receipts"))
     }), "\"", "\\\"")
   })
 }
