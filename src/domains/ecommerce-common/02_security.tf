994a98c6c471af280caf387556947f4a074eda6b
Simone Infante <simone.infante@pagopa.it>
2023-06-26 14:59:18 +0200

    feat: add missing ecommerce secret

--- src/domains/ecommerce-common/02_security.tf
+++ src/domains/ecommerce-common/02_security.tf
@@ -244,3 +244,15 @@ resource "azurerm_key_vault_secret" "payment_method_api_key" {
     ]
   }
 }
+
+resource "azurerm_key_vault_secret" "elastic_otel_token_header" {
+  name         = "elastic-otel-token-header"
+  value        = "<TO UPDATE MANUALLY ON PORTAL>"
+  key_vault_id = module.key_vault.id
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
