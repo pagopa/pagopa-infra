171376b4e664bc201c8ae80aa0b44bf1934ec8e1
Jacopo <jacopo.carlini@gmail.com>
2023-06-27 13:04:16 +0200

    kibana

---
+++ src/elk-monitoring/05_kibana_log_pagopa.tf
@@ -0,0 +1,111 @@
+#################################### [NDP] ####################################
+locals {
+  pagopa_key    = "pagopa"
+  pipeline_name = "json_parser"
+
+  pagopa_space = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/space.json", {
+    name = "${local.ndp_space_name}"
+  }), "\""), "\""), "'", "'\\''")
+
+  pagopa_ingest_pipeline = replace(trimsuffix(trimprefix(file("${path.module}/${local.pagopa_key}/ingest-pipeline.json"), "\""), "\""), "'", "'\\''")
+
+  pagopa_ilm_policy = replace(trimsuffix(trimprefix(templatefile("${path.module}/${local.pagopa_key}/ilm-policy.json", {
+    policy_name = local.default_snapshot_policy_key
+  }), "\""), "\""), "'", "'\\''")
+
+  pagopa_component_template_custom = replace(trimsuffix(trimprefix(templatefile("${path.module}/${local.pagopa_key}/logs-kubernetes.container_logs@custom.json", {
+    name = local.pipeline_name
+  }), "\""), "\""), "'", "'\\''")
+}
+
+## space
+resource "null_resource" "pagopa_kibana_space" {
+  depends_on = [module.elastic_stack]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X POST "${local.kibana_url}/api/spaces/space" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.pagopa_space}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+## nodo
+resource "null_resource" "pagopa_ingest_pipeline" {
+  depends_on = [null_resource.pagopa_kibana_space]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_ingest/pipeline/${local.pipeline_name}" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.pagopa_ingest_pipeline}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "pagopa_ilm_policy" {
+  depends_on = [null_resource.pagopa_ingest_pipeline, null_resource.snapshot_policy]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_ilm/policy/${local.pagopa_key}" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.pagopa_ilm_policy}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "pagopa_component_template_custom" {
+  depends_on = [null_resource.pagopa_ilm_policy]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_component_template/logs-kubernetes.container_logs@custom" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.pagopa_component_template_custom}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+
+}
+
+resource "null_resource" "pagopa_data_stream_rollover" {
+  depends_on = [null_resource.pagopa_component_template_custom]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X POST "${local.elastic_url}/logs-kubernetes.container_logs-default/_rollover/" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
