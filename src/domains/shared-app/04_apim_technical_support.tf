633963b6141b7fd904c908fefa6cb49b14b906c6
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-06-28 12:52:48 +0200

    fix product

--- src/domains/shared-app/04_apim_technical_support.tf
+++ src/domains/shared-app/04_apim_technical_support.tf
@@ -11,7 +11,7 @@ module "apim_technical_support_product" {
   api_management_name = local.pagopa_apim_name
   resource_group_name = local.pagopa_apim_rg
 
-  published             = false
+  published             = true
   subscription_required = true
   approval_required     = false
   subscriptions_limit   = 1000
