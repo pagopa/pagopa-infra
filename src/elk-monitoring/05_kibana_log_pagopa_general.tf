5fce62848838c4ef645fd3cb0659787092944afa
Jacopo <jacopo.carlini@gmail.com>
2023-06-30 12:01:50 +0200

    data view

--- src/elk-monitoring/05_kibana_log_pagopa_general.tf
+++ src/elk-monitoring/05_kibana_log_pagopa_general.tf
@@ -3,8 +3,12 @@ locals {
   pagopa_key    = "pagopa"
   pipeline_name = "json_parser"
 
-  pagopa_space = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/space.json", {
-    name = "${local.ndp_space_name}"
+  pagopa_space = replace(trimsuffix(trimprefix(templatefile("${path.module}/${local.pagopa_key}/space.json", {
+    name = "${local.pagopa_key}"
+  }), "\""), "\""), "'", "'\\''")
+
+  pagopa_data_view = replace(trimsuffix(trimprefix(templatefile("${path.module}/${local.pagopa_key}/data-view.json", {
+    name = local.pagopa_key
   }), "\""), "\""), "'", "'\\''")
 
   pagopa_ingest_pipeline = replace(trimsuffix(trimprefix(file("${path.module}/${local.pagopa_key}/ingest-pipeline.json"), "\""), "\""), "'", "'\\''")
@@ -37,7 +41,6 @@ resource "null_resource" "pagopa_kibana_space" {
   }
 }
 
-## nodo
 resource "null_resource" "pagopa_ingest_pipeline" {
   depends_on = [null_resource.pagopa_kibana_space]
 
@@ -109,3 +112,21 @@ resource "null_resource" "pagopa_data_stream_rollover" {
     interpreter = ["/bin/bash", "-c"]
   }
 }
+
+#resource "null_resource" "pagopa_kibana_data_view" {
+#  depends_on = [null_resource.pagopa_kibana_space]
+#
+#  triggers = {
+#    always_run = "${timestamp()}"
+#  }
+#
+#  provisioner "local-exec" {
+#    command     = <<EOT
+#      curl -k -X POST "${local.kibana_url}/s/${local.pagopa_key}/api/data_views/data_view" \
+#      -H 'kbn-xsrf: true' \
+#      -H 'Content-Type: application/json' \
+#      -d '${local.pagopa_data_view}'
+#    EOT
+#    interpreter = ["/bin/bash", "-c"]
+#  }
+#}
