50f36cbcdcef6e7db1cebe0948d21d8779228ee9
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-07-14 09:35:03 +0200

    wip

--- src/domains/fdr-app/00_network.tf
+++ src/domains/fdr-app/00_network.tf
@@ -3,6 +3,12 @@ data "azurerm_virtual_network" "vnet" {
   resource_group_name = local.vnet_resource_group_name
 }
 
+data "azurerm_subnet" "apim_vnet" {
+  name                 = local.apim_vnet_snet
+  resource_group_name  = local.vnet_resource_group_name
+  virtual_network_name = local.vnet_integration_name
+}
+
 data "azurerm_dns_zone" "public" {
   name = join(".", [var.apim_dns_zone_prefix, var.external_domain])
 }
