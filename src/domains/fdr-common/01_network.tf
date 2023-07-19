f89ff2fe3e673ef578a64e96b61afb31c452b4a9
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-07-19 21:52:22 +0200

    table private link

--- src/domains/fdr-common/01_network.tf
+++ src/domains/fdr-common/01_network.tf
@@ -42,6 +42,11 @@ data "azurerm_private_dns_zone" "privatelink_blob_azure_com" {
   resource_group_name = local.vnet_resource_group_name
 }
 
+data "azurerm_private_dns_zone" "privatelink_table_azure_com" {
+  name                = "privatelink.table.core.windows.net"
+  resource_group_name = local.vnet_resource_group_name
+}
+
 data "azurerm_private_dns_zone" "privatelink_queue_azure_com" {
   name                = "privatelink.queue.core.windows.net"
   resource_group_name = local.vnet_resource_group_name
