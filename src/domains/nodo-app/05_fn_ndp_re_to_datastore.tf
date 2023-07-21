4c8e39015b82f9135d30642bbc35d2ca251734de
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-07-21 12:42:54 +0200

    improvements

--- src/domains/nodo-app/05_fn_ndp_re_to_datastore.tf
+++ src/domains/nodo-app/05_fn_ndp_re_to_datastore.tf
@@ -211,7 +211,7 @@ resource "azurerm_monitor_autoscale_setting" "nodo_re_to_datastore_function" {
         direction = "Increase"
         type      = "ChangeCount"
         value     = "1"
-        cooldown  = "PT1M"
+        cooldown  = "PT5M"
       }
     }
 
@@ -231,7 +231,7 @@ resource "azurerm_monitor_autoscale_setting" "nodo_re_to_datastore_function" {
         direction = "Decrease"
         type      = "ChangeCount"
         value     = "1"
-        cooldown  = "PT1M"
+        cooldown  = "PT5M"
       }
     }
   }
