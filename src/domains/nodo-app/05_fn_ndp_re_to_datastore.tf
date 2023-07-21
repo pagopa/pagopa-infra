4b164ee05bbf1c2b3ce47f0de2cc1b680dd0fcff
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-07-21 12:43:10 +0200

    Revert "improvements"
    This reverts commit 367d0415addbbb0630a53856115f6a4e3216740e.

--- src/domains/nodo-app/05_fn_ndp_re_to_datastore.tf
+++ src/domains/nodo-app/05_fn_ndp_re_to_datastore.tf
@@ -222,7 +222,7 @@ resource "azurerm_monitor_autoscale_setting" "nodo_re_to_datastore_function" {
         direction = "Increase"
         type      = "ChangeCount"
         value     = "1"
-        cooldown  = "PT5M"
+        cooldown  = "PT1M"
       }
     }
 
@@ -242,7 +242,7 @@ resource "azurerm_monitor_autoscale_setting" "nodo_re_to_datastore_function" {
         direction = "Decrease"
         type      = "ChangeCount"
         value     = "1"
-        cooldown  = "PT5M"
+        cooldown  = "PT1M"
       }
     }
   }
