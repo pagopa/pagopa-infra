99a7f4dda9ce06f45109014f21142dc86aa398ff
Simone Infante <simone.infante@pagopa.it>
2023-07-24 11:52:38 +0200

    fix: rs refs with count

--- src/domains/wallet-common/03_cosmosdb.tf
+++ src/domains/wallet-common/03_cosmosdb.tf
@@ -29,7 +29,7 @@ module "cosmosdb_account_mongodb" {
 
   name                = "${local.project}-cosmos-account"
   location            = var.location
-  resource_group_name = azurerm_resource_group.cosmosdb_wallet_rg.name
+  resource_group_name = azurerm_resource_group.cosmosdb_wallet_rg[0].name
   domain              = var.domain
 
   offer_type           = var.cosmos_mongo_db_params.offer_type
@@ -40,13 +40,13 @@ module "cosmosdb_account_mongodb" {
 
   public_network_access_enabled      = var.cosmos_mongo_db_params.public_network_access_enabled
   private_endpoint_enabled           = var.cosmos_mongo_db_params.private_endpoint_enabled
-  subnet_id                          = module.cosmosdb_wallet_snet.id
+  subnet_id                          = module.cosmosdb_wallet_snet[0].id
   private_dns_zone_ids               = [data.azurerm_private_dns_zone.cosmos.id]
   is_virtual_network_filter_enabled  = var.cosmos_mongo_db_params.is_virtual_network_filter_enabled
   allowed_virtual_network_subnet_ids = var.cosmos_mongo_db_params.public_network_access_enabled ? [] : [data.azurerm_subnet.aks_subnet.id]
 
   consistency_policy               = var.cosmos_mongo_db_params.consistency_policy
-  main_geo_location_location       = azurerm_resource_group.cosmosdb_wallet_rg.location
+  main_geo_location_location       = azurerm_resource_group.cosmosdb_wallet_rg[0].location
   main_geo_location_zone_redundant = var.cosmos_mongo_db_params.main_geo_location_zone_redundant
   additional_geo_locations         = var.cosmos_mongo_db_params.additional_geo_locations
 
@@ -59,8 +59,8 @@ resource "azurerm_cosmosdb_mongo_database" "wallet" {
   count = var.env_short != "p" ? 1 : 0
 
   name                = "wallet"
-  resource_group_name = azurerm_resource_group.cosmosdb_wallet_rg.name
-  account_name        = module.cosmosdb_account_mongodb.name
+  resource_group_name = azurerm_resource_group.cosmosdb_wallet_rg[0].name
+  account_name        = module.cosmosdb_account_mongodb[0].name
 
   throughput = var.cosmos_mongo_db_wallet_params.enable_autoscaling || var.cosmos_mongo_db_wallet_params.enable_serverless ? null : var.cosmos_mongo_db_wallet_params.throughput
 
