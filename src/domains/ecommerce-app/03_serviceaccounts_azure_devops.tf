7746802b98dc76dcfa9d7541fa78fc4449382337
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-07-24 16:58:40 +0200

    set secrets only for prod env

--- src/domains/ecommerce-app/03_serviceaccounts_azure_devops.tf
+++ src/domains/ecommerce-app/03_serviceaccounts_azure_devops.tf
@@ -44,7 +44,8 @@ resource "azurerm_key_vault_secret" "azure_devops_sa_cacrt" {
 }
 
 resource "azurerm_key_vault_secret" "azure_webhook_dashboard_creation_token" {
-  name         = "azure-webhook-dashboard-creation-token"
+  count        = var.env_short == "p" ? 1 : 0
+  name         = "${var.prefix}-${var.env_short}-azure-webhook-dashboard-creation-token"
   value        = "<TO UPDATE MANUALLY ON PORTAL>"
   key_vault_id = module.key_vault.id
 
