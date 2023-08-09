941f2c6894e108f66ec6331f4542a75e64b1e2f7
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-08-09 12:20:45 +0200

    [PAGOPA-1143] fix: added publication and required subscription

--- src/domains/shared-app/04_apim_authorizer_config.tf
+++ src/domains/shared-app/04_apim_authorizer_config.tf
@@ -20,8 +20,8 @@ module "apim_authorizer_config_product" {
   api_management_name = local.pagopa_apim_name
   resource_group_name = local.pagopa_apim_rg
 
-  published             = false
-  subscription_required = false
+  published             = true
+  subscription_required = true
   approval_required     = false
   # subscriptions_limit   = 1000
 
