f86dd5d6d49282e7d0af1e2f3a7dc1e94925764e
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-08-09 18:52:45 +0200

    improved alert threashold

--- src/domains/gps-app/00_alert_gpd_payments.tf
+++ src/domains/gps-app/00_alert_gpd_payments.tf
@@ -26,10 +26,10 @@ AzureDiagnostics
   )
   severity    = 2
   frequency   = 5
-  time_window = 5
+  time_window = 10
   trigger {
     operator  = "GreaterThanOrEqual"
-    threshold = 1
+    threshold = 2
   }
 }
 ## GPD-Payments SOAP Availability ##
@@ -97,10 +97,10 @@ AzureDiagnostics
   )
   severity    = 2
   frequency   = 5
-  time_window = 5
+  time_window = 10
   trigger {
     operator  = "GreaterThanOrEqual"
-    threshold = 1
+    threshold = 2
   }
 }
 ## GPD-Payments REST Availability ##
