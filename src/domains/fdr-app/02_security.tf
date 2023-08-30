5259e2f6459a4f62a111c059572a5b8e2954dd9f
marcomeretta <121177306+marcomeretta@users.noreply.github.com>
2023-08-30 12:00:16 +0200

    feat: fn json to xml (#1259)

--- src/domains/fdr-app/02_security.tf
+++ src/domains/fdr-app/02_security.tf
@@ -7,3 +7,8 @@ data "azurerm_key_vault_secret" "fdr_product_subscription_key" {
   name         = "fdr-product-subscription-key"
   key_vault_id = data.azurerm_key_vault.kv.id
 }
+
+data "azurerm_key_vault_secret" "fdr_nodo_product_subscription_key" {
+  name         = "fdr-nodo-product-subscription-key"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
