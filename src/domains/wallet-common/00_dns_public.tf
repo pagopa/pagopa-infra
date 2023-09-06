922eb3115181164233dd755112afe0f855fc595a
Giovanni Berti <giovanni.berti@pagopa.it>
2023-09-06 16:51:59 +0200

    feat: add dns servers with delegation

--- src/domains/wallet-common/00_dns_public.tf
+++ src/domains/wallet-common/00_dns_public.tf
@@ -12,10 +12,10 @@ resource "azurerm_dns_ns_record" "dev_payment_wallet" {
   zone_name           = azurerm_dns_zone.payment_wallet_public.name
   resource_group_name = data.azurerm_resource_group.rg_vnet.name
   records = [
-    "ns1-02.azure-dns.com.",
-    "ns2-02.azure-dns.net.",
-    "ns3-02.azure-dns.org.",
-    "ns4-02.azure-dns.info."
+    "ns1-35.azure-dns.com.",
+    "ns2-35.azure-dns.net.",
+    "ns3-35.azure-dns.org.",
+    "ns4-35.azure-dns.info."
   ]
   ttl  = var.dns_default_ttl_sec
   tags = var.tags
@@ -28,10 +28,10 @@ resource "azurerm_dns_ns_record" "uat_payment_wallet" {
   zone_name           = azurerm_dns_zone.payment_wallet_public.name
   resource_group_name = data.azurerm_resource_group.rg_vnet.name
   records = [
-    "ns1-02.azure-dns.com.",
-    "ns2-02.azure-dns.net.",
-    "ns3-02.azure-dns.org.",
-    "ns4-02.azure-dns.info."
+    "ns1-35.azure-dns.com.",
+    "ns2-35.azure-dns.net.",
+    "ns3-35.azure-dns.org.",
+    "ns4-35.azure-dns.info."
   ]
   ttl  = var.dns_default_ttl_sec
   tags = var.tags
