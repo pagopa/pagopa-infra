0a71b6f27143d48cbcb64bf33d16ed474ee74a35
Jacopo <jacopo.carlini@gmail.com>
2023-09-06 16:44:19 +0200

    fix name

--- src/domains/apiconfig-app/00_alert_apiconfig_core.tf
+++ src/domains/apiconfig-app/00_alert_apiconfig_core.tf
@@ -2,9 +2,13 @@
 ## API-CONFIG CORE ##
 #####################
 
+locals {
+  app_name = format("%s-%s-app-api-config", var.prefix, var.env_short)
+}
+
 # Node database availability
 resource "azurerm_monitor_scheduled_query_rules_alert" "apiconfig_db_healthcheck" {
-  name                = format("%s-%s", module.api_config_app_service.name, "db-healthcheck")
+  name                = format("%s-%s", local.app_name, "db-healthcheck")
   resource_group_name = azurerm_resource_group.api_config_rg.name
   location            = var.location
 
@@ -24,7 +28,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "apiconfig_db_healthcheck
     | extend Availability=((Success*1.0)/Total)*100
     | where toint(Availability) < 99
   QUERY
-    , module.api_config_app_service.name
+    , local.app_name
   )
   severity    = 1
   frequency   = 45
@@ -37,7 +41,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "apiconfig_db_healthcheck
 
 # JDBC connection problem
 resource "azurerm_monitor_scheduled_query_rules_alert" "apiconfig_jdbc_connection" {
-  name                = format("%s-%s", module.api_config_app_service.name, "jdbc-connection")
+  name                = format("%s-%s", local.app_name, "db-healthcheck")
   resource_group_name = azurerm_resource_group.api_config_rg.name
   location            = var.location
 
@@ -61,7 +65,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "apiconfig_jdbc_connectio
     | where Problem > threshold
     | order by length desc
     QUERY
-    , module.api_config_app_service.name
+    , local.app_name
   )
 
   severity    = 1
