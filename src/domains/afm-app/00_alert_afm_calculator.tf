e4b4a2e2d8f5828200e3e54f8ece1298e08a1185
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2023-09-08 17:57:36 +0200

    fix

--- src/domains/afm-app/00_alert_afm_calculator.tf
+++ src/domains/afm-app/00_alert_afm_calculator.tf
@@ -11,7 +11,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "opex_pagopa-afm-calculat
   }
 
   data_source_id = data.azurerm_api_management.apim.id
-  description    = "Response time for /psps/[^/]+/fees is less than or equal to 1.5s - https://portal.azure.com/?l=en.en-us#@pagopait.onmicrosoft.com/dashboard/arm/subscriptions/b9fc9419-6097-45fe-9f74-ba0641c91912/resourcegroups/dashboards/providers/microsoft.portal/dashboards/pagopa-p-opex_pagopa-debt-position"
+  description    = "Response time for /psps/[^/]+/fees is less than or equal to 1.5s - https://portal.azure.com/?l=en.en-us#@pagopait.onmicrosoft.com/dashboard/arm/subscriptions/b9fc9419-6097-45fe-9f74-ba0641c91912/resourcegroups/dashboards/providers/microsoft.portal/dashboards/pagopa-p-opex_pagopa-afm-calculator"
   enabled        = true
   query = (<<-QUERY
 let threshold = 1500;
@@ -44,7 +44,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "opex_pagopa-afm-calculat
   }
 
   data_source_id = data.azurerm_api_management.apim.id
-  description    = "Availability for /psps/[^/]+/fees is less than or equal to 99% - https://portal.azure.com/?l=en.en-us#@pagopait.onmicrosoft.com/dashboard/arm/subscriptions/b9fc9419-6097-45fe-9f74-ba0641c91912/resourcegroups/dashboards/providers/microsoft.portal/dashboards/pagopa-p-opex_pagopa-debt-position"
+  description    = "Availability for /psps/[^/]+/fees is less than or equal to 99% - https://portal.azure.com/?l=en.en-us#@pagopait.onmicrosoft.com/dashboard/arm/subscriptions/b9fc9419-6097-45fe-9f74-ba0641c91912/resourcegroups/dashboards/providers/microsoft.portal/dashboards/pagopa-p-opex_pagopa-afm-calculator"
   enabled        = true
   query = (<<-QUERY
 let threshold = 0.99;
@@ -80,7 +80,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "opex_pagopa-afm-calculat
   }
 
   data_source_id = data.azurerm_api_management.apim.id
-  description    = "Response time for /psps/[^/]+/fees is less than or equal to 1.5s - https://portal.azure.com/?l=en.en-us#@pagopait.onmicrosoft.com/dashboard/arm/subscriptions/b9fc9419-6097-45fe-9f74-ba0641c91912/resourcegroups/dashboards/providers/microsoft.portal/dashboards/pagopa-p-opex_pagopa-debt-position"
+  description    = "Response time for /psps/[^/]+/fees is less than or equal to 1.5s - https://portal.azure.com/?l=en.en-us#@pagopait.onmicrosoft.com/dashboard/arm/subscriptions/b9fc9419-6097-45fe-9f74-ba0641c91912/resourcegroups/dashboards/providers/microsoft.portal/dashboards/pagopa-p-opex_pagopa-afm-calculator"
   enabled        = true
   query = (<<-QUERY
 let threshold = 1500;
@@ -113,7 +113,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "opex_pagopa-afm-calculat
   }
 
   data_source_id = data.azurerm_api_management.apim.id
-  description    = "Availability for /psps/[^/]+/fees is less than or equal to 99% - https://portal.azure.com/?l=en.en-us#@pagopait.onmicrosoft.com/dashboard/arm/subscriptions/b9fc9419-6097-45fe-9f74-ba0641c91912/resourcegroups/dashboards/providers/microsoft.portal/dashboards/pagopa-p-opex_pagopa-debt-position"
+  description    = "Availability for /psps/[^/]+/fees is less than or equal to 99% - https://portal.azure.com/?l=en.en-us#@pagopait.onmicrosoft.com/dashboard/arm/subscriptions/b9fc9419-6097-45fe-9f74-ba0641c91912/resourcegroups/dashboards/providers/microsoft.portal/dashboards/pagopa-p-opex_pagopa-afm-calculator"
   enabled        = true
   query = (<<-QUERY
 let threshold = 0.99;
