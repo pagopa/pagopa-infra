6dfac63008ad9a8cd20a9f62103c5ce1fa56705a
Pietro Tota <pietro.tota@pagopa.it>
2023-09-15 13:51:19 +0200

    fix: remove commented code

--- src/domains/qi-app/04_apim_qi.tf
+++ src/domains/qi-app/04_apim_qi.tf
@@ -41,30 +41,3 @@ resource "azurerm_api_management_api_version_set" "qi_api" {
   display_name        = local.apim_qi_api.display_name
   versioning_scheme   = "Segment"
 }
-
-# module "apim_qi_api_v1" {
-#   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.3.0"
-
-#   name                  = format("%s-api", local.project)
-#   api_management_name   = local.pagopa_apim_name
-#   resource_group_name   = local.pagopa_apim_rg
-#   product_ids           = [module.apim_qi_product.product_id]
-#   subscription_required = local.apim_qi_api.subscription_required
-#   version_set_id        = azurerm_api_management_api_version_set.qi_api.id
-#   api_version           = "v1"
-
-#   description  = local.apim_qi_api.description
-#   display_name = local.apim_qi_api.display_name
-#   path         = local.apim_qi_api.path
-#   protocols    = ["https"]
-#   service_url  = local.apim_qi_api.service_url
-
-#   content_format = "openapi"
-#   content_value = templatefile("./api/qi/v1/_openapi.json.tpl", {
-#     hostname = local.apim_hostname
-#   })
-
-#   xml_content = templatefile("./api/qi/v1/_base_policy.xml.tpl", {
-#     hostname = local.qi_hostname
-#   })
-# }
