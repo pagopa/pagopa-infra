56bb9f697061dc3173a018d9b09cb6ebb8961cfd
Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>
2023-09-30 17:10:15 +0200

    feat: PQI-47 creazione pipeline data factory (#1347)

--- src/domains/observability/04_datafactory_dataset.tf
+++ src/domains/observability/04_datafactory_dataset.tf
@@ -1,7 +1,6 @@
 locals {
-  # datasets = { for filename in fileset(path.module, "datafactory/datasets/*.json") : replace(basename(filename), ".json", "") => file("${path.module}/${filename}") }
-
-  datasets = { for filename in fileset(path.module, "datafactory/${var.env_short == "p" ? "NOT_FOUND" : "datasets"}/*.json") : replace(basename(filename), ".json", "") => file("${path.module}/${filename}") }
+  datasets = { for filename in fileset(path.module, "datafactory/datasets/*.json") : replace(basename(filename), ".json", "") => file("${path.module}/${filename}") }
+  # datasets = { for filename in fileset(path.module, "datafactory/${var.env_short == "p" ? "NOT_FOUND" : "datasets"}/*.json") : replace(basename(filename), ".json", "") => file("${path.module}/${filename}") }
 }
 
 
