3e4cb9933db4e100ce63d1ec91067e1bbe1521b4
acialini <ale.cialini@gmail.com>
2023-10-03 12:51:05 +0200

    feat: add-notifier-alerts

--- src/domains/receipts-app/00_alert.tf
+++ src/domains/receipts-app/00_alert.tf
@@ -57,7 +57,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "receipts-sending-receipt
 # This alert cover the following error case:
 # 1. BizEventToReceiptProcessor execution logs that a Receipt instance has been set to NOT_QUEUE_SENT
 # https://github.com/pagopa/pagopa-receipt-pdf-datastore/blob/1a0b36f9693c17ceeffe5d35bf7ace7371a72a13/src/main/java/it/gov/pagopa/receipt/pdf/datastore/service/BizEventToReceiptService.java#L58C17-L58C28
-# 
+#
 resource "azurerm_monitor_scheduled_query_rules_alert" "receipts-datastore-not-sent-to-queue-alert" {
   count               = var.env_short != "d" ? 1 : 0
   resource_group_name = "dashboards"
@@ -146,7 +146,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "receipts-cart-event-disc
   query = format(<<-QUERY
   traces
     | where cloud_RoleName == "%s"
-    | where message contains "discarded because is part of a payment cart" 
+    | where message contains "discarded because is part of a payment cart"
     | order by timestamp desc
   QUERY
     , "pagopareceiptpdfdatastore" # from HELM's parameter WEBSITE_SITE_NAME https://github.com/pagopa/pagopa-receipt-pdf-datastore/blob/1a0b36f9693c17ceeffe5d35bf7ace7371a72a13/helm/values-prod.yaml#L81
@@ -159,3 +159,75 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "receipts-cart-event-disc
     threshold = 1
   }
 }
+
+## Alert
+# This alert cover the following error case:
+# 1. NotifierRetry execution logs that a new entry has been set in error
+# https://github.com/pagopa/pagopa-receipt-pdf-notifier/src/main/java/it/gov/pagopa/receipt/pdf/generator/NotifierRetry.java#L50
+resource "azurerm_monitor_scheduled_query_rules_alert" "receipts-to-notify-in-retry-alert" {
+  count               = var.env_short != "d" ? 1 : 0
+  resource_group_name = "dashboards"
+  name                = "pagopa-${var.env_short}-notify-in-error-retry"
+  location            = var.location
+
+  action {
+    action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
+    email_subject          = "NotifierRetry function called"
+    custom_webhook_payload = "{}"
+  }
+  data_source_id = data.azurerm_application_insights.application_insights.id
+  description    = "Retry on notification to IO error for a receipt"
+  enabled        = true
+  query = format(<<-QUERY
+  traces
+    | where cloud_RoleName == "%s"
+    | order by timestamp desc
+    | where message contains "NotifierRetry function called"
+  QUERY
+    , "pagopareceiptpdfnotifier" # from HELM's parameter WEBSITE_SITE_NAME https://github.com/pagopa/pagopa-receipt-pdf-notifier/helm/values-prod.yaml
+  )
+  severity    = 2 // Sev 2	Warning
+  frequency   = 15
+  time_window = 15
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 1
+  }
+}
+
+## Alert
+# This alert cover the following error case:
+# 1. ReceiptToIoService execution logs that a receipt could not be notified (due to maximum retries, or failing to send to message queue)
+# https://github.com/pagopa/pagopa-receipt-pdf-notifier/src/main/java/it/gov/pagopa/receipt/pdf/generator/ReceiptToIOServiceImpl.java#L354
+resource "azurerm_monitor_scheduled_query_rules_alert" "receipt-unable-to-notify-alert" {
+  count               = var.env_short != "d" ? 1 : 0
+  resource_group_name = "dashboards"
+  name                = "pagopa-${var.env_short}-unable-to-send-notify"
+  location            = var.location
+
+  action {
+    action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
+    email_subject          = "Failed to notify receipt to IO"
+    custom_webhook_payload = "{}"
+  }
+  data_source_id = data.azurerm_application_insights.application_insights.id
+  description    = "Unable to notify to IO due to exceeding number of retries, or failing to send to retry management queue"
+  enabled        = true
+  query = format(<<-QUERY
+  traces
+    | where cloud_RoleName == "%s"
+    | order by timestamp desc
+    | where message contains "Receipt updated with status UNABLE_TO_SEND"
+  QUERY
+    , "pagopareceiptpdfnotifier" # from HELM's parameter WEBSITE_SITE_NAME https://github.com/pagopa/pagopa-receipt-pdf-notifier/helm/values-prod.yaml
+  )
+  severity    = 2 // Sev 2	Warning
+  frequency   = 15
+  time_window = 15
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 1
+  }
+}
+
+
