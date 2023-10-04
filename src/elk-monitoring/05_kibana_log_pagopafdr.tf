d8f3d793fca9272236d643059dd2ca842cf92321
Francesco Parisi <francesco.parisi@tasgroup.eu>
2023-10-04 06:54:47 +0200

    NOD-515: Modifica pagopa-infra ELK dashboard import

--- src/elk-monitoring/05_kibana_log_pagopafdr.tf
+++ src/elk-monitoring/05_kibana_log_pagopafdr.tf
@@ -61,7 +61,7 @@ locals {
   ## fdr
   pagopafdr_key = "pagopafdr"
 
-  pagopafdr_ingest_pipeline = replace(trimsuffix(trimprefix(file("${path.module}/log-template/ingest-pipeline.json"), "\""), "\""), "'", "'\\''")
+  pagopafdr_ingest_pipeline = replace(trimsuffix(trimprefix(file("${path.module}/fdr/${local.pagopafdr_key}/ingest-pipeline.json"), "\""), "\""), "'", "'\\''")
   pagopafdr_ilm_policy = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/ilm-policy.json", {
     name        = local.pagopafdr_key,
     managed     = false,
@@ -213,8 +213,6 @@ resource "null_resource" "pagopafdrnodo_data_stream_rollover" {
   }
 }
 
-
-
 resource "null_resource" "pagopafdrnodo_kibana_data_view" {
   depends_on = [null_resource.fdr_kibana_space]
 
@@ -228,6 +226,134 @@ resource "null_resource" "pagopafdrnodo_kibana_data_view" {
       -H 'kbn-xsrf: true' \
       -H 'Content-Type: application/json' \
       -d '${local.pagopafdrnodo_data_view}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+
+## fdr
+resource "null_resource" "pagopafdr_ingest_pipeline" {
+  depends_on = [null_resource.fdr_kibana_space]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_ingest/pipeline/${local.pagopafdr_key}" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.pagopafdr_ingest_pipeline}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "pagopafdr_ilm_policy" {
+  depends_on = [null_resource.pagopafdr_ingest_pipeline, null_resource.snapshot_policy]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_ilm/policy/${local.pagopafdr_key}" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.pagopafdr_ilm_policy}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "pagopafdr_component_template_package" {
+  depends_on = [null_resource.pagopafdr_ilm_policy]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_component_template/${local.pagopafdr_key}@package" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.pagopafdr_component_template_package}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "pagopafdr_component_template_custom" {
+  depends_on = [null_resource.pagopafdr_component_template_package]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_component_template/${local.pagopafdr_key}@custom" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.pagopafdr_component_template_custom}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+
+}
+
+resource "null_resource" "pagopafdr_index_template" {
+  depends_on = [null_resource.pagopafdr_component_template_custom]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_index_template/${local.pagopafdr_key}" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.pagopafdr_index_template}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "pagopafdr_data_stream_rollover" {
+  depends_on = [null_resource.pagopafdr_index_template]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X POST "${local.elastic_url}/logs-${local.pagopafdr_key}-default/_rollover/" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "pagopafdr_kibana_data_view" {
+  depends_on = [null_resource.fdr_kibana_space]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X POST "${local.kibana_url}/s/${local.fdr_space_name}/api/data_views/data_view" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.pagopafdr_data_view}'
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
