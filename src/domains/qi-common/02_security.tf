353a00e4552b9a037e6cf3dd0eaa20742699d8ef
Pietro Tota <pietro.tota@pagopa.it>
2023-10-18 11:29:55 +0200

    feat(sops): introduce sops for managing secrets

--- src/domains/qi-common/02_security.tf
+++ src/domains/qi-common/02_security.tf
@@ -41,7 +41,7 @@ resource "azurerm_key_vault_access_policy" "adgroup_developers_policy" {
   tenant_id = data.azurerm_client_config.current.tenant_id
   object_id = data.azuread_group.adgroup_developers.object_id
 
-  key_permissions     = ["Get", "List", "Update", "Create", "Import", "Delete", ]
+  key_permissions     = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt" ]
   secret_permissions  = ["Get", "List", "Set", "Delete", ]
   storage_permissions = []
   certificate_permissions = [
@@ -165,46 +165,4 @@ module "letsencrypt_qi" {
   env               = var.env_short
   key_vault_name    = "${local.product}-${var.domain}-kv"
   subscription_name = local.subscription_name
-}
-
-#tfsec:ignore:azure-keyvault-ensure-secret-expiry tfsec:ignore:azure-keyvault-content-type-for-secret
-resource "azurerm_key_vault_secret" "qi_jira_url" {
-  name  = format("qi-%s-jira-url", var.env_short)
-  value = "<TO_UPDATE_MANUALLY_BY_PORTAL>"
-
-  key_vault_id = module.key_vault.id
-
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
-}
-
-#tfsec:ignore:azure-keyvault-ensure-secret-expiry tfsec:ignore:azure-keyvault-content-type-for-secret
-resource "azurerm_key_vault_secret" "qi_jira_username" {
-  name  = format("qi-%s-jira-username", var.env_short)
-  value = "<TO_UPDATE_MANUALLY_BY_PORTAL>"
-
-  key_vault_id = module.key_vault.id
-
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
-}
-
-#tfsec:ignore:azure-keyvault-ensure-secret-expiry tfsec:ignore:azure-keyvault-content-type-for-secret
-resource "azurerm_key_vault_secret" "qi_jira_token" {
-  name  = format("qi-%s-jira-token", var.env_short)
-  value = "<TO_UPDATE_MANUALLY_BY_PORTAL>"
-
-  key_vault_id = module.key_vault.id
-
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
 }
---
+++ src/domains/qi-secrets/env/weu-uat/terraform.tfvars
@@ -0,0 +1,26 @@
+prefix          = "pagopa"
+env_short       = "u"
+env             = "uat"
+domain          = "qi"
+location        = "westeurope"
+location_short  = "weu"
+location_string = "West Europe"
+instance        = "uat"
+
+tags = {
+  CreatedBy   = "Terraform"
+  Environment = "Uat"
+  Owner       = "pagoPA"
+  Source      = "https://github.com/pagopa/pagopa-infra/tree/main/src/domains/fdr-secret"
+  CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
+}
+
+### External resources
+
+monitor_resource_group_name                 = "pagopa-u-monitor-rg"
+log_analytics_workspace_name                = "pagopa-u-law"
+log_analytics_workspace_resource_group_name = "pagopa-u-monitor-rg"
+
+input_file = "./secret/weu-uat/configs.json"
+
+enable_iac_pipeline = true
