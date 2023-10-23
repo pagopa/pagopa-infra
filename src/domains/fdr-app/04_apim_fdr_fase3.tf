aca242dc7a834ccb5cf6b41d3948b8db4a43fe6d
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-10-23 12:33:27 +0200

    fix openapi fase3

--- src/domains/fdr-app/04_apim_fdr_fase3.tf
+++ src/domains/fdr-app/04_apim_fdr_fase3.tf
@@ -38,17 +38,26 @@ module "apim_fdr_product_internal" {
   policy_xml = file("./api_product/fdr-service-internal/_base_policy.xml")
 }
 
-#################
-##    API fdr  ##
-#################
+###############
+##  API FdR  ##
+###############
 locals {
-  apim_fdr_service_api = {
-    display_name          = "FDR - Flussi di rendicontazione"
-    description           = "FDR - Flussi di rendicontazione"
-    path                  = "fdr/service"
+  apim_fdr_psp_service_api = {
+    display_name          = "FDR - Flussi di rendicontazione [PSP]"
+    description           = "FDR - Flussi di rendicontazione [PSP]"
+    path                  = "fdr-psp/service"
     subscription_required = true
     service_url           = null
   }
+
+  apim_fdr_org_service_api = {
+    display_name          = "FDR - Flussi di rendicontazione [ORG]"
+    description           = "FDR - Flussi di rendicontazione [ORG]"
+    path                  = "fdr-org/service"
+    subscription_required = true
+    service_url           = null
+  }
+
   apim_fdr_service_api_internal = {
     display_name          = "FDR - (INTERNAL) Flussi di rendicontazione"
     description           = "FDR - (INTERNAL) Flussi di rendicontazione"
@@ -58,43 +67,93 @@ locals {
   }
 }
 
-resource "azurerm_api_management_api_version_set" "api_fdr_api" {
-  name                = "${var.env_short}-fdr-service-api"
+##################
+##  API FdR PSP ##
+##################
+
+resource "azurerm_api_management_api_version_set" "api_fdr_api_psp" {
+  name                = "${var.env_short}-fdr-service-api-psp"
   resource_group_name = local.pagopa_apim_rg
   api_management_name = local.pagopa_apim_name
-  display_name        = local.apim_fdr_service_api.display_name
+  display_name        = local.apim_fdr_psp_service_api.display_name
   versioning_scheme   = "Segment"
 }
 
 
-module "apim_api_fdr_api_v1" {
+module "apim_api_fdr_api_v1_psp" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.3.0"
 
-  name                  = "${local.project}-fdr-service-api"
+  name                  = "${local.project}-fdr-service-api-psp"
   api_management_name   = local.pagopa_apim_name
   resource_group_name   = local.pagopa_apim_rg
   product_ids           = [module.apim_fdr_product.product_id]
-  subscription_required = local.apim_fdr_service_api.subscription_required
-  version_set_id        = azurerm_api_management_api_version_set.api_fdr_api.id
+  subscription_required = local.apim_fdr_psp_service_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.api_fdr_api_psp.id
   api_version           = "v1"
 
-  description  = local.apim_fdr_service_api.description
-  display_name = local.apim_fdr_service_api.display_name
-  path         = local.apim_fdr_service_api.path
+  description  = local.apim_fdr_psp_service_api.description
+  display_name = local.apim_fdr_psp_service_api.display_name
+  path         = local.apim_fdr_psp_service_api.path
   protocols    = ["https"]
-  service_url  = local.apim_fdr_service_api.service_url
+  service_url  = local.apim_fdr_psp_service_api.service_url
 
   content_format = "openapi"
-  content_value = templatefile("./api/fdr-service/v1/_openapi.json.tpl", {
+
+  content_value = templatefile("./api/fdr-service/psp/v1/_openapi.json.tpl", {
     host    = local.apim_hostname
     service = module.apim_fdr_product.product_id
   })
 
-  xml_content = templatefile("./api/fdr-service/v1/_base_policy.xml.tpl", {
+  xml_content = templatefile("./api/fdr-service/psp/v1/_base_policy.xml.tpl", {
     hostname = local.hostname
   })
 }
 
+##################
+##  API FdR ORG ##
+##################
+
+resource "azurerm_api_management_api_version_set" "api_fdr_api_org" {
+  name                = "${var.env_short}-fdr-service-api"
+  resource_group_name = local.pagopa_apim_rg
+  api_management_name = local.pagopa_apim_name
+  display_name        = local.apim_fdr_org_service_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+
+module "apim_api_fdr_api_v1_org" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.3.0"
+
+  name                  = "${local.project}-fdr-service-api-org"
+  api_management_name   = local.pagopa_apim_name
+  resource_group_name   = local.pagopa_apim_rg
+  product_ids           = [module.apim_fdr_product.product_id]
+  subscription_required = local.apim_fdr_org_service_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.api_fdr_api_org.id
+  api_version           = "v1"
+
+  description  = local.apim_fdr_org_service_api.description
+  display_name = local.apim_fdr_org_service_api.display_name
+  path         = local.apim_fdr_org_service_api.path
+  protocols    = ["https"]
+  service_url  = local.apim_fdr_org_service_api.service_url
+
+  content_format = "openapi"
+
+  content_value = templatefile("./api/fdr-service/org/v1/_openapi.json.tpl", {
+    host    = local.apim_hostname
+    service = module.apim_fdr_product.product_id
+  })
+
+  xml_content = templatefile("./api/fdr-service/org/v1/_base_policy.xml.tpl", {
+    hostname = local.hostname
+  })
+}
+
+#######################
+##  API FdR INTERNAL ##
+#######################
 resource "azurerm_api_management_api_version_set" "api_fdr_api_internal" {
 
   name                = "${var.env_short}-fdr-service-api-internal"
@@ -132,6 +191,9 @@ module "apim_api_fdr_api_v1_internal" {
   })
 }
 
+########################
+##  Info for FdR Rend ##
+########################
 resource "azurerm_api_management_named_value" "fdrcontainername" {
   name                = "fdrcontainername"
   api_management_name = data.azurerm_api_management.apim.name
