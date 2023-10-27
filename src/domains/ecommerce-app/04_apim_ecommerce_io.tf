b66d80beedf9a05df38369ba56bd0065f5a22919
Giovanni Berti <giovanni.berti@pagopa.it>
2023-10-27 15:25:40 +0200

    [CHK-2046] feat(ecommerce-for-io): add PM mapping policy for endpoint `POST /auth-requests` (#1417)
    Co-authored-by: Simone infante <52280205+infantesimone@users.noreply.github.com>
    Co-authored-by: Simone Infante <simone.infante@pagopa.it>
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>

--- src/domains/ecommerce-app/04_apim_ecommerce_io.tf
+++ src/domains/ecommerce-app/04_apim_ecommerce_io.tf
@@ -29,6 +29,14 @@ locals {
     subscription_required = true
     service_url           = null
   }
+
+  apim_ecommerce_io_webview_pay = {
+    display_name          = "eCommerce-PM web view for IO App"
+    description           = "Web view designed to aid compatibility between eCommerce API for IO App and Payment Manager"
+    path                  = "ecommerce/io-webview"
+    subscription_required = false
+    service_url           = null
+  }
 }
 
 resource "azurerm_api_management_api_version_set" "ecommerce_io_api_v1" {
@@ -63,6 +71,41 @@ module "apim_ecommerce_io_api_v1" {
 
   xml_content = templatefile("./api/ecommerce-io/v1/_base_policy.xml.tpl", {
     ecommerce_ingress_hostname = local.ecommerce_hostname
+  })
+}
+
+resource "azurerm_api_management_api_version_set" "ecommerce_io_webview_pay_v1" {
+  name                = "${local.project}-io-api-webview-pay"
+  resource_group_name = local.pagopa_apim_rg
+  api_management_name = local.pagopa_apim_name
+  display_name        = local.apim_ecommerce_io_webview_pay.display_name
+  versioning_scheme   = "Segment"
+}
+
+module "apim_ecommerce_io_webview_pay_v1" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.6.0"
+
+  name                  = "${local.project}-io-api-webiew-pay"
+  resource_group_name   = local.pagopa_apim_rg
+  api_management_name   = local.pagopa_apim_name
+  product_ids           = [module.apim_ecommerce_io_product.product_id]
+  subscription_required = local.apim_ecommerce_io_webview_pay.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.ecommerce_io_webview_pay_v1.id
+  api_version           = "v1"
+  service_url           = local.apim_ecommerce_io_webview_pay.service_url
+
+  description  = local.apim_ecommerce_io_webview_pay.description
+  display_name = local.apim_ecommerce_io_webview_pay.display_name
+  path         = local.apim_ecommerce_io_webview_pay.path
+  protocols    = ["https"]
+
+  content_format = "openapi"
+  content_value = templatefile("./api/ecommerce-io/v1/_webview_openapi.json.tpl", {
+    hostname = local.apim_hostname
+  })
+
+  xml_content = templatefile("./api/ecommerce-io/v1/pay-pm-webview.xml.tpl", {
+    pm_webview_path = "${local.apim_hostname}/pp-restapi-CD/v3/webview/transactions/pay"
   })
 }
 
@@ -123,9 +166,8 @@ resource "azurerm_api_management_api_operation_policy" "io_transaction_authoriza
   operation_id        = "requestTransactionAuthorization"
 
   xml_content = templatefile("./api/ecommerce-io/v1/_auth_request.xml.tpl", {
-    ecommerce_xpay_psps_list = var.ecommerce_xpay_psps_list
-    ecommerce_vpos_psps_list = var.ecommerce_vpos_psps_list
-    ecommerce_npg_psps_list  = var.ecommerce_npg_psps_list
+    webview_host      = local.apim_hostname
+    webview_path      = "ecommerce/io-webview/v1/pay"
   })
 }
 
