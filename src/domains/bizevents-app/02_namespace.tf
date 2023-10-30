004847f07dc13911bb0105219479fb80bbc98c1d
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-10-30 16:06:35 +0100

    bizevents added pdb

--- src/domains/bizevents-app/02_namespace.tf
+++ src/domains/bizevents-app/02_namespace.tf
@@ -18,3 +18,19 @@ module "pod_identity" {
 
   secret_permissions = ["Get"]
 }
+
+resource "kubernetes_pod_disruption_budget_v1" "bizevents" {
+
+  for_each = var.pod_disruption_budgets
+
+  metadata {
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    name      = each.key
+  }
+  spec {
+    min_available = each.value.minAvailable
+    selector {
+      match_labels = each.value.matchLabels
+    }
+  }
+}
