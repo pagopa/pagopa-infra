c7ac0af6f6dcf349b9762af82f053e29b01d2ec4
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-10-30 22:50:16 +0100

    minor fix in nodo-cron

--- src/domains/nodo-cron/02_namespace.tf
+++ src/domains/nodo-cron/02_namespace.tf
@@ -20,15 +20,3 @@ module "pod_identity" {
   secret_permissions = ["Get"]
 }
 
-resource "helm_release" "reloader" {
-  name       = "reloader"
-  repository = "https://stakater.github.io/stakater-charts"
-  chart      = "reloader"
-  version    = "v1.0.48"
-  namespace  = kubernetes_namespace.namespace.metadata[0].name
-
-  set {
-    name  = "reloader.watchGlobally"
-    value = "false"
-  }
-}
