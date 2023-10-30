ff7294e9f7ddb891661e731e9324aa903e7b37b4
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-10-30 23:05:28 +0100

    shared added pdb

--- src/domains/shared-app/02_namespace.tf
+++ src/domains/shared-app/02_namespace.tf
@@ -22,3 +22,19 @@ module "pod_identity" {
     kubernetes_namespace.namespace
   ]
 }
+
+resource "kubernetes_pod_disruption_budget_v1" "shared" {
+
+  for_each = var.pod_disruption_budgets
+
+  metadata {
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    name      = each.key
+  }
+  spec {
+    min_available = each.value.minAvailable
+    selector {
+      match_labels = each.value.matchLabels
+    }
+  }
+}
