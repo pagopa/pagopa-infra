f865117720c7caba9c6872b3f199bbb2b140dc98
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-10-30 22:35:45 +0100

    gps added pdb

--- src/domains/gps-app/02_namespace.tf
+++ src/domains/gps-app/02_namespace.tf
@@ -19,3 +19,18 @@ module "pod_identity" {
   secret_permissions = ["Get"]
 }
 
+resource "kubernetes_pod_disruption_budget_v1" "gps" {
+
+  for_each = var.pod_disruption_budgets
+
+  metadata {
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    name      = each.key
+  }
+  spec {
+    min_available = each.value.minAvailable
+    selector {
+      match_labels = each.value.matchLabels
+    }
+  }
+}
