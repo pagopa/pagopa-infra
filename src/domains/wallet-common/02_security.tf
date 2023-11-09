39e58156601539ccf694674dc37969f478ef2911
Gianluca Ciuffa <gianlucaciuffa@192.168.1.45>
2023-11-09 18:32:01 +0100

    static analisys fix

--- src/domains/wallet-common/02_security.tf
+++ src/domains/wallet-common/02_security.tf
@@ -129,6 +129,7 @@ resource "azurerm_key_vault_secret" "payment-method-api-key" {
 }
 
 resource "azurerm_key_vault_secret" "wallet-token-test-key" {
+  count        = var.env_short != "p" ? 1 : 0
   name         = "wallet-token-test-key"
   value        = "<TO UPDATE MANUALLY ON PORTAL>"
   key_vault_id = module.key_vault.id
