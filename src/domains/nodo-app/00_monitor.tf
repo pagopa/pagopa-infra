a041699f82394744a007d90f5dc42c43b5ec5e86
Angelo Caporaso <56113767+AngeloCaporaso@users.noreply.github.com>
2023-11-15 15:26:22 +0100

    chore(nodo-alert): Update fault-code alert (#1490)
    Co-authored-by: pasqualespica <36746022+pasqualespica@users.noreply.github.com>

--- src/domains/nodo-app/00_monitor.tf
+++ src/domains/nodo-app/00_monitor.tf
@@ -28,6 +28,8 @@ data "azurerm_monitor_action_group" "email" {
 }
 
 data "azurerm_monitor_action_group" "opsgenie" {
+  count = var.env_short == "p" ? 1 : 0
+
   resource_group_name = var.monitor_resource_group_name
   name                = local.monitor_action_group_opsgenie_name
 }
