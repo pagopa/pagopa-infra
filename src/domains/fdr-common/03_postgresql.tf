4be5bf4e966deb2aba04844564a31df084e44b87
Marco Mari <130982006+mamari90@users.noreply.github.com>
2023-11-15 17:04:05 +0100

    feat: DIS-40 DIS-39 (#1501)

--- src/domains/fdr-common/03_postgresql.tf
+++ src/domains/fdr-common/03_postgresql.tf
@@ -45,7 +45,7 @@ module "postgres_flexible_server_fdr" {
   private_dns_zone_id         = var.env_short != "d" ? data.azurerm_private_dns_zone.postgres[0].id : null
   delegated_subnet_id         = var.env_short != "d" ? module.postgres_flexible_snet.id : null
   high_availability_enabled   = var.pgres_flex_params.pgres_flex_ha_enabled
-  standby_availability_zone   = var.env_short != "d" ? var.pgres_flex_params.zone : null
+  standby_availability_zone   = var.env_short != "d" ? var.pgres_flex_params.standby_zone : null
   pgbouncer_enabled           = var.pgres_flex_params.pgres_flex_pgbouncer_enabled
   diagnostic_settings_enabled = var.pgres_flex_params.pgres_flex_diagnostic_settings_enabled
   administrator_login         = data.azurerm_key_vault_secret.pgres_flex_admin_login.value
@@ -58,6 +58,7 @@ module "postgres_flexible_server_fdr" {
   backup_retention_days        = var.pgres_flex_params.backup_retention_days
   geo_redundant_backup_enabled = var.pgres_flex_params.geo_redundant_backup_enabled
   create_mode                  = var.pgres_flex_params.create_mode
+
 
   log_analytics_workspace_id = var.env_short != "d" ? data.azurerm_log_analytics_workspace.log_analytics.id : null
   custom_metric_alerts       = var.custom_metric_alerts
