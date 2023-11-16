40b0ae8e0230b39faae045347d04c427168ac80a
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-11-16 14:13:52 +0100

    restore wallet-token-test-key

--- src/domains/ecommerce-common/02_security.tf
+++ src/domains/ecommerce-common/02_security.tf
@@ -405,6 +405,19 @@ resource "azurerm_key_vault_secret" "nodo_nodeforpm_api_key" {
   }
 }
 
+resource "azurerm_key_vault_secret" "wallet-token-test-key" {
+  count        = var.env_short != "p" ? 1 : 0
+  name         = "wallet-token-test-key"
+  value        = "<TO UPDATE MANUALLY ON PORTAL>"
+  key_vault_id = module.key_vault.id
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
 resource "azurerm_key_vault_secret" "wallet-token-test-creditcard-key" {
   count        = var.env_short != "p" ? 1 : 0
   name         = "wallet-token-test-creditcard-key"
