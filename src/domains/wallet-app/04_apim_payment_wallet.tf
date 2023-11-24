681fb131d7e3a6cddfbbb73c6e987cdf25cb6b30
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-11-24 12:20:40 +0100

    add policy to evaluate wallet token to obtain user-id for get wallets

--- src/domains/wallet-app/04_apim_payment_wallet.tf
+++ src/domains/wallet-app/04_apim_payment_wallet.tf
@@ -76,13 +76,9 @@ resource "azurerm_api_management_api_operation_policy" "get_wallets_for_user" {
   api_management_name = local.pagopa_apim_name
   operation_id        = "getWalletsByIdUser"
 
-  xml_content = templatefile("./api/payment-wallet/v1/get_wallets_by_user.xml.tpl", {
-    ecommerce-basepath = local.ecommerce_hostname
-  })
-
   xml_content = var.payment_wallet_with_pm_enabled ? templatefile("./api/payment-wallet/v1/_get_wallets_by_user_with_pm.xml.tpl", {
     ecommerce-basepath = local.ecommerce_hostname
-  }) : file("./api/payment-wallet/v1/_get_wallets_by_user.xml.tpl")
+  }) : templatefile("./api/payment-wallet/v1/_get_wallets_by_user.xml.tpl", {pdv_api_base_path = var.pdv_api_base_path, io_backend_base_path = var.io_backend_base_path})
 
 }
 
