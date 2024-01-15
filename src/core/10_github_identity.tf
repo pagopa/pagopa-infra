6fecfb93ac9210b4d2df45148e7ee1fbc55f6d64
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-01-15 17:51:49 +0100

    feat: Add cd identity

--- src/core/10_github_identity.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "identity_rg" {
-  name     = "${var.prefix}-${var.env_short}-identity-rg"
-  location = var.location
-
-  tags = var.tags
-}
--- src/domains/gps-common/10_github_identity.tf
+++ src/domains/gps-common/10_github_identity.tf
@@ -2,19 +2,50 @@ data "azurerm_resource_group" "identity_rg" {
   name = "${local.product}-identity-rg"
 }
 
-module "identity_cd" {
-  source = "github.com/pagopa/terraform-azurerm-v3//github_federated_identity?ref=v7.28.0"
+# repos must be lower than 20 items
+locals {
+  repos_01 = [
+    "pagopa-gpd-upload",
+    "pagopa-gpd-upload-function"
+  ]
 
+  federations_01 = [
+    for repo in local.repos_01 : {
+      repository = repo
+      subject    = var.env
+    }
+  ]
+
+  environment_cd_roles = {
+    subscription = [
+      "Reader"
+    ]
+    resource_groups = {
+      "${local.product}-${var.domain}-sec-rg" = [
+        "Key Vault Reader"
+      ],
+      "${local.product}-${var.location_short}-${var.env}-aks-rg" = [
+        "Contributor"
+      ]
+    }
+  }
+}
+
+# create a module for each 20 repos
+module "identity_cd_01" {
+  source = "github.com/pagopa/terraform-azurerm-v3//github_federated_identity?ref=v7.45.0"
+  # pagopa-<ENV><DOMAIN>-<COUNTER>-github-<PERMS>-identity
   prefix    = var.prefix
   env_short = var.env_short
+  domain    = "${var.domain}-01"
 
   identity_role = "cd"
 
-  github_federations = var.cd_github_federations
+  github_federations = local.federations_01
 
   cd_rbac_roles = {
-    subscription_roles = var.environment_cd_roles.subscription
-    resource_groups    = var.environment_cd_roles.resource_groups
+    subscription_roles = local.environment_cd_roles.subscription
+    resource_groups    = local.environment_cd_roles.resource_groups
   }
 
   tags = var.tags
