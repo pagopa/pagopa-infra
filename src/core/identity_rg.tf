979c9661a7378f38a9f2efffc5a74371038126b0
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-01-16 17:05:41 +0100

    commented lock

--- src/core/identity_rg.tf
+++ src/core/identity_rg.tf
@@ -6,9 +6,9 @@ resource "azurerm_resource_group" "managed_identities_rg" {
 }
 
 # to avoid to delete the resource group
-resource "azurerm_management_lock" "managed_identities_rg_lock" {
-  name       = "identity-rg-lock"
-  scope      = azurerm_resource_group.managed_identities_rg.id
-  lock_level = "CanNotDelete"
-  notes      = "Items can't be deleted in this subscription!"
-}
+#resource "azurerm_management_lock" "managed_identities_rg_lock" {
+#  name       = "identity-rg-lock"
+#  scope      = azurerm_resource_group.managed_identities_rg.id
+#  lock_level = "CanNotDelete"
+#  notes      = "Items can't be deleted in this subscription!"
+#}
