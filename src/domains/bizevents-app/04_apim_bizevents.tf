d2500996a6ea30a467bdc4f9c4c50c5128e979d7
acialini <alessio.cialini@nttdata.com>
2024-01-18 16:49:42 +0100

    [PRDP-317] feat: Refactored api for distinct transaction apim management

--- src/domains/bizevents-app/04_apim_bizevents.tf
+++ src/domains/bizevents-app/04_apim_bizevents.tf
@@ -20,14 +20,39 @@ module "apim_bizevents_product" {
   policy_xml = file("./api_product/bizevents-service/_base_policy.xml")
 }
 
+module "apim_transactions_product" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v6.4.1"
+
+  product_id   = "bizevent-transactions"
+  display_name = "Biz Events Transaction Service"
+  description  = "Servizio per gestire API transazioni degli eventi di pagamento"
+
+  api_management_name = local.pagopa_apim_name
+  resource_group_name = local.pagopa_apim_rg
+
+  published             = true
+  subscription_required = true
+  approval_required     = true
+  subscriptions_limit   = 1000
+
+  policy_xml = file("./api_product/transaction-service/_base_policy.xml")
+}
+
 #################
-##    API Biz Events  ##
+##    API Biz Events & Transaction ##
 #################
 locals {
   apim_bizevents_service_api = {
     display_name          = "Biz Events Service"
     description           = "API to handle biz events payments"
     path                  = "bizevents/service"
+    subscription_required = true
+    service_url           = null
+  },
+  apim_transaction_service_api = {
+    display_name          = "Biz Events Transaction Service"
+    description           = "API to handle biz events transactions"
+    path                  = "bizevents/tx-service"
     subscription_required = true
     service_url           = null
   }
@@ -39,6 +64,15 @@ resource "azurerm_api_management_api_version_set" "api_bizevents_api" {
   resource_group_name = local.pagopa_apim_rg
   api_management_name = local.pagopa_apim_name
   display_name        = local.apim_bizevents_service_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+resource "azurerm_api_management_api_version_set" "api_bizevents_transactions_api" {
+
+  name                = format("%s-bizevents-transaction-service-api", var.env_short)
+  resource_group_name = local.pagopa_apim_rg
+  api_management_name = local.pagopa_apim_name
+  display_name        = local.apim_transaction_service_api.display_name
   versioning_scheme   = "Segment"
 }
 
@@ -66,6 +100,33 @@ module "apim_api_bizevents_api_v1" {
   })
 
   xml_content = templatefile("./api/bizevents-service/v1/_base_policy.xml", {
+    hostname = local.bizevents_hostname
+  })
+}
+
+module "apim_api_bizevents_transactions_api_v1" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.4.1"
+
+  name                  = format("%s-bizevents-transaction-service-api", local.project)
+  api_management_name   = local.pagopa_apim_name
+  resource_group_name   = local.pagopa_apim_rg
+  product_ids           = [module.apim_transactions_product.product_id]
+  subscription_required = local.apim_transaction_service_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.api_bizevents_transactions_api.id
+  api_version           = "v1"
+
+  description  = local.apim_transaction_service_api.description
+  display_name = local.apim_transaction_service_api.display_name
+  path         = local.apim_transaction_service_api.path
+  protocols    = ["https"]
+  service_url  = local.apim_transaction_service_api.service_url
+
+  content_format = "openapi"
+  content_value = templatefile("./api/transaction-service/v1/_openapi.json.tpl", {
+    host = local.apim_hostname
+  })
+
+  xml_content = templatefile("./api/transaction-service/v1/_base_policy.xml", {
     hostname = local.bizevents_hostname
   })
 }
