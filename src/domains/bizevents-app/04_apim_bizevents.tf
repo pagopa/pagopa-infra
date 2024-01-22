68f99fcaf14892a69eb3b53ed0ee2b220b8e8d97
acialini <alessio.cialini@nttdata.com>
2024-01-22 10:16:23 +0100

    [PRDP-331] feat: Refactored api for distinct helpdesk apim management on bizevents

--- src/domains/bizevents-app/04_apim_bizevents.tf
+++ src/domains/bizevents-app/04_apim_bizevents.tf
@@ -20,6 +20,24 @@ module "apim_bizevents_product" {
   policy_xml = file("./api_product/bizevents-service/_base_policy.xml")
 }
 
+module "apim_bizevents_helpdesk_product" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v6.4.1"
+
+  product_id   = "bizevents-helpdesk"
+  display_name = "Biz Events Helpdesk"
+  description  = "Servizio per gestire helpdesk degli eventi di pagamento"
+
+  api_management_name = local.pagopa_apim_name
+  resource_group_name = local.pagopa_apim_rg
+
+  published             = true
+  subscription_required = true
+  approval_required     = true
+  subscriptions_limit   = 1000
+
+  policy_xml = file("./api_product/bizevents-helpdesk/_base_policy.xml")
+}
+
 module "apim_transactions_product" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v6.4.1"
 
@@ -49,6 +67,13 @@ locals {
     subscription_required = true
     service_url           = null
   },
+  apim_bizevents_helpdesk_api = {
+    display_name          = "Biz Events Helpdesk"
+    description           = "API for helpdesk on biz events payments"
+    path                  = "bizevents/helpdesk"
+    subscription_required = true
+    service_url           = null
+  },
   apim_transaction_service_api = {
     display_name          = "Biz Events Transaction Service"
     description           = "API to handle biz events transactions"
@@ -64,6 +89,15 @@ resource "azurerm_api_management_api_version_set" "api_bizevents_api" {
   resource_group_name = local.pagopa_apim_rg
   api_management_name = local.pagopa_apim_name
   display_name        = local.apim_bizevents_service_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+resource "azurerm_api_management_api_version_set" "api_bizevents_helpdesk_api" {
+
+  name                = format("%s-bizevents-helpdesk-api", var.env_short)
+  resource_group_name = local.pagopa_apim_rg
+  api_management_name = local.pagopa_apim_name
+  display_name        = local.apim_bizevents_helpdesk_api.display_name
   versioning_scheme   = "Segment"
 }
 
@@ -100,6 +134,33 @@ module "apim_api_bizevents_api_v1" {
   })
 
   xml_content = templatefile("./api/bizevents-service/v1/_base_policy.xml", {
+    hostname = local.bizevents_hostname
+  })
+}
+
+module "apim_api_bizevents_helpdesk_api_v1" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.4.1"
+
+  name                  = format("%s-bizevents-helpdesk-api", local.project)
+  api_management_name   = local.pagopa_apim_name
+  resource_group_name   = local.pagopa_apim_rg
+  product_ids           = [module.apim_api_bizevents_helpdesk_api_v1.product_id]
+  subscription_required = local.apim_bizevents_helpdesk_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.api_bizevents_helpdesk_api.id
+  api_version           = "v1"
+
+  description  = local.apim_bizevents_helpdesk_api.description
+  display_name = local.apim_bizevents_helpdesk_api.display_name
+  path         = local.apim_bizevents_helpdesk_api.path
+  protocols    = ["https"]
+  service_url  = local.apim_bizevents_helpdesk_api.service_url
+
+  content_format = "openapi"
+  content_value = templatefile("./api/bizevents-helpdesk/v1/_openapi.json.tpl", {
+    host = local.apim_hostname
+  })
+
+  xml_content = templatefile("./api/bizevents-helpdesk/v1/_base_policy.xml", {
     hostname = local.bizevents_hostname
   })
 }
