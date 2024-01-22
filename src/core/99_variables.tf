515d968c00144b4fda9046b5f02641b6af231b34
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2024-01-22 11:59:11 +0100

    feat: Increase kv access policy for admin group (#1664)

--- src/core/99_variables.tf
+++ src/core/99_variables.tf
@@ -1,3 +1,13 @@
+locals {
+  apim_x_node_product_id = "apim_for_node"
+
+  soap_basepath_nodo_postgres_pagopa = "nodo"
+
+  azdo_managed_identity_name = "${var.env}-pagopa"
+  azdo_managed_identity_rg_name = "pagopa-${var.env_short}-identity-rg"
+}
+
+
 variable "location" {
   type        = string
   description = "One of westeurope, northeurope"
