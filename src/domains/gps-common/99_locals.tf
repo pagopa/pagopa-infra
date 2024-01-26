0ac217107d259e09325c770c30bf020221448b26
Angelo Caporaso <56113767+AngeloCaporaso@users.noreply.github.com>
2024-01-26 17:11:35 +0100

    feat: Add GPD Upload service infra [PAGOPA-1360] (#1532)
    Co-authored-by: FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
    Co-authored-by: pasqualespica <36746022+pasqualespica@users.noreply.github.com>

--- src/domains/gps-common/99_locals.tf
+++ src/domains/gps-common/99_locals.tf
@@ -33,6 +33,9 @@ locals {
   cosmos_table_dns_zone_name                = "privatelink.table.cosmos.azure.com"
   cosmos_table_dns_zone_resource_group_name = "${local.product}-vnet-rg"
 
+  storage_dns_zone_name                = "privatelink.blob.core.windows.net"
+  storage_dns_zone_resource_group_name = "${local.product}-vnet-rg"
+
   aks_subnet_name = "${var.prefix}-${var.env_short}-${var.location_short}-${var.env}-aks-snet"
 
   gpd_hostname = var.env_short != "d" ? module.postgres_flexible_server_private[0].fqdn : module.postgresql[0].fqdn
