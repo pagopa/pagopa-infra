6079bf191ec2cf9851b95fa70933008ea6e3827b
Pietro Tota <pietro.tota@pagopa.it>
2024-01-29 14:14:06 +0100

    fix: product description

--- src/domains/ecommerce-app/04_apim_ecommerce_redirect_outcome.tf
+++ src/domains/ecommerce-app/04_apim_ecommerce_redirect_outcome.tf
@@ -17,7 +17,7 @@ module "apim_ecommerce_redirect_product" {
 
   product_id   = "ecommerce-redirect"
   display_name = "eCommerce for redirect pagoPA"
-  description  = "Ecommerce pagoPA product dedicated to redirect payment flow integration"
+  description  = "eCommerce pagoPA product dedicated to redirect payment flow integration"
 
   api_management_name = local.pagopa_apim_name
   resource_group_name = local.pagopa_apim_rg
