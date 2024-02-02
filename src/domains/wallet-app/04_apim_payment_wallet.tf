0a5032cdbbbd73678540b365b72abd4866bedfea
Simone Infante <simone.infante@pagopa.it>
2024-02-02 11:41:22 +0100

    feat: add nv to jwt check wallet

--- src/domains/wallet-app/04_apim_payment_wallet.tf
+++ src/domains/wallet-app/04_apim_payment_wallet.tf
@@ -141,6 +141,20 @@ locals {
   }
 }
 
+data "azurerm_key_vault_secret" "npg_notifications_jwt_secret_key" {
+  name         = "npg-notifications-jwt-secret-key"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+resource "azurerm_api_management_named_value" "wallet_npg_notifications_jwt_secret_key" {
+  name                = "wallet-npg-notifications-jwt-secret-key"
+  api_management_name = local.pagopa_apim_name
+  resource_group_name = local.pagopa_apim_rg
+  display_name        = "wallet-npg-notifications-jwt-secret-key"
+  value               = data.azurerm_key_vault_secret.npg_notifications_jwt_secret_key.value
+  secret              = true
+}
+
 # NPG notifications service APIs
 resource "azurerm_api_management_api_version_set" "npg_notifications_api" {
   name                = "${local.project}-npg-notifications-api"
