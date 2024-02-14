a9c7662734d49d901d12b1f6d932734003202b57
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-02-14 10:52:28 +0100

    receipts: added azdo kv permissions

--- src/domains/receipts-secrets/99_locals.tf
+++ src/domains/receipts-secrets/99_locals.tf
@@ -7,4 +7,7 @@ locals {
 
   subscription_name = "${var.env}-${var.prefix}"
 
+  azdo_managed_identity_rg_name = "pagopa-${var.env_short}-identity-rg"
+  azdo_iac_managed_identities = toset(["azdo-${var.env}-pagopa-iac-deploy", "azdo-${var.env}-pagopa-iac-plan"])
+
 }
