b5a11422daec29a13066bb3ac9b31eccb518994e
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-02-16 13:24:22 +0100

    fix dev, added uat and prod

--- src/domains/nodo-app/04_apim_standin_manager.tf
+++ src/domains/nodo-app/04_apim_standin_manager.tf
@@ -5,7 +5,7 @@ module "apim_standin_manager_product" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v6.20.0"
 
   product_id   = "stand-in-manager"
-  display_name = "API StandIn"
+  display_name = "API Stand-In Manager"
   description  = "Internal API for StandIn"
 
   api_management_name = local.pagopa_apim_name
@@ -19,20 +19,6 @@ module "apim_standin_manager_product" {
   policy_xml = file("./api_product/standin-manager/_base_policy.xml")
 }
 
-##############
-##  Group   ##
-##############
-resource "azurerm_api_management_group" "standin_manager_group" {
-  name                = "standin-manager-group"
-  resource_group_name = local.pagopa_apim_rg
-  api_management_name = local.pagopa_apim_name
-  display_name        = "API StandIn"
-  description         = "This is the API management group of StandIn Manager"
-}
-
-resource "azurerm_api_management_product_group" "standin_manager_group_association" {
-  product_id          = module.apim_standin_manager_product.product_id
-  group_name          = azurerm_api_management_group.standin_manager_group.name
-  api_management_name = local.pagopa_apim_name
-  resource_group_name = local.pagopa_apim_rg
-}
+############################
+## API defined in repository
+############################
