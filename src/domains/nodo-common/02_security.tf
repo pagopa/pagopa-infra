d071539f298f49d8ca5b11384ba91de82eb423bc
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-02-22 16:46:20 +0100

    fix: CFG for Nodo [sync and storage account] (#1745)
    Co-authored-by: Francesco Parisi <francesco.parisi@tasgroup.eu>
    Co-authored-by: Lorenzo Catalano <lorenzo.catalano@tasgroup.eu>

--- src/domains/nodo-common/02_security.tf
+++ src/domains/nodo-common/02_security.tf
@@ -2,3 +2,13 @@ data "azurerm_key_vault" "key_vault" {
   name                = "${local.product}-${var.domain}-kv"
   resource_group_name = "${local.product}-${var.domain}-sec-rg"
 }
+
+resource "azurerm_key_vault_secret" "node_cfg_sync_re_sa_connection_string" {
+  name         = "node-cfg-sync-re-sa-connection-string-key"
+  value        = module.nodo_cfg_sync_re_storage_account.primary_connection_string
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+
+  depends_on = [
+    module.nodo_cfg_sync_re_storage_account
+  ]
+}
