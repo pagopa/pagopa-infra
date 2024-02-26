1758e1853958c89f318c86a74c7b90dc0bd4f62f
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-02-26 11:03:02 +0100

    fix verifyko dev

--- src/domains/nodo-common/02_security.tf
+++ src/domains/nodo-common/02_security.tf
@@ -3,6 +3,9 @@ data "azurerm_key_vault" "key_vault" {
   resource_group_name = "${local.product}-${var.domain}-sec-rg"
 }
 
+/*****************
+Storage Account
+*****************/
 resource "azurerm_key_vault_secret" "node_cfg_sync_re_sa_connection_string" {
   name         = "node-cfg-sync-re-sa-connection-string-key"
   value        = module.nodo_cfg_sync_re_storage_account.primary_connection_string
@@ -16,6 +19,7 @@ resource "azurerm_key_vault_secret" "node_cfg_sync_re_sa_connection_string" {
 /**********
 Event Hub
 ***********/
+### cache
 resource "azurerm_key_vault_secret" "evthub_nodo_dei_pagamenti_cache_sync_rx" {
   name         = "nodo-dei-pagamenti-cache-sync-rx-connection-string-key"
   value        = data.azurerm_eventhub_authorization_rule.pagopa-weu-core-evh-ns04_nodo-dei-pagamenti-cache-sync-rx.primary_connection_string
@@ -24,6 +28,7 @@ resource "azurerm_key_vault_secret" "evthub_nodo_dei_pagamenti_cache_sync_rx" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
+### stand-in
 resource "azurerm_key_vault_secret" "evthub_nodo_dei_pagamenti_stand_in_tx" {
   name         = "nodo-dei-pagamenti-stand-in-tx-connection-string-key"
   value        = data.azurerm_eventhub_authorization_rule.pagopa-weu-core-evh-ns04_nodo-dei-pagamenti-stand-in-tx.primary_connection_string
@@ -35,6 +40,31 @@ resource "azurerm_key_vault_secret" "evthub_nodo_dei_pagamenti_stand_in_tx" {
 resource "azurerm_key_vault_secret" "evthub_nodo_dei_pagamenti_stand_in_sync_rx" {
   name         = "nodo-dei-pagamenti-stand-in-sync-rx-connection-string-key"
   value        = data.azurerm_eventhub_authorization_rule.pagopa-weu-core-evh-ns04_nodo-dei-pagamenti-stand-in-sync-rx.primary_connection_string
+  content_type = "text/plain"
+
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+### verify ko
+resource "azurerm_key_vault_secret" "evthub_nodo_dei_pagamenti_verify_ko_tx" {
+  name         = "ehub-verifyko-tx-connection-string"
+  value        = data.azurerm_eventhub_authorization_rule.pagopa-evh-ns01_nodo-dei-pagamenti-verify-ko-tx.primary_connection_string
+  content_type = "text/plain"
+
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "evthub_nodo_dei_pagamenti_verify_ko_datastore_rx" {
+  name         = "ehub-verifyko-datastore-rx-connection-string"
+  value        = data.azurerm_eventhub_authorization_rule.pagopa-evh-ns01_nodo-dei-pagamenti-verify-ko-datastore-rx.primary_connection_string
+  content_type = "text/plain"
+
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "evthub_nodo_dei_pagamenti_verify_ko_tablestorage_rx" {
+  name         = "ehub-verifyko-tablestorage-rx-connection-string"
+  value        = data.azurerm_eventhub_authorization_rule.pagopa-evh-ns01_nodo-dei-pagamenti-verify-ko-tablestorage-rx.primary_connection_string
   content_type = "text/plain"
 
   key_vault_id = data.azurerm_key_vault.key_vault.id
