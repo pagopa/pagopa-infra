1c4c4a4c1bbc22b38c1310190316efe28d292b06
Simone Infante <simone.infante@pagopa.it>
2024-02-29 15:54:03 +0100

    feat: add wollet token test for migration

--- src/domains/wallet-common/02_security.tf
+++ src/domains/wallet-common/02_security.tf
@@ -188,3 +188,15 @@ resource "azurerm_key_vault_secret" "npg_notifications_jwt_secret_key" {
   }
 }
 
+resource "azurerm_key_vault_secret" "wallet-token-test-key" {
+  count        = var.env_short != "p" ? 1 : 0
+  name         = "migration-wallet-token-test-key"
+  value        = "<TO UPDATE MANUALLY ON PORTAL>"
+  key_vault_id = module.key_vault.id
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
