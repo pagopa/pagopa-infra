c7943f14d4d4a6905ad8c74a96478fa5cde31ee4
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-03-04 16:52:57 +0100

    fix locals

--- src/core/00_network_next_core.tf
+++ src/core/00_network_next_core.tf
@@ -1,8 +1,8 @@
 data "azurerm_virtual_network" "vnet_ita" {
-  name                = local.vnet_itn_name
-  resource_group_name = local.vnet_itn_resource_group_name
+  name                = local.vnet_ita_name
+  resource_group_name = local.vnet_ita_resource_group_name
 }
 
 data "azurerm_resource_group" "rg_vnet_ita" {
-  name = local.vnet_itn_resource_group_name
+  name = local.vnet_ita_resource_group_name
 }
--- src/core/99_locals.tf
+++ src/core/99_locals.tf
@@ -14,7 +14,7 @@ locals {
   pagopa_apim_v2_name = "${local.project}-${var.location_short}-core-apim-v2"
   pagopa_apim_v2_rg   = "${local.project}-api-rg"
 
-  vnet_itn_name = "pagopa-${var.location_short}-itn-vnet"
-  vnet_itn_resource_group_name = "pagopa-${var.location_short}-itn-vnet-rg"
+  vnet_ita_name                = "pagopa-${var.env_short}-itn-vnet"
+  vnet_ita_resource_group_name = "pagopa-${var.env_short}-itn-vnet-rg"
 
 }
