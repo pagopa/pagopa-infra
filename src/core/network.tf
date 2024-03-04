f013a3445b6e41501be85f2bb0de0f7399459cda
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-03-04 10:38:47 +0100

    minor fix

--- src/core/network.tf
+++ src/core/network.tf
@@ -29,18 +29,6 @@ module "vnet_integration" {
   tags = var.tags
 }
 
-# vnet integration
-module "vnet_integration" {
-  source               = "git::https://github.com/pagopa/azurerm.git//virtual_network?ref=v4.18.1"
-  name                 = "${local.project}-vnet-integration"
-  location             = azurerm_resource_group.rg_vnet.location
-  resource_group_name  = azurerm_resource_group.rg_vnet.name
-  address_space        = var.cidr_vnet_integration
-  ddos_protection_plan = var.ddos_protection_plan
-
-  tags = var.tags
-}
-
 #
 # Peerings
 #
