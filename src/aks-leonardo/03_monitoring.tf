b656994669cbe6bbec67ef013590b0e88f6b1b8d
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-03-07 11:54:32 +0100

    added monitoring prometheus

--- src/aks-leonardo/03_monitoring.tf
+++ src/aks-leonardo/03_monitoring.tf
@@ -5,29 +5,8 @@ resource "kubernetes_namespace" "monitoring" {
 }
 
 module "aks_prometheus_install" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_prometheus_install?ref=v7.20.0"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_prometheus_install?ref=prometheus-update"
 
   prometheus_namespace = kubernetes_namespace.monitoring.metadata[0].name
-  storage_class_name   = "default-zrs" #example of ZRS storage class created by kubernetes_storage_class
-}
-
-resource "helm_release" "monitoring_reloader" {
-  name       = "reloader"
-  repository = "https://stakater.github.io/stakater-charts"
-  chart      = "reloader"
-  version    = var.reloader_helm.chart_version
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
-
-  set {
-    name  = "reloader.watchGlobally"
-    value = "false"
-  }
-  set {
-    name  = "reloader.deployment.image.name"
-    value = var.reloader_helm.image_name
-  }
-  set {
-    name  = "reloader.deployment.image.tag"
-    value = var.reloader_helm.image_tag
-  }
+  storage_class_name   = "default-zrs"
 }
