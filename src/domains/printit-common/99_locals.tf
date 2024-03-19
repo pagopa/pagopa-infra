9a61786a1fd9b18798934eb9031fc806d109a427
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2024-03-19 12:54:38 +0100

    feat: Init printit domain (#1808)

---
+++ src/domains/printit-common/99_locals.tf
@@ -0,0 +1,20 @@
+locals {
+  project = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
+  product = "${var.prefix}-${var.env_short}"
+
+  monitor_appinsights_name        = "${local.product}-appinsights"
+  monitor_action_group_slack_name = "SlackPagoPA"
+  monitor_action_group_email_name = "PagoPA"
+
+  vnet_name                = "${local.product}-vnet"
+  vnet_resource_group_name = "${local.product}-vnet-rg"
+
+  internal_dns_zone_name                = "${var.dns_zone_internal_prefix}.${var.external_domain}"
+  internal_dns_zone_resource_group_name = "${local.product}-vnet-rg"
+
+  azdo_managed_identity_rg_name = "pagopa-${var.env_short}-identity-rg"
+  azdo_iac_managed_identities = toset(
+    ["azdo-${var.env}-pagopa-iac-deploy",
+    "azdo-${var.env}-pagopa-iac-plan"]
+  )
+}
