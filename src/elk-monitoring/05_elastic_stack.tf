a433dc4a1a29359a1e52dbedd033160e3df84d5c
Alessio Cialini <63233981+alessio-cialini@users.noreply.github.com>
2024-03-25 12:41:25 +0100

    [VAS-869] feat: Introduce Payments Pull APIM and Kibana configs (#1842)
    Co-authored-by: Jacopo <jacopo.carlini@gmail.com>
    Co-authored-by: Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>

--- src/elk-monitoring/05_elastic_stack.tf
+++ src/elk-monitoring/05_elastic_stack.tf
@@ -98,8 +98,8 @@ module "elastic_stack" {
     /* bizevents */ "pagopabizeventsdatastore-microservice-chart", "pagopabizeventsservice-microservice-chart", "pagopanegativebizeventsdatastore-microservice-chart",
     /* apiconfig */ "pagopaapiconfig-postgresql", "pagopaapiconfig-oracle", "apiconfig-selfcare-integration-microservice-chart", "cache-oracle", "cache-postgresql", "cache-replica-oracle", "cache-replica-postgresql",
     /* ecommerce */ "pagopaecommerceeventdispatcherservice-microservice-chart", "pagopaecommercepaymentmethodsservice-microservice-chart", "pagopaecommercepaymentrequestsservice-microservice-chart", "pagopaecommercetransactionsservice-microservice-chart", "pagopaecommercetxschedulerservice-microservice-chart", "pagopanotificationsservice-microservice-chart",
-    /* selfcare */ "pagopaselfcaremsbackofficebackend-microservice-chart",
-    /* gps */ "gpd-core-microservice-chart", "pagopagpdpayments-microservice-chart", "pagopareportingorgsenrollment-microservice-chart", "pagopaspontaneouspayments-microservice-chart"
+    /* selfcare */ "pagopaselfcaremsbackofficebackend-microservice-chart", "backoffice-external",
+    /* gps */ "gpd-core-microservice-chart", "pagopagpdpayments-microservice-chart", "pagopareportingorgsenrollment-microservice-chart", "pagopaspontaneouspayments-microservice-chart", "gpd-payments-pull"
 
   ]
 
