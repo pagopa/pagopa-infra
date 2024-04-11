dc1b1f967a29174e3063a73c6ba9d478bdd9fa7c
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-04-11 12:32:55 +0200

    added alert

--- src/domains/nodo-app/00_alert_node_cfg_sync.tf
+++ src/domains/nodo-app/00_alert_node_cfg_sync.tf
@@ -33,6 +33,19 @@ locals {
       frequency   = 30
       time_window = 1440
     },
+    genericProblem               = {
+      query       = <<-QUERY
+          traces
+          | where cloud_RoleName == "%s"
+          | where message contains "[api-config-cache][ALERT] Generic Error"
+          | order by timestamp desc
+          | summarize Total=count() by length=bin(timestamp,1m)
+          | order by length desc
+          QUERY
+      severity    = 0
+      frequency   = 5
+      time_window = 5
+    },
   }
 }
 
@@ -82,6 +95,31 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "node_cfg_sync_trigger_pr
   time_window = local.pagopa_node_cfg_sync.dumpTrigger.time_window
   trigger {
     operator  = "LessThan"
+    threshold = 1
+  }
+}
+
+# Generic error
+resource "azurerm_monitor_scheduled_query_rules_alert" "node_cfg_sync_generic_problem" {
+  name                = format("%s-%s", local.pagopa_node_cfg_sync.name, "cache-generic-problem")
+  resource_group_name = data.azurerm_resource_group.node_cfg_sync_rg.name
+  location            = var.location
+
+  action {
+    action_group           = var.env_short != "d" ? [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.opsgenie[0].id] : [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
+    email_subject          = "[${var.env}] Generic problem regarding cache dumping"
+    custom_webhook_payload = "{}"
+  }
+  data_source_id = data.azurerm_application_insights.application_insights.id
+  description    = "[${var.env}] Generic problem regarding cache dumping"
+  enabled        = true
+  query          = format(local.pagopa_node_cfg_sync.dumpProblem.query, local.pagopa_node_cfg_sync.name)
+
+  severity    = local.pagopa_node_cfg_sync.genericProblem.severity
+  frequency   = local.pagopa_node_cfg_sync.genericProblem.frequency
+  time_window = local.pagopa_node_cfg_sync.genericProblem.time_window
+  trigger {
+    operator  = "GreaterThanOrEqual"
     threshold = 1
   }
 }
