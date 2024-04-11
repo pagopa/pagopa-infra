17a3646d0d9c40f824966c0d42cf88923d6f377a
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-04-11 14:33:32 +0200

    [PAGOPA-1668] feat: gps-donation federated identity

--- src/domains/gps-common/10_github_identity.tf
+++ src/domains/gps-common/10_github_identity.tf
@@ -12,7 +12,8 @@ locals {
   repos_01 = [
     "pagopa-gpd-upload",
     "pagopa-gpd-upload-function",
-    "pagopa-gpd-payments-pull"
+    "pagopa-gpd-payments-pull",
+    "pagopa-gps-donation-service"
   ]
 
   federations_01 = [
