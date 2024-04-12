591e7f1b7824e4727a066c55fc4b133c9f5ef56c
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-04-12 16:29:11 +0200

    [NOD-790] fix: moved SOPS secrets as key vault secret resources

--- src/domains/nodo-common/02_security.tf
+++ src/domains/nodo-common/02_security.tf
@@ -9,11 +9,20 @@ data "azurerm_redis_cache" "redis_cache" {
 }
 
 data "azurerm_redis_cache" "redis_cache_ha" {
-  count = var.redis_ha_enabled ? 1 : 0
+  count               = var.redis_ha_enabled ? 1 : 0
   name                = format("%s-%s-%s-redis", var.prefix, var.env_short, var.location_short)
   resource_group_name = format("%s-%s-data-rg", var.prefix, var.env_short)
 }
 
+data "azurerm_cosmosdb_account" "bizevents_datastore_cosmosdb_account" {
+  name                = format("%s-%s-%s-bizevents-ds-cosmos-account", var.prefix, var.env_short, var.location_short)
+  resource_group_name = format("%s-%s-%s-bizevents-rg", var.prefix, var.env_short, var.location_short)
+}
+
+data "azurerm_cosmosdb_account" "bizevents_neg_datastore_cosmosdb_account" {
+  name                = format("%s-%s-%s-bizevents-neg-ds-cosmos-account", var.prefix, var.env_short, var.location_short)
+  resource_group_name = format("%s-%s-%s-bizevents-rg", var.prefix, var.env_short, var.location_short)
+}
 
 
 /*****************
@@ -110,6 +119,14 @@ resource "azurerm_key_vault_secret" "verifyko_datastore_primary_key" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
+resource "azurerm_key_vault_secret" "cosmos_verifyko_account_key" {
+  name         = "cosmos-verifyko-account-key"
+  value        = module.cosmosdb_account_nodo_verifyko.secondary_key
+  content_type = "text/plain"
+
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
 /*****************
 CosmosDB
 *****************/
@@ -125,6 +142,21 @@ resource "azurerm_key_vault_secret" "wisp_converter_cosmosdb_account_key" {
   ]
 }
 
+resource "azurerm_key_vault_secret" "cosmos_neg_biz_account_key" {
+  name         = "cosmos-neg-biz-account-key"
+  value        = data.azurerm_cosmosdb_account.bizevents_datastore_cosmosdb_account.secondary_key
+  content_type = "text/plain"
+
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "cosmos_biz_account_key" {
+  name         = "cosmos-biz-account-key"
+  value        = data.azurerm_cosmosdb_account.bizevents_neg_datastore_cosmosdb_account.secondary_key
+  content_type = "text/plain"
+
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
 
 /*****************
 Redis
