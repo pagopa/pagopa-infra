db50b1a8e6553604325dafef330713e53d7e45ae
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-04-16 17:55:18 +0200

    minor fix

--- src/core-itn/00_network.tf
+++ src/core-itn/00_network.tf
@@ -1,3 +1,6 @@
+#
+# VNET
+#
 data "azurerm_virtual_network" "vnet_core" {
   name                = local.vnet_core_name
   resource_group_name = local.vnet_core_resource_group_name
@@ -7,7 +10,15 @@ data "azurerm_resource_group" "rg_vnet_core" {
   name = local.vnet_core_resource_group_name
 }
 
+#
+# Eventhub
+#
 data "azurerm_private_dns_zone" "eventhub" {
   name                = "privatelink.servicebus.windows.net"
   resource_group_name = local.msg_resource_group_name
 }
+
+data "azurerm_resource_group" "rg_event_private_dns_zone" {
+  name = local.msg_resource_group_name
+}
+
