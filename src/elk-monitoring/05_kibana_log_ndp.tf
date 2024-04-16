3c77cc8bfb2e15ffaff2c6ae1e5b214da4388a90
Francesco Parisi <francesco.parisi@tasgroup.eu>
2024-04-16 18:13:23 +0200

    NOD-821: [Dismissione WISP] ELK Monitoring wisp-converter

--- src/elk-monitoring/05_kibana_log_ndp.tf
+++ src/elk-monitoring/05_kibana_log_ndp.tf
@@ -164,6 +164,31 @@ locals {
   ndp_pagopawfespwfesp_data_view = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/data-view.json", {
     name = local.ndp_pagopawfespwfesp_key
   }), "\""), "\""), "'", "'\\''")
+
+  ## wispconverter
+  ndp_wispconverter_key = "wispconverter"
+
+  ndp_wispconverter_ingest_pipeline = replace(trimsuffix(trimprefix(file("${path.module}/log-template/ingest-pipeline.json"), "\""), "\""), "'", "'\\''")
+  ndp_wispconverter_ilm_policy = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/ilm-policy.json", {
+    name        = local.ndp_wispconverter_key,
+    managed     = false,
+    policy_name = local.default_snapshot_policy_key
+  }), "\""), "\""), "'", "'\\''")
+  ndp_wispconverter_component_template_package = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/component@package.json", {
+    name = local.ndp_wispconverter_key
+  }), "\""), "\""), "'", "'\\''")
+  ndp_wispconverter_component_template_custom = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/component@custom.json", {
+    name = local.ndp_wispconverter_key
+  }), "\""), "\""), "'", "'\\''")
+  ndp_wispconverter_index_template = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/index-template.json", {
+    name                       = local.ndp_wispconverter_key
+    component_template_package = "${local.ndp_wispconverter_key}@package"
+    component_template_custom  = "${local.ndp_wispconverter_key}@custom"
+  }), "\""), "\""), "'", "'\\''")
+  ndp_wispconverter_data_view = replace(trimsuffix(trimprefix(templatefile("${path.module}/log-template/data-view.json", {
+    name = local.ndp_wispconverter_key
+  }), "\""), "\""), "'", "'\\''")
+
 }
 
 ## space
@@ -909,6 +934,134 @@ resource "null_resource" "ndp_pagopawfespwfesp_component_template_custom" {
       -H 'kbn-xsrf: true' \
       -H 'Content-Type: application/json' \
       -d '${local.ndp_pagopawfespwfesp_component_template_custom}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+
+}
+
+resource "null_resource" "ndp_pagopawfespwfesp_index_template" {
+  depends_on = [null_resource.ndp_pagopawfespwfesp_component_template_custom]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_index_template/${local.ndp_pagopawfespwfesp_key}" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.ndp_pagopawfespwfesp_index_template}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "ndp_pagopawfespwfesp_data_stream_rollover" {
+  depends_on = [null_resource.ndp_pagopawfespwfesp_index_template]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X POST "${local.elastic_url}/logs-${local.ndp_pagopawfespwfesp_key}-default/_rollover/" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "ndp_pagopawfespwfesp_kibana_data_view" {
+  depends_on = [null_resource.ndp_kibana_space]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X POST "${local.kibana_url}/s/${local.ndp_space_name}/api/data_views/data_view" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.ndp_pagopawfespwfesp_data_view}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+
+## wispconverter
+resource "null_resource" "ndp_wispconverter_ingest_pipeline" {
+  depends_on = [null_resource.ndp_kibana_space]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_ingest/pipeline/${local.ndp_wispconverter_key}" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.ndp_wispconverter_ingest_pipeline}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "ndp_wispconverter_ilm_policy" {
+  depends_on = [null_resource.ndp_wispconverter_ingest_pipeline, null_resource.snapshot_policy]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_ilm/policy/${local.ndp_wispconverter_key}" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.ndp_wispconverter_ilm_policy}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "ndp_wispconverter_component_template_package" {
+  depends_on = [null_resource.ndp_wispconverter_ilm_policy]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_component_template/${local.ndp_wispconverter_key}@package" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.ndp_wispconverter_component_template_package}'
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "ndp_wispconverter_component_template_custom" {
+  depends_on = [null_resource.ndp_wispconverter_component_template_package]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      curl -k -X PUT "${local.elastic_url}/_component_template/${local.ndp_wispconverter_key}@custom" \
+      -H 'kbn-xsrf: true' \
+      -H 'Content-Type: application/json' \
+      -d '${local.ndp_wispconverter_component_template_custom}'
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
