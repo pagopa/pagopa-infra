11592a88611dfb886978d25877a96be888255e52
Simone infante <52280205+infantesimone@users.noreply.github.com>
2024-04-23 17:23:50 +0200

    feat(storage): add storage for payment wallet (#1961)
    * feat: init common for pay wallet
    
    * feat: init app pay-wallet
    
    * fix: docs
    
    * fix: duplicat web test for dev
    
    * Update src/domains/pay-wallet-app/05_aks_middleware_tools.tf
    
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
    
    * Update src/domains/pay-wallet-app/99_locals.tf
    
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
    
    * feat: add cosmosdb
    
    * fix: hostname var
    
    * feat: add redis conf
    
    * feat: add storage
    
    * minor fixs
    
    * pre-commit fixs
    
    * pre-commit fixs
    
    * minor fixs
    
    * added data for dns privatelink dns
    
    * added subnets for all wallet products
    
    * added features flags for resoruces
    
    * fix subnets cirds and added subnet
    
    * added placeholders for pay wallet subnets
    
    * fix cirds values
    
    * upgrated azurerm
    
    * updated all modules to the last version
    
    * pre-commit fixs
    
    * pre-commit fixs
    
    * pre-commit fixs
    
    ---------
    
    Co-authored-by: diegoitaliait <diego.lagosmorales@pagopa.it>
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
    Co-authored-by: Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>

--- src/domains/pay-wallet-common/01_network.tf
+++ src/domains/pay-wallet-common/01_network.tf
@@ -1,17 +1,3 @@
-data "azurerm_resource_group" "rg_vnet" {
-  name = local.vnet_resource_group_name
-}
-
-data "azurerm_virtual_network" "vnet" {
-  name                = local.vnet_name
-  resource_group_name = data.azurerm_resource_group.rg_vnet.name
-}
-
-data "azurerm_private_dns_zone" "internal" {
-  name                = local.internal_dns_zone_name
-  resource_group_name = local.internal_dns_zone_resource_group_name
-}
-
 resource "azurerm_private_dns_a_record" "ingress" {
   name                = local.ingress_hostname
   zone_name           = data.azurerm_private_dns_zone.internal.name
@@ -19,3 +5,48 @@ resource "azurerm_private_dns_a_record" "ingress" {
   ttl                 = 3600
   records             = [var.ingress_load_balancer_ip]
 }
+
+#
+# Subnets
+#
+
+module "cosmosdb_pay_wallet_snet" {
+  source               = "git::https://github.com/pagopa/terraform-azurerm-v3.git//subnet?ref=v8.5.0"
+  name                 = "${local.project}-cosmosb-snet"
+  address_prefixes     = var.cidr_subnet_cosmosdb_pay_wallet
+  resource_group_name  = local.vnet_italy_resource_group_name
+  virtual_network_name = local.vnet_italy_name
+
+  private_endpoint_network_policies_enabled = true
+
+  service_endpoints = [
+    "Microsoft.Web",
+    "Microsoft.AzureCosmosDB",
+  ]
+}
+
+module "redis_pagopa_pay_wallet_snet" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//subnet?ref=v8.5.0"
+
+  name                                      = "${local.project}-redis-snet"
+  address_prefixes                          = var.cidr_subnet_redis_pay_wallet
+  resource_group_name                       = local.vnet_italy_resource_group_name
+  virtual_network_name                      = local.vnet_italy_name
+  private_endpoint_network_policies_enabled = true
+}
+
+module "storage_pay_wallet_snet" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//subnet?ref=v8.5.0"
+
+  name                 = "${local.project}-storage-snet"
+  address_prefixes     = var.cidr_subnet_storage_pay_wallet
+  resource_group_name  = local.vnet_italy_resource_group_name
+  virtual_network_name = local.vnet_italy_name
+
+  private_endpoint_network_policies_enabled = true
+
+  service_endpoints = [
+    "Microsoft.Storage",
+  ]
+}
+
