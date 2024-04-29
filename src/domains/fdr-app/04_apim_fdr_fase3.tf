dfcdde3ad739280841bab823e9c0043cb5a0d57e
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-04-29 18:22:08 +0200

    fix api name

--- src/domains/fdr-app/04_apim_fdr_fase3.tf
+++ src/domains/fdr-app/04_apim_fdr_fase3.tf
@@ -54,7 +54,7 @@ module "apim_fdr_product_internal" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v6.3.0"
 
   product_id   = "fdr_internal"
-  display_name = "FDR Fase 3 - Flussi di rendicontazione (INTERNAL)"
+  display_name = "FDR - Flussi di rendicontazione (INTERNAL)"
   description  = "Manage FDR (INTERNAL) ( aka \"Flussi di Rendicontazione\" ) exchanged between PSP and EC"
 
   api_management_name = local.pagopa_apim_name
@@ -91,7 +91,7 @@ locals {
   }
 
   apim_fdr_service_api_internal = {
-    display_name          = "FDR - Flussi di rendicontazione (INTERNAL)"
+    display_name          = "FDR Fase 3 - Flussi di rendicontazione (INTERNAL)"
     description           = "FDR - Flussi di rendicontazione (INTERNAL)"
     path                  = "fdr-internal/service"
     subscription_required = true
