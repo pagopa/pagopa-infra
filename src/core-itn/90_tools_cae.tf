96572fcb8296542c38709972e82bc5c789e3cf84
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-05-06 12:47:01 +0200

    pre-commit fixs

--- src/core-itn/90_tools_cae.tf
+++ src/core-itn/90_tools_cae.tf
@@ -10,10 +10,10 @@ resource "azurerm_resource_group" "tools_rg" {
 resource "azurerm_container_app_environment" "tools_cae" {
   count = var.is_feature_enabled.container_app_tools_cae ? 1 : 0
 
-  name                       = "${local.project}-tools-cae"
+  name = "${local.project}-tools-cae"
 
-  location                   = azurerm_resource_group.tools_rg[0].location
-  resource_group_name        = azurerm_resource_group.tools_rg[0].name
+  location            = azurerm_resource_group.tools_rg[0].location
+  resource_group_name = azurerm_resource_group.tools_rg[0].name
 
   log_analytics_workspace_id = data.azurerm_log_analytics_workspace.log_analytics.id
   infrastructure_subnet_id   = azurerm_subnet.subnet_container_app_tools.id
