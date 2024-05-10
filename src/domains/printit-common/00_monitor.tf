c3886c9d49bc742ee7b277c37b32d2c9c5749e9e
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-05-10 12:18:41 +0200

    printit-common fix monitoring in italy

--- src/domains/printit-common/00_monitor.tf
+++ src/domains/printit-common/00_monitor.tf
@@ -15,20 +15,20 @@ data "azurerm_application_insights" "application_insights_italy" {
   resource_group_name = data.azurerm_resource_group.monitor_italy_rg.name
 }
 
-### 🇪🇺
-data "azurerm_resource_group" "monitor_rg" {
-  name = var.monitor_resource_group_name
-}
-
-data "azurerm_log_analytics_workspace" "log_analytics" {
-  name                = var.log_analytics_workspace_name
-  resource_group_name = var.log_analytics_workspace_resource_group_name
-}
-
-data "azurerm_application_insights" "application_insights" {
-  name                = local.monitor_appinsights_name
-  resource_group_name = data.azurerm_resource_group.monitor_rg.name
-}
+# ### 🇪🇺
+# data "azurerm_resource_group" "monitor_rg" {
+#   name = var.monitor_resource_group_name
+# }
+#
+# data "azurerm_log_analytics_workspace" "log_analytics" {
+#   name                = var.log_analytics_workspace_name
+#   resource_group_name = var.log_analytics_workspace_resource_group_name
+# }
+#
+# data "azurerm_application_insights" "application_insights" {
+#   name                = local.monitor_appinsights_name
+#   resource_group_name = data.azurerm_resource_group.monitor_rg.name
+# }
 
 #
 # Action Groups
