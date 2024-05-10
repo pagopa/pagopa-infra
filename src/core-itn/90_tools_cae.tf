77912a4fc4c8dabed1d320af4cb450f6969ad9c6
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2024-05-10 14:33:29 +0200

    feat: KV in italy + monitoring in italy for cae (#2010)
    * minor fixs
    
    * added kv in core italy
    
    * added action group in italy
    
    * changed container app env to monitor in italy
    
    * pre-commit fixs

--- src/core-itn/90_tools_cae.tf
+++ src/core-itn/90_tools_cae.tf
@@ -15,6 +15,6 @@ resource "azurerm_container_app_environment" "tools_cae" {
   location            = azurerm_resource_group.tools_rg[0].location
   resource_group_name = azurerm_resource_group.tools_rg[0].name
 
-  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.log_analytics.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
   infrastructure_subnet_id   = azurerm_subnet.subnet_container_app_tools.id
 }
