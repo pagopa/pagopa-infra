35338958b173987c1c9194afc32731ec325f8929
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-05-21 11:01:07 +0200

    [NOD-906] feat: add pagopa-mdb repository for GH identity

--- src/domains/nodo-common/10_github_identity.tf
+++ src/domains/nodo-common/10_github_identity.tf
@@ -17,7 +17,8 @@ locals {
     "pagopa-wisp-soap-converter",
     "pagopa-wisp-converter",
     "pagopa-wisp-converter-technical-support",
-    "pagopa-node-technical-support-worker"
+    "pagopa-node-technical-support-worker",
+    "pagopa-mdb"
   ]
 
   federations_01 = [
