20acfc8608501071bc162010851ac328e9a0be06
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-05-27 17:54:18 +0200

    updated dns aks vnet linking

--- src/aks-leonardo/03_aks_dns_private_link.tf
+++ src/aks-leonardo/03_aks_dns_private_link.tf
@@ -14,7 +14,12 @@ data "external" "get_dns_zone" {
   ]
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_private_link_vs_vnet_core" {
+moved {
+  from = azurerm_private_dns_zone_virtual_network_link.aks_dns_private_link_vs_vnet_core
+  to   = azurerm_private_dns_zone_virtual_network_link.aks_dns_private_link_vs_vnet_ita
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_private_link_vs_vnet_ita" {
   count = var.aks_private_cluster_enabled ? 1 : 0
 
   name                  = data.azurerm_virtual_network.vnet_ita.name
@@ -39,47 +44,5 @@ resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_private_link_v
   depends_on = [
     module.aks_leonardo,
     data.external.get_dns_zone
-  ]
-}
-
-
-# linking aks network to core vnet
-resource "null_resource" "create_vnet_core_aks_link" {
-
-  count = var.aks_enabled && var.aks_private_cluster_enabled ? 1 : 0
-  triggers = {
-    cluster_name = module.aks_leonardo.name
-    vnet_id      = data.azurerm_virtual_network.vnet_core.id
-    vnet_name    = data.azurerm_virtual_network.vnet_core.name
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-       dns_zone_name=$(az network private-dns zone list --output tsv --query "[?contains(id,'${self.triggers.cluster_name}')].{name:name}")
-       dns_zone_resource_group_name=$(az network private-dns zone list --output tsv --query "[?contains(id,'${self.triggers.cluster_name}')].{resourceGroup:resourceGroup}")
-       az network private-dns link vnet create \
-         --name ${self.triggers.vnet_name} \
-         --registration-enabled false \
-         --resource-group $dns_zone_resource_group_name \
-         --virtual-network ${self.triggers.vnet_id} \
-         --zone-name $dns_zone_name
-     EOT
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<EOT
-       dns_zone_name=$(az network private-dns zone list --output tsv --query "[?contains(id,'${self.triggers.cluster_name}')].{name:name}")
-       dns_zone_resource_group_name=$(az network private-dns zone list --output tsv --query "[?contains(id,'${self.triggers.cluster_name}')].{resourceGroup:resourceGroup}")
-       az network private-dns link vnet delete \
-         --name ${self.triggers.vnet_name} \
-         --resource-group $dns_zone_resource_group_name \
-         --zone-name $dns_zone_name \
-         --yes
-     EOT
-  }
-
-  depends_on = [
-    module.aks_leonardo
   ]
 }
