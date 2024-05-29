2a9d319292891492cf0abe6e0cc31fdac1f0350f
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-05-29 17:11:54 +0200

    updated modules

--- src/domains/printit-app/04_apim_notice_service.tf
+++ src/domains/printit-app/04_apim_notice_service.tf
@@ -7,7 +7,7 @@ locals {
 
 
 module "apim_notices_service_product" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v8.5.0"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v8.18.0"
   count  = var.is_feature_enabled.printit ? 1 : 0
 
   product_id   = "pagoa_notices_service"
