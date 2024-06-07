c0b6fb663690e9d407a4c09d335886af87e19335
Alessio Cialini <63233981+alessio-cialini@users.noreply.github.com>
2024-06-07 11:00:44 +0200

    feat: [VAS-976] Introduced notices subkey base to security (#2081)
    * [VAS-976] feat: Introduced notices subkey base to security
    
    * [VAS-976] feat: Updated security and subkey
    
    * Update src/domains/selfcare-app/05_subkey.tf
    
    * readme
    
    * fix
    
    * precommit
    
    ---------
    
    Co-authored-by: Jacopo Carlini <jacopo.carlini@gmail.com>

--- src/domains/printit-app/99_locals.tf
+++ src/domains/printit-app/99_locals.tf
@@ -1,7 +1,7 @@
 locals {
-  product          = "${var.prefix}-${var.env_short}"
+  product       = "${var.prefix}-${var.env_short}"
   project_short = "${var.prefix}-${var.env_short}-${var.domain}"
-  project          = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
+  project       = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
 
   project_core_itn = "${var.prefix}-${var.env_short}-${var.location_short}-core"
 
