eaad24bcecd20440d768ee1d41d664e0bf4c9fc9
acialini <alessio.cialini@nttdata.com>
2024-06-07 10:41:24 +0200

    feat: add payment notices flag

--- src/domains/selfcare-common/04_app_configuration_feature.tf
+++ src/domains/selfcare-common/04_app_configuration_feature.tf
@@ -145,6 +145,21 @@ resource "azurerm_app_configuration_feature" "payments_receipts_flag" {
   }
 }
 
+resource "azurerm_app_configuration_feature" "payments_notices_flag" {
+  configuration_store_id = azurerm_app_configuration.selfcare_appconf.id
+  description            = "It enables the payments notices' page"
+  name                   = "payments-notices"
+  enabled                = false
+
+  lifecycle {
+    ignore_changes = [
+      enabled,
+      targeting_filter,
+      timewindow_filter
+    ]
+  }
+}
+
 resource "azurerm_app_configuration_feature" "test_stations_flag" {
   configuration_store_id = azurerm_app_configuration.selfcare_appconf.id
   description            = "It enables the station testing"
