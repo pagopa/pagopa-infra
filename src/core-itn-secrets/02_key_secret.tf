95344d14126d5d2d8aee1628c7840ea171d0ca3f
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-06-11 11:32:00 +0200

    minor fix

--- src/core-itn-secrets/02_key_secret.tf
+++ src/core-itn-secrets/02_key_secret.tf
@@ -1,26 +1,25 @@
-data "external" "external" {
+data "external" "terrasops" {
   program = [
     "bash", "terrasops.sh"
   ]
   query = {
     env = "${var.location_short}-${var.env}"
   }
-
 }
 
 locals {
-  all_enc_secrets_value = can(data.external.external.result) ? flatten([
-    for k, v in data.external.external.result : {
-      valore = v
-      chiave = k
+  all_enc_secrets_value = can(data.external.terrasops.result) ? flatten([
+    for k, v in data.external.terrasops.result : {
+      value = v
+      key = k
     }
   ]) : []
 
   config_secret_data = jsondecode(file(var.input_file))
   all_config_secrets_value = flatten([
     for kc, vc in local.config_secret_data : {
-      valore = vc
-      chiave = kc
+      value = vc
+      key = kc
     }
   ])
 
@@ -31,16 +30,16 @@ locals {
 
 ## Upload all encrypted secrets
 resource "azurerm_key_vault_secret" "secret" {
-  for_each = { for i, v in local.all_secrets_value : local.all_secrets_value[i].chiave => i }
+  for_each = { for i, v in local.all_secrets_value : local.all_secrets_value[i].key => i }
 
   key_vault_id = data.azurerm_key_vault.kv.id
-  name         = local.all_secrets_value[each.value].chiave
-  value        = local.all_secrets_value[each.value].valore
+  name         = local.all_secrets_value[each.value].key
+  value        = local.all_secrets_value[each.value].value
 
   depends_on = [
     data.azurerm_key_vault.kv,
     azurerm_key_vault_key.generate_key_sops,
-    data.external.external,
+    data.external.terrasops,
   ]
 }
 
