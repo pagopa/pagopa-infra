0b5a39eaf3b332a01eebd9611c3fe80aa885af74
acialini <alessio.cialini@nttdata.com>
2024-06-14 14:57:26 +0200

    [VAS-1107] feat: updated 04_apim_notice_service.tf, README.md

--- src/domains/selfcare-app/05_subkey.tf
+++ src/domains/selfcare-app/05_subkey.tf
@@ -1,5 +1,5 @@
 data "azurerm_api_management_product" "apim_notices_product" {
-  product_id          = "pagoa_notices_service"
+  product_id          = "pagopa_notices_service_internal"
   api_management_name = local.pagopa_apim_name
   resource_group_name = local.pagopa_apim_rg
 }
