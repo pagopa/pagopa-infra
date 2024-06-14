74662e673b15d824762b3538116c212ae2d2b40b
mamari90 <130982006+mamari90@users.noreply.github.com>
2024-06-14 09:58:50 +0200

    added domains apimv2 alerts

---
+++ src/domains/fdr-app/00_alert_fdr_fase3_apimv2.tf
@@ -0,0 +1,116 @@
+data "azurerm_api_management" "apim_v2" {
+  name                = "${local.product}-weu-core-apim-v2"
+  resource_group_name = data.azurerm_resource_group.rg_api.name
+}
+
+# Exception FdR Fase3 - Internal APIs
+resource "azurerm_monitor_scheduled_query_rules_alert" "alert_fdr_internal_availability-v2" {
+  count               = var.env_short == "p" ? 1 : 0
+  resource_group_name = "dashboards"
+  name                = "fdr-internal-app-exception-v2"
+  location            = var.location
+
+  action {
+    action_group           = local.action_groups
+    email_subject          = "FdR Internal - Error"
+    custom_webhook_payload = "{}"
+  }
+
+  data_source_id = data.azurerm_api_management.apim_v2.id
+  description    = "Availability for /fdr-internal/service is less than or equal to 99% - [DASHBOARD LINK]"
+  enabled        = true
+  query = (<<-QUERY
+      let threshold = 0.99;
+      AzureDiagnostics
+      | where url_s matches regex "/fdr-internal/service/"
+      | summarize
+          Total=count(),
+          Success=count(responseCode_d < 500)
+          by bin(TimeGenerated, 5m)
+      | extend availability=toreal(Success) / Total
+      | where availability < threshold
+    QUERY
+  )
+  severity    = 3
+  frequency   = 15
+  time_window = 15
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 1
+  }
+}
+
+# Exception FdR Fase3 - PSP APIs
+resource "azurerm_monitor_scheduled_query_rules_alert" "alert_fdr_psp_availability-v2" {
+  count               = var.env_short == "p" ? 1 : 0
+  resource_group_name = "dashboards"
+  name                = "fdr-psp-app-exception-v2"
+  location            = var.location
+
+  action {
+    action_group           = local.action_groups
+    email_subject          = "FdR PSP - Error"
+    custom_webhook_payload = "{}"
+  }
+
+  data_source_id = data.azurerm_api_management.apim_v2.id
+  description    = "Availability for /fdr-psp/service is less than or equal to 99% - [DASHBOARD LINK]"
+  enabled        = true
+  query = (<<-QUERY
+      let threshold = 0.99;
+      AzureDiagnostics
+      | where url_s matches regex "/fdr-psp/service/"
+      | summarize
+          Total=count(),
+          Success=count(responseCode_d < 500)
+          by bin(TimeGenerated, 5m)
+      | extend availability=toreal(Success) / Total
+      | where availability < threshold
+    QUERY
+  )
+  severity    = 3
+  frequency   = 15
+  time_window = 15
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 1
+  }
+}
+
+# Exception FdR Fase3 - Organizations APIs
+resource "azurerm_monitor_scheduled_query_rules_alert" "alert_fdr_org_availability-v2" {
+  count               = var.env_short == "p" ? 1 : 0
+  resource_group_name = "dashboards"
+  name                = "fdr-org-app-exception-v2"
+  location            = var.location
+
+  action {
+    action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.opsgenie[0].id]
+    email_subject          = "FdR Orgs - Error"
+    custom_webhook_payload = "{}"
+  }
+
+  data_source_id = data.azurerm_api_management.apim_v2.id
+  description    = "Availability for /fdr-org/service is less than or equal to 99% - [DASHBOARD LINK]"
+  enabled        = true
+  query = (<<-QUERY
+      let threshold = 0.99;
+      AzureDiagnostics
+      | where url_s matches regex "/fdr-org/service/"
+      | summarize
+          Total=count(),
+          Success=count(responseCode_d < 500)
+          by bin(TimeGenerated, 5m)
+      | extend availability=toreal(Success) / Total
+      | where availability < threshold
+    QUERY
+  )
+  severity    = 3
+  frequency   = 15
+  time_window = 15
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 1
+  }
+}
+
