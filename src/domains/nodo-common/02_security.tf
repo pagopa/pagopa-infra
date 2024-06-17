1765e0c33e2d9bf777d5450ea622186f776801f1
Andrea De Rinaldis <117269497+andrea-deri@users.noreply.github.com>
2024-06-17 10:11:34 +0200

    fix: [NOD-942] Removed unused paaInviaRT topic (#2093)
    [NOD-942] fix: removed paaInviaRT topic, no more used
    
    Co-authored-by: Francesco Cesareo <cesareo.francesco@gmail.com>

--- src/domains/nodo-common/02_security.tf
+++ src/domains/nodo-common/02_security.tf
@@ -111,25 +111,6 @@ resource "azurerm_key_vault_secret" "evthub_nodo_dei_pagamenti_verify_ko_tablest
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
-### dismissione WISP
-resource "azurerm_key_vault_secret" "evthub_dismissione_wisp_paaInviaRT_rx" {
-  count        = var.enable_wisp_converter ? 1 : 0
-  name         = "dismissione-wisp-paainviart-rx-connection-string"
-  value        = data.azurerm_eventhub_authorization_rule.pagopa-weu-core-evh-ns04_dismissione-wisp-paainviart-rx[0].primary_connection_string
-  content_type = "text/plain"
-
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "evthub_dismissione_wisp_paaInviaRT_tx" {
-  count        = var.enable_wisp_converter ? 1 : 0
-  name         = "dismissione-wisp-paainviart-tx-connection-string"
-  value        = data.azurerm_eventhub_authorization_rule.pagopa-weu-core-evh-ns04_dismissione-wisp-paainviart-tx[0].primary_connection_string
-  content_type = "text/plain"
-
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
-
 /*****************
 CosmosDB
 *****************/
