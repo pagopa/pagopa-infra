8a0091ef2250a31593efa7a0c1b1e268a02e618d
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-06-17 15:23:07 +0200

    [PAGOPA-1835] chore(WISP): rebase

--- src/domains/nodo-common/06_service_bus.tf
+++ src/domains/nodo-common/06_service_bus.tf
@@ -1,10 +1,10 @@
 resource "azurerm_servicebus_namespace" "wisp-converter-servicebus" {
-#   count = var.env_short == "d" ? 0 : 1
+  #   count = var.env_short == "d" ? 0 : 1
 
   name                = "${local.project}-servicebus"
   location            = azurerm_resource_group.wisp_converter_rg[0].location
   resource_group_name = azurerm_resource_group.wisp_converter_rg[0].name
-  sku                 = "Standard"
+  sku                 = "Basic"
 
   tags = var.tags
 
@@ -12,11 +12,26 @@ resource "azurerm_servicebus_namespace" "wisp-converter-servicebus" {
     azurerm_resource_group.wisp_converter_rg
   ]
 }
-
-data "azurerm_servicebus_queue" "paaInviaRT" {
-  name         = "wispconverterpaaInviaRTqueue"
+resource "azurerm_servicebus_namespace_authorization_rule" "res-2" {
+  listen       = true
+  manage       = true
+  name         = "RootManageSharedAccessKey"
   namespace_id = azurerm_servicebus_namespace.wisp-converter-servicebus.id
+  send         = true
+  depends_on = [
+    azurerm_servicebus_namespace.wisp-converter-servicebus
+  ]
+}
+resource "azurerm_servicebus_namespace_network_rule_set" "res-3" {
+  namespace_id = azurerm_servicebus_namespace.wisp-converter-servicebus.id
+  depends_on = [
+    azurerm_servicebus_namespace.wisp-converter-servicebus,
+  ]
+}
 
+resource "azurerm_servicebus_queue" "paainviartqueue" {
+  name         = "paainviart"
+  namespace_id = azurerm_servicebus_namespace.wisp-converter-servicebus.id
   depends_on = [
     azurerm_servicebus_namespace.wisp-converter-servicebus
   ]
