009ab7f895130feb61c392dd2905d97e9b9bd530
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-06-18 16:01:13 +0200

    fix policy and backend url wisp converter

--- src/domains/nodo-app/04_apim_wisp_converter_01_product.tf
+++ src/domains/nodo-app/04_apim_wisp_converter_01_product.tf
@@ -84,5 +84,5 @@ resource "azurerm_api_management_named_value" "wisp_dismantling_converter_base_u
   api_management_name = local.pagopa_apim_name
   resource_group_name = local.pagopa_apim_rg
   display_name        = "wisp-dismantling-converter-base-url"
-  value               = "https://${local.nodo_hostname}/wisp-converter"
+  value               = "https://${local.nodo_hostname}/pagopa-wisp-converter"
 }
