98e256d5f66205eb566f94cbe3917a4ababd51f6
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-06-23 20:19:09 +0200

    chore: [PAGOPA-1856] Inbound and Outbound policy sendPaymentResult (#2189)
    * send payment result
    
    * improvements

--- src/core/apim_nodo_services_auth_02_product_wisp.tf
+++ src/core/apim_nodo_services_auth_02_product_wisp.tf
@@ -78,7 +78,7 @@ resource "azurerm_api_management_api_operation_policy" "sendpaymentoutcome_v2_wi
   operation_id        = "addUserReceipt"
 
   #tfsec:ignore:GEN005
-  xml_content = file("./api/nodopagamenti_api/wisp/wisp-sendpaymentresult-outbound.xml")
+  xml_content = file("./api/nodopagamenti_api/wisp/wisp-sendpaymentresult.xml")
 }
 
 resource "azurerm_api_management_api_version_set" "nodo_per_pm_api_ndp_wisp" {
@@ -195,22 +195,3 @@ resource "azapi_resource" "decoupler_activate_outbound_wisp" {
   }
 }
 
-resource "azapi_resource" "wisp_dismantling_sendrt" {
-
-  type      = "Microsoft.ApiManagement/service/policyFragments@2022-04-01-preview"
-  name      = "wisp-dismantling-sendrt"
-#  parent_id = module.apim[0].id
-  parent_id = data.azurerm_api_management.apim_migrated[0].id
-
-  body = jsonencode({
-    properties = {
-      description = "Sending RT request if required for WISP dismantling"
-      format      = "rawxml"
-      value       = file("./api_product/nodo_pagamenti_api/wisp_decoupler/wisp-dismantling-sendrt.xml")
-    }
-  })
-
-  lifecycle {
-    ignore_changes = [output]
-  }
-}
