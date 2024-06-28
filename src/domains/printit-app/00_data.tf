d21f070bbd3bdd0e6b06867a0cfe3dde04b5da76
Jacopo <jacopo.carlini@gmail.com>
2024-06-28 12:11:35 +0200

    fix eh printit

--- src/domains/printit-app/00_data.tf
+++ src/domains/printit-app/00_data.tf
@@ -1,20 +1,20 @@
 data "azurerm_eventhub_authorization_rule" "notices_evt_authorization_rule" {
   name                = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-notice-evt-rx"
-  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-evenhub-rg"
+  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-evh-rg"
   eventhub_name       = "${var.prefix}-${var.domain}-evh"
-  namespace_name      = "${var.prefix}-${var.env_short}-${var.location_short}-core-evh-meucci"
+  namespace_name      = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-evh"
 }
 data "azurerm_eventhub_authorization_rule" "notices_evt_complete_authorization_rule" {
   name                = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-notice-evt-complete-rx"
-  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-evenhub-rg"
+  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-evh-rg"
   eventhub_name       = "${var.prefix}-${var.domain}-complete-evh"
-  namespace_name      = "${var.prefix}-${var.env_short}-${var.location_short}-core-evh-meucci"
+  namespace_name      = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-evh"
 }
 data "azurerm_eventhub_authorization_rule" "notices_evt_errors_authorization_rule" {
   name                = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-notice-evt-errors-rx"
-  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-evenhub-rg"
+  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-evh-rg"
   eventhub_name       = "${var.prefix}-${var.domain}-errors-evh"
-  namespace_name      = "${var.prefix}-${var.env_short}-${var.location_short}-core-evh-meucci"
+  namespace_name      = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-evh"
 }
 
 
