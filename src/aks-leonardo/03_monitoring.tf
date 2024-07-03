55f2376bcf8a4a050b01e6fced6b9f4dbdda01f5
acialini <alessio.cialini@nttdata.com>
2024-07-03 14:28:27 +0200

    [VAS-1046] feat: introducing elastic agent in aks monitoring

--- src/aks-leonardo/03_monitoring.tf
+++ src/aks-leonardo/03_monitoring.tf
@@ -10,3 +10,17 @@ module "aks_prometheus_install" {
   prometheus_namespace = kubernetes_namespace.monitoring.metadata[0].name
   storage_class_name   = "default-zrs"
 }
+
+module "elastic_agent" {
+
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//elastic_agent?ref=3ab498bdc16ee65c8d1d221a1fd64784ff6548c7"
+
+  eck_version = "2.9"
+
+  namespace      = kubernetes_namespace.monitoring.id
+
+  dedicated_log_instance_name = [
+    /* printit */ "print-payment-notice-service-microservice-chart", "print-payment-notice-generator-microservice-chart", "print-payment-notice-functions-microservice-chart"
+  ]
+
+}
