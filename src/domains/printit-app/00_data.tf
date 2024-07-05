d0cfb664200da4c0d63c3bdce6bb406c6ea758e4
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-07-05 16:18:36 +0200

    removed meucci datat

--- src/domains/printit-app/00_data.tf
+++ src/domains/printit-app/00_data.tf
@@ -1,23 +1,3 @@
-data "azurerm_eventhub_authorization_rule" "notices_evt_authorization_rule" {
-  name                = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-notice-evt-rx"
-  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-evenhub-rg"
-  eventhub_name       = "${var.prefix}-${var.domain}-evh"
-  namespace_name      = "${var.prefix}-${var.env_short}-${var.location_short}-core-evh-meucci"
-}
-data "azurerm_eventhub_authorization_rule" "notices_evt_complete_authorization_rule" {
-  name                = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-notice-evt-complete-rx"
-  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-evenhub-rg"
-  eventhub_name       = "${var.prefix}-${var.domain}-complete-evh"
-  namespace_name      = "${var.prefix}-${var.env_short}-${var.location_short}-core-evh-meucci"
-}
-data "azurerm_eventhub_authorization_rule" "notices_evt_errors_authorization_rule" {
-  name                = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-notice-evt-errors-rx"
-  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-evenhub-rg"
-  eventhub_name       = "${var.prefix}-${var.domain}-errors-evh"
-  namespace_name      = "${var.prefix}-${var.env_short}-${var.location_short}-core-evh-meucci"
-}
-
-
 data "azurerm_cosmosdb_account" "notices_cosmos_account" {
   name                = "pagopa-${var.env_short}-${var.location_short}-${var.domain}-cosmos-account"
   resource_group_name = "pagopa-${var.env_short}-${var.location_short}-${var.domain}-db-rg"
