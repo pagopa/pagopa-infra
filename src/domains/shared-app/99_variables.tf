f71829a6576e3458ae88d9115546d2fb59ee5763
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2024-07-16 12:56:22 +0200

    fix description

--- src/domains/shared-app/99_variables.tf
+++ src/domains/shared-app/99_variables.tf
@@ -332,6 +332,6 @@ variable "ecommerce_io_pm_enabled" {
 
 variable "ecommerce_for_io_pm_npg" {
   type        = string
-  description = "eCommerce for io gateway (3 values allowed: 'NPG','PM','FF')"
+  description = "eCommerce for io gateway (3 values allowed: 'NPG','PM','NPGFF')"
   default     = "PM"
 }
