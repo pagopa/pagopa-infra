e3dbbc4113521532970773045e9d65085cc05b14
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-07-22 09:29:57 +0200

    feat(nodo-app): Add app insight API alerts

--- src/domains/nodo-app/00_alert_wisp_dismantling.tf
+++ src/domains/nodo-app/00_alert_wisp_dismantling.tf
@@ -72,3 +72,35 @@ traces
     threshold = 1
   }
 }
+
+resource "azurerm_monitor_scheduled_query_rules_alert" "opex_pagopa-wisp-converter-ai-error" {
+  for_each = var.env_short == "p" ? toset(["receiptKo", "receiptOk", "createTimer", "deleteTimer"]) : []
+
+  resource_group_name = "dashboards"
+  name                = "pagopa-${var.env_short}-opex_pagopa-wisp-converter-${each.value}-error"
+  location            = var.location
+
+  action {
+    action_group           = local.action_groups
+    email_subject          = "Alert pagopa-wisp-converter-${each.value}-error"
+    custom_webhook_payload = "{}"
+  }
+
+  data_source_id = data.azurerm_application_insights.application_insights.id
+  description    = "Availability for wisp-converter API ${each.value} is less than or equal to 99% - https://portal.azure.com/?l=en.en-us#@pagopait.onmicrosoft.com/dashboard/arm/subscriptions/b9fc9419-6097-45fe-9f74-ba0641c91912/resourcegroups/dashboards/providers/microsoft.portal/dashboards/pagopa-p-opex_pagopa-wisp-converter"
+  enabled        = true
+  query = (<<-QUERY
+let threshold = 0.99;
+traces
+| where cloud_RoleName == "pagopawispconverter"
+| where message startswith "Failed API operation ${each.value}"
+  QUERY
+  )
+  severity    = 1
+  frequency   = 5
+  time_window = 5
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 1
+  }
+}
