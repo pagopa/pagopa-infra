579b943d33bc87e194cec19689cc8c24c2145e54
pasqualespica <pasqualespica@gmail.com>
2024-10-24 12:26:03 +0200

    fix

--- src/domains/afm-common/03_cosmosdb_afm.tf
+++ src/domains/afm-common/03_cosmosdb_afm.tf
@@ -22,41 +22,34 @@ module "afm_marketplace_cosmosdb_snet" {
 }
 
 module "afm_marketplace_cosmosdb_account" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cosmosdb_account?ref=v6.7.0"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cosmosdb_account?ref=add-analytical_storage_enabled-2-cosmos"
 
-  name     = "${local.project}-marketplace-cosmos-account"
-  location = var.location
-  domain   = var.domain
-
+  name                = "${local.project}-marketplace-cosmos-account"
+  location            = var.location
   resource_group_name = azurerm_resource_group.afm_rg.name
-  offer_type          = var.afm_marketplace_cosmos_db_params.offer_type
-  kind                = var.afm_marketplace_cosmos_db_params.kind
+  domain              = var.domain
 
-  public_network_access_enabled    = var.afm_marketplace_cosmos_db_params.public_network_access_enabled
-  main_geo_location_zone_redundant = var.afm_marketplace_cosmos_db_params.main_geo_location_zone_redundant
+  offer_type                 = var.afm_marketplace_cosmos_db_params.offer_type
+  kind                       = var.afm_marketplace_cosmos_db_params.kind
+  capabilities               = var.afm_marketplace_cosmos_db_params.capabilities
+  enable_free_tier           = var.afm_marketplace_cosmos_db_params.enable_free_tier
+  analytical_storage_enabled = var.afm_marketplace_cosmos_db_params.analytical_storage_enabled
 
-  enable_free_tier          = var.afm_marketplace_cosmos_db_params.enable_free_tier
-  enable_automatic_failover = true
-
-  capabilities       = var.afm_marketplace_cosmos_db_params.capabilities
-  consistency_policy = var.afm_marketplace_cosmos_db_params.consistency_policy
-
-  main_geo_location_location = var.location
-  additional_geo_locations   = var.afm_marketplace_cosmos_db_params.additional_geo_locations
-  backup_continuous_enabled  = var.afm_marketplace_cosmos_db_params.backup_continuous_enabled
-
-  is_virtual_network_filter_enabled = var.afm_marketplace_cosmos_db_params.is_virtual_network_filter_enabled
-
-  ip_range = ""
-
-  # add data.azurerm_subnet.<my_service>.id
+  public_network_access_enabled      = var.afm_marketplace_cosmos_db_params.public_network_access_enabled
+  private_endpoint_enabled           = var.afm_marketplace_cosmos_db_params.private_endpoint_enabled
+  subnet_id                          = module.afm_marketplace_cosmosdb_snet.id
+  private_dns_zone_sql_ids           = [data.azurerm_private_dns_zone.cosmos.id]
+  is_virtual_network_filter_enabled  = var.afm_marketplace_cosmos_db_params.is_virtual_network_filter_enabled
   allowed_virtual_network_subnet_ids = var.afm_marketplace_cosmos_db_params.public_network_access_enabled ? [] : [data.azurerm_subnet.aks_subnet.id, data.azurerm_subnet.apiconfig_subnet.id]
 
-  # private endpoint
-  private_endpoint_name    = "${local.project}-marketplace-cosmos-sql-endpoint"
-  private_endpoint_enabled = var.afm_marketplace_cosmos_db_params.private_endpoint_enabled
-  subnet_id                = module.afm_marketplace_cosmosdb_snet.id
-  private_dns_zone_ids     = [data.azurerm_private_dns_zone.cosmos.id]
+  consistency_policy               = var.afm_marketplace_cosmos_db_params.consistency_policy
+  main_geo_location_location       = var.location
+  main_geo_location_zone_redundant = var.afm_marketplace_cosmos_db_params.main_geo_location_zone_redundant
+  additional_geo_locations         = var.afm_marketplace_cosmos_db_params.additional_geo_locations
+
+  backup_continuous_enabled = var.afm_marketplace_cosmos_db_params.backup_continuous_enabled
+  enable_automatic_failover = true
+  ip_range                  = ""
 
   tags = var.tags
 }
