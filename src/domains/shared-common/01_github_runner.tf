1fa4df1a45b68021ca6bb423cdfd86a30a797555
mamari90 <130982006+mamari90@users.noreply.github.com>
2024-11-07 15:47:02 +0100

    test

--- src/domains/shared-common/01_github_runner.tf
+++ src/domains/shared-common/01_github_runner.tf
@@ -12,22 +12,22 @@ resource "azurerm_subnet" "github_runner_snet" {
   address_prefixes     = var.github_runner.subnet_address_prefixes
 }
 
-module "github_runner_environment" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//container_app_environment?ref=v8.53.0"
-
-  name                      = "${var.prefix}-${var.env_short}-${var.location_short}-github-runner-cae"
-  resource_group_name       = azurerm_resource_group.github_runner_rg.name
-  location                  = var.location
-  vnet_internal             = true
-  subnet_id                 = azurerm_subnet.github_runner_snet.id
-  log_destination           = "log-analytics"
-  log_analytics_customer_id = data.azurerm_log_analytics_workspace.log_analytics.workspace_id
-  log_analytics_shared_key  = data.azurerm_log_analytics_workspace.log_analytics.primary_shared_key
-
-  zone_redundant            = false
-
-  tags = var.tags
-}
+# module "github_runner_environment" {
+#   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//container_app_environment?ref=v8.53.0"
+#
+#   name                      = "${var.prefix}-${var.env_short}-${var.location_short}-github-runner-cae"
+#   resource_group_name       = azurerm_resource_group.github_runner_rg.name
+#   location                  = var.location
+#   vnet_internal             = true
+#   subnet_id                 = azurerm_subnet.github_runner_snet.id
+#   log_destination           = "log-analytics"
+#   log_analytics_customer_id = data.azurerm_log_analytics_workspace.log_analytics.workspace_id
+#   log_analytics_shared_key  = data.azurerm_log_analytics_workspace.log_analytics.primary_shared_key
+#
+#   zone_redundant            = false
+#
+#   tags = var.tags
+# }
 
 
 locals {
