06ce8effd955f35545d40ff174e0d47953d34c45
mamari90 <130982006+mamari90@users.noreply.github.com>
2024-11-13 16:33:48 +0100

    removed external data, upgraded aks module

--- src/aks-leonardo/03_aks_dns_private_link.tf
+++ src/aks-leonardo/03_aks_dns_private_link.tf
@@ -3,16 +3,6 @@
 # vnet needs a vnet link with aks private dns zone
 # aks terraform module doesn't export private dns zone
 
-data "external" "get_dns_zone" {
-  count      = var.aks_private_cluster_enabled ? 1 : 0
-  depends_on = [module.aks_leonardo]
-  program = ["bash", "-c", <<-EOH
-    dns_zone_name=$(az network private-dns zone list --output tsv --query "[?contains(id,'${module.aks_leonardo.name}')].{name:name}")
-    dns_zone_resource_group_name=$(az network private-dns zone list --output tsv --query "[?contains(id,'${module.aks_leonardo.name}')].{resourceGroup:resourceGroup}")
-    echo "{\"dns_zone_name\": \"$dns_zone_name\", \"dns_zone_resource_group_name\": \"$dns_zone_resource_group_name\"}"
-  EOH
-  ]
-}
 
 moved {
   from = azurerm_private_dns_zone_virtual_network_link.aks_dns_private_link_vs_vnet_core
@@ -22,7 +12,7 @@ moved {
 
 locals {
   # "pagopa-u-itn-uat-3arbio4v.e2d0397d-0af5-42b4-aaf1-e2e7956499a9.privatelink.italynorth.azmk8s.io"
-  aks_fqdn_levels = length(split(".", module.aks_leonardo.private_fqdn))
+  aks_fqdn_levels                   = length(split(".", module.aks_leonardo.private_fqdn))
   aks_managed_private_dns_zone_name = join(".", slice(split(".", module.aks_leonardo.private_fqdn), 1, local.aks_fqdn_levels))
 
 }
@@ -32,8 +22,8 @@ resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_private_link_v
 
   name                  = data.azurerm_virtual_network.vnet_ita.name
   virtual_network_id    = data.azurerm_virtual_network.vnet_ita.id
-  resource_group_name   = module.aks_leonardo.managed_resource_group_name    #external.get_dns_zone[0].result["dns_zone_resource_group_name"]
-  private_dns_zone_name = local.aks_managed_private_dns_zone_name #data.external.get_dns_zone[0].result["dns_zone_name"]
+  resource_group_name   = module.aks_leonardo.managed_resource_group_name #external.get_dns_zone[0].result["dns_zone_resource_group_name"]
+  private_dns_zone_name = local.aks_managed_private_dns_zone_name         #data.external.get_dns_zone[0].result["dns_zone_name"]
 
   depends_on = [
     module.aks_leonardo,
@@ -48,7 +38,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_private_link_v
   name                  = data.azurerm_virtual_network.vnet_core.name
   virtual_network_id    = data.azurerm_virtual_network.vnet_core.id
   resource_group_name   = module.aks_leonardo.managed_resource_group_name #data.external.get_dns_zone[0].result["dns_zone_resource_group_name"]
-  private_dns_zone_name = local.aks_managed_private_dns_zone_name #data.external.get_dns_zone[0].result["dns_zone_name"]
+  private_dns_zone_name = local.aks_managed_private_dns_zone_name         #data.external.get_dns_zone[0].result["dns_zone_name"]
 
   depends_on = [
     module.aks_leonardo,
