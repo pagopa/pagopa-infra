b6fc967148ff962d42d597a6c02db83e9f04dec2
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2024-11-14 14:20:28 +0100

    PAGOPA-2195 adding new file for new apis

---
+++ src/domains/aca-app/04_apim_gpd_for_aca.tf
@@ -0,0 +1,40 @@
+###############
+# GPD FOR ACA #
+###############
+
+## API ##
+
+resource "azurerm_api_management_api_version_set" "api_debt_positions_for_aca_api" {
+  name                = format("%s-debt-positions-for-aca-service-api", var.env_short)
+  api_management_name = local.pagopa_apim_name
+  resource_group_name = local.pagopa_apim_rg
+  display_name        = local.apim_debt_positions_for_aca_service_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+module "apim_api_debt_positions_for_aca_api_v1" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3//api_management_api?ref=v6.11.2"
+
+  name                = format("%s-debt-positions-for-aca-service-api", local.product)
+  api_management_name = local.pagopa_apim_name
+  resource_group_name = local.pagopa_apim_rg
+  product_ids         = [module.apim_aca_product.product_id]
+
+  subscription_required = local.apim_debt_positions_for_aca_service_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.api_debt_positions_for_aca_api.id
+  api_version           = "v1"
+
+  description  = local.apim_debt_positions_for_aca_service_api.description
+  display_name = local.apim_debt_positions_for_aca_service_api.display_name
+  path         = local.apim_debt_positions_for_aca_service_api.path
+  protocols    = ["https"]
+  service_url  = local.apim_debt_positions_for_aca_service_api.service_url
+
+  content_format = "openapi"
+  content_value = templatefile("./api/gpd_api/debt-position-for-aca-services/v1/_openapi.json.tpl", {
+    host    = local.apim_hostname
+    service = module.apim_aca_product.product_id
+  })
+
+  xml_content = file("./api/gpd_api/debt-position-for-aca-services/v1/_base_policy.xml")
+}
