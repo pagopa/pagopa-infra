44c285d0bbb848e642897d9580dbb48fda32c2bd
Marco Mari <130982006+mamari90@users.noreply.github.com>
2024-11-22 11:51:04 +0100

    feat: Paymcloud 172 fdr app (#2581)
    * upgraded tf provider fdr-app

--- src/domains/fdr-app/99_main.tf
+++ src/domains/fdr-app/99_main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.38.0"
+      version = "<= 3.116.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
@@ -20,7 +20,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "= 2.5.1"
+      version = "<= 2.12.1"
     }
   }
 
@@ -47,4 +47,9 @@ provider "helm" {
   kubernetes {
     config_path = "${var.k8s_kube_config_path_prefix}/config-${local.aks_name}"
   }
+}
+
+module "__v3__" {
+  # v8.59.0
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3?ref=3fc1dafaf4354e24ca8673005ec0caf4106343a3"
 }
