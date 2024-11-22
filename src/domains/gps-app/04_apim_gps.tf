7be83a2f7a532b4ad16bdc8717f5b6c07aae7a34
Marco Mari <130982006+mamari90@users.noreply.github.com>
2024-11-22 12:45:49 +0100

    feat: Upgraded gpsapp tf provider (#2583)
    upgraded gpsapp tf provider

--- src/domains/gps-app/04_apim_gps.tf
+++ src/domains/gps-app/04_apim_gps.tf
@@ -3,7 +3,7 @@
 ##############
 
 module "apim_gps_product" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v6.4.1"
+  source = "./.terraform/modules/__v3__/api_management_product"
 
   product_id   = "spontaneouspayments"
   display_name = "GPS pagoPA"
@@ -44,7 +44,7 @@ resource "azurerm_api_management_api_version_set" "api_gps_api" {
 
 
 module "apim_api_gps_api_v1" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.4.1"
+  source = "./.terraform/modules/__v3__/api_management_api"
 
   name                  = format("%s-spontaneous-payments-service-api", local.project)
   api_management_name   = local.pagopa_apim_name
