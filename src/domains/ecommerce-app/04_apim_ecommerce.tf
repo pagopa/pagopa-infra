b442ac571f5f60f020696d52497abec737494099
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2024-12-18 14:58:24 +0100

    Update src/domains/ecommerce-app/04_apim_ecommerce.tf
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>

--- src/domains/ecommerce-app/04_apim_ecommerce.tf
+++ src/domains/ecommerce-app/04_apim_ecommerce.tf
@@ -674,6 +674,6 @@ resource "azurerm_api_management_named_value" "ecommerce_dev_sendpaymentresult_s
   api_management_name = local.pagopa_apim_name
   resource_group_name = local.pagopa_apim_rg
   display_name        = "ecommerce-dev-sendpaymentresult-subscription-key-value"
-  value               = data.azurerm_key_vault_secret.ecommerce_dev_sendpaymentresult_subscription_key.value
+  value               = data.azurerm_key_vault_secret.ecommerce_dev_sendpaymentresult_subscription_key[0].value
   secret              = true
 }
