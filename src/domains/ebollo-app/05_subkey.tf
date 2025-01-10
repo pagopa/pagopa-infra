ae63a06448c3b388eba1bf75ab5934183cdff22d
Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>
2025-01-10 14:38:52 +0100

    feat: Add-subkey-gpd-4-mbd-integ-test (#2710)
    add-subkey-gpd-4-mbd-integ-test

--- src/domains/ebollo-app/05_subkey.tf
+++ src/domains/ebollo-app/05_subkey.tf
@@ -44,3 +44,14 @@ resource "azurerm_api_management_subscription" "mbd_service_integration_test_sub
   allow_tracing       = false
   state               = "active"
 }
+
+
+resource "azurerm_api_management_subscription" "gdp_debt_positions_productintegration_test_subkey" {
+  count               = var.env_short != "p" ? 1 : 0
+  api_management_name = data.azurerm_api_management.apim.name
+  resource_group_name = data.azurerm_api_management.apim.resource_group_name
+  product_id          = data.azurerm_api_management_product.apim_gdp_debt_positions_product.id
+  display_name        = "Subscription MBD Service for Integration Test to GDP"
+  allow_tracing       = false
+  state               = "active"
+}
