0830a1b8d06594e114d1bb109d5c7cb715d468f0
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2025-01-17 13:27:33 +0100

    fix: [CHK-3593] Parametric percentage threshold for real time processing queue alert (#2725)
    * use percentage threshold in place of fixed value for high traffic
    
    * set percentage threshold for real time processing queue
    
    * fix description
    
    ---------
    
    Co-authored-by: Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>

--- src/domains/ecommerce-common/03_storage.tf
+++ src/domains/ecommerce-common/03_storage.tf
@@ -338,7 +338,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "ecommerce_transient_enqu
     custom_webhook_payload = "{}"
   }
   data_source_id = module.ecommerce_storage_transient.id
-  description    = format("Enqueuing rate for queue %s > ${each.value.threshold} during last ${each.value.time_window} minutes", replace("${each.value.queue_key}", "-", " "))
+  description    = format("Enqueuing rate for queue %s > 1 percent threshold of 'PutMessage' (at least 10) during last ${each.value.time_window} minutes", replace("${each.value.queue_key}", "-", " "))
   enabled        = true
   query = format(<<-QUERY
     let OpCountForQueue = (operation: string, queueKey: string) {
@@ -365,7 +365,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "ecommerce_transient_enqu
         | project PutCount, DeleteCount, Diff
     };
     MessageRateForQueue("%s")
-    | where Diff > ${each.value.threshold}
+    | where Diff > max_of(PutCount/100, ${each.value.threshold})
     QUERY
     , "/${module.ecommerce_storage_transient.name}/${local.project}-${each.value.queue_key}"
   )
