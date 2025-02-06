14933a453483c89f852dbdbdf3835412b61c7248
Andrea Ferracci <andrea.ferracci@gmail.com>
2025-02-06 15:59:30 +0100

    [PQ-346] gpd-subkey-creation

--- src/domains/gps-app/05_subkey.tf
+++ src/domains/gps-app/05_subkey.tf
@@ -15,3 +15,13 @@ resource "azurerm_api_management_subscription" "gps_spontaneous_payments_service
   allow_tracing       = false
   state               = "active"
 }
+
+#### QA subkey for integration testing
+resource "azurerm_api_management_subscription" "gpd_integration_qa_subkey" {
+  api_management_name = data.azurerm_api_management.apim.name
+  resource_group_name = data.azurerm_api_management.apim.resource_group_name
+  product_id          = data.azurerm_api_management_product.apim_gpd_integration_product.id
+  display_name        = "QA-GPD"
+  allow_tracing       = false
+  state               = "active"
+}
