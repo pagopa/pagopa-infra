123e83ee706fc2294688cd307318fa061e295c91
sfarralorenzo <lorenzo.sfarra@pagopa.it>
2025-02-10 11:32:57 +0100

    feat: adding namespace and certificate related logic for for the checkout domain

---
+++ src/domains/checkout-app/02_namespace.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_namespace" "namespace" {
+  metadata {
+    name = var.domain
+  }
+}
+
+module "pod_identity" {
+  source = "./.terraform/modules/__v3__/kubernetes_pod_identity"
+
+  resource_group_name = local.aks_resource_group_name
+  location            = var.location
+  tenant_id           = data.azurerm_subscription.current.tenant_id
+  cluster_name        = local.aks_name
+
+  identity_name = "${kubernetes_namespace.namespace.metadata[0].name}-pod-identity" // TODO add env in name
+  namespace     = kubernetes_namespace.namespace.metadata[0].name
+  key_vault_id  = data.azurerm_key_vault.key_vault.id
+
+  secret_permissions = ["Get"]
+}
