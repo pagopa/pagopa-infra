08a5830ee3eb19a8bfe01294fc4bcf8564afe766
EmanueleBVtech <emanuele.serrao@bvtech.com>
2025-02-18 16:58:57 +0100

    Update src/domains/checkout-app/04_apim_checkout_feature_flags.tf
    Co-authored-by: Simone infante <52280205+infantesimone@users.noreply.github.com>

--- src/domains/checkout-app/04_apim_checkout_feature_flags.tf
+++ src/domains/checkout-app/04_apim_checkout_feature_flags.tf
@@ -36,7 +36,7 @@ resource "azurerm_api_management_api_version_set" "checkout_feature_flags_api_v1
 module "apim_checkout_feature_flags_v1" {
   source = "./.terraform/modules/__v3__/api_management_api"
 
-  name                  = "${local.parent_project}-feature-flags-api"
+  name                  = "${local.project}-feature-flags-api"
   api_management_name   = data.azurerm_api_management.apim.name
   resource_group_name   = data.azurerm_resource_group.rg_api.name
   product_ids           = [module.apim_checkout_featureflags.product_id]
