1ee2692c947cb0f9caa1ae9dd7903bc11fa45098
Pietro Tota <pietro.tota@pagopa.it>
2025-03-12 11:31:15 +0100

    feat: GPD for ACA availability alert

--- src/domains/aca-common/00_monitor.tf
+++ src/domains/aca-common/00_monitor.tf
@@ -6,3 +6,17 @@ data "azurerm_application_insights" "application_insights" {
   name                = local.monitor_appinsights_name
   resource_group_name = data.azurerm_resource_group.monitor_rg.name
 }
+
+
+#
+# Action Groups
+#
+data "azurerm_monitor_action_group" "slack" {
+  resource_group_name = data.azurerm_resource_group.monitor_rg.name
+  name                = local.monitor_action_group_slack_name
+}
+
+data "azurerm_monitor_action_group" "email" {
+  resource_group_name = data.azurerm_resource_group.monitor_rg.name
+  name                = local.monitor_action_group_email_name
+}
