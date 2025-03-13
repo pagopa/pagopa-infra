65c93f2d91cf41a4e8b046fd51af093beb90e813
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-03-13 14:57:51 +0100

    added storico db changelog

--- src/psql/nodo/liquibase/changelog_storico/admin/3.32.0/sql/dblink.sql
+++ src/psql/nodo/liquibase/changelog_storico/admin/3.32.0/sql/dblink.sql
@@ -1,3 +1,4 @@
+CREATE EXTENSION dblink;
 CREATE SERVER connSVInodo FOREIGN DATA WRAPPER dblink_fdw OPTIONS (host '${nodo-db-host}', port '${nodo-db-port}', dbname 'nodo', sslmode 'require');
 CREATE USER MAPPING FOR partition SERVER connSVInodo OPTIONS (user 'partition',password '${partition-password}');
 GRANT USAGE ON FOREIGN SERVER connSVInodo TO partition;
