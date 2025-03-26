fceb2cda2cca796e4f95878429a87779fa827e6d
pasqualespica <pasqualespica@gmail.com>
2025-03-26 18:09:22 +0100

    fix

--- src/domains/gps-app/06_aks_middleware_tools.tf
+++ src/domains/gps-app/06_aks_middleware_tools.tf
@@ -3,22 +3,22 @@
 module "tls_checker" {
   source = "./.terraform/modules/__v3__/tls_checker"
 
-  https_endpoint = local.gps_hostname
-  alert_name = local.gps_hostname
-  alert_enabled = true
-  helm_chart_present = true
-  namespace = kubernetes_namespace.namespace.metadata[0].name
-  location_string = var.location_string
+  https_endpoint                                            = local.gps_hostname
+  alert_name                                                = local.gps_hostname
+  alert_enabled                                             = true
+  helm_chart_present                                        = true
+  namespace                                                 = kubernetes_namespace.namespace.metadata[0].name
+  location_string                                           = var.location_string
   kv_secret_name_for_application_insights_connection_string = "ai-${var.env_short}-connection-string"
-  application_insights_resource_group = data.azurerm_resource_group.monitor_rg.name
-  application_insights_id = data.azurerm_application_insights.application_insights.id
-  application_insights_action_group_ids = [data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.email.id]
-  keyvault_name = data.azurerm_key_vault.kv.name
-  keyvault_tenant_id = data.azurerm_client_config.current.tenant_id
+  application_insights_resource_group                       = data.azurerm_resource_group.monitor_rg.name
+  application_insights_id                                   = data.azurerm_application_insights.application_insights.id
+  application_insights_action_group_ids                     = [data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.email.id]
+  keyvault_name                                             = data.azurerm_key_vault.kv.name
+  keyvault_tenant_id                                        = data.azurerm_client_config.current.tenant_id
 
-  workload_identity_enabled = true
+  workload_identity_enabled              = true
   workload_identity_service_account_name = module.workload_identity.workload_identity_service_account_name
-  workload_identity_client_id = module.workload_identity.workload_identity_client_id
+  workload_identity_client_id            = module.workload_identity.workload_identity_client_id
 
   depends_on = [module.workload_identity]
 }
@@ -29,14 +29,14 @@ module "tls_checker" {
 module "cert_mounter" {
   source = "./.terraform/modules/__v3__/cert_mounter"
 
-  namespace = var.domain
+  namespace        = var.domain
   certificate_name = replace(local.gps_hostname, ".", "-")
-  kv_name = data.azurerm_key_vault.kv.name
-  tenant_id = data.azurerm_subscription.current.tenant_id
+  kv_name          = data.azurerm_key_vault.kv.name
+  tenant_id        = data.azurerm_subscription.current.tenant_id
 
-  workload_identity_enabled = true
+  workload_identity_enabled              = true
   workload_identity_service_account_name = module.workload_identity.workload_identity_service_account_name
-  workload_identity_client_id = module.workload_identity.workload_identity_client_id
+  workload_identity_client_id            = module.workload_identity.workload_identity_client_id
 
   depends_on = [module.workload_identity]
 }
