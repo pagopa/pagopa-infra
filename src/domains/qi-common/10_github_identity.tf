06279398e43aa630b6b64e6f71c139f558e03134
pasqualespica <pasqualespica@gmail.com>
2025-04-01 10:05:31 +0200

    minor fix

--- src/domains/qi-common/10_github_identity.tf
+++ src/domains/qi-common/10_github_identity.tf
@@ -7,6 +7,11 @@ data "azurerm_kubernetes_cluster" "aks" {
   resource_group_name = "${local.product}-${var.location_short}-${var.instance}-aks-rg"
 }
 
+data "azurerm_key_vault" "key_vault" {
+  name                = "${local.product}-${var.domain}-kv"
+  resource_group_name = "${local.product}-${var.domain}-sec-rg"
+}
+
 # repos must be lower than 20 items
 locals {
   repos_01 = [
@@ -19,13 +24,6 @@ locals {
     for repo in local.repos_01 : {
       repository = repo
       subject    = var.env
-    }
-  ]
-
-  federations_01_oidc = [
-    for repo in local.repos_01 : {
-      repository = repo
-      subject    = "oidc"
     }
   ]
 
@@ -85,52 +83,22 @@ module "identity_cd_01" {
   ]
 }
 
-# create a module for each 20 repos
-module "identity_ci_01" {
-  count  = var.env_short == "p" ? 0 : 1
-  source = "github.com/pagopa/terraform-azurerm-v3//github_federated_identity"
-  # pagopa-<ENV><DOMAIN>-<COUNTER>-github-<PERMS>-identity
-  prefix    = var.prefix
-  env_short = var.env_short
-  domain    = "${var.domain}-01"
+resource "azurerm_key_vault_access_policy" "gha_iac_managed_identities" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.identity_cd_01.identity_principal_id
 
-  identity_role = "ci"
+  secret_permissions = ["Get", "List", "Set", ]
 
-  github_federations = local.federations_01
-
-  ci_rbac_roles = {
-    subscription_roles = local.environment_ci_roles.subscription
-    resource_groups    = local.environment_ci_roles.resource_groups
-  }
-
-  tags = var.tags
-
-  depends_on = [
-    data.azurerm_resource_group.identity_rg
+  certificate_permissions = [
+    "SetIssuers", "DeleteIssuers", "Purge", "List", "Get"
   ]
-}
-
-# create a module for each 20 repos
-module "identity_oidc_01" {
-  source    = "github.com/pagopa/terraform-azurerm-v3//github_federated_identity?ref=v8.36.1"
-  prefix    = var.prefix
-  env_short = var.env_short
-  domain    = "${var.domain}-01-oidc"
-
-  identity_role = "cd"
-
-  github_federations = local.federations_01_oidc
-
-  cd_rbac_roles = {
-    subscription_roles = local.environment_cd_roles.subscription
-    resource_groups    = local.environment_cd_roles.resource_groups
-  }
-
-  tags = var.tags
-
-  depends_on = [
-    data.azurerm_resource_group.identity_rg
+  key_permissions = [
+    "Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt",
+    "GetRotationPolicy"
   ]
+
+  storage_permissions = []
 }
 
 resource "null_resource" "github_runner_app_permissions_to_namespace_cd_01" {
@@ -163,40 +131,6 @@ resource "null_resource" "github_runner_app_permissions_to_namespace_cd_01" {
 
   depends_on = [
     module.identity_cd_01
-  ]
-}
-
-resource "null_resource" "github_runner_app_permissions_to_namespace_ci_01" {
-  count = var.env_short == "p" ? 0 : 1
-  triggers = {
-    aks_id               = data.azurerm_kubernetes_cluster.aks.id
-    service_principal_id = module.identity_ci_01[0].identity_client_id
-    namespace            = var.domain
-    version              = "v2"
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      az role assignment create --role "Azure Kubernetes Service RBAC Admin" \
-      --assignee ${self.triggers.service_principal_id} \
-      --scope ${self.triggers.aks_id}/namespaces/${self.triggers.namespace}
-
-      az role assignment list --role "Azure Kubernetes Service RBAC Admin"  \
-      --scope ${self.triggers.aks_id}/namespaces/${self.triggers.namespace}
-    EOT
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<EOT
-      az role assignment delete --role "Azure Kubernetes Service RBAC Admin" \
-      --assignee ${self.triggers.service_principal_id} \
-      --scope ${self.triggers.aks_id}/namespaces/${self.triggers.namespace}
-    EOT
-  }
-
-  depends_on = [
-    module.identity_ci_01
   ]
 }
 
