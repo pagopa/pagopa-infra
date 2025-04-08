b4a73539d3e5c3350ae1e26e9f7e5161b76a2a82
Francesco <cesareo.francesco@gmail.com>
2025-04-08 10:51:30 +0200

    clean code

--- src/domains/fdr-app/00_alert_fdr_xml_to_json.tf
+++ src/domains/fdr-app/00_alert_fdr_xml_to_json.tf
@@ -1,37 +1,3 @@
-# AppException during conversion todo
-# resource "azurerm_monitor_scheduled_query_rules_alert" "alert_fdr_xmltojson_appexception" {
-#   count               = (var.enable_fdr3_features && var.env_short == "p") ? 1 : 0
-#   name                = "${module.fdr_xml_to_json_function[0].name}-app-exception"
-#   resource_group_name = data.azurerm_resource_group.fdr_rg.name
-#   location            = var.location
-#
-#   action {
-#     action_group           = local.local.action_groups_default
-#     email_subject          = "AppException on FdR's XML-to-JSON conversion"
-#     custom_webhook_payload = "{}"
-#   }
-#   data_source_id = data.azurerm_application_insights.application_insights.id
-#   description    = "An AppException occurred during the conversion of FdR from XML to JSON"
-#   enabled        = true
-#   query = format(<<-QUERY
-#     traces
-#       | where cloud_RoleName == "%s"
-#       | where message contains "[ALERT][FdrXmlToJson]"
-#       | order by timestamp desc
-#       | summarize Total=count() by length=bin(timestamp,1m)
-#       | order by length desc
-#   QUERY
-#     , module.fdr_xml_to_json_function[0].name
-#   )
-#   severity    = 3
-#   frequency   = 15
-#   time_window = 15
-#   trigger {
-#     operator  = "GreaterThanOrEqual"
-#     threshold = 1
-#   }
-# }
-
 locals {
   fdr_xmltojson = {
     last_retry_query = format(<<-QUERY
