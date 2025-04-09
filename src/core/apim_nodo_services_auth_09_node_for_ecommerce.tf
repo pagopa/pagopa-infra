1f0d2e1a15bc09a607663983c1dc73ca6f84d289
Simone Infante <52280205+infantesimone@users.noreply.github.com>
2025-04-09 14:24:36 +0200

    fix: naming policy and policy file path

--- src/core/apim_nodo_services_auth_09_node_for_ecommerce.tf
+++ src/core/apim_nodo_services_auth_09_node_for_ecommerce.tf
@@ -38,22 +38,22 @@ module "apim_node_for_ecommerce_api_v1" {
   protocols    = ["https"]
 
   content_format = "openapi"
-  content_value = templatefile("./api/nodopagamenti_api/nodoPerEcommerce/v1/_openapi.json.tpl", {
+  content_value = templatefile("./api/nodopagamenti_api/nodeForEcommerce/v1/_openapi.json.tpl", {
     host    = local.api_domain
     service = module.apim_nodo_dei_pagamenti_product.product_id
   })
 
-  xml_content = templatefile("./api/nodopagamenti_api/nodoPerEcommerce/v1/_base_policy.xml.tpl", {
+  xml_content = templatefile("./api/nodopagamenti_api/nodeForEcommerce/v1/_base_policy.xml.tpl", {
     is-nodo-decoupler-enabled = var.apim_nodo_decoupler_enable
   })
 }
 
-resource "azurerm_api_management_api_operation_policy" "close_payment_api_v1" {
+resource "azurerm_api_management_api_operation_policy" "auth_close_payment_api_v1" {
   api_name            = "${local.project}-node-for-ecommerce-api-v1"
   api_management_name = data.azurerm_api_management.apim_migrated[0].name
   resource_group_name = data.azurerm_resource_group.rg_api.name
   operation_id        = "closePayment"
-  xml_content = templatefile("./api/nodopagamenti_api/nodoPerEcommerce/v1/_add_v1_policy.xml.tpl", {
+  xml_content = templatefile("./api/nodopagamenti_api/nodeForEcommerce/v1/_add_v1_policy.xml.tpl", {
     is-nodo-decoupler-enabled = var.apim_nodo_decoupler_enable
   })
 }
@@ -76,22 +76,22 @@ module "apim_node_for_ecommerce_api_v2" {
   protocols    = ["https"]
 
   content_format = "openapi"
-  content_value = templatefile("./api/nodopagamenti_api/nodoPerEcommerce/v2/_openapi.json.tpl", {
+  content_value = templatefile("./api/nodopagamenti_api/nodeForEcommerce/v2/_openapi.json.tpl", {
     host = local.api_domain
   })
 
-  xml_content = templatefile("./api/nodopagamenti_api/nodoPerEcommerce/v2/_base_policy.xml.tpl", {
+  xml_content = templatefile("./api/nodopagamenti_api/nodeForEcommerce/v2/_base_policy.xml.tpl", {
     is-nodo-decoupler-enabled = var.apim_nodo_decoupler_enable
   })
 }
 
 # WISP closePaymentV2
-resource "azurerm_api_management_api_operation_policy" "close_payment_api_v2_wisp_policy" {
+resource "azurerm_api_management_api_operation_policy" "auth_close_payment_api_v2_wisp_policy" {
   count               = var.create_wisp_converter ? 1 : 0
   api_name            = "${local.project}-node-for-ecommerce-api-v2"
   resource_group_name = data.azurerm_resource_group.rg_api.name
   api_management_name = data.azurerm_api_management.apim_migrated[0].name
   operation_id        = "closePaymentV2"
-  xml_content         = file("./api/nodopagamenti_api/nodoPerEcommerce/v2/wisp-closepayment.xml")
+  xml_content         = file("./api/nodopagamenti_api/nodeForEcommerce/v2/wisp-closepayment.xml")
 }
 
