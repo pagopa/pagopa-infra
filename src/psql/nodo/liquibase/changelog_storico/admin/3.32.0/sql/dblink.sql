32775e11390003e3e605ba00f2be408e679ceb6c
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-04-16 14:56:55 +0200

    wip

--- src/psql/nodo/liquibase/changelog_storico/admin/3.32.0/sql/dblink.sql
+++ src/psql/nodo/liquibase/changelog_storico/admin/3.32.0/sql/dblink.sql
@@ -1,5 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS dblink;
-CREATE SERVER connSVInodo FOREIGN DATA WRAPPER dblink_fdw OPTIONS (host '${nodo-db-host}', port '${nodo-db-port}', dbname 'nodo', sslmode 'require');
+CREATE SERVER IF NOT EXISTS connSVInodo FOREIGN DATA WRAPPER dblink_fdw OPTIONS (host '${nodo-db-host}', port '${nodo-db-port}', dbname 'nodo', sslmode 'require');
 CREATE USER MAPPING FOR partition SERVER connSVInodo OPTIONS (user 'partition',password '${partition-password}');
 CREATE USER MAPPING FOR azureuser SERVER connSVInodo OPTIONS (user 'azureuser',password '${admin-password}');
 GRANT USAGE ON FOREIGN SERVER connSVInodo TO partition;
