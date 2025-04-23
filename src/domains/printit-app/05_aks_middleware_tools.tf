112e717f1b5e7c42afc2dbe97291405d5ae0ad63
pasqualespica <pasqualespica@gmail.com>
2025-04-23 20:00:18 +0200

    fix

--- src/domains/printit-app/05_aks_middleware_tools.tf
+++ src/domains/printit-app/05_aks_middleware_tools.tf
@@ -1,27 +1,25 @@
+
 # WL-IDENTITY
 # https://pagopa.atlassian.net/wiki/spaces/DEVOPS/pages/1227751458/Migrazione+pod+Identity+vs+workload+Identity#%F0%9F%94%AE-tls-cheker
 module "tls_checker" {
   source = "./.terraform/modules/__v3__/tls_checker"
 
-  https_endpoint = local.domain_hostname
-  alert_name = local.domain_hostname
-  alert_enabled = true
-  helm_chart_present = true
-  helm_chart_version                                        = var.tls_cert_check_helm.chart_version
+  https_endpoint                                            = local.domain_hostname
+  alert_name                                                = local.domain_hostname
+  alert_enabled                                             = true
+  helm_chart_present                                        = true
   namespace                                                 = kubernetes_namespace.namespace.metadata[0].name
-  helm_chart_image_name                                     = var.tls_cert_check_helm.image_name
-  helm_chart_image_tag                                      = var.tls_cert_check_helm.image_tag
-  location_string = var.location_string
+  location_string                                           = var.location_string
   kv_secret_name_for_application_insights_connection_string = "app-insight-connection-string"
-  application_insights_resource_group = data.azurerm_resource_group.monitor_italy_rg.name
-  application_insights_id = data.azurerm_application_insights.application_insights_italy.id
-  application_insights_action_group_ids = [data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.email.id]
-  keyvault_name = data.azurerm_key_vault.kv.name
-  keyvault_tenant_id = data.azurerm_client_config.current.tenant_id
+  application_insights_resource_group                       = data.azurerm_resource_group.monitor_italy_rg.name
+  application_insights_id                                   = data.azurerm_application_insights.application_insights_italy.id
+  application_insights_action_group_ids                     = [data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.email.id]
+  keyvault_name                                             = data.azurerm_key_vault.kv.name
+  keyvault_tenant_id                                        = data.azurerm_client_config.current.tenant_id
 
-  workload_identity_enabled = true
+  workload_identity_enabled              = true
   workload_identity_service_account_name = module.workload_identity.workload_identity_service_account_name
-  workload_identity_client_id = module.workload_identity.workload_identity_client_id
+  workload_identity_client_id            = module.workload_identity.workload_identity_client_id
 
   depends_on = [module.workload_identity]
 }
@@ -32,14 +30,14 @@ module "tls_checker" {
 module "cert_mounter" {
   source = "./.terraform/modules/__v3__/cert_mounter"
 
-  namespace = var.domain
-  certificate_name = replace(local.hostname, ".", "-")
-  kv_name = data.azurerm_key_vault.kv.name
-  tenant_id = data.azurerm_subscription.current.tenant_id
+  namespace        = var.domain
+  certificate_name = replace(local.domain_hostname, ".", "-")
+  kv_name          = data.azurerm_key_vault.kv.name
+  tenant_id        = data.azurerm_subscription.current.tenant_id
 
-  workload_identity_enabled = true
+  workload_identity_enabled              = true
   workload_identity_service_account_name = module.workload_identity.workload_identity_service_account_name
-  workload_identity_client_id = module.workload_identity.workload_identity_client_id
+  workload_identity_client_id            = module.workload_identity.workload_identity_client_id
 
   depends_on = [module.workload_identity]
 }
