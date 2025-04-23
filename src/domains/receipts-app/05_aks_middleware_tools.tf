0250293da7774622a8f1e8f7e9b4ef92432aa98e
pasqualespica <pasqualespica@gmail.com>
2025-04-23 20:39:21 +0200

    fix

--- src/domains/receipts-app/05_aks_middleware_tools.tf
+++ src/domains/receipts-app/05_aks_middleware_tools.tf
@@ -13,15 +13,18 @@ module "tls_checker" {
   helm_chart_image_tag                                      = var.tls_cert_check_helm.image_tag
   location_string                                           = var.location_string
   kv_secret_name_for_application_insights_connection_string = "ai-${var.env_short}-connection-string"
-  application_insights_resource_group = data.azurerm_resource_group.monitor_italy_rg.name
-  application_insights_id = data.azurerm_application_insights.application_insights_italy.id
-  application_insights_action_group_ids = [data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.email.id]
-  keyvault_name = data.azurerm_key_vault.kv.name
+  application_insights_resource_group                       = data.azurerm_resource_group.monitor_rg.name
+  application_insights_id                                   = data.azurerm_application_insights.application_insights.id
+  application_insights_action_group_ids = [
+    data.azurerm_monitor_action_group.slack.id,
+    data.azurerm_monitor_action_group.email.id
+  ]
+  keyvault_name      = data.azurerm_key_vault.kv.name
   keyvault_tenant_id = data.azurerm_client_config.current.tenant_id
 
-  workload_identity_enabled = true
+  workload_identity_enabled              = true
   workload_identity_service_account_name = module.workload_identity.workload_identity_service_account_name
-  workload_identity_client_id = module.workload_identity.workload_identity_client_id
+  workload_identity_client_id            = module.workload_identity.workload_identity_client_id
 
   depends_on = [module.workload_identity]
 }
@@ -32,14 +35,14 @@ module "tls_checker" {
 module "cert_mounter" {
   source = "./.terraform/modules/__v3__/cert_mounter"
 
-  namespace = var.domain
-  certificate_name = replace(local.hostname, ".", "-")
-  kv_name = data.azurerm_key_vault.kv.name
-  tenant_id = data.azurerm_subscription.current.tenant_id
+  namespace        = var.domain
+  certificate_name = replace(local.receipts_hostname, ".", "-")
+  kv_name          = data.azurerm_key_vault.kv.name
+  tenant_id        = data.azurerm_subscription.current.tenant_id
 
-  workload_identity_enabled = true
+  workload_identity_enabled              = true
   workload_identity_service_account_name = module.workload_identity.workload_identity_service_account_name
-  workload_identity_client_id = module.workload_identity.workload_identity_client_id
+  workload_identity_client_id            = module.workload_identity.workload_identity_client_id
 
   depends_on = [module.workload_identity]
 }
