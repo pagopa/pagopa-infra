88953579f913f95031785b466748c42f6a527aaf
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2025-04-24 18:50:57 +0200

    feat: remove pod identity (#2999)
    * chore: update all modules
    
    * feat: add worklopad identity
    
    * feat: remove pod identity
    
    ---------
    
    Co-authored-by: pasqualespica <pasqualespica@gmail.com>

--- src/domains/selfcare-app/02_namespace.tf
+++ src/domains/selfcare-app/02_namespace.tf
@@ -6,20 +6,6 @@ resource "kubernetes_namespace" "namespace" {
   }
 }
 
-module "pod_identity" {
-  source = "./.terraform/modules/__v3__/kubernetes_pod_identity"
-
-  resource_group_name = local.aks_resource_group_name
-  location            = var.location
-  tenant_id           = data.azurerm_subscription.current.tenant_id
-  cluster_name        = local.aks_name
-
-  identity_name = "${kubernetes_namespace.namespace.metadata[0].name}-pod-identity" // TODO add env in name
-  namespace     = kubernetes_namespace.namespace.metadata[0].name
-  key_vault_id  = data.azurerm_key_vault.kv.id
-
-  secret_permissions = ["Get"]
-}
 
 resource "kubernetes_pod_disruption_budget_v1" "selfcare" {
 
