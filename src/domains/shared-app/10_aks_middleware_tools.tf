58ed0ebf54777910fbb074b23d15e1025064f49d
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-05-05 17:38:26 +0200

    minor fix

--- src/domains/shared-app/10_aks_middleware_tools.tf
+++ src/domains/shared-app/10_aks_middleware_tools.tf
@@ -25,18 +25,6 @@ module "tls_checker" {
   workload_identity_client_id                               = module.workload_identity.workload_identity_client_id
 }
 
-# module "cert_mounter" {
-#   depends_on                             = [module.workload_identity]
-#   source                                 = "./.terraform/modules/__v3__/cert_mounter"
-#   namespace                              = var.domain
-#   certificate_name                       = replace(local.shared_hostname, ".", "-")
-#   kv_name                                = data.azurerm_key_vault.kv.name
-#   tenant_id                              = data.azurerm_subscription.current.tenant_id
-#   workload_identity_enabled              = true
-#   workload_identity_service_account_name = module.workload_identity.workload_identity_service_account_name
-#   workload_identity_client_id            = module.workload_identity.workload_identity_client_id
-# }
-
 resource "helm_release" "cert_mounter" {
   name         = "cert-mounter-blueprint"
   repository   = "https://pagopa.github.io/aks-helm-cert-mounter-blueprint"
