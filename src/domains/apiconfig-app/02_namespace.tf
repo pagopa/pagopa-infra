7fa3800519c197c9eace8b48f333e25a06199e34
Federico Ruzzier <federicoruzzier96@gmail.com>
2025-05-07 12:01:36 +0200

    fix: move data and rename dependency

--- src/domains/apiconfig-app/02_namespace.tf
+++ src/domains/apiconfig-app/02_namespace.tf
@@ -1,8 +1,3 @@
-data "azurerm_key_vault" "kv_domain" {
-  name                = "${local.product}-${var.domain}-kv"
-  resource_group_name = "${local.product}-${var.domain}-sec-rg"
-}
-
 resource "kubernetes_namespace" "namespace" {
   metadata {
     name = var.domain
@@ -47,7 +42,7 @@ module "workload_identity_configuration" {
   aks_name                              = data.azurerm_kubernetes_cluster.aks.name
   aks_resource_group_name               = data.azurerm_kubernetes_cluster.aks.resource_group_name
   namespace                             = var.domain
-  key_vault_id                      = data.azurerm_key_vault.kv_domain.id
+  key_vault_id                      = data.azurerm_key_vault.kv.id
   key_vault_certificate_permissions = ["Get"]
   key_vault_key_permissions         = ["Get"]
   key_vault_secret_permissions      = ["Get"]
