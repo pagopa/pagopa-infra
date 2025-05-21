82c37203fed586118a0c70a0c158ad7c47f01c7d
Fabio Felici <fabio.felici@pagopa.it>
2025-05-21 10:04:56 +0200

    Remove unused Logic App workflow and associated resources
    Deleted the Logic App workflow, HTTP action, and action group that were no longer in use. This cleanup reduces redundant resources and simplifies the monitoring configuration.
    
    Signed-off-by: Fabio Felici <fabio.felici@pagopa.it>

--- src/domains/nodo-app/00_monitor.tf
+++ src/domains/nodo-app/00_monitor.tf
@@ -34,31 +34,6 @@ data "azurerm_monitor_action_group" "opsgenie" {
   name                = local.monitor_action_group_opsgenie_name
 }
 
-resource "azurerm_logic_app_workflow" "nodo_cron_suspend_job_lapp" {
-  name                = "workflow1"
-  location            = var.location
-  resource_group_name = var.monitor_resource_group_name
-}
-
-resource "azurerm_logic_app_action_http" "nodo_cron_suspend_job_lapp_wh" {
-  name         = "webhook"
-  logic_app_id = azurerm_logic_app_workflow.nodo_cron_suspend_job_lapp.id
-  method       = "GET"
-  uri          = "https://dev.azure.com/{organization}/{project}/_apis/pipelines/{pipelineId}/runs?api-version=7.1-preview.1"
-}
-
-resource "azurerm_monitor_action_group" "nodo_cron_suspend_job_action_group" {
-  name                = "trigger-logicapp"
-  resource_group_name = var.monitor_resource_group_name
-  short_name          = "logicapp"
-
-  webhook_receiver {
-    name                    = "LogicAppTrigger"
-    service_uri             = azurerm_logic_app_workflow.nodo_cron_suspend_job_lapp.access_endpoint
-    use_common_alert_schema = true
-  }
-}
-
 resource "azurerm_monitor_metric_alert" "aks_nodo_moetrics" {
   name                = "${local.aks_name}-nodo-cron-pod_number"
   resource_group_name = var.monitor_resource_group_name
