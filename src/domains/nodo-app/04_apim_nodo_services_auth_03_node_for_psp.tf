606e7db5b9b9be32e94f497fd92d72f512168d47
Francesco <cesareo.francesco@gmail.com>
2025-05-23 17:37:19 +0200

    draft verift

--- src/domains/nodo-app/04_apim_nodo_services_auth_03_node_for_psp.tf
+++ src/domains/nodo-app/04_apim_nodo_services_auth_03_node_for_psp.tf
@@ -48,7 +48,7 @@ module "apim_node_for_psp_api_v1_auth" {
   # source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.7.0"
   source = "./.terraform/modules/__v3__/api_management_api"
 
-  name                  = "${var.env_short}-node-for-psp-api-auth"
+  name                  = "${var.env_short}-node-for-psp-api-auth-2" #TODO [FCADAC] remove -2
   api_management_name   = data.azurerm_api_management.apim.name
   resource_group_name   = data.azurerm_api_management.apim.resource_group_name
   product_ids           = [module.apim_nodo_dei_pagamenti_product_auth.product_id]
@@ -82,26 +82,26 @@ module "apim_node_for_psp_api_v1_auth" {
 
 
 ###### verifyPaymentNotice
-# resource "terraform_data" "sha256_verifyPaymentNotice_v1_policy_auth" {
-#   input = sha256(local.verifyPaymentNotice_v1_policy_file)
-# }
-# resource "azurerm_api_management_api_operation_policy" "verifyPaymentNotice_v1_policy_auth" {
-#   api_name              = module.apim_node_for_psp_api_v1_auth.name
-#   api_management_name   = data.azurerm_api_management.apim.name
-#   resource_group_name   = data.azurerm_api_management.apim.resource_group_name
-#   # operation_id          = var.env_short == "d" ? "637608a0c257810fc0ecfe1c" : var.env_short == "u" ? "636cb7e439519a17ec9bf98b" : "63b6e2daea7c4a25440fdaa0" #TODO [FCADAC] replace
-#   operation_id          = var.env_short == "d" ? "68304e280a23231b9031207c" : "TODO"
-#
-#   #tfsec:ignore:GEN005
-#   xml_content = local.verifyPaymentNotice_v1_policy_file
-#   #TODO [FCADAC] trasformare tutto in namedValue -> url_aks                   = var.env_short == "p" ? "weu${var.env}.apiconfig.internal.platform.pagopa.it" : "weu${var.env}.apiconfig.internal.${var.env}.platform.pagopa.it"
-#
-#   # xml_content = templatefile("./api/nodopagamenti_api/nodeForPsp/v1/activate_nm3.xml", {
-#   #   is-nodo-decoupler-enabled = var.apim_nodo_decoupler_enable
-#   #   urlenvpath                = var.env_short
-#   #   url_aks                   = var.env_short == "p" ? "weu${var.env}.apiconfig.internal.platform.pagopa.it" : "weu${var.env}.apiconfig.internal.${var.env}.platform.pagopa.it"
-#   # })
-# }
+resource "terraform_data" "sha256_verifyPaymentNotice_v1_policy_auth" {
+  input = sha256(local.verifyPaymentNotice_v1_policy_file)
+}
+resource "azurerm_api_management_api_operation_policy" "verifyPaymentNotice_v1_policy_auth" {
+  api_name              = module.apim_node_for_psp_api_v1_auth.name
+  api_management_name   = data.azurerm_api_management.apim.name
+  resource_group_name   = data.azurerm_api_management.apim.resource_group_name
+  # operation_id          = var.env_short == "d" ? "637608a0c257810fc0ecfe1c" : var.env_short == "u" ? "636cb7e439519a17ec9bf98b" : "63b6e2daea7c4a25440fdaa0" #TODO [FCADAC] replace
+  operation_id          = "683085050a23231b90313d94"
+
+  #tfsec:ignore:GEN005
+  xml_content = local.verifyPaymentNotice_v1_policy_file
+  #TODO [FCADAC] trasformare tutto in namedValue -> url_aks                   = var.env_short == "p" ? "weu${var.env}.apiconfig.internal.platform.pagopa.it" : "weu${var.env}.apiconfig.internal.${var.env}.platform.pagopa.it"
+
+  # xml_content = templatefile("./api/nodopagamenti_api/nodeForPsp/v1/activate_nm3.xml", {
+  #   is-nodo-decoupler-enabled = var.apim_nodo_decoupler_enable
+  #   urlenvpath                = var.env_short
+  #   url_aks                   = var.env_short == "p" ? "weu${var.env}.apiconfig.internal.platform.pagopa.it" : "weu${var.env}.apiconfig.internal.${var.env}.platform.pagopa.it"
+  # })
+}
 
 
 
