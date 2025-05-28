bd64d629ac2df9f5755c5561e9ced5211e0823d2
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-05-28 10:20:21 +0200

    [PAGOPA-3076] feat: creating fragment for inbound and outbound logic about POS Fisici context

--- src/domains/nodo-app/04_apim_nodo_services_auth_02_product.tf
+++ src/domains/nodo-app/04_apim_nodo_services_auth_02_product.tf
@@ -29,8 +29,8 @@ resource "terraform_data" "sha256_apim_nodo_dei_pagamenti_product_auth" {
 module "apim_nodo_dei_pagamenti_product_auth" {
   source = "./.terraform/modules/__v3__/api_management_product"
 
-  product_id   = "nodo-auth-2-0" #TODO [FCADAC] remove -2-0
-  display_name = "AAA Nodo dei Pagamenti (Nuova Connettività) 2.0" #TODO [FCADAC] remove AAA 2.0
+  product_id   = "nodo-auth-2-0"                                               #TODO [FCADAC] remove -2-0
+  display_name = "AAA Nodo dei Pagamenti (Nuova Connettività) 2.0"             #TODO [FCADAC] remove AAA 2.0
   description  = "AAA Product for Nodo dei Pagamenti (Nuova Connettività) 2.0" #TODO [FCADAC] remove AAA 2.0
 
   api_management_name = data.azurerm_api_management.apim.name
