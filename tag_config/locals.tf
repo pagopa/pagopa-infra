2f0c3dd96e00f15e1f3bf651e72d0229cbff7dfa
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-05-28 17:36:21 +0200

    added current folder path to source tag

--- tag_config/locals.tf
+++ tag_config/locals.tf
@@ -1,11 +1,14 @@
 locals {
+  cwd_split = split("/", path.cwd)
+  src_idx = index(local.cwd_split, "src")
+  relative_folder = join("/", slice(local.cwd_split, local.src_idx + 1, length(local.cwd_split)))
   tags = {
     CreatedBy   = "Terraform"
     Environment = title(var.environment)
     Owner       = "pagoPA"
-    Source      = "https://github.com/pagopa/pagopa-infra"
+    Source      = "https://github.com/pagopa/pagopa-infra/tree/main/src/${local.relative_folder}"
     # isolates the module working folder, removing the absolute path leading to the cwd and the leading slash
-    Folder     = trimprefix(replace(path.cwd, dirname(path.cwd), ""), "/")
+    Folder     = local.relative_folder
     CostCenter = "TS310 - PAGAMENTI & SERVIZI"
     domain     = var.domain
   }
