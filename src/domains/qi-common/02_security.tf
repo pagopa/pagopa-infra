17a7061579f90b784a69da32ab8090fbf0ecf08e
pasqualespica <pasqualespica@gmail.com>
2025-06-04 14:49:21 +0200

    add-smo-grp-2-kv-qi

--- src/domains/qi-common/02_security.tf
+++ src/domains/qi-common/02_security.tf
@@ -49,6 +49,25 @@ resource "azurerm_key_vault_access_policy" "adgroup_developers_policy" {
     "Delete", "Restore", "Purge", "Recover"
   ]
 }
+
+## ad group policy ##
+resource "azurerm_key_vault_access_policy" "adgroup_operation_policy" {
+  # count = var.env_short != "p" ? 1 : 0
+  count = 1
+
+  key_vault_id = module.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.adgroup_operations.object_id
+
+  key_permissions     = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt"]
+  secret_permissions  = ["Get", "List", "Set", "Delete", ]
+  storage_permissions = []
+  certificate_permissions = [
+    "Get", "List", "Update", "Create", "Import",
+    "Delete", "Restore", "Purge", "Recover"
+  ]
+}
 ## ad group policy ##
 resource "azurerm_key_vault_access_policy" "adgroup_externals_policy" {
   count = var.env_short != "p" ? 1 : 0
