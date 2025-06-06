2117f7d8c207e860b38f9a2162c86a5d149542bc
svariant <samuele.varianti@nttdata.com>
2025-06-06 10:27:45 +0200

    fix: [PAGOPA-3064] [SHARED] pagopa-anonymizer apim

--- src/domains/shared-app/04_apim_anonymizer.tf
+++ src/domains/shared-app/04_apim_anonymizer.tf
@@ -19,45 +19,9 @@ module "apim_anonymizer_product" {
   resource_group_name = local.pagopa_apim_rg
 
   published             = false
-  subscription_required = true
+  subscription_required = false
   approval_required     = false
   # subscriptions_limit   = 1000
 
   policy_xml = file("./api_product/_base_policy.xml")
-}
-
-resource "azurerm_api_management_api_version_set" "api_anonymizer_api" {
-
-  name                = format("%s-anonymizer-api", var.env_short)
-  resource_group_name = local.pagopa_apim_rg
-  api_management_name = local.pagopa_apim_name
-  display_name        = local.apim_anonymizer_api.display_name
-  versioning_scheme   = "Segment"
-}
-
-module "apim_api_anonymizer_api_v1" {
-  source = "./.terraform/modules/__v3__/api_management_api"
-
-  name                  = format("%s-anonymizer-api", local.project)
-  api_management_name   = local.pagopa_apim_name
-  resource_group_name   = local.pagopa_apim_rg
-  product_ids           = [module.apim_anonymizer_product.product_id]
-  subscription_required = local.apim_anonymizer_api.subscription_required
-  version_set_id        = azurerm_api_management_api_version_set.api_anonymizer_api.id
-  api_version           = "v1"
-
-  description  = local.apim_anonymizer_api.description
-  display_name = local.apim_anonymizer_api.display_name
-  path         = local.apim_anonymizer_api.path
-  protocols    = ["https"]
-  service_url  = local.apim_anonymizer_api.service_url
-
-  content_format = "openapi"
-  content_value = templatefile("./api/anonymizer/v1/_openapi.json.tpl", {
-    host = local.apim_hostname
-  })
-
-  xml_content = templatefile("./api/anonymizer/v1/_base_policy.xml", {
-    hostname = local.shared_hostname
-  })
 }
