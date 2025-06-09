5a7f36c10c231332aa44f7c0d4368344407dbc09
Fabio Felici <fabio.felici@pagopa.it>
2025-06-09 11:07:48 +0200

    Add initial configuration for GitHub Runner infrastructure in `dev` environment including Terraform lock file and environment-specific configurations
    Signed-off-by: Fabio Felici <fabio.felici@pagopa.it>

---
+++ src/github-runner/07_gh_runner.tf
@@ -0,0 +1,45 @@
+locals {
+  # because westeurope does not support any other container app environment creation
+  tools_cae_name = "${local.product}-tools-cae"
+  tools_cae_rg   = "${local.product}-core-tools-rg"
+}
+
+module "gh_runner_job" {
+  source = "./.terraform/modules/__v4__/gh_runner_container_app_job_domain_setup"
+
+  domain_name        = var.domain
+  env_short          = var.env_short
+  environment_name   = local.tools_cae_name
+  environment_rg     = local.tools_cae_rg
+  gh_identity_suffix = "job-01"
+  gh_env             = var.env
+  runner_labels      = ["self-hosted-job", var.env]
+  gh_repositories = [
+    {
+      name : "pagopa-infra",
+      short_name : "pagopa-infra"
+    }
+  ]
+  job = {
+    name = var.domain
+  }
+  job_meta = {}
+  key_vault = {
+    name = data.azurerm_key_vault.key_vault.name
+    # Name of the KeyVault which stores PAT as secret
+    rg = data.azurerm_key_vault.key_vault.resource_group_name
+    # Resource group of the KeyVault which stores PAT as secret
+    secret_name = "gh-runner-job-pat"
+    # Data of the KeyVault which stores PAT as secret
+  }
+
+  kubernetes_deploy = {
+    enabled = false
+  }
+
+  location                = var.gh_runner_job_location
+  prefix                  = var.prefix
+  resource_group_name     = data.azurerm_resource_group.identity_rg.name
+  domain_security_rg_name = data.azurerm_key_vault.key_vault.resource_group_name
+  tags                    = module.tag_config.tags
+}
---
+++ src/github-runner/99_variables.tf
@@ -0,0 +1,39 @@
+variable "prefix" {
+  type = string
+  validation {
+    condition = (
+      length(var.prefix) <= 6
+    )
+    error_message = "Max length is 6 chars."
+  }
+}
+
+variable "env" {
+  type = string
+}
+
+variable "env_short" {
+  type = string
+  validation {
+    condition = (
+      length(var.env_short) == 1
+    )
+    error_message = "Length must be 1 chars."
+  }
+}
+
+variable "domain" {
+  type = string
+  validation {
+    condition = (
+      length(var.domain) <= 12
+    )
+    error_message = "Max length is 12 chars."
+  }
+}
+
+variable "gh_runner_job_location" {
+  type        = string
+  description = "(Optional) The GH runner container app job location. Consistent with the container app environment location"
+  default     = "westeurope"
+}
