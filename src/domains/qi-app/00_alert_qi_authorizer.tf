1be035c00048711b41ea91b89bbc670b194755f5
giomella <giomella@emeal.nttdata.com>
2025-06-10 13:05:49 +0200

    [PAGOPA-545] added alert on 403 for domain QI

---
+++ src/domains/qi-app/00_alert_qi_authorizer.tf
@@ -0,0 +1,40 @@
+resource "azurerm_monitor_scheduled_query_rules_alert" "opex_pagopa-qi-unauthorized" {
+  count               = var.env_short == "p" ? 1 : 0
+  resource_group_name = "dashboards"
+  name                = "pagopa-${var.env_short}-opex_pagopa-qi-unauthorized @ _qi"
+  location            = var.location
+
+  action {
+    action_group = [
+      data.azurerm_monitor_action_group.email.id,
+      data.azurerm_monitor_action_group.slack.id,
+      data.azurerm_monitor_action_group.opsgenie[0].id,
+      data.azurerm_monitor_action_group.smo_opsgenie[0].id
+    ]
+    email_subject          = "Email Header"
+    custom_webhook_payload = "{}"
+  }
+
+  data_source_id = data.azurerm_api_management.apim.id
+  description    = "Unauthorized for QI APIs is greater than or equal to 10%"
+  enabled        = true
+  query          = (<<-QUERY
+let threshold = 0.10;
+AzureDiagnostics
+| where url_s matches regex "/qi/fdr-kpi-service"
+| summarize
+    Total=count(),
+    Unauthorized=count(responseCode_d = 403)
+    by bin(TimeGenerated, 5m)
+| extend KO=toreal(Unauthorized) / Total
+| where KO > threshold
+  QUERY
+  )
+  severity    = 1
+  frequency   = 5
+  time_window = 5
+  trigger {
+    operator  = "GreaterThanOrEqual"
+    threshold = 1
+  }
+}
