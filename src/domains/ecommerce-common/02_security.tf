8ee6945f6381aeaf0171358e8673fd32c6cce635
danilo.ciano <danilo.ciano@pagopa.it>
2025-06-12 15:36:50 +0200

    feat(api-key): add active api key secret

--- src/domains/ecommerce-common/02_security.tf
+++ src/domains/ecommerce-common/02_security.tf
@@ -743,6 +743,18 @@ resource "azurerm_key_vault_secret" "ecommerce_payment_requests_secondary_api_ke
   }
 }
 
+resource "azurerm_key_vault_secret" "ecommerce_payment_requests_active_api_key" {
+  name         = "ecommerce-payment-requests-active-api-key"
+  value        = "<TO UPDATE MANUALLY ON PORTAL>"
+  key_vault_id = module.key_vault.id
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
 resource "azurerm_key_vault_certificate" "ecommerce-jwt-token-issuer-certificate" {
   name         = "jwt-token-issuer-cert"
   key_vault_id = module.key_vault.id
