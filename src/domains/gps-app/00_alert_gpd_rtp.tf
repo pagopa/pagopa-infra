738f30f84b0245879e802b88ef3dd06b50efdbf3
svariant <samuele.varianti@nttdata.com>
2025-06-20 11:20:21 +0200

    fix: [PAGOPA-3116] [GPS] Alerts severity 2 action group

--- src/domains/gps-app/00_alert_gpd_rtp.tf
+++ src/domains/gps-app/00_alert_gpd_rtp.tf
@@ -9,7 +9,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "gpd-rtp-error-json" {
   location            = var.location
 
   action {
-    action_group = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.opsgenie[0].id, data.azurerm_monitor_action_group.smo_opsgenie[0].id]
+    action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
     # action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
     email_subject          = "gpd-rtp-error-json"
     custom_webhook_payload = "{}"
@@ -41,7 +41,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "gpd-rtp-error-generic" {
   location            = var.location
 
   action {
-    action_group = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.opsgenie[0].id, data.azurerm_monitor_action_group.smo_opsgenie[0].id]
+    action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
     # action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
     email_subject          = "gpd-rtp-error-generic"
     custom_webhook_payload = "{}"
@@ -73,7 +73,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "gpd-rtp-error-rtp-messag
   location            = var.location
 
   action {
-    action_group = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.opsgenie[0].id, data.azurerm_monitor_action_group.smo_opsgenie[0].id]
+    action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
     # action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
     email_subject          = "gpd-rtp-error-rtp-message-not-sent"
     custom_webhook_payload = "{}"
@@ -105,7 +105,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "gpd-rtp-error-redis-cach
   location            = var.location
 
   action {
-    action_group = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.opsgenie[0].id, data.azurerm_monitor_action_group.smo_opsgenie[0].id]
+    action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
     # action_group           = [data.azurerm_monitor_action_group.email.id, data.azurerm_monitor_action_group.slack.id]
     email_subject          = "gpd-rtp-error-redis-cache-not-updated"
     custom_webhook_payload = "{}"
