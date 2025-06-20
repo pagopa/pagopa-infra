60a8b2bd8eded145bf5ba5c6cd284a362e51c212
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-06-20 16:18:28 +0200

    added audit

---
+++ src/tf-audit/99_main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.16"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.1.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+}
+
+data "azurerm_subscription" "current" {}
+
+data "azurerm_client_config" "current" {}
+
+module "__v4__" {
+  # v7.10.0
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4?ref=8a09391b7066afe579c96dd0eae58e037dd5b82a"
+}
