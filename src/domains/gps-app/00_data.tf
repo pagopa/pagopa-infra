fde68ae26106a587e4b73e1adf6f1170e50eb100
Andrea Ferracci <andrea.ferracci@gmail.com>
2025-06-23 18:58:52 +0200

    [PIDM-419] fixed product_id for apim_fdr_internal

--- src/domains/gps-app/00_data.tf
+++ src/domains/gps-app/00_data.tf
@@ -72,7 +72,7 @@ data "azurerm_api_management_product" "apim_fdr_orgs" {
 }
 
 data "azurerm_api_management_product" "apim_fdr_internal" {
-  product_id          = "fdr-internal"
+  product_id          = "fdr_internal"
   api_management_name = local.pagopa_apim_name
   resource_group_name = local.pagopa_apim_rg
 }
