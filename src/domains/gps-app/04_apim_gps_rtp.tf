6add9555f460c8486eb3b50ad5a9137af7e04551
Jacopo Carlini <jacopo.carlini@pagopa.it>
2025-06-23 15:52:10 +0200

    displayname

--- src/domains/gps-app/04_apim_gps_rtp.tf
+++ src/domains/gps-app/04_apim_gps_rtp.tf
@@ -3,8 +3,8 @@ module "apim_gps_rtp_product" {
   source = "./.terraform/modules/__v3__/api_management_product"
 
   product_id   = "gpsrtp"
-  display_name = "GPS x RTP"
-  description  = "Servizio per inviare i messaggi RTP"
+  display_name = "GPD x RTP - Helpdesk"
+  description  = "Servizio di Helpdesk per GPDxRTP"
 
   api_management_name = local.pagopa_apim_name
   resource_group_name = local.pagopa_apim_rg
