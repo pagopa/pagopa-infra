be53f4d2e8700730c7724dd15da7a61cfbe26b55
danilo.ciano <danilo.ciano@pagopa.it>
2025-06-24 10:38:13 +0200

    feat(api-key): add payment-methods service API keys. Apply pre-commit run

--- src/domains/ecommerce-app/04_apim_ecommerce.tf
+++ src/domains/ecommerce-app/04_apim_ecommerce.tf
@@ -741,3 +741,22 @@ resource "azurerm_api_management_named_value" "ecommerce_payment_requests_api_ke
   value               = var.ecommerce_payment_requests_api_key_use_primary ? data.azurerm_key_vault_secret.ecommerce_payment_requests_primary_api_key.value : data.azurerm_key_vault_secret.ecommerce_payment_requests_secondary_api_key.value
   secret              = true
 }
+
+data "azurerm_key_vault_secret" "ecommerce_payment_methods_primary_api_key" {
+  name         = "ecommerce-payment-methods-primary-api-key"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+data "azurerm_key_vault_secret" "ecommerce_payment_methods_secondary_api_key" {
+  name         = "ecommerce-payment-methods-secondary-api-key"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+resource "azurerm_api_management_named_value" "ecommerce_payment_methods_api_key_value" {
+  name                = "ecommerce-payment-methods-api-key-value"
+  api_management_name = local.pagopa_apim_name
+  resource_group_name = local.pagopa_apim_rg
+  display_name        = "ecommerce-payment-methods-api-key-value"
+  value               = var.ecommerce_payment_methods_api_key_use_primary ? data.azurerm_key_vault_secret.ecommerce_payment_methods_primary_api_key.value : data.azurerm_key_vault_secret.ecommerce_payment_methods_secondary_api_key.value
+  secret              = true
+}
