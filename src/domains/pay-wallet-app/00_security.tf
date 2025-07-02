6fcb92c34db33fd52832107a5f636d939ccbddf0
danilo.ciano <danilo.ciano@pagopa.it>
2025-07-02 14:36:53 +0200

    feat(api-key): move pay wallet jwt issuer api key from app to common

--- src/domains/pay-wallet-app/00_security.tf
+++ src/domains/pay-wallet-app/00_security.tf
@@ -12,9 +12,3 @@ data "azurerm_key_vault_secret" "pay_wallet_jwt_issuer_service_secondary_api_key
   name         = "pay-wallet-jwt-issuer-service-secondary-api-key"
   key_vault_id = data.azurerm_key_vault.kv.id
 }
-
-resource "azurerm_key_vault_secret" "pay_wallet_jwt_issuer_service_active_api_key" {
-  name         = "pay-wallet-jwt-issuer-service-active-api-key"
-  value        = var.pay_wallet_jwt_issuer_api_key_use_primary ? data.azurerm_key_vault_secret.pay_wallet_jwt_issuer_service_primary_api_key.value : data.azurerm_key_vault_secret.pay_wallet_jwt_issuer_service_secondary_api_key.value
-  key_vault_id = data.azurerm_key_vault.kv.id
-}
