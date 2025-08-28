00c0fd0faa981027bc3d98d7da7b686b20cd37ec
svariant <samuele.varianti@nttdata.com>
2025-08-28 16:26:30 +0200

    feat: [PAGOPA-3220] [GPS] Add new gpd-upload api for ACA

--- src/domains/gps-app/04_apim_gpd_upload.tf
+++ src/domains/gps-app/04_apim_gpd_upload.tf
@@ -12,6 +12,13 @@ locals {
     subscription_required = true
     service_url           = var.env == "prod" ? "https://weu${var.env}.gps.internal.platform.pagopa.it/pagopa-gpd-upload" : "https://weu${var.env}.gps.internal.${var.env}.platform.pagopa.it/pagopa-gpd-upload"
   }
+  apim_aca_upload_api = {
+    display_name          = "GPD Upload pagoPA - Massive Upload ACA"
+    description           = "API to support ACA's Debt Positions massive upload for organizations"
+    path                  = "upload/aca/debt-positions-service"
+    subscription_required = true
+    service_url           = var.env == "prod" ? "https://weu${var.env}.gps.internal.platform.pagopa.it/pagopa-gpd-upload" : "https://weu${var.env}.gps.internal.${var.env}.platform.pagopa.it/pagopa-gpd-upload"
+  }
 }
 
 
@@ -40,6 +47,41 @@ module "apim_gpd_upload_api_v1" {
   path         = local.apim_gpd_upload_api.path
   protocols    = ["https"]
   service_url  = local.apim_gpd_upload_api.service_url
+
+  content_format = "openapi"
+  content_value = templatefile("./api/gpd-upload-service/v1/_openapi.json.tpl", {
+    host = local.apim_hostname
+  })
+
+  xml_content = file("./api/gpd-upload-service/v1/_base_policy.xml")
+}
+
+// ACA
+
+resource "azurerm_api_management_api_version_set" "apim_aca_upload_api" {
+  name                = "${var.env_short}-aca-upload-api"
+  api_management_name = local.pagopa_apim_name
+  resource_group_name = local.pagopa_apim_rg
+  display_name        = local.apim_aca_upload_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+module "apim_aca_upload_api_v1" {
+  source = "./.terraform/modules/__v3__/api_management_api"
+
+  name                  = "${local.product}-aca-upload-api"
+  api_management_name   = local.pagopa_apim_name
+  resource_group_name   = local.pagopa_apim_rg
+  product_ids           = [module.apim_debt_positions_product.product_id, module.apim_gpd_integration_product.product_id]
+  subscription_required = local.apim_aca_upload_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.apim_aca_upload_api.id
+  api_version           = "v1"
+
+  description  = local.apim_aca_upload_api.description
+  display_name = local.apim_aca_upload_api.display_name
+  path         = local.apim_aca_upload_api.path
+  protocols    = ["https"]
+  service_url  = local.apim_aca_upload_api.service_url
 
   content_format = "openapi"
   content_value = templatefile("./api/gpd-upload-service/v1/_openapi.json.tpl", {
