857dfa68b623e8673277a88e36994ecc42e661a0
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2025-09-08 17:25:15 +0200

    feat: [CHK-4416] create ratemyopenapi secret (#3326)
    * feat(secret): [CHK-4416] add ratemyopeanpi secret to kv
    
    * fix:: disable advanced_threat_protection fro ecommerce history and ecommerce reporting
    
    * feat(secret): [CHK-4416] run precommit
    
    * feat(secret): [CHK-4416] add ratemyopeanpi secret to kv
    
    * chore update docs
    
    ---------
    
    Co-authored-by: Simone Infante <52280205+infantesimone@users.noreply.github.com>

--- src/domains/ecommerce-common/02_security.tf
+++ src/domains/ecommerce-common/02_security.tf
@@ -1077,9 +1077,9 @@ resource "azurerm_key_vault_secret" "ecommerce_github_packages_read_bot_token" {
   }
 }
 
-resource "azurerm_key_vault_secret" "ecommerce_github_bot_ratemyopenapi_api_key" {
+resource "azurerm_key_vault_secret" "ecommerce_ratemyopenapi_api_key" {
   count        = var.env_short == "p" ? 1 : 0
-  name         = "ecommerce-github-bot-ratemyopenapi-api-key"
+  name         = "ecommerce-ratemyopenapi-api-key"
   value        = "<TO UPDATE MANUALLY ON PORTAL>"
   key_vault_id = module.key_vault.id
 
