10555a0dfd6d6c3587ea5735de8e5e458539d7a3
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-09-11 14:39:49 +0200

    refactor nsg

--- src/network/99_locals.tf
+++ src/network/99_locals.tf
@@ -8,8 +8,6 @@ locals {
   vnet_italy_resource_group_name = "${local.product}-itn-vnet-rg"
   vnet_core_resource_group_name  = "${local.product}-vnet-rg"
   vnet_core_name                 = "${local.product}-vnet"
-  vnet_replica_name                 = "${local.product}-neu-core-replica-vnet"
-  vnet_replica_resource_group_name                 = "${local.product}-vnet-rg"
 
 
 
@@ -31,23 +29,15 @@ locals {
         {
           name    = local.vnet_core_name
           rg_name = local.vnet_core_resource_group_name
+        },
+        {
+          name    = local.vnet_italy_name
+          rg_name = local.vnet_italy_resource_group_name
         }
       ]
       log_analytics_workspace_name = local.log_analytics_weu_workspace_name
       log_analytics_workspace_rg   = local.log_analytics_weu_workspace_resource_group_name
       nsg = local.westeurope_nsg
-    }
-    northeurope = {
-      short_name = "neu"
-      vnets = [
-        {
-          name    = local.vnet_replica_name
-          rg_name = local.vnet_replica_resource_group_name
-        }
-      ]
-      log_analytics_workspace_name = local.log_analytics_weu_workspace_name #fixme non esiste in neu
-      log_analytics_workspace_rg   = local.log_analytics_weu_workspace_resource_group_name #fixme non esiste in neu
-      nsg = local.northeurope_nsg
     }
     italynorth = {
       short_name                   = "itn"
@@ -55,6 +45,10 @@ locals {
         {
           name    = local.vnet_italy_name
           rg_name = local.vnet_italy_resource_group_name
+        },
+        {
+          name    = local.vnet_core_name
+          rg_name = local.vnet_core_resource_group_name
         }
       ]
       log_analytics_workspace_name = local.log_analytics_italy_workspace_name
--- src/network/99_nsg_definitions.tf
+++ src/network/99_nsg_definitions.tf
@@ -1,18 +1,14 @@
+
 locals {
   westeurope_nsg = {
     fdrWeuPostgre = {
       target_subnet_name      = "pagopa-${var.env_short}-weu-fdr-pgres-flexible-snet"
       target_subnet_vnet_name = local.vnet_core_name
-      watcher_enabled         = false
+      watcher_enabled         = var.nsg_network_watcher_enabled
 
       inbound_rules  = [
-        {
-          name                       = "AllowPostgreSQL"
-          priority                   = 200
-          target_service             = "postgresql"
-          source_address_prefixes    = ["*"]
-          description                = "Allow all subnets to access PostgreSQL"
-        }
+        local.nsg_rule_library.allow_all_to_postgres,
+        local.nsg_rule_library.allow_vpn_to_postgres
       ]
       outbound_rules = []
     }
@@ -20,16 +16,11 @@ locals {
     gpsWeuPostgre = {
       target_subnet_name      = "pagopa-${var.env_short}-pgres-flexible-snet"
       target_subnet_vnet_name = local.vnet_core_name
-      watcher_enabled         = false
+      watcher_enabled         = var.nsg_network_watcher_enabled
 
       inbound_rules  = [
-        {
-          name                       = "AllowPostgreSQL"
-          priority                   = 200
-          target_service             = "postgresql"
-          source_address_prefixes    = ["*"]
-          description                = "Allow all subnets to access PostgreSQL"
-        }
+        local.nsg_rule_library.allow_all_to_postgres,
+        local.nsg_rule_library.allow_vpn_to_postgres
       ]
       outbound_rules = []
     }
@@ -37,16 +28,11 @@ locals {
     nodoWeuPostgre = {
       target_subnet_name      = "pagopa-${var.env_short}-weu-nodo-pgres-flexible-snet"
       target_subnet_vnet_name = local.vnet_core_name
-      watcher_enabled         = false
+      watcher_enabled         = var.nsg_network_watcher_enabled
 
       inbound_rules  = [
-        {
-          name                       = "AllowPostgreSQL"
-          priority                   = 200
-          target_service             = "postgresql"
-          source_address_prefixes    = ["*"]
-          description                = "Allow all subnets to access PostgreSQL"
-        }
+        local.nsg_rule_library.allow_all_to_postgres,
+        local.nsg_rule_library.allow_vpn_to_postgres
       ]
       outbound_rules = []
     }
@@ -54,16 +40,11 @@ locals {
     nodoStoricoWeuPostgre = {
       target_subnet_name      = "pagopa-${var.env_short}-weu-nodo-storico-pgres-flexible-snet"
       target_subnet_vnet_name = local.vnet_core_name
-      watcher_enabled         = false
+      watcher_enabled         = var.nsg_network_watcher_enabled
 
       inbound_rules  = [
-        {
-          name                       = "AllowPostgreSQL"
-          priority                   = 200
-          target_service             = "postgresql"
-          source_address_prefixes    = ["*"]
-          description                = "Allow all subnets to access PostgreSQL"
-        }
+        local.nsg_rule_library.allow_all_to_postgres,
+        local.nsg_rule_library.allow_vpn_to_postgres
       ]
       outbound_rules = []
     }
@@ -73,16 +54,11 @@ locals {
     crusc8ItnPostgre = {
       target_subnet_name      = "pagopa-${var.env_short}-itn-crusc8-pgres-flexible-snet"
       target_subnet_vnet_name = local.vnet_italy_name
-      watcher_enabled         = false
+      watcher_enabled         = var.nsg_network_watcher_enabled
 
       inbound_rules  = [
-        {
-          name                       = "AllowPostgreSQL"
-          priority                   = 200
-          target_service             = "postgresql"
-          source_address_prefixes    = ["*"]
-          description                = "Allow all subnets to access PostgreSQL"
-        }
+        local.nsg_rule_library.allow_all_to_postgres,
+        local.nsg_rule_library.allow_vpn_to_postgres
       ]
       outbound_rules = []
     }
@@ -92,73 +68,112 @@ locals {
     metabaseItnPostgre = {
       target_subnet_name      = "pagopa-${var.env_short}-itn-dbsecurity-pgflex-snet"
       target_subnet_vnet_name = local.vnet_italy_name
-      watcher_enabled         = false
+      watcher_enabled         = var.nsg_network_watcher_enabled
 
       inbound_rules  = [
-        {
-          name                       = "AllowPostgreSQL"
-          priority                   = 200
-          target_service             = "postgresql"
-          source_address_prefixes    = ["*"]
-          description                = "Allow all subnets to access PostgreSQL"
-        }
+        local.nsg_rule_library.allow_all_to_postgres,
+        local.nsg_rule_library.allow_vpn_to_postgres
+      ]
+      outbound_rules = []
+    }
+
+    fdrItnPostgre = {
+      target_subnet_name      = "pagopa-${var.env_short}-itn-fdr-pgres-flexible-snet"
+      target_subnet_vnet_name = local.vnet_italy_name
+      watcher_enabled         = var.nsg_network_watcher_enabled
+
+      inbound_rules  = [
+        local.nsg_rule_library.allow_all_to_postgres,
+        local.nsg_rule_library.allow_vpn_to_postgres
+      ]
+      outbound_rules = []
+    }
+
+
+    gpsItnPostgre = {
+      target_subnet_name      = "pagopa-${var.env_short}-itn-gps-pgres-flexible-snet"
+      target_subnet_vnet_name = local.vnet_italy_name
+      watcher_enabled         = var.nsg_network_watcher_enabled
+
+      inbound_rules  = [
+        local.nsg_rule_library.allow_all_to_postgres,
+        local.nsg_rule_library.allow_vpn_to_postgres
+      ]
+      outbound_rules = []
+    }
+
+    nodoItnPostgre = {
+      target_subnet_name      = "pagopa-${var.env_short}-itn-nodo-pgres-flexible-snet"
+      target_subnet_vnet_name = local.vnet_italy_name
+      watcher_enabled         = var.nsg_network_watcher_enabled
+
+      inbound_rules  = [
+        local.nsg_rule_library.allow_all_to_postgres,
+        local.nsg_rule_library.allow_vpn_to_postgres
       ]
       outbound_rules = []
     }
   }
 
-  northeurope_nsg = {
-    fdrNeuPostgre = {
-      target_subnet_name      = "pagopa-${var.env_short}-neu-fdr-pgres-flexible-snet"
-      target_subnet_vnet_name = local.vnet_replica_name
-      watcher_enabled         = false
 
-      inbound_rules  = [
-        {
+  nsg_rule_library = {
+      allow_all_to_postgres = {
           name                       = "AllowPostgreSQL"
-          priority                   = 200
+          priority                   = 100
           target_service             = "postgresql"
           source_address_prefixes    = ["*"]
           description                = "Allow all subnets to access PostgreSQL"
-        }
-      ]
-      outbound_rules = []
-    }
-
-
-    gpsNeuPostgre = {
-      target_subnet_name      = "pagopa-${var.env_short}-neu-gps-pgres-flexible-snet"
-      target_subnet_vnet_name = local.vnet_replica_name
-      watcher_enabled         = false
-
-      inbound_rules  = [
-        {
-          name                       = "AllowPostgreSQL"
-          priority                   = 200
+      }
+      allow_vpn_to_postgres = {
+        name                       = "AllowVpnPostgreSQL"
+        priority                   = 300
+        target_service             = "postgresql"
+        source_subnet_name         = "GatewaySubnet"
+        source_subnet_vnet_name    = "pagopa-${var.env_short}-vnet"
+        description                = "Allow vpn to access PostgreSQL"
+      }
+      allow_aks_weu_to_postgres = {
+          name                       = "AllowAKSWeuPostgreSQL"
+          priority                   = 400
           target_service             = "postgresql"
-          source_address_prefixes    = ["*"]
-          description                = "Allow all subnets to access PostgreSQL"
-        }
-      ]
-      outbound_rules = []
-    }
-
-    nodoNeuPostgre = {
-      target_subnet_name      = "pagopa-${var.env_short}-neu-nodo-pgres-flexible-snet"
-      target_subnet_vnet_name = local.vnet_replica_name
-      watcher_enabled         = false
-
-      inbound_rules  = [
-        {
-          name                       = "AllowPostgreSQL"
-          priority                   = 200
+          source_subnet_name         = "pagopa-${var.env_short}-weu-${var.env}-aks-snet"
+          source_subnet_vnet_name    = "pagopa-${var.env_short}-vnet"
+          description                = "Allow AKS weu to access PostgreSQL"
+      }
+      allow_aks_itn_user_to_postgres = {
+          name                       = "AllowAKSItnUserPostgreSQL"
+          priority                   = 400
           target_service             = "postgresql"
-          source_address_prefixes    = ["*"]
-          description                = "Allow all subnets to access PostgreSQL"
-        }
-      ]
-      outbound_rules = []
-    }
+          source_subnet_name         = "pagopa-${var.env_short}-itn-${var.env}-user-aks"
+          source_subnet_vnet_name    = "pagopa-${var.env_short}-itn-vnet"
+          description                = "Allow AKS user itn to access PostgreSQL"
+      }
+      allow_aks_itn_system_to_postgres = {
+          name                       = "AllowAKSItnSystemPostgreSQL"
+          priority                   = 400
+          target_service             = "postgresql"
+          source_subnet_name         = "pagopa-${var.env_short}-itn-${var.env}-system-aks"
+          source_subnet_vnet_name    = "pagopa-${var.env_short}-itn-vnet"
+          description                = "Allow AKS system itn to access PostgreSQL"
+      }
+      allow_aks_itn_paywallet_to_postgres = {
+          name                       = "AllowAKSItnPaywalletPostgreSQL"
+          priority                   = 400
+          target_service             = "postgresql"
+          source_subnet_name         = "pagopa-${var.env_short}-itn-pay-wallet-user-aks"
+          source_subnet_vnet_name    = "pagopa-${var.env_short}-itn-vnet"
+          description                = "Allow AKS pay wallet itn to access PostgreSQL"
+      }
+      allow_metabase_to_postgres = {
+          name                       = "AllowMetabasePostgreSQL"
+          priority                   = 400
+          target_service             = "postgresql"
+          source_subnet_name         = "pagopa-${var.env_short}-itn-dbsecurity-app-snet"
+          source_subnet_vnet_name    = "pagopa-${var.env_short}-itn-vnet"
+          description                = "Allow metabase to access PostgreSQL"
+      }
+
 
   }
+
 }
