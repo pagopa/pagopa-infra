48196ffb495021360c3f7bc7bae1d00fe0bca3f0
Federico Ruzzier <federicoruzzier96@gmail.com>
2025-09-22 17:23:43 +0200

    chore: add v2 api for gpd-upload PAGOPA-3285

--- src/domains/gps-app/04_apim_gpd_upload.tf
+++ src/domains/gps-app/04_apim_gpd_upload.tf
@@ -8,7 +8,7 @@ locals {
   apim_gpd_upload_api = {
     display_name          = "GPD Upload pagoPA - Massive Upload"
     description           = "API to support Debt Positions massive upload for organizations"
-    path                  = "upload/gpd/debt-positions-service"
+    path                  = "gpd/upload/debt-positions-service"
     subscription_required = true
     service_url           = var.env == "prod" ? "https://weu${var.env}.gps.internal.platform.pagopa.it/pagopa-gpd-upload" : "https://weu${var.env}.gps.internal.${var.env}.platform.pagopa.it/pagopa-gpd-upload"
   }
@@ -33,7 +33,7 @@ module "apim_gpd_upload_api_v1" {
   product_ids           = [module.apim_debt_positions_product.product_id, module.apim_gpd_integration_product.product_id]
   subscription_required = local.apim_gpd_upload_api.subscription_required
   version_set_id        = azurerm_api_management_api_version_set.apim_gpd_upload_api.id
-  api_version           = "v1"
+  api_version           = "v2"
 
   description  = local.apim_gpd_upload_api.description
   display_name = local.apim_gpd_upload_api.display_name
@@ -47,6 +47,33 @@ module "apim_gpd_upload_api_v1" {
   })
 
   xml_content = templatefile("./api/gpd-upload-service/v1/_base_policy.xml", {
+    service_type_value = "GPD"
+  })
+}
+
+module "apim_gpd_upload_api_v2" {
+  source = "./.terraform/modules/__v3__/api_management_api"
+
+  name                  = "${local.product}-gpd-upload-api"
+  api_management_name   = local.pagopa_apim_name
+  resource_group_name   = local.pagopa_apim_rg
+  product_ids           = [module.apim_debt_positions_product.product_id, module.apim_gpd_integration_product.product_id]
+  subscription_required = local.apim_gpd_upload_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.apim_gpd_upload_api.id
+  api_version           = "v1"
+
+  description  = local.apim_gpd_upload_api.description
+  display_name = local.apim_gpd_upload_api.display_name
+  path         = local.apim_gpd_upload_api.path
+  protocols    = ["https"]
+  service_url  = local.apim_gpd_upload_api.service_url
+
+  content_format = "openapi"
+  content_value = templatefile("./api/gpd-upload-service/v2/_openapi.json.tpl", {
+    host = local.apim_hostname
+  })
+
+  xml_content = templatefile("./api/gpd-upload-service/v2/_base_policy.xml", {
     service_type_value = "GPD"
   })
 }
