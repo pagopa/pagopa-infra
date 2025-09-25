6ba7877852ee41a659ca35973ff99100522b24aa
Jacopo Carlini <jacopo.carlini@pagopa.it>
2025-09-25 16:41:25 +0200

    feat: apiconfig cache product for external

--- src/domains/apiconfig-app/05_subkey.tf
+++ src/domains/apiconfig-app/05_subkey.tf
@@ -1,7 +1,7 @@
 resource "azurerm_api_management_subscription" "apiconfig_cache_for_rtp_subkey" {
   api_management_name = data.azurerm_api_management.apim.name
   resource_group_name = data.azurerm_api_management.apim.resource_group_name
-  product_id          = module.apim_apiconfig_cache_product.id
+  product_id          = module.apim_apiconfig_cache_external_product.id
   display_name        = "ApiConfig Cache for RTP"
   allow_tracing       = false
   state               = "active"
