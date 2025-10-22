e72b7bc85f4bd6a1899058ebfb7a4b72ebc3ce89
sfarralorenzo <lorenzo.sfarra@pagopa.it>
2025-10-22 18:59:28 +0200

    feat: [CHK-4543] add ecommerce dns zone (#3432)
    * feat: add ecommerce dns zone
    
    * chore: remove comments
    
    * fix: prefer interpolation
    
    * fix: update alert for payment-request
    
    ---------
    
    Co-authored-by: Simone Infante <52280205+infantesimone@users.noreply.github.com>

---
+++ src/domains/ecommerce-common/00_dns_public.tf
@@ -0,0 +1,28 @@
+resource "azurerm_dns_zone" "ecommerce_public" {
+  count               = (var.dns_zone_ecommerce == null || var.external_domain == null) ? 0 : 1
+  name                = join(".", [var.dns_zone_ecommerce, var.external_domain])
+  resource_group_name = data.azurerm_resource_group.rg_vnet.name
+
+  tags = module.tag_config.tags
+}
+
+resource "azurerm_dns_caa_record" "ecommerce_pagopa_it" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.ecommerce_public[0].name
+  resource_group_name = data.azurerm_resource_group.rg_vnet.name
+  ttl                 = var.dns_default_ttl_sec
+
+  record {
+    flags = 0
+    tag   = "issue"
+    value = "letsencrypt.org"
+  }
+
+  record {
+    flags = 0
+    tag   = "iodef"
+    value = "mailto:security+caa@pagopa.it"
+  }
+
+  tags = module.tag_config.tags
+}
