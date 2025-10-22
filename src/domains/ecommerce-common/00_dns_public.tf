8d1deff1466995a5ec2b047d615b3cbab8da02c3
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2025-10-22 19:04:28 +0200

    set dns record for dev and uat for ecommerce.pagopa.it

--- src/domains/ecommerce-common/00_dns_public.tf
+++ src/domains/ecommerce-common/00_dns_public.tf
@@ -6,6 +6,38 @@ resource "azurerm_dns_zone" "ecommerce_public" {
   tags = module.tag_config.tags
 }
 
+//Prod ONLY record to DEV public DNS delegation
+resource "azurerm_dns_ns_record" "dev_checkout" {
+  count               = var.env_short == "p" ? 1 : 0
+  name                = "dev"
+  zone_name           = azurerm_dns_zone.ecommerce_public[0].name
+  resource_group_name = data.azurerm_resource_group.rg_vnet.name
+  records = [
+    "ns1-02.azure-dns.com.",
+    "ns2-02.azure-dns.net.",
+    "ns3-02.azure-dns.org.",
+    "ns4-02.azure-dns.info."
+  ]
+  ttl  = var.dns_default_ttl_sec
+  tags = module.tag_config.tags
+}
+
+//Prod ONLY record to UAT public DNS delegation
+resource "azurerm_dns_ns_record" "uat_checkout" {
+  count               = var.env_short == "p" ? 1 : 0
+  name                = "uat"
+  zone_name           = azurerm_dns_zone.ecommerce_public[0].name
+  resource_group_name = data.azurerm_resource_group.rg_vnet.name
+  records = [
+    "ns1-06.azure-dns.com.",
+    "ns2-06.azure-dns.net.",
+    "ns3-06.azure-dns.org.",
+    "ns4-06.azure-dns.info."
+  ]
+  ttl  = var.dns_default_ttl_sec
+  tags = module.tag_config.tags
+}
+
 resource "azurerm_dns_caa_record" "ecommerce_pagopa_it" {
   name                = "@"
   zone_name           = azurerm_dns_zone.ecommerce_public[0].name
