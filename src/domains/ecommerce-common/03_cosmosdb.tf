9ec0d7298b711f04a0838b1d79028cdf649ede61
danilo.ciano <danilo.ciano@pagopa.it>
2025-10-31 15:53:47 +0100

    chore(cosmos): add comment

--- src/domains/ecommerce-common/03_cosmosdb.tf
+++ src/domains/ecommerce-common/03_cosmosdb.tf
@@ -138,7 +138,7 @@ locals {
         }
       ]
       shard_key           = "transactionId",
-      default_ttl_seconds = "315360000"
+      default_ttl_seconds = "315360000"  #10 years
     },
     {
       name = "transactions-view"
@@ -168,7 +168,7 @@ locals {
         }
       ]
       shard_key           = "_id",
-      default_ttl_seconds = "315360000"
+      default_ttl_seconds = "315360000" #10 years
     },
     {
       name = "dead-letter-events"
