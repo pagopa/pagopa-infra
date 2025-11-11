de359e3776a55f7f33eb536b7ecbce302f7e5870
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-11-11 12:35:34 +0100

    added nexi db configuration

--- src/network/00_data.tf
+++ src/network/00_data.tf
@@ -1,4 +1,17 @@
-data "azurerm_key_vault" "kv_core" {
+data "azurerm_key_vault" "kv_domain" {
   name                = "${local.project}-kv"
   resource_group_name = "${local.project}-sec-rg"
+}
+
+
+data "azurerm_key_vault_secret" "external_database_username" {
+  for_each     = var.external_database_connection
+  name         = each.value.user_secret_name
+  key_vault_id = data.azurerm_key_vault.kv_domain.id
+}
+
+data "azurerm_key_vault_secret" "external_database_password" {
+  for_each     = var.external_database_connection
+  name         = each.value.password_secret_name
+  key_vault_id = data.azurerm_key_vault.kv_domain.id
 }
