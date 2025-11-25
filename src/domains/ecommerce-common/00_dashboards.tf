0b1b1cc58508bfdd01ea2ade57734d8b7ef93f14
pietro.tota <pietro.tota@pagopa.it>
2025-11-25 15:17:04 +0100

    fix: refactoring + pre-commit

--- src/domains/ecommerce-common/00_dashboards.tf
+++ src/domains/ecommerce-common/00_dashboards.tf
@@ -9,9 +9,11 @@ resource "azurerm_portal_dashboard" "ecommerce_mongodb_dashboard" {
   #convert json dashboard output from Azure export function to the one handled by azurerm portal dashboard provider
   #see https://github.com/hashicorp/terraform-provider-azurerm/issues/27117 issue for more info
   dashboard_properties = jsonencode(
+    merge(
+      jsondecode(file("./dashboards/ecommerce-mongodb-dashboard.json")).properties,
       {
         "lenses" = {
-          for lens_index, lens in jsondecode(file("./dashboards/eCommerce CDC Mongo DB monitoring.json")).properties.lenses :
+          for lens_index, lens in jsondecode(file("./dashboards/ecommerce-mongodb-dashboard.json")).properties.lenses :
           tostring(lens_index) => merge(lens, {
             "parts" = {
               for part_index, part in lens.parts :
@@ -21,5 +23,6 @@ resource "azurerm_portal_dashboard" "ecommerce_mongodb_dashboard" {
         }
       }
     )
-  
+  )
+
 }
