911b87cd3e89ef56630e9420bcbffc155f5abedb
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2025-11-26 17:42:59 +0100

    feat: [CHK-4602] add checkout payment wallet terraform config

---
+++ src/domains/checkout-app/04_apim_checkout_payment_wallet.tf
@@ -0,0 +1,53 @@
+data "azurerm_api_management_product" "apim_payment_wallet_product" {
+  product_id          = "payment-wallet"
+  api_management_name = data.azurerm_api_management.apim.name
+  resource_group_name = data.azurerm_resource_group.rg_api.name
+}
+
+locals {
+  apim_checkout_payment_wallet_api = {
+    display_name          = "Checkout - Payment Wallet API"
+    description           = "API dedicated to checkout to retrieve wallet data useful to show to the authenticated user."
+    path                  = "checkout/payment-wallet"
+    subscription_required = true
+    service_url           = null
+  }
+}
+
+resource "azurerm_api_management_api_version_set" "checkout_payment_wallet_api_v1" {
+  name                = "${local.parent_project}-payment-wallet-api"
+  resource_group_name = data.azurerm_resource_group.rg_api.name
+  api_management_name = data.azurerm_api_management.apim.name
+  display_name        = local.apim_checkout_payment_wallet_api.display_name
+  versioning_scheme   = "Segment"
+}
+
+module "apim_checkout_payment_wallet_api_v1" {
+  source = "./.terraform/modules/__v3__/api_management_api"
+
+  name                  = "${local.parent_project}-payment-wallet-api"
+  api_management_name   = data.azurerm_api_management.apim.name
+  resource_group_name   = data.azurerm_resource_group.rg_api.name
+  product_ids           = [data.azurerm_api_management_product.apim_payment_wallet_product.product_id]
+  subscription_required = local.apim_checkout_payment_wallet_api.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.checkout_payment_wallet_api_v1.id
+  api_version           = "v1"
+  service_url           = local.apim_checkout_payment_wallet_api.service_url
+
+  description  = local.apim_checkout_payment_wallet_api.description
+  display_name = local.apim_checkout_payment_wallet_api.display_name
+  path         = local.apim_checkout_payment_wallet_api.path
+  protocols    = ["https"]
+
+  content_format = "openapi"
+  content_value = templatefile("./api/checkout/checkout_payment_wallet/v1/_openapi.json.tpl", {
+    host = local.apim_hostname
+  })
+
+  xml_content = templatefile("./api/checkout/checkout_payment_wallet/v1/_base_policy.xml.tpl", {
+    hostname                  = local.payment_wallet_hostname
+    checkout_ingress_hostname = local.checkout_ingress_hostname
+    pdv_api_base_path         = var.pdv_api_base_path
+    checkout_origin           = var.env_short == "d" ? "*" : "https://${var.dns_zone_checkout}.${var.external_domain}"
+  })
+}
