1646d650c87d69122e74a8454ab106ef89d3f44a
svariant <samuele.varianti@nttdata.com>
2025-12-11 10:19:15 +0100

    feat: [PIDM-1272] [RECEIPT] Fix alert cosmos ru provisioned throughput

--- src/domains/receipts-common/03_cosmosdb.tf
+++ src/domains/receipts-common/03_cosmosdb.tf
@@ -230,10 +230,10 @@ resource "azurerm_monitor_metric_alert" "cosmos_db_normalized_ru_exceeded" {
 }
 
 
-resource "azurerm_monitor_metric_alert" "cosmos_db_provisioned_throughput_exceeded_ProvisionedThroughput" { # https://github.com/pagopa/terraform-azurerm-v3/blob/58f14dc120e10bd3515bcc34e0685e74d1d11047/cosmosdb_account/main.tf#L205
+resource "azurerm_monitor_metric_alert" "cosmos_db_provisioned_throughput_exceeded" { # https://github.com/pagopa/terraform-azurerm-v3/blob/58f14dc120e10bd3515bcc34e0685e74d1d11047/cosmosdb_account/main.tf#L205
   count = var.env_short == "p" ? 1 : 0
 
-  name                = "[${var.domain != null ? "${var.domain} | " : ""}${module.receipts_datastore_cosmosdb_account.name}] Provisioned Throughput Exceeded - ProvisionedThroughput"
+  name                = "[${var.domain != null ? "${var.domain} | " : ""}${module.receipts_datastore_cosmosdb_account.name}] Provisioned Throughput Exceeded"
   resource_group_name = azurerm_resource_group.receipts_rg.name
   scopes              = [module.receipts_datastore_cosmosdb_account.id]
   description         = "A collection throughput (RU/s) exceed provisioned throughput, and it's raising 429 errors. Please, consider to increase RU. Runbook: not needed."
