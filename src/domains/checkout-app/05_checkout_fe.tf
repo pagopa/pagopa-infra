5aa7da28591a80e843c44cdaf7e7dbe99192f788
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2026-01-15 17:07:22 +0100

    chore: [PIDM-1151] revert file to use classic cdn

--- src/domains/checkout-app/05_checkout_fe.tf
+++ src/domains/checkout-app/05_checkout_fe.tf
@@ -2,80 +2,141 @@
 locals {
   npg_sdk_hostname                    = var.env_short == "p" ? "xpay.nexigroup.com" : "stg-ta.nexigroup.com"
   content_security_policy_header_name = "Content-Security-Policy"
-  cdn_storage_account_name            = "${local.project}cdnsa"
-  cdn_index_document                  = "index.html"
-  cdn_error_document                  = "index.html"
-  # DNS Zone Key for the main CDN (the one configured in the module)
-  dns_zone_key = "${var.dns_zone_checkout}.${var.external_domain}"
+}
 
-  custom_domains = var.checkout_enabled ? [
-    {
-      domain_name             = local.dns_zone_key
-      dns_name                = data.azurerm_dns_zone.checkout_public[0].name
-      dns_resource_group_name = data.azurerm_dns_zone.checkout_public[0].resource_group_name
-      ttl                     = var.dns_default_ttl_sec
+/**
+ * Checkout resource group
+ **/
+resource "azurerm_resource_group" "checkout_fe_rg" {
+  count    = var.checkout_enabled ? 1 : 0
+  name     = format("%s-checkout-fe-rg", local.parent_project)
+  location = var.location
+
+  tags = module.tag_config.tags
+}
+
+/**
+ * CDN
+ */
+module "checkout_cdn" {
+  source = "./.terraform/modules/__v4__/cdn"
+
+  count                 = var.checkout_enabled ? 1 : 0
+  name                  = "checkout"
+  prefix                = local.parent_project
+  resource_group_name   = azurerm_resource_group.checkout_fe_rg[0].name
+  location              = var.location
+  hostname              = format("%s.%s", var.dns_zone_checkout, var.external_domain)
+  https_rewrite_enabled = true
+
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.log_analytics.id
+
+  index_document     = "index.html"
+  error_404_document = "index.html"
+
+  dns_zone_name                = data.azurerm_dns_zone.checkout_public[0].name
+  dns_zone_resource_group_name = data.azurerm_dns_zone.checkout_public[0].resource_group_name
+
+  keyvault_resource_group_name = data.azurerm_key_vault.key_vault.resource_group_name
+  keyvault_subscription_id     = data.azurerm_subscription.current.subscription_id
+  keyvault_vault_name          = data.azurerm_key_vault.key_vault.name
+
+  querystring_caching_behaviour = "BypassCaching"
+
+  storage_account_replication_type = var.checkout_cdn_storage_replication_type
+
+  global_delivery_rule = {
+    cache_expiration_action       = []
+    cache_key_query_string_action = []
+    modify_request_header_action  = []
+
+    # HSTS
+    modify_response_header_action = [{
+      action = "Overwrite"
+      name   = "Strict-Transport-Security"
+      value  = "max-age=31536000"
+      },
+      # Content-Security-Policy
+      {
+        action = "Overwrite"
+        name   = local.content_security_policy_header_name
+        value  = format("default-src 'self'; connect-src 'self' https://api.%s.%s https://api-eu.mixpanel.com https://wisp2.pagopa.gov.it https://privacyportalde-cdn.onetrust.com https://privacyportal-de.onetrust.com", var.dns_zone_prefix, var.external_domain)
+      },
+      {
+        action = "Append"
+        name   = local.content_security_policy_header_name
+        value  = " https://acardste.vaservices.eu:* https://recaptcha.net/;"
+      },
+      {
+        action = "Append"
+        name   = local.content_security_policy_header_name
+        value  = "frame-ancestors 'none'; object-src 'none'; frame-src 'self' https://www.google.com *.platform.pagopa.it *.sia.eu *.nexigroup.com *.recaptcha.net recaptcha.net https://recaptcha.google.com;"
+      },
+      {
+        action = "Append"
+        name   = local.content_security_policy_header_name
+        value  = "img-src 'self' https://acardste.vaservices.eu:* https://wisp2.pagopa.gov.it https://assets.cdn.io.italia.it www.gstatic.com/recaptcha data: https://assets.cdn.platform.pagopa.it https://privacyportalde-cdn.onetrust.com;"
+      },
+      {
+        action = "Append"
+        name   = local.content_security_policy_header_name
+        value  = "script-src 'self' 'sha256-LIYUdRhA1kkKYXZ4mrNoTMM7+5ehEwuxwv4/FRhgems=' https://www.google.com https://www.gstatic.com https://www.recaptcha.net https://recaptcha.net https://www.gstatic.com/recaptcha/ https://www.gstatic.cn/recaptcha/ https://privacyportalde-cdn.onetrust.com https://${local.npg_sdk_hostname};"
+      },
+      {
+        action = "Append"
+        name   = local.content_security_policy_header_name
+        value  = "style-src 'self'  'unsafe-inline' https://privacyportalde-cdn.onetrust.com; font-src 'self' https://privacyportalde-cdn.onetrust.com; worker-src www.recaptcha.net blob:;"
+      },
+      {
+        action = "Overwrite"
+        name   = "X-Frame-Options"
+        value  = "SAMEORIGIN"
+      },
+    ]
+  }
+
+  delivery_rule_rewrite = [{
+    name  = "RewriteRules"
+    order = 2
+
+    conditions = [{
+      condition_type   = "url_path_condition"
+      operator         = "EndsWith"
+      match_values     = ["/dona", "/dona/"]
+      transforms       = []
+      negate_condition = false
+    }]
+
+    url_rewrite_action = {
+      source_pattern          = "/"
+      destination             = "/dona.html"
+      preserve_unmatched_path = false
     }
-  ] : []
-
-  global_delivery_rules = [
-    {
-      order = 1
-      # HSTS and Content-Security-Policy
-      modify_response_header_actions = [
-        {
-          action = "Overwrite"
-          name   = "Strict-Transport-Security"
-          value  = "max-age=31536000"
-        },
-        # Content-Security-Policy
-        {
-          action = "Overwrite"
-          name   = local.content_security_policy_header_name
-          value  = format("default-src 'self'; connect-src 'self' https://api.%s.%s https://api-eu.mixpanel.com https://wisp2.pagopa.gov.it https://privacyportalde-cdn.onetrust.com https://privacyportal-de.onetrust.com", var.dns_zone_prefix, var.external_domain)
-        },
-        {
-          action = "Append"
-          name   = local.content_security_policy_header_name
-          value  = " https://acardste.vaservices.eu:* https://recaptcha.net/;"
-        },
-        {
-          action = "Append"
-          name   = local.content_security_policy_header_name
-          value  = "frame-ancestors 'none'; object-src 'none'; frame-src 'self' https://www.google.com *.platform.pagopa.it *.sia.eu *.nexigroup.com *.recaptcha.net recaptcha.net https://recaptcha.google.com;"
-        }
-      ]
     },
     {
-      order = 2
-      modify_response_header_actions = [
-        {
-          action = "Append"
-          name   = local.content_security_policy_header_name
-          value  = "img-src 'self' https://acardste.vaservices.eu:* https://wisp2.pagopa.gov.it https://assets.cdn.io.italia.it www.gstatic.com/recaptcha data: https://assets.cdn.platform.pagopa.it https://privacyportalde-cdn.onetrust.com;"
-        },
-        {
-          action = "Append"
-          name   = local.content_security_policy_header_name
-          value  = "script-src 'self' 'sha256-LIYUdRhA1kkKYXZ4mrNoTMM7+5ehEwuxwv4/FRhgems=' https://www.google.com https://www.gstatic.com https://www.recaptcha.net https://recaptcha.net https://www.gstatic.com/recaptcha/ https://www.gstatic.cn/recaptcha/ https://privacyportalde-cdn.onetrust.com https://${local.npg_sdk_hostname};"
-        },
-        {
-          action = "Append"
-          name   = local.content_security_policy_header_name
-          value  = "style-src 'self'  'unsafe-inline' https://privacyportalde-cdn.onetrust.com; font-src 'self' https://privacyportalde-cdn.onetrust.com; worker-src www.recaptcha.net blob:;"
-        },
-        {
-          action = "Overwrite"
-          name   = "X-Frame-Options"
-          value  = "SAMEORIGIN"
-        }
-      ]
+      name  = "RewriteRulesTerms"
+      order = 3
+
+      conditions = [{
+        condition_type   = "url_path_condition"
+        operator         = "Equal"
+        match_values     = ["/termini-di-servizio"]
+        transforms       = []
+        negate_condition = false
+      }]
+
+      url_rewrite_action = {
+        source_pattern          = "/"
+        destination             = "/terms/it.html"
+        preserve_unmatched_path = false
+      }
     }
   ]
 
-  delivery_custom_rules = [
+  delivery_rule = [
     {
       name  = "CorsFontForNPG"
-      order = 5
+      order = 4
 
       // conditions
       url_path_conditions       = []
@@ -105,112 +166,36 @@ locals {
         name   = "Access-Control-Allow-Origin"
         value  = "https://${local.npg_sdk_hostname}"
       }]
-      url_redirect_actions = []
-      url_rewrite_actions  = []
+      cache_expiration_actions       = []
+      cache_key_query_string_actions = []
+      modify_request_header_actions  = []
+      url_redirect_actions           = []
+      url_rewrite_actions            = []
     }
   ]
-
-  delivery_rule_rewrites = [
-    {
-      name  = "RewriteRules"
-      order = 3
-
-      url_path_conditions = [{
-        condition_type   = "url_path_condition"
-        operator         = "EndsWith"
-        match_values     = ["/dona", "/dona/"]
-        transforms       = []
-        negate_condition = false
-      }]
-
-      url_rewrite_actions = [{
-        source_pattern          = "/"
-        destination             = "/dona.html"
-        preserve_unmatched_path = false
-      }]
-    },
-    {
-      name  = "RewriteRulesTerms"
-      order = 4
-
-      url_path_conditions = [{
-        condition_type   = "url_path_condition"
-        operator         = "Equal"
-        match_values     = ["/termini-di-servizio"]
-        transforms       = []
-        negate_condition = false
-      }]
-
-      url_rewrite_actions = [{
-        source_pattern          = "/"
-        destination             = "/terms/it.html"
-        preserve_unmatched_path = false
-      }]
-    }
-  ]
-}
-
-/**
- * Checkout resource group
- **/
-resource "azurerm_resource_group" "checkout_fe_rg" {
-  count    = var.checkout_enabled ? 1 : 0
-  name     = format("%s-checkout-fe-rg", local.parent_project)
-  location = var.location
 
   tags = module.tag_config.tags
 }
 
-/**
- * CDN Front Door
- */
-module "checkout_cdn" {
-  source = "./.terraform/modules/__v4__/cdn_frontdoor"
+resource "azurerm_application_insights_web_test" "checkout_fe_web_test" {
+  count                   = var.checkout_enabled && var.env_short == "p" ? 1 : 0
+  name                    = format("%s-checkout-fe-web-test", local.parent_project)
+  location                = var.location
+  resource_group_name     = data.azurerm_resource_group.monitor_rg.name
+  application_insights_id = data.azurerm_application_insights.application_insights.id
+  kind                    = "ping"
+  frequency               = 300
+  timeout                 = 10
+  enabled                 = true
+  geo_locations           = ["emea-nl-ams-azr"]
 
-  count = var.checkout_enabled ? 1 : 0
+  configuration = <<XML
+<WebTest Name="checkout_fe_web_test" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="10" WorkItemIds=""
+    xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
+    <Items>
+        <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="${format("https://%s.%s/index.html", var.dns_zone_checkout, var.external_domain)}" ThinkTime="0" Timeout="10" ParseDependentRequests="False" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+    </Items>
+</WebTest>
+XML
 
-  cdn_prefix_name     = local.project
-  resource_group_name = azurerm_resource_group.checkout_fe_rg[0].name
-  location            = var.location
-
-  https_rewrite_enabled = true
-
-  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.log_analytics.id
-
-  storage_account_name               = local.cdn_storage_account_name
-  storage_account_index_document     = local.cdn_index_document
-  storage_account_error_404_document = local.cdn_error_document
-  storage_account_replication_type   = var.checkout_cdn_storage_replication_type
-
-  keyvault_id = data.azurerm_key_vault.key_vault.id
-  tenant_id   = data.azurerm_client_config.current.tenant_id
-
-  querystring_caching_behaviour = "IgnoreQueryString"
-
-  custom_domains         = local.custom_domains
-  global_delivery_rules  = local.global_delivery_rules
-  delivery_custom_rules  = local.delivery_custom_rules
-  delivery_rule_rewrites = local.delivery_rule_rewrites
-
-  tags = module.tag_config.tags
-}
-
-module "checkout_fe_web_test" {
-  count                                 = var.checkout_enabled && var.env_short == "p" ? 1 : 0
-  source                                = "./.terraform/modules/__v4__/application_insights_standard_web_test"
-  https_endpoint                        = "https://${module.checkout_cdn[0].fqdn}"
-  https_endpoint_path                   = "/index.html"
-  alert_name                            = "${local.project}-fe-web-test"
-  location                              = var.location
-  alert_enabled                         = true
-  application_insights_resource_group   = data.azurerm_resource_group.monitor_rg.name
-  application_insights_id               = data.azurerm_application_insights.application_insights.id
-  application_insights_action_group_ids = [data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.email.id]
-  https_probe_method                    = "GET"
-  timeout                               = 10
-  frequency                             = 300
-  https_probe_threshold                 = 99
-  metric_frequency                      = "PT5M"
-  metric_window_size                    = "PT1H"
-  retry_enabled                         = true
 }
