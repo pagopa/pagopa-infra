4c4368d893be528559fd9568b8dc1e338fff0f5b
mamari90 <130982006+mamari90@users.noreply.github.com>
2026-01-16 20:32:58 +0100

    precommit

--- src/network/99_locals.tf
+++ src/network/99_locals.tf
@@ -28,32 +28,20 @@ locals {
   nsg_network_regions = {
     westeurope = {
       short_name = "weu"
-      vnets = [
-        {
-          name    = local.vnet_core_name
-          rg_name = local.vnet_core_resource_group_name
-        },
-        {
-          name    = local.vnet_italy_name
-          rg_name = local.vnet_italy_resource_group_name
-        }
-      ]
+      vnets = {
+        "${local.vnet_core_name}"  = local.vnet_core_resource_group_name
+        "${local.vnet_italy_name}" = local.vnet_italy_resource_group_name
+      }
       log_analytics_workspace_name = local.log_analytics_weu_workspace_name
       log_analytics_workspace_rg   = local.log_analytics_weu_workspace_resource_group_name
       nsg                          = local.westeurope_nsg
     }
     italynorth = {
       short_name = "itn"
-      vnets = [
-        {
-          name    = local.vnet_italy_name
-          rg_name = local.vnet_italy_resource_group_name
-        },
-        {
-          name    = local.vnet_core_name
-          rg_name = local.vnet_core_resource_group_name
-        }
-      ]
+      vnets = {
+        "${local.vnet_core_name}"  = local.vnet_core_resource_group_name
+        "${local.vnet_italy_name}" = local.vnet_italy_resource_group_name
+      }
       log_analytics_workspace_name = local.log_analytics_italy_workspace_name
       log_analytics_workspace_rg   = local.log_analytics_italy_workspace_resource_group_name
       nsg                          = local.italynorth_nsg
