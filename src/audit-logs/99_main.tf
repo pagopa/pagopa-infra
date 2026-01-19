23b0b68c16a8344321aa4351e0a6f9a8a7650798
re_sh_cloud_arch <94049389+re-sh-cloud-arch@users.noreply.github.com>
2026-01-19 12:40:27 +0100

    feat: Pagopa audit log (#3583)
    * pagopa audit log
    
    * custom query
    
    * vnet data name
    
    * fix
    
    * rename
    
    * module 1.2.0

---
+++ src/audit-logs/99_main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+
+
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+
+data "azurerm_subscription" "current" {}
+
+data "azurerm_client_config" "current" {}
+
+
+
+
+module "__v4__" {
+  # v8.1.3
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4?ref=da30369e66508e38252f34aa7209c645ba208546"
+}
