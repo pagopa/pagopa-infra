f92a4f096ade0ddd87024864b7745012f8ab0907
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2026-01-27 12:39:38 +0100

    feat: update checkout-app domain to v4 (#3475)
    * feat: update checkout-app domain to v4
    
    * feat: [PIDM-1151] upgrade checkout_payment_wallet modules to v4
    
    * chore: [PIDM-1151] run tf init and pre-commit from scratch
    
    * chore: [PIDM-1151] update azurerm version constraint
    
    * fix: [PIDM-1151] remove duplicate entry
    
    * chore: [PIDM-1151] upgrade tf module version to lates avail
    
    ---------
    
    Co-authored-by: Dylan Tangredi <dylan.tangredi.ext@jakala.com>
    Co-authored-by: Simone infante <52280205+infantesimone@users.noreply.github.com>

--- src/domains/checkout-app/04_apim_checkout_authentication.tf
+++ src/domains/checkout-app/04_apim_checkout_authentication.tf
@@ -9,7 +9,7 @@ locals {
 }
 
 module "apim_checkout_authentication" {
-  source = "./.terraform/modules/__v3__/api_management_product"
+  source = "./.terraform/modules/__v4__/api_management_product"
 
   product_id   = "checkout-authentication"
   display_name = "Checkout Authentication"
@@ -34,7 +34,7 @@ resource "azurerm_api_management_api_version_set" "checkout_auth_service_api_v1"
 }
 
 module "apim_checkout_auth_service_v1" {
-  source = "./.terraform/modules/__v3__/api_management_api"
+  source = "./.terraform/modules/__v4__/api_management_api"
 
   name                  = "${local.project_short}-auth-service-api"
   api_management_name   = data.azurerm_api_management.apim.name
