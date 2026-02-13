d33f8d08208f6ad44e96c457585ac4c414c09d11
pietro.tota <pietro.tota@pagopa.it>
2026-02-13 16:18:15 +0100

    chore: switch index fields

--- src/domains/selfcare-common/03_cosmosdb_bopagopa_datastore.tf
+++ src/domains/selfcare-common/03_cosmosdb_bopagopa_datastore.tf
@@ -197,7 +197,7 @@ module "mongdb_collection_institutions_services_rtp_consent" {
       unique = true
     },
     {
-      keys   = ["consent", "consentDate"]
+      keys   = ["consentDate", "consent"]
       unique = false
     }
   ]
