2f0d95179e512e5389e44404301dad05125c0bed
ffppa <fabio.felici@pagopa.it>
2026-02-18 11:11:54 +0100

    feat: [PAYMCLOUD-574] Bump Terraform module source to v9.0.1 in db-security configuration (#3667)
    chore: Bump Terraform module source to v9.0.1 in db-security configuration
    
    Signed-off-by: ffppa <fabio.felici@pagopa.it>

--- src/db-security/99_main.tf
+++ src/db-security/99_main.tf
@@ -31,6 +31,6 @@ data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
 module "__v4__" {
-  # v9.0.0
-  source = "git::https://github.com/pagopa/terraform-azurerm-v4?ref=a8779d9a41e1afb803cacbcfd778acb2e86e9a0a"
+  # v9.0.1
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4?ref=498aa770edfe95264936e9e8f7e209322e4a3820"
 }
