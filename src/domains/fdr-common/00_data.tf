1160879fe3d81673b225e13bc36a9e823a51187c
Francesco <cesareo.francesco@gmail.com>
2026-02-23 15:17:13 +0100

    clean code

--- src/domains/fdr-common/00_data.tf
+++ src/domains/fdr-common/00_data.tf
@@ -25,3 +25,13 @@ data "azurerm_api_management_product" "fdr_internal_product" {
   api_management_name = data.azurerm_api_management.apim.name
   resource_group_name = data.azurerm_resource_group.rg_api.name
 }
+
+data "azurerm_key_vault_secret" "pgres_flex_admin_login" {
+  name         = "db-administrator-login"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "pgres_flex_admin_pwd" {
+  name         = "db-administrator-login-password"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
