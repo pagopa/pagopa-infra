dc6392ec6ae837b666965c2eaf49caee999a0b51
Federico Ruzzier <federicoruzzier96@gmail.com>
2026-02-25 13:05:05 +0100

    chore: adding linked service for gpd storico PAGOPA-3470

--- src/domains/gps-common/04_datafactory_pipeline.tf
+++ src/domains/gps-common/04_datafactory_pipeline.tf
@@ -42,6 +42,44 @@ resource "azapi_resource" "gpd_postgres_linked_service" {
   }
 }
 
+resource "azapi_resource" "gpd_postgres_archive_linked_service" {
+  depends_on = [
+    azurerm_data_factory_linked_service_key_vault.gps_kv_linked_service
+  ]
+
+  type                      = "Microsoft.DataFactory/factories/linkedservices@2018-06-01"
+  name                      = "gpd-${var.env}-postgres-archive-ls"
+  parent_id                 = data.azurerm_data_factory.data_factory.id
+  schema_validation_enabled = false
+
+  body = {
+    properties = {
+      connectVia = {
+        parameters    = {}
+        referenceName = "AutoResolveIntegrationRuntime"
+        type          = "IntegrationRuntimeReference"
+      }
+      version = "2.0"
+      type    = "AzurePostgreSql"
+      typeProperties = {
+        database = "${var.gpd_db_storico_name}"
+        password = {
+          type = "AzureKeyVaultSecret",
+          store = {
+            referenceName = "${azurerm_data_factory_linked_service_key_vault.gps_kv_linked_service.name}",
+            type          = "LinkedServiceReference"
+          },
+          secretName = "${data.azurerm_key_vault_secret.gpd_db_pwd.name}"
+        }
+        port     = "5433"                                                  // "8432" // adhoc private endpoint port
+        server   = "gpd-storico-db.${var.env_short}.internal.postgresql.pagopa.it" // "172.205.217.81" // adhoc private endpoint host
+        sslMode  = 3
+        username = "${data.azurerm_key_vault_secret.gpd_db_usr.value}"
+      }
+    }
+  }
+}
+
 resource "azurerm_data_factory_pipeline" "pipeline_odp_backfill" {
   depends_on = [
     azapi_resource.gpd_postgres_linked_service
