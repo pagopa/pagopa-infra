ea911ad40764843bafdc77e64b3b3680d0acb265
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2026-02-27 15:04:25 +0100

    fix: [PIDM-1592] - eCommerce azure dashboards (#3699)
    * fix io dashboard tiles
    
    * fix: add redirect to update status + minor fixes
    
    * fix: update tiles
    
    * chore: set default time to 4h
    
    ---------
    
    Co-authored-by: pietro.tota <pietro.tota@pagopa.it>
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>

--- src/domains/ecommerce-common/00_dashboards.tf
+++ src/domains/ecommerce-common/00_dashboards.tf
@@ -82,3 +82,32 @@ resource "azurerm_portal_dashboard" "checkout_payment_flow_dashboard" {
   )
 
 }
+
+resource "azurerm_portal_dashboard" "pagopa_eCommerce_update_transactions_status" {
+  count               = var.env_short == "p" ? 1 : 0
+  name                = "pagopa-eCommerce-update-transactions-status"
+  resource_group_name = var.monitor_resource_group_name
+  location            = var.location
+  tags = {
+    source = "terraform"
+  }
+  #convert json dashboard output from Azure export function to the one handled by azurerm portal dashboard provider
+  #see https://github.com/hashicorp/terraform-provider-azurerm/issues/27117 issue for more info
+  dashboard_properties = jsonencode(
+    merge(
+      jsondecode(file("./dashboards/pagopa-eCommerce-update-transactions-status.json")).properties,
+      {
+        "lenses" = {
+          for lens_index, lens in jsondecode(file("./dashboards/pagopa-eCommerce-update-transactions-status.json")).properties.lenses :
+          tostring(lens_index) => merge(lens, {
+            "parts" = {
+              for part_index, part in lens.parts :
+              tostring(part_index) => part
+            }
+          })
+        }
+      }
+    )
+  )
+
+}
