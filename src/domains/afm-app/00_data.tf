30f3d27ee43acd8eb0d978d87b25779f35f7e99f
svariant <samuele.varianti@nttdata.com>
2026-03-10 15:16:43 +0100

    chore: [PIDM-1633] [AFM] Migrate data apiconfig-fe

--- src/domains/afm-app/00_data.tf
+++ src/domains/afm-app/00_data.tf
@@ -1,7 +1,3 @@
 data "azurerm_resource_group" "identity_rg" {
   name = "${local.product}-identity-rg"
 }
-
-data "azuread_application" "apiconfig-fe" {
-  display_name = format("pagopa-%s-apiconfig-fe", var.env_short)
-}
