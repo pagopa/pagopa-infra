1c771b51dbc49586c2eeb75f8d13c158971bd59a
svariant <samuele.varianti@nttdata.com>
2026-03-18 12:02:02 +0100

    chore: [PIDM-1633] [AFM] Precommit

--- src/domains/afm-app/04_apim_afm_marketplace_jwt.tf
+++ src/domains/afm-app/04_apim_afm_marketplace_jwt.tf
@@ -3,10 +3,10 @@
 #################################
 locals {
   apim_afm_marketplace_service_api_jwt = {
-    display_name          = "AFM Marketplace JWT pagoPA - marketplace of advanced fees management service API with JWT authentication"
-    description           = "Marketplace API to support advanced fees management service with JWT authentication"
-    path                  = "afm/marketplace-service"
-    service_url           = null
+    display_name = "AFM Marketplace JWT pagoPA - marketplace of advanced fees management service API with JWT authentication"
+    description  = "Marketplace API to support advanced fees management service with JWT authentication"
+    path         = "afm/marketplace-service"
+    service_url  = null
   }
 }
 
@@ -24,7 +24,7 @@ module "apim_afm_marketplace_jwt_product" {
   subscription_required = false
   approval_required     = false
 
-  policy_xml = templatefile("./api_product/marketplace/_base_policy_jwt.xml",{
+  policy_xml = templatefile("./api_product/marketplace/_base_policy_jwt.xml", {
     pagopa_tenant_id       = data.azurerm_client_config.current.tenant_id,
     apiconfig_fe_client_id = data.azuread_application.apiconfig-fe.application_id
   })
