fa10764063d38fea40b522bbb68e1701a99f5443
gioelemella <128155546+gioelemella@users.noreply.github.com>
2026-03-24 15:34:41 +0100

    chore: [PIDM-1661] Increase Receipts RU (#3737)
    * [PIDM-1661] persist RU increase for biz-events-view-user container done via portal in PROD
    
    * [PIDM-1661] readme
    
    * [PIDM-1661] increase RU for receipts-io-messages-evt container in PROD

--- src/domains/receipts-common/03_cosmosdb.tf
+++ src/domains/receipts-common/03_cosmosdb.tf
@@ -146,7 +146,7 @@ locals {
       name                       = "receipts-io-messages-evt",
       partition_key_path         = "/eventId",
       default_ttl                = var.receipts_datastore_cosmos_db_params.container_default_ttl
-      autoscale_settings         = { max_throughput = var.receipts_datastore_cosmos_db_params.max_throughput_alt },
+      autoscale_settings         = { max_throughput = var.receipts_datastore_cosmos_db_params.max_throughput_io_messages },
       conflict_resolution_policy = { mode = "LastWriterWins", path = "/_ts", procedure = null }
     },
     {
