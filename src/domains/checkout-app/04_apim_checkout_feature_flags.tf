16312da41d587bc14d8d157c79ecdd26aaf2d0b5
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2026-03-31 12:07:08 +0200

    feat: [PIDM-1671] add ff as data source

--- src/domains/checkout-app/04_apim_checkout_feature_flags.tf
+++ src/domains/checkout-app/04_apim_checkout_feature_flags.tf
@@ -74,6 +74,20 @@ resource "azurerm_api_management_named_value" "apim_checkout_feature_flags_map_v
   secret              = true
 }
 
+data "azurerm_key_vault_secret" "checkout_apim_frontend_enabled" {
+  name         = "checkout-apim-frontend-enabled"
+  key_vault_id = data.azurerm_key_vault.key_vault_checkout.id
+}
+
+resource "azurerm_api_management_named_value" "apim_checkout_frontend_enabled" {
+  name                = "checkout-apim-frontend-enabled"
+  api_management_name = data.azurerm_api_management.apim.name
+  resource_group_name = data.azurerm_resource_group.rg_api.name
+  display_name        = "checkout-apim-frontend-enabled"
+  value               = data.azurerm_key_vault_secret.checkout_apim_frontend_enabled.value
+  secret              = true
+}
+
 #######################################################################
 ## Fragment policy to filter feature flags based on different rules  ##
 #######################################################################
