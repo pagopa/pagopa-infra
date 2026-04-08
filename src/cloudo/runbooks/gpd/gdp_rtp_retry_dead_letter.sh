ef284f6ef1e70cb109fbfa77044ad6af08725ed6
svariant <samuele.varianti@nttdata.com>
2026-04-08 20:01:32 +0200

    feat: [PIDM-1668] [CLOUDO] Improve gpdxrtp retry script with env url definition

--- src/cloudo/runbooks/gpd/gdp_rtp_retry_dead_letter.sh
+++ src/cloudo/runbooks/gpd/gdp_rtp_retry_dead_letter.sh
@@ -2,7 +2,13 @@
 set -e
 
 BASE_URL="https://weudev.gps.internal.dev.platform.pagopa.it/gpd-rtp-service"
+if [ "$CLOUDO_ENVIRONMENT" = "uat" ]; then
+  BASE_URL="https://weudev.gps.internal.uat.platform.pagopa.it/gpd-rtp-service"
+elif [ "$CLOUDO_ENVIRONMENT" = "prod" ]; then
+  BASE_URL="https://weudev.gps.internal.platform.pagopa.it/gpd-rtp-service"
+fi
 
+# Retrieve Dead Letter messages to retry
 RESPONSE=$(curl -s -k "${BASE_URL}/error-messages")
 
 if [ -z "$RESPONSE" ] || [ "$RESPONSE" = "[]" ]; then
@@ -10,6 +16,7 @@ if [ -z "$RESPONSE" ] || [ "$RESPONSE" = "[]" ]; then
   exit 0
 fi
 
+# Send retry request for the retrieved messages
 curl -s -k -X POST \
   -H "Content-Type: application/json" \
   -d "$RESPONSE" \
