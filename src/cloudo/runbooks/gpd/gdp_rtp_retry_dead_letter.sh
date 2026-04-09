f3c8f6cc0250d9d5555b77e0a1c8a6d11697daab
svariant <samuele.varianti@nttdata.com>
2026-04-09 09:24:50 +0200

    feat: [PIDM-1668] [CLOUDO] Fix gpdxrtp retry script url

--- src/cloudo/runbooks/gpd/gdp_rtp_retry_dead_letter.sh
+++ src/cloudo/runbooks/gpd/gdp_rtp_retry_dead_letter.sh
@@ -3,9 +3,9 @@ set -e
 
 BASE_URL="https://weudev.gps.internal.dev.platform.pagopa.it/gpd-rtp-service"
 if [ "$CLOUDO_ENVIRONMENT" = "uat" ]; then
-  BASE_URL="https://weudev.gps.internal.uat.platform.pagopa.it/gpd-rtp-service"
+  BASE_URL="https://weuuat.gps.internal.uat.platform.pagopa.it/gpd-rtp-service"
 elif [ "$CLOUDO_ENVIRONMENT" = "prod" ]; then
-  BASE_URL="https://weudev.gps.internal.platform.pagopa.it/gpd-rtp-service"
+  BASE_URL="https://weuprod.gps.internal.platform.pagopa.it/gpd-rtp-service"
 fi
 
 # Retrieve Dead Letter messages to retry
