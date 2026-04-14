7577881f26d1dd5cbc593c17f5119c85ab4e27d0
svariant <samuele.varianti@nttdata.com>
2026-04-14 15:52:31 +0200

    feat: [PIDM-1668] [CLOUDO] Change gpdxrtp retry script with new API retryAll

--- src/cloudo/runbooks/gpd/gdp_rtp_retry_dead_letter.sh
+++ src/cloudo/runbooks/gpd/gdp_rtp_retry_dead_letter.sh
@@ -8,16 +8,8 @@ elif [ "$CLOUDO_ENVIRONMENT" = "prod" ]; then
   BASE_URL="https://weuprod.gps.internal.platform.pagopa.it/gpd-rtp-service"
 fi
 
-# Retrieve Dead Letter messages to retry
-RESPONSE=$(curl -s -k "${BASE_URL}/error-messages")
-
-if [ -z "$RESPONSE" ] || [ "$RESPONSE" = "[]" ]; then
-  echo "No messages to retry"
-  exit 0
-fi
-
-# Send retry request for the retrieved messages
-curl -s -k -X POST \
+# Send retry request for all dead letter messages with default minutes offset of 2 (ignore messages newer than 2 minutes)
+curl -sS -X POST \
   -H "Content-Type: application/json" \
   -d "$RESPONSE" \
-  "${BASE_URL}/error-messages/retry"
+  "${BASE_URL}/error-messages/retry/all"
