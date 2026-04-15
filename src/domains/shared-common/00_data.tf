cf716f6cae267d87a4d64c2b70fb4bbd2a88a231
Francesco <cesareo.francesco@gmail.com>
2026-04-15 14:42:47 +0200

    added secret

--- src/domains/shared-common/00_data.tf
+++ src/domains/shared-common/00_data.tf
@@ -2,3 +2,9 @@ data "azurerm_key_vault_secret" "slack_pagopa_pagamenti_alert_email" {
   name         = "slack-pagopa-pagamenti-alert-email"
   key_vault_id = module.key_vault.id
 }
+
+data "azurerm_cosmosdb_account" "bizevents_ds_cosmos" {
+  count               = var.env_short != "p" ? 1 : 0
+  name                = "${local.product}-${var.location_short}-bizevents-ds-cosmos-account"
+  resource_group_name = "${local.product}-${var.location_short}-bizevents-rg"
+}
