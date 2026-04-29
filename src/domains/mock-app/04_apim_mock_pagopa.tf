7dab99996d7879290891499b40421314f8e87af6
Francesco <cesareo.francesco@gmail.com>
2026-04-29 13:20:29 +0200

    fix mock product

--- src/domains/mock-app/04_apim_mock_pagopa.tf
+++ src/domains/mock-app/04_apim_mock_pagopa.tf
@@ -2,13 +2,13 @@
 ## Products ##
 ##############
 
-module "apim_mock_ec_pagopa_product" {
+module "apim_mock_pagopa_product" {
   source = "./.terraform/modules/__v3__/api_management_product"
   count  = var.env_short != "p" ? 1 : 0
 
-  product_id   = "mock_ec_pagopa"
-  display_name = "Mock EC for PagoPA"
-  description  = "Mock EC for PagoPA"
+  product_id   = "mock_pagopa"
+  display_name = "Mock for PagoPA"
+  description  = "Mock for PagoPA"
 
   api_management_name = local.pagopa_apim_name
   resource_group_name = local.pagopa_apim_rg
@@ -18,5 +18,5 @@ module "apim_mock_ec_pagopa_product" {
   approval_required     = false
   subscriptions_limit   = 0
 
-  policy_xml = file("./api_product/mock-ec-pagopa/_base_policy.xml")
+  policy_xml = file("./api_product/mock-pagopa/_base_policy.xml")
 }
