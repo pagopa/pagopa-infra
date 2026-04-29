d7815dd3f73ed6201906ee2ba844f371f6c8f355
mamari90 <130982006+mamari90@users.noreply.github.com>
2026-04-29 12:04:27 +0200

    upgrade v4

--- src/aks-leonardo/06_non_core_node_pool.tf
+++ src/aks-leonardo/06_non_core_node_pool.tf
@@ -13,7 +13,7 @@ module "non_critical_node_pool" {
     vnet_name    = data.azurerm_virtual_network.vnet_ita.name
     vnet_rg_name = data.azurerm_virtual_network.vnet_ita.resource_group_name
     subnet_name  = "${local.project}-non-critical"
-    natgw_id     = data.azurerm_nat_gateway.pagopa_nat.id
+    natgw_id     = null
   }
 
   node_count_min = 1
