00db13c516bf1563ff4b5d805d759a18267ce8e4
Francesco <cesareo.francesco@gmail.com>
2026-05-06 15:11:07 +0200

    added sops perm

--- src/next-core/01_security.tf
+++ src/next-core/01_security.tf
@@ -72,7 +72,7 @@ resource "azurerm_key_vault_access_policy" "adgroup_admin_dev_policy" {
   tenant_id = data.azurerm_client_config.current.tenant_id
   object_id = data.azuread_group.adgroup_admin_dev.object_id
 
-  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete"]
+  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt", "GetRotationPolicy"]
   secret_permissions      = ["Get", "List", "Set", "Delete"]
   storage_permissions     = []
   certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Purge", "Recover"]
