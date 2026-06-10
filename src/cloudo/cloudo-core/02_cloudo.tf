9621cd2e4ad19935da9f0717e190a0903531b421
ffppa <fabio.felici@pagopa.it>
2026-06-10 16:33:31 +0200

    feat: [PAYMCLOUD-999] Bump cloudo module and components to v0.24.0 (#3887)
    feat: bump cloudo module and related components to v0.24.0
    
    Signed-off-by: ffppa <fabio.felici@pagopa.it>

--- src/cloudo/cloudo-core/02_cloudo.tf
+++ src/cloudo/cloudo-core/02_cloudo.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "cloudo" {
-  source = "git::https://github.com/pagopa/payments-ClouDO.git//src/core/iac?ref=e2a672c545ee7b69ed9426d2dea85612a91826a5" #0.23.0
+  source = "git::https://github.com/pagopa/payments-ClouDO.git//src/core/iac?ref=2c330037f90f8302a160141b5421fab15a6c0c37" #0.24.0
 
   prefix                    = local.product
   product_name              = var.prefix
