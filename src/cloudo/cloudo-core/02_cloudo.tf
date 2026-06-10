b3c15e54de0e7000ba1b6b2a32111f8ca3cab433
mamari90 <130982006+mamari90@users.noreply.github.com>
2026-06-10 17:40:58 +0200

    fix cloudo runbook branch

--- src/cloudo/cloudo-core/02_cloudo.tf
+++ src/cloudo/cloudo-core/02_cloudo.tf
@@ -31,7 +31,7 @@ module "cloudo" {
 
   github_repo_info = {
     repo_name    = "pagopa/pagopa-infra"
-    repo_branch  = "apiconfig-cache-loudo-runbook"
+    repo_branch  = "main"
     runbook_path = "src/cloudo/cloudo-core/runbooks"
   }
 
