cb5765192411354a7d72b323ef28960b44da7b78
Jacopo Carlini <jacopo.carlini@gmail.com>
2026-06-11 10:25:52 +0200

    response time

--- src/domains/gps-common/04_datafactory_pipeline.tf
+++ src/domains/gps-common/04_datafactory_pipeline.tf
@@ -191,6 +191,8 @@ resource "azurerm_data_factory_trigger_schedule" "pipeline_lifecycle_management_
   interval  = 1
   frequency = "Day"
 
+  start_time = "2026-06-11T20:00:00Z"
+
   pipeline_parameters = {
     ChunkSize          = 10000
     MaxAmountToMigrate = 500000
