1961a2fb39b644388b29673da9b3f978d4b76c9c
pietro.tota <pietro.tota@pagopa.it>
2026-08-06 15:34:54 +0200

    feat: [CHK-4940] add pos-gateway domain (#3980)
    * Add domain artifacts for pos-gateway (Request ID: bezrswmk69s)
    
    * chore: pre-commit run
    
    * chore: remove gh runner definition
    
    * chore: add prefix variable
    
    * chore: pre-commit run
    
    * chore: add database and collections definition
    
    * chore: pre-commit
    
    * chore: add terraform lock
    
    * chore: add missing variables
    
    * feat: add IAC pipelines
    
    * chore: fix some typo + disable iac
    
    * chore: remove unused var
    
    * chore: remove unused vars
    
    * chore: remove comment
    
    * chore: add depends on
    
    * fix: secret name
    
    * chore: rename pipeline
    
    * fix: sops kv name
    
    * chore: add sops file
    
    * fix: env folders
    
    * fix: local variable values
    
    * fix: pipeline
    
    * fix: secrets domain folder
    
    * chore: update v4 module version
    
    * fix: ingress hostname
    
    * chore: disable uat and prod envs in pipelines
    
    * fix: enable dev
    
    * chore: pipelines only for dev
    
    * chore: use managed redis
    
    * trigger CR pipeline
    
    * chore: disable aks for secret domain
    
    * chore: add missing key permission
    
    * fix: add missing permission
    
    * fix: domain name pos gateway
    
    * fix: use IDH/key_vault_access_policy
    
    * chore: rename from pos-gateway
    
    * chore: rename IAC pipelines
    
    * chore: move terraform state blobs
    
    ---------
    
    Co-authored-by: GitHub Action Bot <action@github.com>
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
    Co-authored-by: pietro.tota <pietro.tota@pagopa.it>

---
+++ src/domains/posgw-common/99_variables.tf
@@ -0,0 +1,74 @@
+variable "env" {
+  type = string
+}
+
+variable "env_short" {
+  type = string
+  validation {
+    condition = (
+      length(var.env_short) == 1
+    )
+    error_message = "Length must be 1 chars."
+  }
+}
+
+variable "location" {
+  type        = string
+  description = "One of westeurope, northeurope"
+}
+
+variable "location_short" {
+  type = string
+  validation {
+    condition = (
+      length(var.location_short) == 3
+    )
+    error_message = "Length must be 3 chars."
+  }
+  description = "One of wue, neu"
+  default     = "itn"
+}
+
+variable "dns_zone_internal_prefix" {
+  type        = string
+  default     = null
+  description = "The dns subdomain."
+}
+
+variable "external_domain" {
+  type        = string
+  default     = null
+  description = "Domain for delegation"
+}
+
+variable "alert_use_opsgenie" {
+  type        = bool
+  default     = true
+  description = "Use opsgenie for alerts"
+}
+
+variable "redis_idh_resource_tier" {
+  type        = string
+  description = "The IDH resource tier for the Redis cache."
+}
+
+
+variable "cosmos_idh_resource_tier" {
+  type        = string
+  description = "The IDH resource tier for the cosmosdb "
+}
+
+variable "cosmos_mongo_db_params" {
+  type = object({
+    capabilities = list(string)
+  })
+}
+
+variable "cosmos_mongo_db_pos_gateway_params" {
+  type = object({
+    enable_autoscaling = bool
+    enable_serverless  = bool
+    throughput         = number
+    max_throughput     = number
+  })
+}
