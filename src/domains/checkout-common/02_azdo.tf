2afa671f7ba99a71df2e824cbe2b38fc4042ce11
pietro.tota <pietro.tota@pagopa.it>
2026-09-07 15:12:45 +0200

    chore: add delete permission

--- src/domains/checkout-common/02_azdo.tf
+++ src/domains/checkout-common/02_azdo.tf
@@ -20,7 +20,7 @@ resource "azurerm_key_vault_access_policy" "azdevops_iac_managed_identities" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_user_assigned_identity.iac_federated_azdo[each.key].principal_id
 
-  secret_permissions = ["Get", "List", "Set", ]
+  secret_permissions = ["Get", "List", "Set", "Delete"]
   key_permissions    = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt", "GetRotationPolicy"]
 
   certificate_permissions = ["SetIssuers", "DeleteIssuers", "Purge", "List", "Get"]
