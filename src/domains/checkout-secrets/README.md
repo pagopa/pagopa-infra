738542113914cfd18efdc4bdefdf79d0bc260023
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2026-09-09 15:49:15 +0200

    feat: CHK-4973 add ecommerce payment request dashboard (#4012)
    * add ecommerce payment request dashboard
    
    * add resource
    
    * update payment request dashboard
    
    ---------
    
    Co-authored-by: pietro.tota <pietro.tota@pagopa.it>

--- src/domains/checkout-secrets/README.md
+++ src/domains/checkout-secrets/README.md
@@ -23,7 +23,6 @@
 | [azurerm_key_vault_secret.secret](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/key_vault_secret) | resource |
 | [azurerm_client_config.current](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/client_config) | data source |
 | [azurerm_key_vault.checkout_kv](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/key_vault) | data source |
-| [azurerm_key_vault_secret.to_import](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/key_vault_secret) | data source |
 | [azurerm_resource_group.sec_rg](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/resource_group) | data source |
 | [azurerm_subscription.current](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/subscription) | data source |
 | [external_external.terrasops](https://registry.terraform.io/providers/hashicorp/external/latest/docs/data-sources/external) | data source |
--- src/domains/ecommerce-common/00_dashboards.tf
+++ src/domains/ecommerce-common/00_dashboards.tf
@@ -140,3 +140,31 @@ resource "azurerm_portal_dashboard" "pagopa_ecommerce_event_dispatcher_dashboard
   )
 
 }
+
+resource "azurerm_portal_dashboard" "ecommerce_payment_request_dashboard" {
+  count               = var.env_short == "p" ? 1 : 0
+  name                = "ecommerce-payment-request-dashboard"
+  resource_group_name = var.monitor_resource_group_name
+  location            = var.location
+  tags = {
+    source = "terraform"
+  }
+  #convert json dashboard output from Azure export function to the one handled by azurerm portal dashboard provider
+  #see https://github.com/hashicorp/terraform-provider-azurerm/issues/27117 issue for more info
+  dashboard_properties = jsonencode(
+    merge(
+      jsondecode(file("./dashboards/ecommerce-payment-request-dashboard.json")).properties,
+      {
+        "lenses" = {
+          for lens_index, lens in jsondecode(file("./dashboards/ecommerce-payment-request-dashboard.json")).properties.lenses :
+          tostring(lens_index) => merge(lens, {
+            "parts" = {
+              for part_index, part in lens.parts :
+              tostring(part_index) => part
+            }
+          })
+        }
+      }
+    )
+  )
+}
