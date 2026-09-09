dfc849ad08436a299127c3680b3b5d61c56af50a
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2026-09-09 15:45:11 +0200

    feat: CHK-4974 add ecommerce event dispatcher dashboard (#4011)
    * add ecommerce event dispatcher dashboard
    
    * add resource
    
    * fix resource name
    
    ---------
    
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>

--- src/domains/ecommerce-common/00_dashboards.tf
+++ src/domains/ecommerce-common/00_dashboards.tf
@@ -111,3 +111,32 @@ resource "azurerm_portal_dashboard" "pagopa_eCommerce_update_transactions_status
   )
 
 }
+
+resource "azurerm_portal_dashboard" "pagopa_ecommerce_event_dispatcher_dashboard" {
+  count               = var.env_short == "p" ? 1 : 0
+  name                = "pagopa-ecommerce-event-dispatcher-dashboard"
+  resource_group_name = var.monitor_resource_group_name
+  location            = var.location
+  tags = {
+    source = "terraform"
+  }
+  #convert json dashboard output from Azure export function to the one handled by azurerm portal dashboard provider
+  #see https://github.com/hashicorp/terraform-provider-azurerm/issues/27117 issue for more info
+  dashboard_properties = jsonencode(
+    merge(
+      jsondecode(file("./dashboards/pagopa-ecommerce-event-dispatcher-dashboard.json")).properties,
+      {
+        "lenses" = {
+          for lens_index, lens in jsondecode(file("./dashboards/pagopa-ecommerce-event-dispatcher-dashboard.json")).properties.lenses :
+          tostring(lens_index) => merge(lens, {
+            "parts" = {
+              for part_index, part in lens.parts :
+              tostring(part_index) => part
+            }
+          })
+        }
+      }
+    )
+  )
+
+}
