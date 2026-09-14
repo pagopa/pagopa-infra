44da0e6fa5079c1d0f3a5ce555b122ce959adae3
Ivan Ferrando <ivan.ferrando@pagopa.it>
2026-09-14 12:50:18 +0200

    Apply batched suggestions from code review
    Co-authored-by: CianoDanilo <danilo.ciano@pagopa.it>

--- src/domains/pay-wallet-secrets/01_keyvault.tf
+++ src/domains/pay-wallet-secrets/01_keyvault.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "sec_rg" {
 }
 
 
-data "azurerm_key_vault" "checkout_kv" {
+data "azurerm_key_vault" "pay_wallet_kv" {
   name                = "${local.product}-${var.domain}-kv"
   resource_group_name = data.azurerm_resource_group.sec_rg.name
 }
--- src/domains/pay-wallet-secrets/02_init_sops.tf
+++ src/domains/pay-wallet-secrets/02_init_sops.tf
@@ -1,6 +1,6 @@
 resource "azurerm_key_vault_key" "sops_key" {
   name         = "${local.product}-${var.domain}-sops-key"
-  key_vault_id = data.azurerm_key_vault.checkout_kv.id
+  key_vault_id = data.azurerm_key_vault.pay_wallet_kv.id
   key_type     = "RSA"
   key_size     = 2048
 
