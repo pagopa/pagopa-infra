7406d2bc891912161b18218d8a599a258105e583
alessio-acitelli <97529051+alessio-acitelli@users.noreply.github.com>
2026-09-14 16:21:46 +0200

    feat: GPD Payments dead letter [PIDM - 2360]  (#4006)
    * [PIDM-2360] add gpd_receipt_dead_letter container
    
    * [PIDM-2360] add helpdesk product
    
    * precommit
    
    ---------
    
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
    Co-authored-by: Jacopo Carlini <jacopo.carlini@gmail.com>

--- src/domains/gps-app/04_apim_gpd_payments.tf
+++ src/domains/gps-app/04_apim_gpd_payments.tf
@@ -143,6 +143,23 @@ module "apim_gpd_payments_rest_external_product" {
   policy_xml = file("./api_product/payments-service/external/_base_policy.xml")
 }
 
+module "apim_gpd_payments_helpdesk_product" {
+  source = "./.terraform/modules/__v3__/api_management_product"
+
+  product_id   = "gpd-payments-helpdesk"
+  display_name = "GPD Payments pagoPA - Helpdesk"
+  description  = "Servizio di Helpdesk per GPD Payments"
+
+  api_management_name = local.pagopa_apim_name
+  resource_group_name = local.pagopa_apim_rg
+
+  published             = false
+  subscription_required = true
+  approval_required     = false
+
+  policy_xml = file("./api_product/payments-service/helpdesk/_base_policy.xml")
+}
+
 ##############
 ## REST API ##
 ##############
